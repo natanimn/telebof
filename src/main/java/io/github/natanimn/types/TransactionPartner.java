dc1c58cc34424344461e44ec47dd0350a985ccff
natanimn <natanimn@yahoo.com>
2025-04-12 10:54:05 +0300

    Added field paid_media

--- src/main/java/io/github/natanimn/types/TransactionPartner.java
+++ src/main/java/io/github/natanimn/types/TransactionPartner.java
@@ -1,5 +1,6 @@
 package io.github.natanimn.types;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -10,6 +11,7 @@ public class TransactionPartner {
     public String type, invoice_payload;
     public User user;
     public RevenueWithdrawalState withdrawal_state;
+    public List<PaidMedia> paid_media;
 
     @Override
     public boolean equals(Object object) {
@@ -19,11 +21,12 @@ public class TransactionPartner {
         return Objects.equals(type, that.type) &&
                 Objects.equals(invoice_payload, that.invoice_payload) &&
                 Objects.equals(user, that.user) &&
-                Objects.equals(withdrawal_state, that.withdrawal_state);
+                Objects.equals(withdrawal_state, that.withdrawal_state) &&
+                Objects.equals(paid_media, that.paid_media);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type, invoice_payload, user, withdrawal_state);
+        return Objects.hash(type, invoice_payload, user, withdrawal_state, paid_media);
     }
 }
