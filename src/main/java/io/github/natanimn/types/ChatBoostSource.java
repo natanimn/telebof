f1145ee0eeafae65a83cd153404102ce10a8f363
natanimn <natanimn@yahoo.com>
2025-04-17 11:29:45 +0300

    Added the field prize_star_count

--- src/main/java/io/github/natanimn/types/ChatBoostSource.java
+++ src/main/java/io/github/natanimn/types/ChatBoostSource.java
@@ -8,7 +8,8 @@ import java.util.Objects;
  *  3 March 2025
  */
 public class ChatBoostSource implements Serializable {
-    public String source, giveaway_message_id;
+    public String source;
+    Integer giveaway_message_id, prize_star_count;
     public User user;
     public Boolean is_unclaimed;
 
@@ -18,11 +19,12 @@ public class ChatBoostSource implements Serializable {
         if (object == null || getClass() != object.getClass()) return false;
         ChatBoostSource that = (ChatBoostSource) object;
         return Objects.equals(source, that.source) && Objects.equals(giveaway_message_id, that.giveaway_message_id)
-                && Objects.equals(user, that.user) && Objects.equals(is_unclaimed, that.is_unclaimed);
+                && Objects.equals(user, that.user) && Objects.equals(is_unclaimed, that.is_unclaimed) &&
+                Objects.equals(prize_star_count, that.prize_star_count);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(source, giveaway_message_id, user, is_unclaimed);
+        return Objects.hash(source, giveaway_message_id, user, is_unclaimed, prize_star_count);
     }
 }
