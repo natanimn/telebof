fd5604148a1138c15d74dd1f6d935e6bd8ec0742
natanimn <natanimn@yahoo.com>
2025-04-17 11:32:06 +0300

    Added the field is_star_giveaway

--- src/main/java/io/github/natanimn/types/GiveawayCompleted.java
+++ src/main/java/io/github/natanimn/types/GiveawayCompleted.java
@@ -10,6 +10,7 @@ public class GiveawayCompleted {
     public int winner_count;
     public Integer unclaimed_prize_count;
     public Message giveaway_message;
+    public Boolean is_star_giveaway;
 
     @Override
     public boolean equals(Object object) {
@@ -17,11 +18,12 @@ public class GiveawayCompleted {
         if (object == null || getClass() != object.getClass()) return false;
         GiveawayCompleted that = (GiveawayCompleted) object;
         return winner_count == that.winner_count && Objects.equals(unclaimed_prize_count, that.unclaimed_prize_count)
-                && Objects.equals(giveaway_message, that.giveaway_message);
+                && Objects.equals(giveaway_message, that.giveaway_message) &&
+                Objects.equals(is_star_giveaway, that.is_star_giveaway);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(winner_count, unclaimed_prize_count, giveaway_message);
+        return Objects.hash(winner_count, unclaimed_prize_count, giveaway_message, is_star_giveaway);
     }
 }
