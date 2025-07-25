44b23e4be8d4d7809d00d6de892ff6b1501be79a
natanimn <natanimco@gmail.com>
2025-07-25 18:21:39 +0300

    Added class SendGift

---
+++ src/main/java/io/github/natanimn/requests/SendGift.java
@@ -0,0 +1,61 @@
+package io.github.natanimn.requests;
+
+import io.github.natanimn.enums.ParseMode;
+import io.github.natanimn.types.MessageEntity;
+import java.util.List;
+
+/**
+ * SendGift class for sending gifts
+ * @author Natanim
+ * @since July 25, 2025
+ * @version 0.6
+ */
+
+public class SendGift extends AbstractBaseRequest<SendGift, Boolean>{
+    public SendGift(Object chat_id, String gift_id,  RequestSender rs){
+        super(chat_id, rs, "sendGift");
+        add("gift_id", gift_id);
+    }
+
+    public SendGift(long user_id, String gift_id,  RequestSender rs){
+        super(rs, "sendGift");
+        add("user_id", user_id);
+        add("gift_id", gift_id);
+    }
+
+    /**
+     * Optional
+     * @param pay_for_upgrade Pass True to pay for the gift upgrade from the bot's balance, thereby making the upgrade free for the receiver.
+     * @return {@link SendGift}
+     */
+    public SendGift payForUpgrade(Boolean pay_for_upgrade){
+        return add("pay_for_upgrade", pay_for_upgrade);
+    }
+
+    /**
+     * Optional
+     * @param text Text that will be shown along with the gift; 0-128 characters
+     * @return {@link SendGift}
+     */
+    public SendGift text(String text){
+        return add("add", text);
+    }
+
+    /**
+     * Optional
+     * @param text_parse_mode Mode for parsing entities in the text.
+     * @return {@link SendGift}
+     */
+    public SendGift textParseMode(ParseMode text_parse_mode){
+        return add("text_parse_mode", text_parse_mode.name());
+    }
+
+    /**
+     * Optional
+     * @param text_entities A list of {@link MessageEntity}, special entities that appear in the gift text. It can be specified instead of text_parse_mode
+     * @return {@link SendGift}
+     */
+    public SendGift textEntities(List<MessageEntity> text_entities){
+        return add("text_entities", text_entities);
+    }
+}
