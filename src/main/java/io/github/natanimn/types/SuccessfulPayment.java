aa2b68cfe13f122ca5af44a3d7014e6f3f174cc5
natanimn <natanimco@gmail.com>
2025-07-25 18:38:03 +0300

    Added fields subscription_expiration_date, is_recurring, is_first_recurring

--- src/main/java/io/github/natanimn/types/SuccessfulPayment.java
+++ src/main/java/io/github/natanimn/types/SuccessfulPayment.java
@@ -4,15 +4,16 @@ import java.io.Serializable;
 import java.util.Objects;
 
 /**
- * Natanim Negash
- *  3 March 2025
+ * @author Natanim
+ * @since 3 March 2025
  */
 public class SuccessfulPayment implements Serializable {
     public String currency;
-    public Integer total_amount;
+    public Integer total_amount, subscription_expiration_date;
     public String invoice_payload, shipping_option_id;
     public OrderInfo order_info;
     public String telegram_payment_charge_id, provider_payment_charge_id;
+    public Boolean is_recurring, is_first_recurring;
 
     @Override
     public boolean equals(Object object) {
@@ -24,11 +25,16 @@ public class SuccessfulPayment implements Serializable {
                 Objects.equals(shipping_option_id, that.shipping_option_id) &&
                 Objects.equals(order_info, that.order_info) &&
                 Objects.equals(telegram_payment_charge_id, that.telegram_payment_charge_id) &&
-                Objects.equals(provider_payment_charge_id, that.provider_payment_charge_id);
+                Objects.equals(provider_payment_charge_id, that.provider_payment_charge_id) &&
+                Objects.equals(subscription_expiration_date, that.subscription_expiration_date) &&
+                Objects.equals(is_recurring, that.is_recurring) &&
+                Objects.equals(is_first_recurring, that.is_first_recurring);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(currency, total_amount, invoice_payload, shipping_option_id, order_info, telegram_payment_charge_id, provider_payment_charge_id);
+        return Objects.hash(currency, total_amount, invoice_payload, shipping_option_id, order_info,
+                telegram_payment_charge_id, provider_payment_charge_id, subscription_expiration_date,
+                is_recurring, is_first_recurring);
     }
 }
