5b7220ce6a1d3207d10f1c26f6a6dc4dc6ead569
natanimn <natanimco@gmail.com>
2025-07-25 18:12:15 +0300

    Added class GetAvailableGifts

---
+++ src/main/java/io/github/natanimn/requests/GetAvailableGifts.java
@@ -0,0 +1,14 @@
+package io.github.natanimn.requests;
+
+import io.github.natanimn.types.Gifts;
+
+/**
+ * @author Natanim
+ * @since July 25, 2025
+ * @version 0.6
+ */
+public class GetAvailableGifts extends AbstractBaseRequest<GetAvailableGifts, Gifts> {
+    public GetAvailableGifts(RequestSender rs){
+        super(rs, "getAvailableGifts");
+    }
+}
