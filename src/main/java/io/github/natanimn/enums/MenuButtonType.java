9fdb2b06afb8ef8844a4b36de17bd47039d14918
natanimn <natanimco@gmail.com>
2025-07-26 20:40:33 +0300

    Supports serializations

--- src/main/java/io/github/natanimn/enums/MenuButtonType.java
+++ src/main/java/io/github/natanimn/enums/MenuButtonType.java
@@ -1,7 +1,19 @@
 package io.github.natanimn.enums;
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Natanim
+ * @since 3 March, 2025
+ * @version 0.7
+ */
 public enum MenuButtonType {
+    @SerializedName("commands")
     COMMANDS,
+
+    @SerializedName("default")
     DEFAULT,
+
+    @SerializedName("web_app")
     WEB_APP;
 }
