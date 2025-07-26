43493aeee41faaeca8bf5154c3bd12779d119319
natanimn <natanimco@gmail.com>
2025-07-26 20:52:20 +0300

    Supports serialization

--- src/main/java/io/github/natanimn/enums/TypeReaction.java
+++ src/main/java/io/github/natanimn/enums/TypeReaction.java
@@ -1,6 +1,18 @@
 package io.github.natanimn.enums;
 
+import com.google.gson.annotations.SerializedName;
+import io.github.natanimn.types.media_and_service.ReactionType;
+
+/**
+ * Represents one of {@link ReactionType} type
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 0.7
+ */
 public enum TypeReaction {
+    @SerializedName("emoji")
     EMOJI,
+
+    @SerializedName("custom_emoji")
     CUSTOM_EMOJI
 }
