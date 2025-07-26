45116dc8093c2b5e15b6cf068fd19f4a9aee1474
natanimn <natanimco@gmail.com>
2025-07-26 20:04:05 +0300

    Fixed a bug and improved the code

--- src/main/java/io/github/natanimn/requests/SendChatAction.java
+++ src/main/java/io/github/natanimn/requests/SendChatAction.java
@@ -3,20 +3,20 @@ package io.github.natanimn.requests;
 import io.github.natanimn.enums.ChatAction;
 
 /**
- * Natanim Negash
- *  3 March 2025
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 0.7
  */
 public class SendChatAction extends AbstractBaseRequest<SendChatAction, Boolean> {
 
     public SendChatAction(Object chatId, ChatAction chatAction, RequestSender requestSender) {
         super(chatId, requestSender, "sendChatAction");
-        add("chatAction", chatAction.name().toLowerCase());
+        add("action", chatAction);
     }
 
     public SendChatAction messageThreadId(int messageThreadId) {
         return add("message_thread_id", messageThreadId);
     }
-
 
     public SendChatAction businessConnectionId(String business_connection_id){
         return add("business_connection_id", business_connection_id);
