f7457b3f07c4f4719960fc9f664bffc3685c4520
natanimn <natanimco@gmail.com>
2025-07-26 20:38:10 +0300

    Added javadocs

--- src/main/java/io/github/natanimn/types/payments/LabeledPrice.java
+++ src/main/java/io/github/natanimn/types/payments/LabeledPrice.java
@@ -1,17 +1,26 @@
-package io.github.natanimn.types;
+package io.github.natanimn.types.payments;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 
 /**
- * Natanim Negash 
- *  3 March 2025
+ * This object represents a portion of the price for goods or services.
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 0.7
  */
 public class LabeledPrice implements Serializable {
     private String label;
     private Integer amount;
 
+    /**
+     *
+     * @param label Portion label
+     * @param amount Price of the product in the smallest units of the currency (integer, not float/double).
+     *               For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json,
+     *               it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
+     */
     public LabeledPrice(String label, int amount){
         this.label = label;
         this.amount = amount;
