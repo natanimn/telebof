1f8fab8f3baa9aa8aefc578af2ca704fcf82f015
natanimn <natanimco@gmail.com>
2025-07-26 20:36:18 +0300

    Added javadocs

--- src/main/java/io/github/natanimn/types/updates/InlineQuery.java
+++ src/main/java/io/github/natanimn/types/updates/InlineQuery.java
@@ -1,15 +1,48 @@
-package io.github.natanimn.types;
+package io.github.natanimn.types.updates;
+
+import io.github.natanimn.types.chat_and_user.User;
+import io.github.natanimn.types.media_and_service.Location;
 
 import java.util.Objects;
 
 /**
- * Natanim Negash
- *  3 March 2025
+ * This object represents an incoming inline query.
+ * When the user sends an empty query, your bot could return some default or trending results.
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 0.7
  */
 public class InlineQuery {
+    /**
+     * Unique identifier for this query
+     */
     public String id;
+
+    /**
+     * Sender
+     */
     public User from;
-    public String query, offset, chat_type;
+
+    /**
+     * Text of the query (up to 256 characters)
+     */
+    public String query;
+
+    /**
+     * Offset of the results to be returned, can be controlled by the bot
+     */
+    public String offset;
+
+    /**
+     * Type of the chat from which the inline query was sent.
+     * Can be either “sender” for a private chat with the inline query sender, “private”, “group”, “supergroup”, or “channel”.
+     * The chat type should be always known for requests sent from official clients and most third-party clients, unless the request was sent from a secret chat
+     */
+    public String chat_type;
+
+    /**
+     * Sender location, only for bots that request user location
+     */
     public Location location;
 
     @Override
