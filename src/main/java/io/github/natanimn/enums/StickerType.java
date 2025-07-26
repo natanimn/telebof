2ace928461d5ad9aff53c59de3d258c90d7d2f75
natanimn <natanimco@gmail.com>
2025-07-26 20:53:58 +0300

    Supports serialization

--- src/main/java/io/github/natanimn/enums/StickerType.java
+++ src/main/java/io/github/natanimn/enums/StickerType.java
@@ -1,9 +1,19 @@
 package io.github.natanimn.enums;
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Natanim
+ * @since 3 March, 2025
+ * @version 0.7
+ */
 public enum StickerType {
+    @SerializedName("regular")
     REGULAR,
 
+    @SerializedName("mask")
     MASK,
 
+    @SerializedName("custom_emoji")
     CUSTOM_EMOJI
 }
