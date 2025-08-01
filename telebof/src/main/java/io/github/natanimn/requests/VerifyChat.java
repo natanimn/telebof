b55f70c4906abfc7030370f5676d9e3c29be4c58
natanimn <natanimco@gmail.com>
2025-08-01 12:59:52 +0300

    Added class VerifyChat

---
+++ telebof/src/main/java/io/github/natanimn/requests/VerifyChat.java
@@ -0,0 +1,27 @@
+package io.github.natanimn.requests;
+
+/**
+ * Verifies a chat on <a hre="https://telegram.org/verify#third-party-verification">behalf of the organization</a> which is represented by the bot.
+ * @author Natanim
+ * @since 2 August 2025
+ * @version 0.8
+ */
+public class VerifyChat extends AbstractBaseRequest<VerifyChat, Boolean>{
+    /**
+     * @param chat_id Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+     * @param rs Request
+     */
+    public VerifyChat(Object chat_id, RequestSender rs){
+        super(chat_id, rs, "verifyChat");
+    }
+
+    /**
+     * Optional
+     * @param custom_description Custom description for the verification; 0-70 characters. Must be empty if the organization
+     *                           isn't allowed to provide a custom verification description.
+     * @return {@link #VerifyChat}
+     */
+    public VerifyChat customDescription(String custom_description){
+        return add("custom_description", custom_description);
+    }
+}
