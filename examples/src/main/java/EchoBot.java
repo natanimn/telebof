4715af14438670911c2f1374a9f81d17d8ac3f41
natanimn <natanimco@gmail.com>
2025-08-01 20:45:14 +0300

    Added example

--- examples/src/main/java/EchoBot.java
+++ examples/src/main/java/EchoBot.java
@@ -1,14 +1,14 @@
 /***
  * This file is part of telebof.
- * It is example of an echo bot.
+ * It is example of echo bot.
  * Before going through, you must obtain your bot token from @BotFather first.
  */
 
 import io.github.natanimn.BotClient;
-import io.github.natanimn.filters.Filter;
 
 public class EchoBot {
-    static final String TOKEN = System.getenv("TELEGRAM_BOT_TOKEN");
+    static final String TOKEN = System.getenv("TOKEN");
+
     public static void main(String[] args){
         // Create bot instance
         var bot = new BotClient(TOKEN);
@@ -19,10 +19,10 @@ public class EchoBot {
         });
 
         //Register incoming text handler
-        bot.onMessage(Filter::text, (ctx, message) -> {
+        bot.onMessage(filter -> filter.text(), (ctx, message) -> {
             //Echo as a reply
             ctx.reply(message.text).exec();
-            ;
+
         });
 
         // Run the bot
---
+++ examples/src/main/java/PaymentExampleBot.java
@@ -0,0 +1,81 @@
+import io.github.natanimn.BotClient;
+import io.github.natanimn.BotContext;
+import io.github.natanimn.enums.ParseMode;
+import io.github.natanimn.types.chat_and_user.User;
+import io.github.natanimn.types.payments.LabeledPrice;
+import io.github.natanimn.types.payments.SuccessfulPayment;
+import io.github.natanimn.types.updates.Message;
+import io.github.natanimn.types.updates.PreCheckoutQuery;
+
+/**
+ * Payment example bot of telebof library
+ * For simplicity, we will use telegram star.
+ * Obtain your bot token from @BotFather, and replace with the field TOKEN
+ */
+
+public class PaymentExampleBot {
+    static String TOKEN = System.getenv("TOKEN");
+    public static void main(String[] args){
+        // Initialize
+        BotClient bot = new BotClient(TOKEN);
+
+        // Register /start command
+        bot.onMessage(filter -> filter.commands("start"), PaymentExampleBot::start);
+
+        // Register /pay command
+        bot.onMessage(filter -> filter.commands("pay"), PaymentExampleBot::pay);
+
+        // Register pre checkout
+        bot.onPreCheckout(PaymentExampleBot::processPrecheckOut);
+
+        // Register successful payment update
+        bot.onMessage(filter -> filter.successfulPayment(), PaymentExampleBot::acceptPayment);
+
+        // Run the bot
+        bot.run();
+    }
+
+    static void start(BotContext context, Message message){
+        User user = message.from;
+        context.sendMessage(String.format("<b>Hello %s!</b>,\n\nI am payment example bot.\n<i>Send /pay to try it out.</i>", user.mention()))
+                .parseMode(ParseMode.HTML)
+                .exec();
+    }
+
+    static void pay(BotContext context, Message message){
+        // Payment is with Telegram star
+
+        LabeledPrice price = new LabeledPrice("Pay 10 star", 10);
+
+        /** Pass:
+         * title,
+         * description,
+         * payload,
+         * array of labeled price
+         */
+        context.sendInvoice(
+                "Payment test",
+                "Pay 10 start for telebof payment test bot",
+                "pay-10",
+                "XTR",
+                new LabeledPrice[]{price}
+        ).exec();
+    }
+
+    static void processPrecheckOut(BotContext context, PreCheckoutQuery query){
+        // Confirm checkout
+        context.answerPreCheckout(true).exec();
+    }
+
+    static void acceptPayment(BotContext context, Message message){
+        SuccessfulPayment payment = message.successful_payment;
+
+        // Refund the star payment to the user
+        context.refundStarPayment(message.from.id, payment.telegram_payment_charge_id).exec();
+
+        context.sendMessage(
+                "<b>Yay, your money is refunded.</b>\n\n" +
+                "<i>Thanks for your generous donation</i>"
+        ).parseMode(ParseMode.HTML).exec();
+    }
+}
