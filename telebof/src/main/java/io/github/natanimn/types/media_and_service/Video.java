aa1de15f01b48fb7168635b751ffd32f0f1b47c1
natanimn <natanimco@gmail.com>
2025-08-04 13:13:34 +0300

    Added fields start_timestamp and cover, and added javadocs

--- telebof/src/main/java/io/github/natanimn/types/media_and_service/Video.java
+++ telebof/src/main/java/io/github/natanimn/types/media_and_service/Video.java
@@ -1,17 +1,70 @@
 package io.github.natanimn.types.media_and_service;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 0.9
  */
 public class Video{
-    public String file_id, file_unique_id, file_name, mime_type;
-    public Integer width, height, duration;
+
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    public String file_id;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
+     * Can't be used to download or reuse the file.
+     */
+    public String file_unique_id;
+
+    /**
+     * Video width as defined by the sender
+     */
+    public Integer width;
+
+    /**
+     * Video height as defined by the sender
+     */
+    public Integer height;
+
+    /**
+     * Duration of the video in seconds as defined by the sender
+     */
+    public Integer duration;
+
+    /**
+     * Original filename as defined by the sender
+     */
+    public String file_name;
+
+    /**
+     *  MIME type of the file as defined by the sender
+     */
+    public String mime_type;
+
+    /**
+     * File size in bytes.
+     */
     public Integer file_size;
+
+    /**
+     * Video thumbnail
+     */
     public PhotoSize thumbnail;
+
+    /**
+     * Timestamp in seconds from which the video will play in the message
+     */
+    public Integer start_timestamp;
+
+    /**
+     * Available sizes of the cover of the video in the message
+     */
+    public List<PhotoSize> cover;
 
     @Override
     public boolean equals(Object object) {
