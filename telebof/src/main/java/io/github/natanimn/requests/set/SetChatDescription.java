8d16027ab34a9835bc4ad8fc7839e47e668c470e
natanimn <natanimco@gmail.com>
2025-08-12 13:51:22 +0300

    Added javadocs

---
+++ telebof/src/main/java/io/github/natanimn/requests/set/SetChatDescription.java
@@ -0,0 +1,25 @@
+package io.github.natanimn.requests.set;
+
+import io.github.natanimn.requests.AbstractBaseRequest;
+import io.github.natanimn.requests.RequestSender;
+
+/**
+ * SetChatDescription class. Returns True on success.
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 0.9
+ * @implNote {@link io.github.natanimn.BotContext#setChatDescription}
+ */
+public class SetChatDescription extends AbstractBaseRequest<SetChatDescription, Boolean> {
+    /**
+     * Required
+     * @param chat_id chat id
+     * @param title title
+     * @param requestSender request sender
+     */
+    public SetChatDescription(Object chat_id, String title, RequestSender requestSender) {
+        super(chat_id, requestSender, "setChatDescription", Boolean.class);
+        add("description", title);
+    }
+
+}
