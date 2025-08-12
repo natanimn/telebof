e78441a8cc775383672b14f7f92b1d9af62cae75
natanimn <natanimco@gmail.com>
2025-08-12 14:16:55 +0300

    Added javadocs and fixed bugs

--- telebof/src/main/java/io/github/natanimn/BotLog.java
+++ telebof/src/main/java/io/github/natanimn/BotLog.java
@@ -1,120 +1,143 @@
 package io.github.natanimn;
-import java.util.logging.*;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Formatter;
-import java.util.logging.LogRecord;
+import java.util.logging.*;
 
+/**
+ * Log writer
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 0.9
+ */
 class LogFormatter extends Formatter {
-    // ANSI escape code
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
+
+    private final boolean useColor;
+
+    public LogFormatter(boolean useColor) {
+        this.useColor = useColor;
+    }
 
     @Override
     public String format(LogRecord record) {
         StringBuilder builder = new StringBuilder();
-        builder.append(ANSI_CYAN);
-        builder.append(calcDate(record.getMillis()));
-        builder.append(" ");
 
+        String date = calcDate(record.getMillis());
         String levelName;
-        String color;
-        String textColor;
+        String color = "";
+        String reset = "";
 
-        if (record.getLevel().getName().equals("SEVERE")) {
-            levelName = "ERROR";
-            color = ANSI_RED;
-            textColor = ANSI_RED;
+        if (useColor) reset = ANSI_RESET;
+
+        switch (record.getLevel().getName()) {
+            case "SEVERE":
+                levelName = "ERROR";
+                if (useColor) color = ANSI_RED;
+                break;
+            case "CONFIG":
+                levelName = "DEBUG";
+                if (useColor) color = ANSI_WHITE;
+                break;
+            case "WARNING":
+                levelName = "WARN";
+                if (useColor) color = ANSI_YELLOW;
+                break;
+            default:
+                levelName = record.getLevel().getName();
+                if (useColor) color = ANSI_GREEN;
+                break;
         }
 
-        else if (record.getLevel().getName().equals("CONFIG")) {
-            levelName = "DEBUG";
-            color = ANSI_GREEN;
-            textColor = ANSI_WHITE;
-        }
+        String caller = getCallerClassName();
 
-        else  if (record.getLevel().getName().equals("WARNING")){
-            levelName = record.getLevel().getName();
-            color = ANSI_YELLOW;
-            textColor = ANSI_YELLOW;
-        }
+        builder.append(useColor ? ANSI_CYAN : "")
+                .append(date).append(" ")
+                .append(color)
+                .append("[Telebof][").append(levelName).append("]")
+                .append(reset).append(" ")
+                .append(caller)
+                .append(" - ")
+                .append(record.getMessage())
+                .append("\n");
 
-        else {
-            levelName = record.getLevel().getName();
-            color = ANSI_GREEN;
-            textColor = ANSI_GREEN;
-        }
-        builder.append(color);
-        builder.append("[");
-        builder.append(levelName);
-        builder.append("]");
-        builder.append(" - ");
-        builder.append(textColor);
-        builder.append(record.getMessage());
-
-        Object[] params = record.getParameters();
-
-        if (params != null) {
-            builder.append("\t");
-            for (int i = 0; i < params.length; i++) {
-                builder.append(params[i]);
-                if (i < params.length - 1)
-                    builder.append(", ");
-            }
-        }
-
-        builder.append("\n");
         return builder.toString();
     }
 
-    private String calcDate(long milli_sec) {
-        SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date resultdate = new Date(milli_sec);
-        return date_format.format(resultdate);
+    private String calcDate(long millis) {
+        String pattern = System.getProperty("telebof.dateFormat", "yyyy-MM-dd HH:mm:ss");
+        return new SimpleDateFormat(pattern).format(new Date(millis));
     }
 
+    private String getCallerClassName() {
+        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+        boolean foundLoggerClass = false;
+
+        for (StackTraceElement element : stack) {
+            String className = element.getClassName();
+            if (className.equals(BotLog.class.getName()) || className.startsWith("java.util.logging")) {
+                foundLoggerClass = true;
+                continue;
+            }
+            if (foundLoggerClass) {
+                return className.substring(className.lastIndexOf('.') + 1);
+            }
+        }
+        return "UnknownClass";
+    }
 }
 
-/**
- * Natanim Negash
- *  3 March 2025
- */    /**
- * Natanim Negash
- *  3 March 2025
- */
 public class BotLog {
-    static Logger logger;
+    private static final Logger logger;
 
     static {
-        logger = Logger.getLogger("Telebof");
+        logger = Logger.getLogger("io.github.natanimn");
         logger.setUseParentHandlers(false);
+
         ConsoleHandler handler = new ConsoleHandler();
-        handler.setLevel(Level.ALL);
-        Formatter formatter = new LogFormatter();
-        handler.setFormatter(formatter);
+        handler.setLevel(Level.WARNING);
+        handler.setFormatter(new LogFormatter(true));
+
         logger.addHandler(handler);
+
+        String prop = System.getProperty("telebof.logLevel");
+        if (prop != null) {
+            try {
+                Level level = Level.parse(prop.toUpperCase());
+                logger.setLevel(level);
+                handler.setLevel(level);
+            } catch (IllegalArgumentException e) {
+                logger.setLevel(Level.WARNING);
+                handler.setLevel(Level.WARNING);
+            }
+        }
     }
 
-    public static void info(String text){
-        logger.info(text);
+    public static void setLogLevel(Level level) {
+        logger.setLevel(level);
+        for (Handler h : logger.getHandlers()) {
+            h.setLevel(level);
+        }
     }
 
-    public static void error(String text){
-        logger.severe(text);
+    public static void info(String msg) {
+        logger.log(Level.INFO, msg);
     }
 
-    public static void warn(String text){
-        logger.warning(text);
+    public static void warn(String msg) {
+        logger.log(Level.WARNING, msg);
     }
 
-    public static void debug(String text){
-        logger.config(text);
+    public static void error(String msg) {
+        logger.log(Level.SEVERE, msg);
+    }
+
+    public static void debug(String msg) {
+        logger.log(Level.CONFIG, msg);
     }
 }
