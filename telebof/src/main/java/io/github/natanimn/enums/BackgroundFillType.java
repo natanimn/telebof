0a8be1e09ba639c31837aeef65e600b38b41ca79
natanimn <natanimco@gmail.com>
2025-08-12 14:04:18 +0300

    Added java docs

--- telebof/src/main/java/io/github/natanimn/enums/BackgroundFillType.java
+++ telebof/src/main/java/io/github/natanimn/enums/BackgroundFillType.java
@@ -2,6 +2,12 @@ package io.github.natanimn.enums;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Describes of {@link io.github.natanimn.types.media_and_service.BackgroundFill#type}
+ * @author Natanim
+ * @since 12 August 2025
+ * @version 0.9
+ */
 public enum BackgroundFillType {
     @SerializedName("solid")
     SOLID,
--- telebof/src/main/java/io/github/natanimn/enums/BackgroundTypeType.java
+++ telebof/src/main/java/io/github/natanimn/enums/BackgroundTypeType.java
@@ -2,6 +2,12 @@ package io.github.natanimn.enums;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Describes of {@link io.github.natanimn.types.media_and_service.BackgroundType#type}
+ * @author Natanim
+ * @since 12 August 2025
+ * @version 0.9
+ */
 public enum BackgroundTypeType {
     @SerializedName("fill")
     FILL,
