7329125a9ac16d522075d73221917ea98aa8a354
natanimn <natanimco@gmail.com>
2025-08-19 15:41:13 +0300

    Added class SetBusinessAccountProfilePhoto

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetBusinessAccountProfilePhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetBusinessAccountProfilePhoto.java
@@ -2,32 +2,38 @@ package io.github.natanimn.telebof.requests.set;
 
 import io.github.natanimn.telebof.requests.AbstractBaseRequest;
 import io.github.natanimn.telebof.requests.Api;
+import io.github.natanimn.telebof.types.input.InputProfilePhoto;
 
 /**
  * SetBusinessAccountProfilePhoto class. Returns True on success.
+ *
  * @author Natanim
- * @since 19 August 2025
  * @version 1.0.0
  * @see io.github.natanimn.telebof.BotContext#setBusinessAccountProfilePhoto
+ * @since 19 August 2025
  */
 public class SetBusinessAccountProfilePhoto extends AbstractBaseRequest<SetBusinessAccountProfilePhoto, Boolean> {
     /**
      * Required
+     *
      * @param business_connection_id business connection id
-     * @param api api
+     * @param photo                  photo
+     * @param api                    api
      */
-    public SetBusinessAccountProfilePhoto(String business_connection_id, InputP Api api){
+    public SetBusinessAccountProfilePhoto(String business_connection_id, InputProfilePhoto photo, Api api) {
         super(api, "setBusinessAccountProfilePhoto", Boolean.class);
         add("business_connection_id", business_connection_id);
+        add("photo", photo);
     }
 
     /**
      * Optional
+     *
      * @param is_public Pass True to set the public photo, which will be visible even if the main photo is hidden by the business account's privacy settings.
      *                  An account can have only one public photo.
      * @return {@link SetBusinessAccountProfilePhoto}
      */
-    public SetBusinessAccountProfilePhoto isPublic(boolean is_public){
+    public SetBusinessAccountProfilePhoto isPublic(boolean is_public) {
         return add("is_public", is_public);
     }
 }
