fc062b42556bede4f509c5659bbb9b20a6e862dd
natanimn <natanimco@gmail.com>
2025-08-19 14:55:52 +0300

    Added interface InputProfilePhoto

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputProfilePhoto.java
@@ -0,0 +1,9 @@
+package io.github.natanimn.telebof.types.input;
+
+/**
+ * This object describes a profile photo to set.
+ * @author Natanim
+ * @since 19 August 2025
+ * @version 1.0.0
+ */
+public interface InputProfilePhoto extends InputMedia {}
