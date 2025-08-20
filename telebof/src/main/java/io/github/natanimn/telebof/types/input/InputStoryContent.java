1c16c4414aa3e7504c0b0f37973bc1846d588163
natanimn <natanimco@gmail.com>
2025-08-20 14:36:20 +0300

    Added interface InputStoryContent

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContent.java
@@ -0,0 +1,9 @@
+package io.github.natanimn.telebof.types.input;
+
+/**
+ * This object describes the content of a story to post.
+ * @author Natanim
+ * @since 19 August 2025
+ * @version 1.0.0
+ */
+public interface InputStoryContent extends InputMedia{ }
