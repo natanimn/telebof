5cc7df120f2ed7df4d8a3a402d1fb7634cb81676
natanimn <natanimco@gmail.com>
2025-08-20 14:27:34 +0300

    Added class UpgradeGiftSymbol

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftSymbol.java
@@ -0,0 +1,27 @@
+package io.github.natanimn.telebof.types.gift_and_giveaway;
+
+import io.github.natanimn.telebof.types.media_and_service.Sticker;
+
+/**
+ * This object describes the symbol shown on the pattern of a unique gift.
+ * @author Natanim
+ * @since 19 August 2025
+ * @version 1.0.0
+ */
+public class UniqueGiftSymbol {
+    /**
+     * Name of the symbol
+     */
+    public String name;
+
+    /**
+     * The sticker that represents the unique gift
+     */
+    public Sticker sticker;
+
+    /**
+     * The number of unique gifts that receive this model for every 1000 gifts upgraded
+     */
+    public Integer rarity_per_mille;
+}
+
