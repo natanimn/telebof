493340fba0180e9342c2b22dbb851a4ceff80e45
natanimn <natanimco@gmail.com>
2025-08-20 14:36:28 +0300

    Added interface StoryAreaType

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaType.java
@@ -0,0 +1,8 @@
+package io.github.natanimn.telebof.types.story;
+
+import java.io.Serializable;
+
+/**
+ * Describes the type of a clickable area on a story.
+ */
+public interface StoryAreaType extends Serializable { }
