386d2ad69925886a31ab52be863274e9046fff59
natanimn <natanimco@gmail.com>
2025-09-02 23:45:33 +0300

    Added enum

---
+++ telebof/src/main/java/io/github/natanimn/telebof/enums/MessageType.java
@@ -0,0 +1,26 @@
+package io.github.natanimn.telebof.enums;
+
+public enum MessageType {
+    TEXT,
+
+    PHOTO,
+
+    VIDEO,
+
+    AUDIO,
+
+    ANIMATION,
+
+    DOCUMENT,
+
+    VOICE,
+
+    VIDEO_NOTE,
+
+    CONTENT,
+
+    GAME,
+
+    INVOICE
+
+}
