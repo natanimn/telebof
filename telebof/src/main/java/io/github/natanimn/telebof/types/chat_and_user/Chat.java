0193c599173f7e62e023a4357f2d7c6fd06afe17
natanimn <natanimco@gmail.com>
2025-09-07 20:26:03 +0300

    Added field is_direct_messages

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/Chat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/Chat.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * Represents a chat.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.2.0
  */
 public class Chat{
     /**
@@ -46,23 +46,10 @@ public class Chat{
      */
     public Boolean is_forum;
 
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Chat chat = (Chat) object;
-        return id == chat.id && Objects.equals(type, chat.type) &&
-                Objects.equals(title, chat.title) &&
-                Objects.equals(username, chat.username) &&
-                Objects.equals(first_name, chat.first_name) &&
-                Objects.equals(last_name, chat.last_name) &&
-                Objects.equals(is_forum, chat.is_forum);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, type, title, username, first_name, last_name, is_forum);
-    }
+    /**
+     * Optional. True, if the chat is the direct messages chat of a channel
+     */
+    public Boolean is_direct_messages;
 
     @Override
     public String toString() {
@@ -74,6 +61,7 @@ public class Chat{
                 ", first_name='" + first_name + '\'' +
                 ", last_name='" + last_name + '\'' +
                 ", is_forum=" + is_forum +
+                ", is_direct_messages=" + is_direct_messages +
                 '}';
     }
 }
