bd04bcc67fd5b30b885782f0304f895ae3322d6d
natanimn <natanimco@gmail.com>
2025-09-11 13:41:56 +0300

    Added exception class TimeoutException

---
+++ telebof/src/main/java/io/github/natanimn/telebof/exceptions/TimeoutException.java
@@ -0,0 +1,11 @@
+package io.github.natanimn.telebof.exceptions;
+
+/**
+ * @author Natanim
+ * @since 1.2.3
+ */
+public class TimeoutException extends RuntimeException {
+    public TimeoutException(String message) {
+        super(message);
+    }
+}
