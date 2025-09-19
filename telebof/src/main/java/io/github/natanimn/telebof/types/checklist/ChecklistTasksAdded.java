b803925f8b1b585fa91eeff8e5700c1f5b40d5e7
natanimn <natanimco@gmail.com>
2025-09-19 18:20:10 +0300

    Refactor ChecklistTasksAdded class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTasksAdded.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTasksAdded.java
@@ -1,23 +1,18 @@
 package io.github.natanimn.telebof.types.checklist;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * This object represents a service message about tasks added to a checklist.
+ * @param checklistMessage Optional. Message containing the checklist to which the tasks were added. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+ * @param tasks List of tasks added to the checklist
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.2.4
  */
-public class ChecklistTasksAdded {
-    /**
-     * Optional. Message containing the checklist to which the tasks were added.
-     * Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
-     */
-    public Message checklist_message;
-
-    /**
-     * List of tasks added to the checklist
-     */
-    public List<ChecklistTask> tasks;
-}
+public record ChecklistTasksAdded(
+        @SerializedName("checklist_message") Message checklistMessage,
+        List<ChecklistTask> tasks
+) {}
