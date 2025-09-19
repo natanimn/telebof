be086155f797beac7be947310fdebfc1d609df6c
natanimn <natanimco@gmail.com>
2025-09-19 18:20:00 +0300

    Refactor ChecklistTask class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTask.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTask.java
@@ -2,37 +2,24 @@ package io.github.natanimn.telebof.types.checklist;
 
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
 import io.github.natanimn.telebof.types.chat_and_user.User;
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * This object represents a task in a checklist.
+ * @param id Unique identifier of the task
+ * @param text Text of the task
+ * @param textEntities Optional. Special entities that appear in the task text
+ * @param completedByUser Optional. User that completed the task; omitted if the task wasn't completed
+ * @param completionDate Optional. Point in time (Unix timestamp) when the task was completed; 0 if the task wasn't completed
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.2.4
  */
-public class ChecklistTask {
-    /**
-     * Unique identifier of the task
-     */
-    public Integer id;
-
-    /**
-     * Text of the task
-     */
-    public String text;
-
-    /**
-     * Optional. Special entities that appear in the task text
-     */
-    public List<MessageEntity> text_entities;
-
-    /**
-     * Optional. User that completed the task; omitted if the task wasn't completed
-     */
-    public User completed_by_user;
-
-    /**
-     * Optional. Point in time (Unix timestamp) when the task was completed; 0 if the task wasn't completed
-     */
-    public Integer completion_date;
-}
+public record ChecklistTask(
+        Integer id,
+        String text,
+        @SerializedName("text_entities") List<MessageEntity> textEntities,
+        @SerializedName("completed_by_user") User completedByUser,
+        @SerializedName("completion_date") Integer completionDate
+) {}
