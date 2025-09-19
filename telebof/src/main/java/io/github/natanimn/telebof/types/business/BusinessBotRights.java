f18a66dbd159bccb02dd339b5db3676d2230f3bb
natanimn <natanimco@gmail.com>
2025-09-19 18:15:38 +0300

    Refactor BusinessBotRights class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessBotRights.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessBotRights.java
@@ -1,93 +1,67 @@
 package io.github.natanimn.telebof.types.business;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents the rights of a business bot.
+ * @param canReply Optional. True, if the bot can send and edit messages in the  chats that had incoming messages in the last 24 hours
+ * @param canReadMessages Optional. True, if the bot can mark incoming  messages as read
+ * @param canDeleteSentMessages Optional. True, if the bot can delete messages sent by the bot
+ * @param canDeleteAllMessages Optional. True, if the bot can delete all  messages in managed chats
+ * @param canEditName Optional. True, if the bot can edit the first and last name of the business account
+ * @param canEditBio Optional. True, if the bot can edit the bio of the business account
+ * @param canEditProfilePhoto True, if the bot can edit the profile photo of the business account
+ * @param canEditUsername True, if the bot can edit the username of the business account
+ * @param canChangeGiftSettings True, if the bot can change the privacy settings pertaining to gifts for the business account
+ * @param canViewGiftsAndStars True, if the bot can view gifts and the amount of Telegram Stars owned by the business account
+ * @param canConvertGiftsToStars True, if the bot can convert regular gifts owned by the business account to Telegram Stars
+ * @param canTransferAndUpgradeGifts True, if the bot can transfer and upgrade gifts owned by the business account
+ * @param canTransferStars True, if the bot can transfer Telegram Stars received by the business account to its own account, or use them to upgrade and transfer gifts
+ * @param canManageStories True, if the bot can post, edit and delete stories on behalf of the business account
  * @author Natanim
  * @since 19 August 20025
- * @version 1.0.0
+ * @version 1.2.4
  */
-public class BusinessBotRights {
-    /**
-     * Optional.
-     * True, if the bot can send and edit messages in the private chats that had incoming messages in the last 24 hours
-     */
-    public Boolean can_reply;
+public record BusinessBotRights(
+    @SerializedName("can_reply")
+     Boolean canReply,
 
-    /**
-     * Optional.
-     * True, if the bot can mark incoming private messages as read
-     */
-    public Boolean can_read_messages;
+    @SerializedName("can_read_messages")
+     Boolean canReadMessages,
 
-    /**
-     * Optional.
-     * True, if the bot can delete messages sent by the bot
-     */
-    public Boolean can_delete_sent_messages;
+    @SerializedName("can_delete_sent_messages")
+     Boolean canDeleteSentMessages,
 
-    /**
-     * Optional.
-     * True, if the bot can delete all private messages in managed chats
-     */
-    public Boolean can_delete_all_messages;
+    @SerializedName("can_delete_all_messages")
+     Boolean canDeleteAllMessages,
 
-    /**
-     * Optional.
-     * True, if the bot can edit the first and last name of the business account
-     */
-    public Boolean can_edit_name;
+    @SerializedName("can_edit_name")
+     Boolean canEditName,
 
-    /**
-     * Optional.
-     * True, if the bot can edit the bio of the business account
-     */
-    public Boolean can_edit_bio;
+    @SerializedName("can_edit_bio")
+     Boolean canEditBio,
 
-    /**
-     * Optional.
-     * True, if the bot can edit the profile photo of the business account
-     */
-    public Boolean can_edit_profile_photo;
+    @SerializedName("can_edit_profile_photo")
+     Boolean canEditProfilePhoto,
 
-    /**
-     * Optional.
-     * True, if the bot can edit the username of the business account
-     */
-    public Boolean can_edit_username;
+    @SerializedName("can_edit_username")
+     Boolean canEditUsername,
 
-    /**
-     * Optional.
-     * True, if the bot can change the privacy settings pertaining to gifts for the business account
-     */
-    public Boolean can_change_gift_settings;
+    @SerializedName("can_change_gift_settings")
+     Boolean canChangeGiftSettings,
 
-    /**
-     * Optional.
-     * True, if the bot can view gifts and the amount of Telegram Stars owned by the business account
-     */
-    public Boolean can_view_gifts_and_stars;
+    @SerializedName("can_view_gifts_and_stars")
+     Boolean canViewGiftsAndStars,
 
-    /**
-     * Optional.
-     * True, if the bot can convert regular gifts owned by the business account to Telegram Stars
-     */
-    public Boolean can_convert_gifts_to_stars;
+    @SerializedName("can_convert_gifts_to_stars")
+     Boolean canConvertGiftsToStars,
 
-    /**
-     * Optional.
-     * True, if the bot can transfer and upgrade gifts owned by the business account
-     */
-    public Boolean can_transfer_and_upgrade_gifts;
+    @SerializedName("can_transfer_and_upgrade_gifts")
+     Boolean canTransferAndUpgradeGifts,
 
-    /**
-     * Optional.
-     * True, if the bot can transfer Telegram Stars received by the business account to its own account, or use them to upgrade and transfer gifts
-     */
-    public Boolean can_transfer_stars;
+    @SerializedName("can_transfer_stars")
+     Boolean canTransferStars,
 
-    /**
-     * Optional.
-     * True, if the bot can post, edit and delete stories on behalf of the business account
-     */
-    public Boolean can_manage_stories;
-}
+    @SerializedName("can_manage_stories")
+     Boolean canManageStories
+    ){ }
