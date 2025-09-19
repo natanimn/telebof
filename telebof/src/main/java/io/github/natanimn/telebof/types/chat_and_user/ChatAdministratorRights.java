0437225d64a2346faff3b09e68827ce5ea471f0e
natanimn <natanimco@gmail.com>
2025-09-19 18:17:53 +0300

    Refactor ChatAdministratorRights class into a record, added method builder, build and inner class ChatAdministratorRightsBuilder for using setter

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatAdministratorRights.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatAdministratorRights.java
@@ -1,256 +1,176 @@
 package io.github.natanimn.telebof.types.chat_and_user;
 
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
-import java.util.Objects;
-
 
 /**
  * Represents the rights of an administrator in a chat.
- * @author Natanim 
+ * @param isAnonymous True, if the user's presence in the chat is hidden
+ * @param canManageChat True, if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members, report spam messages, ignore slow mode, and send messages to the chat without paying Telegram Stars. Implied by any other administrator privilege.
+ * @param canDeleteMessages True, if the administrator can delete messages of other user
+ * @param canManageVideoChats True, if the administrator can manage video chats
+ * @param canRestrictMembers True, if the administrator can restrict, ban or unban chat members, or access supergroup statistics
+ * @param canPromoteMembers True, if the administrator can add new administrators with a subset of their own privileges or demote administrators that they have promoted, directly or indirectly (promoted by administrators that were appointed by the user)
+ * @param canChangeInfo True, if the user is allowed to change the chat title, photo and other settings
+ * @param canInviteUsers True, if the user is allowed to invite new users to the chat
+ * @param canPostMessages True, if the administrator can post messages in the channel, approve suggested posts, or access channel statistics; for channels only
+ * @param canEditMessages True, if the administrator can edit messages of other users and can pin messages; for channels only
+ * @param canPinMessages True, if the user is allowed to pin messages; for groups and supergroups only
+ * @param canManageTopics True, if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
+ * @param canPostStories True, if the administrator can post stories to the chat
+ * @param canEditStories True, if the administrator can edit stories posted by other users, post stories to the chat page, pin chat stories, and access the chat's story archive
+ * @param canDeleteStories True, if the administrator can delete stories posted by other users
+ * @param canManageDirectMessages True, if the administrator can manage direct messages of the channel and decline suggested posts; for channels only
+ * @author Natanim
  * @since 3 March 2025
- * @version 1.2.0
+ * @version 1.2.4
  */
-public class ChatAdministratorRights implements Serializable {
-    /**
-     * True, if the user's presence in the chat is hidden
-     */
-    public Boolean is_anonymous;
+public record ChatAdministratorRights(
+        @SerializedName("is_anonymous") Boolean isAnonymous,
+        @SerializedName("can_manage_chat") Boolean canManageChat,
+        @SerializedName("can_delete_messages") Boolean canDeleteMessages,
+        @SerializedName("can_manage_video_chats") Boolean canManageVideoChats,
+        @SerializedName("can_restrict_members") Boolean canRestrictMembers,
+        @SerializedName("can_promote_members") Boolean canPromoteMembers,
+        @SerializedName("can_change_info") Boolean canChangeInfo,
+        @SerializedName("can_invite_users") Boolean canInviteUsers,
+        @SerializedName("can_post_messages") Boolean canPostMessages,
+        @SerializedName("can_edit_messages") Boolean canEditMessages,
+        @SerializedName("can_pin_messages") Boolean canPinMessages,
+        @SerializedName("can_manage_topics") Boolean canManageTopics,
+        @SerializedName("can_post_stories") Boolean canPostStories,
+        @SerializedName("can_edit_stories") Boolean canEditStories,
+        @SerializedName("can_delete_stories") Boolean canDeleteStories,
+        @SerializedName("can_manage_direct_messages") Boolean canManageDirectMessages
+) implements Serializable {
 
     /**
-     * True, if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members,
-     * report spam messages, ignore slow mode, and send messages to the chat without paying Telegram Stars. Implied by any other administrator privilege.
+     * Creates a new ChatAdministratorRights builder
      */
-    public Boolean can_manage_chat;
-
-    /**
-     * True, if the administrator can delete messages of other user
-     */
-    public Boolean can_delete_messages;
-
-    /**
-     * True, if the administrator can manage video chats
-     */
-    public Boolean can_manage_video_chats;
-
-    /**
-     * True, if the administrator can restrict, ban or unban chat members, or access supergroup statistics
-     */
-    public Boolean can_restrict_members;
-
-    /**
-     * True, if the administrator can add new administrators with a subset of their own privileges or demote administrators that they have promoted,
-     * directly or indirectly (promoted by administrators that were appointed by the user)
-     */
-    public Boolean can_promote_members;
-
-    /**
-     * True, if the user is allowed to change the chat title, photo and other settings
-     */
-    public Boolean can_change_info;
-
-    /**
-     * True, if the user is allowed to invite new users to the chat
-     */
-    public Boolean can_invite_users;
-
-    /**
-     * True, if the administrator can post messages in the channel, approve suggested posts, or access channel statistics; for channels only
-     */
-    public Boolean can_post_messages;
-
-    /**
-     * True, if the administrator can edit messages of other users and can pin messages; for channels only
-     */
-    public Boolean can_edit_messages;
-
-    /**
-     * True, if the user is allowed to pin messages; for groups and supergroups only
-     */
-    public Boolean can_pin_messages;
-
-    /**
-     * True, if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
-     */
-    public Boolean can_manage_topics;
-
-    /**
-     * True, if the administrator can post stories to the chat
-     */
-    public Boolean can_post_stories;
-
-    /**
-     * True, if the administrator can edit stories posted by other users, post stories to the chat page, pin chat stories, and access the chat's story archive
-     */
-    public Boolean can_edit_stories;
-
-    /**
-     * True, if the administrator can delete stories posted by other users
-     */
-    public Boolean can_delete_stories;
-
-    /**
-     * Optional. True, if the administrator can manage direct messages of the channel and decline suggested posts; for channels only
-     */
-    public Boolean can_manage_direct_messages;
-
-    /**
-     * Setter method
-     * @param is_anonymous makes the user's presence anonymous/hidden or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights isAnonymous(boolean is_anonymous) {
-        this.is_anonymous = is_anonymous;
-        return this;
+    public static ChatAdministratorRightsBuilder builder() {
+        return new ChatAdministratorRightsBuilder();
     }
 
     /**
-     * Setter method
-     * @param can_manage_chat whether the admin can manage chat or not
-     * @return {@link ChatAdministratorRights}
+     * Builder class for ChatAdministratorRights
      */
-    public ChatAdministratorRights canManageChat(boolean can_manage_chat) {
-        this.can_manage_chat = can_manage_chat;
-        return this;
-    }
+    public static class ChatAdministratorRightsBuilder {
+        private Boolean isAnonymous;
+        private Boolean canManageChat;
+        private Boolean canDeleteMessages;
+        private Boolean canManageVideoChats;
+        private Boolean canRestrictMembers;
+        private Boolean canPromoteMembers;
+        private Boolean canChangeInfo;
+        private Boolean canInviteUsers;
+        private Boolean canPostMessages;
+        private Boolean canEditMessages;
+        private Boolean canPinMessages;
+        private Boolean canManageTopics;
+        private Boolean canPostStories;
+        private Boolean canEditStories;
+        private Boolean canDeleteStories;
+        private Boolean canManageDirectMessages;
 
-    /**
-     * Setter method
-     * @param can_post_messages whether the user can send message or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canPostMessages(boolean can_post_messages) {
-        this.can_post_messages = can_post_messages;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder isAnonymous(boolean isAnonymous) {
+            this.isAnonymous = isAnonymous;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_edit_messages whether the admin can edit messages or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canEditMessages(boolean can_edit_messages) {
-        this.can_edit_messages = can_edit_messages;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canManageChat(boolean canManageChat) {
+            this.canManageChat = canManageChat;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_delete_messages whether the admin can delete messages or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canDeleteMessages(boolean can_delete_messages) {
-        this.can_delete_messages = can_delete_messages;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canDeleteMessages(boolean canDeleteMessages) {
+            this.canDeleteMessages = canDeleteMessages;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_manage_video_chats whether the admin can manage video chats or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canManageVideoChats(boolean can_manage_video_chats) {
-        this.can_manage_video_chats = can_manage_video_chats;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canManageVideoChats(boolean canManageVideoChats) {
+            this.canManageVideoChats = canManageVideoChats;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_restrict_members whether the admin can restrict members or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canRestrictMembers(boolean can_restrict_members) {
-        this.can_restrict_members = can_restrict_members;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canRestrictMembers(boolean canRestrictMembers) {
+            this.canRestrictMembers = canRestrictMembers;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_promote_members whether the admin can promote members or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canPromoteMembers(boolean can_promote_members) {
-        this.can_promote_members = can_promote_members;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canPromoteMembers(boolean canPromoteMembers) {
+            this.canPromoteMembers = canPromoteMembers;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_change_info whether the admin can change info or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canChangeInfo(boolean can_change_info) {
-        this.can_change_info = can_change_info;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canChangeInfo(boolean canChangeInfo) {
+            this.canChangeInfo = canChangeInfo;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_invite_users whether the user can invite users or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canInviteUsers(boolean can_invite_users) {
-        this.can_invite_users = can_invite_users;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canInviteUsers(boolean canInviteUsers) {
+            this.canInviteUsers = canInviteUsers;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_pin_messages whether the admin can pin messages or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canPinMessages(boolean can_pin_messages) {
-        this.can_pin_messages = can_pin_messages;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canPostMessages(boolean canPostMessages) {
+            this.canPostMessages = canPostMessages;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_manage_topics whether the admin can manage topics or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canManageTopics(boolean can_manage_topics) {
-        this.can_manage_topics = can_manage_topics;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canEditMessages(boolean canEditMessages) {
+            this.canEditMessages = canEditMessages;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_post_stories whether the admin can post stories or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canPostStories(boolean can_post_stories){
-        this.can_post_stories = can_post_stories;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canPinMessages(boolean canPinMessages) {
+            this.canPinMessages = canPinMessages;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_edit_stories whether the admin can edit stories or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canEditStories(boolean can_edit_stories){
-        this.can_edit_stories = can_edit_stories;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canManageTopics(boolean canManageTopics) {
+            this.canManageTopics = canManageTopics;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_delete_stories whether the admin can delete stories or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canDeleteStories(boolean can_delete_stories){
-        this.can_delete_stories = can_delete_stories;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canPostStories(boolean canPostStories) {
+            this.canPostStories = canPostStories;
+            return this;
+        }
 
-    /**
-     * Setter method
-     * @param can_manage_direct_messages whether the admin can manage direct messages or not
-     * @return {@link ChatAdministratorRights}
-     */
-    public ChatAdministratorRights canManageDirectMessages(boolean can_manage_direct_messages){
-        this.can_manage_direct_messages = can_manage_direct_messages;
-        return this;
-    }
+        public ChatAdministratorRightsBuilder canEditStories(boolean canEditStories) {
+            this.canEditStories = canEditStories;
+            return this;
+        }
 
+        public ChatAdministratorRightsBuilder canDeleteStories(boolean canDeleteStories) {
+            this.canDeleteStories = canDeleteStories;
+            return this;
+        }
+
+        public ChatAdministratorRightsBuilder canManageDirectMessages(boolean canManageDirectMessages) {
+            this.canManageDirectMessages = canManageDirectMessages;
+            return this;
+        }
+
+        public ChatAdministratorRights build() {
+            return new ChatAdministratorRights(
+                    isAnonymous,
+                    canManageChat,
+                    canDeleteMessages,
+                    canManageVideoChats,
+                    canRestrictMembers,
+                    canPromoteMembers,
+                    canChangeInfo,
+                    canInviteUsers,
+                    canPostMessages,
+                    canEditMessages,
+                    canPinMessages,
+                    canManageTopics,
+                    canPostStories,
+                    canEditStories,
+                    canDeleteStories,
+                    canManageDirectMessages
+            );
+        }
+    }
 }
