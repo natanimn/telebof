e61d1c6a267fe0ec11a71d9eaac98b0cb8cb56fc
natanimn <natanimco@gmail.com>
2025-09-19 18:20:19 +0300

    Refactor ChecklistTasksDone class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTasksDone.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTasksDone.java
@@ -1,28 +1,20 @@
 package io.github.natanimn.telebof.types.checklist;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * This object represents a service message about checklist tasks marked as done or not done.
+ * @param checklistMessage Optional. Message containing the checklist whose tasks were marked as done or not done. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+ * @param markedAsDoneTaskIds Optional. Identifiers of the tasks that were marked as done
+ * @param markedAsNotDoneTaskIds Optional. Identifiers of the tasks that were marked as not done
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.2.4
  */
-public class ChecklistTasksDone {
-    /**
-     * Optional. Message containing the checklist whose tasks were marked as done or not done.
-     * Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
-     */
-    public Message checklist_message;
-
-    /**
-     * Optional. Identifiers of the tasks that were marked as done
-     */
-    public List<Integer> marked_as_done_task_ids;
-
-    /**
-     * Optional. Identifiers of the tasks that were marked as not done
-     */
-    public List<Integer> marked_as_not_done_task_ids;
-}
+public record ChecklistTasksDone(
+        @SerializedName("checklist_message") Message checklistMessage,
+        @SerializedName("marked_as_done_task_ids") List<Integer> markedAsDoneTaskIds,
+        @SerializedName("marked_as_not_done_task_ids") List<Integer> markedAsNotDoneTaskIds
+) {}
