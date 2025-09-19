d1df6f025cd1c839d762c39a189668db14ef9727
natanimn <natanimco@gmail.com>
2025-09-19 18:16:20 +0300

    Refactor BusinessOpeningHoursInterval class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHoursInterval.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHoursInterval.java
@@ -1,36 +1,19 @@
 package io.github.natanimn.telebof.types.business;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes the opening hours of a business.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.2.4
+ * @param openingMinute The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 * 24 * 60
+ * @param closingMinute The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 * 24 * 60
  */
-public class BusinessOpeningHoursInterval{
-    /**
-     * The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which
-     * the business is open; 0 - 7 * 24 * 60
-     */
-    public Integer opening_minute;
+public record BusinessOpeningHoursInterval(
+    @SerializedName("opening_minute")
+    Integer openingMinute,
 
-    /**
-     * The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which
-     * the business is open; 0 - 8 * 24 * 60
-     */
-    public Integer closing_minute;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        BusinessOpeningHoursInterval that = (BusinessOpeningHoursInterval) object;
-        return Objects.equals(opening_minute, that.opening_minute) && Objects.equals(closing_minute, that.closing_minute);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(opening_minute, closing_minute);
-    }
-}
+    @SerializedName("closing_minute")
+    Integer closingMinute
+){}
