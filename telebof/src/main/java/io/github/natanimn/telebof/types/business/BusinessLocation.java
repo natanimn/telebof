995e11af23f0134540ce504570029dd2df68b693
natanimn <natanimco@gmail.com>
2025-09-19 18:16:00 +0300

    Refactor BusinessLocation class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessLocation.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessLocation.java
@@ -1,35 +1,13 @@
 package io.github.natanimn.telebof.types.business;
 
 import io.github.natanimn.telebof.types.media_and_service.Location;
-import java.util.Objects;
 
 /**
  * Contains information about the location of a Telegram Business account.
  * @author Natanim
  * @since 3 March 2025
  * @version 0.8
+ * @param address Address of the business
+ * @param location Location of the business
  */
-public class BusinessLocation{
-    /**
-     * Address of the business
-     */
-    public String address;
-
-    /**
-     * Location of the business
-     */
-    public Location location;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        BusinessLocation that = (BusinessLocation) object;
-        return Objects.equals(address, that.address) && Objects.equals(location, that.location);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(address, location);
-    }
-}
+public record BusinessLocation(String address, Location location){ }
