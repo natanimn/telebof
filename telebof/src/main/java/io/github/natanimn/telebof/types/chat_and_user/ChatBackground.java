d79c08c19fb9864db2975f210a7525eb49746eb6
natanimn <natanimco@gmail.com>
2025-09-19 18:18:06 +0300

    Refactor ChatBackground class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBackground.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBackground.java
@@ -2,30 +2,11 @@ package io.github.natanimn.telebof.types.chat_and_user;
 
 import io.github.natanimn.telebof.types.BackgroundType;
 
-import java.util.Objects;
-
 /**
  * This class represents a chat background.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.2.4
+ * @param type Type of the background
  */
-public class ChatBackground {
-    /**
-     * Type of the background
-     */
-    public BackgroundType type;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ChatBackground that = (ChatBackground) object;
-        return Objects.equals(type, that.type);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(type);
-    }
-}
+public record ChatBackground(BackgroundType type) { }
