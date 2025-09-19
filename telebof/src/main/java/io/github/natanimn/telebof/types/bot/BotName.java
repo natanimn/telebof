257c5699120136c21e49c5151cb995eb6e5955b8
natanimn <natanimco@gmail.com>
2025-09-19 18:15:10 +0300

    Refactor BotName class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotName.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotName.java
@@ -1,29 +1,11 @@
 package io.github.natanimn.telebof.types.bot;
 
-import java.util.Objects;
 
 /**
  * This object represents the bot's name.
  * @author Natanim
  * @since 3 March 2025
  * @version 0.8
+ * @param name The bot's name
  */
-public class BotName {
-    /**
-     * The bot's name
-     */
-    public String name;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        BotName botName = (BotName) object;
-        return Objects.equals(name, botName.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-}
+public record BotName(String name) { }
