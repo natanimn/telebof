2817120d7b39de919e2f2ed63bd5d0d22e03c10c
natanimn <natanimco@gmail.com>
2025-09-19 18:15:49 +0300

    Refactor BusinessIntro class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessIntro.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessIntro.java
@@ -1,42 +1,14 @@
 package io.github.natanimn.telebof.types.business;
 
 import io.github.natanimn.telebof.types.media_and_service.Sticker;
-import java.util.Objects;
 
 /**
  * Contains information about the start page settings of a Telegram Business account.
+ * @param title Title text of the business intro
+ * @param message Message text of the business intro
+ * @param sticker Sticker of the business intro
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.2.4
  */
-public class BusinessIntro{
-    /**
-     * Title text of the business intro
-     */
-    public String title;
-
-    /**
-     * Message text of the business intro
-     */
-    public String message;
-
-    /**
-     * Sticker of the business intro
-     */
-    public Sticker sticker;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        BusinessIntro that = (BusinessIntro) object;
-        return Objects.equals(title, that.title) &&
-                Objects.equals(message, that.message) &&
-                Objects.equals(sticker, that.sticker);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, message, sticker);
-    }
-}
+public record BusinessIntro(String title, String message, Sticker sticker){ }
