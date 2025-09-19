ac6dd33e953c4e7e8f4285ecbd3983bb8fc6d05a
natanimn <natanimco@gmail.com>
2025-09-19 18:22:03 +0300

    Refactor UserChatBoosts class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/UserChatBoosts.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/UserChatBoosts.java
@@ -1,17 +1,13 @@
 package io.github.natanimn.telebof.types.chat_and_user;
 
 import java.util.List;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a list of boosts added to a chat by a user.
+ * @param boosts The list of boosts added to the chat by the user
  * @author Natanim
  * @since 3 March 2025
  * @version 0.9
  */
-public class UserChatBoosts {
-    /**
-     * The list of boosts added to the chat by the user
-     */
-    public List<ChatBoost> boosts;
-
-}
+public record UserChatBoosts(List<ChatBoost> boosts) {}
