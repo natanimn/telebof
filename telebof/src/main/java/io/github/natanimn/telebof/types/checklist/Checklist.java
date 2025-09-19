20a6a0c154dfcf14ba70bde92d93ace32c4a0f28
natanimn <natanimco@gmail.com>
2025-09-19 18:19:51 +0300

    Refactor Checklist class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/checklist/Checklist.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/checklist/Checklist.java
@@ -1,37 +1,24 @@
 package io.github.natanimn.telebof.types.checklist;
 
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * This object represents a checklist.
+ * @param title Title of the checklist
+ * @param titleEntities Optional. Special entities that appear in the checklist title
+ * @param tasks List of tasks in the checklist
+ * @param othersCanAddTasks Optional. True, if users other than the creator of the list can add tasks to the list
+ * @param othersCanMarkTasksAsDone Optional. True, if users other than the creator of the list can mark tasks as done or not done
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.2.4
  */
-public class Checklist {
-    /**
-     * Title of the checklist
-     */
-    public String title;
-
-    /**
-     * Optional. Special entities that appear in the checklist title
-     */
-    public List<MessageEntity> title_entities;
-
-    /**
-     * List of tasks in the checklist
-     */
-    public List<ChecklistTask> tasks;
-
-    /**
-     * Optional. True, if users other than the creator of the list can add tasks to the list
-     */
-    public Boolean others_can_add_tasks;
-
-    /**
-     * Optional. True, if users other than the creator of the list can mark tasks as done or not done
-     */
-    public Boolean others_can_mark_tasks_as_done;
-}
+public record Checklist(
+        String title,
+        @SerializedName("title_entities") List<MessageEntity> titleEntities,
+        List<ChecklistTask> tasks,
+        @SerializedName("others_can_add_tasks") Boolean othersCanAddTasks,
+        @SerializedName("others_can_mark_tasks_as_done") Boolean othersCanMarkTasksAsDone
+) {}
