482823e9036bf50786fdc599baa871141ba8910c
natanimn <natanimco@gmail.com>
2025-09-19 18:19:38 +0300

    Refactor ChatShared class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatShared.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatShared.java
@@ -1,53 +1,25 @@
 package io.github.natanimn.telebof.types.chat_and_user;
 
 import io.github.natanimn.telebof.types.media_and_service.PhotoSize;
-
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 import java.util.Objects;
 
 /**
  * This object contains information about a chat that was shared with the bot using a {@link io.github.natanimn.telebof.types.keyboard.KeyboardButtonRequestChat} button.
+ * @param requestId Identifier of the request
+ * @param chatId Identifier of the shared chat.
+ * @param title Optional. Title of the chat, if the title was requested by the bot.
+ * @param username Optional. Username of the chat, if the username was requested by the bot and available.
+ * @param photo Optional. Available sizes of the chat photo, if the photo was requested by the bot
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.2.4
  */
-public class ChatShared {
-    /**
-     * Identifier of the request
-     */
-    public Integer request_id;
-
-    /**
-     * Identifier of the shared chat.
-     */
-    public Long chat_id;
-
-    /**
-     * Optional. Title of the chat, if the title was requested by the bot.
-     */
-    public String title;
-
-    /**
-     * Optional. Username of the chat, if the username was requested by the bot and available.
-     */
-    public String username;
-
-    /**
-     * Optional. Available sizes of the chat photo, if the photo was requested by the bot
-     */
-    public List<PhotoSize> photo;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ChatShared that = (ChatShared) object;
-        return Objects.equals(request_id, that.request_id) && Objects.equals(chat_id, that.chat_id) &&
-                Objects.equals(title, that.title) && Objects.equals(username, that.username) && Objects.equals(photo, that.photo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(request_id, chat_id, title, username, photo);
-    }
-}
+public record ChatShared(
+        @SerializedName("request_id") Integer requestId,
+        @SerializedName("chat_id") Long chatId,
+        String title,
+        String username,
+        List<PhotoSize> photo
+) { }
