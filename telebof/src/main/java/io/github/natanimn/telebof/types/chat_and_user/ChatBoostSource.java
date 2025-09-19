58822692b6b2b04c048a826837206cb6d00ba207
natanimn <natanimco@gmail.com>
2025-09-19 18:18:33 +0300

    Refactor ChatBoostSource class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoostSource.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoostSource.java
@@ -1,52 +1,30 @@
 package io.github.natanimn.telebof.types.chat_and_user;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes the source of a chat boost.
+ * @param source Source of the boost, one of “premium”, "gift_code", "giveaway"
+ * @param giveawayMessageId Identifier of a message in the chat with the giveaway; the message could have been deleted already.
+ *                          May be 0 if the message isn't sent yet.
+ * @param prizeStarCount The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
+ * @param user User
+ * @param isUnclaimed True, if the giveaway was completed, but there was no user to win the prize
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.2.4
  */
-public class ChatBoostSource {
-    /**
-     * Source of the boost, one of “premium”, "gift_code", "giveaway"
-     */
-    public String source;
+public record ChatBoostSource(
+    String source,
 
-    /**
-     * Identifier of a message in the chat with the giveaway; the message could have been deleted already.
-     * May be 0 if the message isn't sent yet.
-     */
-    public Integer giveaway_message_id;
+    @SerializedName("giveaway_message_id")
+    Integer giveawayMessageId,
 
-    /**
-     * The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
-     */
-    public Integer prize_star_count;
+    @SerializedName("prize_star_count")
+    Integer prizeStarCount,
 
-    /**
-     * User
-     */
-    public User user;
+    User user,
 
-    /**
-     * True, if the giveaway was completed, but there was no user to win the prize
-     */
-    public Boolean is_unclaimed;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ChatBoostSource that = (ChatBoostSource) object;
-        return Objects.equals(source, that.source) && Objects.equals(giveaway_message_id, that.giveaway_message_id)
-                && Objects.equals(user, that.user) && Objects.equals(is_unclaimed, that.is_unclaimed) &&
-                Objects.equals(prize_star_count, that.prize_star_count);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(source, giveaway_message_id, user, is_unclaimed, prize_star_count);
-    }
-}
+    @SerializedName("is_unclaimed")
+    Boolean isUnclaimed
+){ }
