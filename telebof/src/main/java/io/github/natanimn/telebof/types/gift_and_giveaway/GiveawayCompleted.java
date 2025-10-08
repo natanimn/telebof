6462e17f8fd91d26836b42ab96d9e2ff79d5e600
natanimn <natanimco@gmail.com>
2025-10-08 15:29:33 +0300

    Refactor GiveawayCompleted class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCompleted.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCompleted.java
@@ -1,32 +1,21 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the completion of a giveaway without public winners.
- * @author Natanim 
+ * @param winnerCount Number of winners in the giveaway
+ * @param unclaimedPrizeCount Optional. Number of undistributed prizes
+ * @param giveawayMessage Optional. Message with the giveaway that was completed, if it wasn't deleted
+ * @param isStarGiveaway Optional. True, if the giveaway is a Telegram Star giveaway. Otherwise, currently, the giveaway is a Telegram Premium giveaway.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class GiveawayCompleted {
-    /**
-     * Number of winners in the giveaway
-     */
-    public Integer winner_count;
-
-    /**
-     * Optional. Number of undistributed prizes
-     */
-    public Integer unclaimed_prize_count;
-
-    /**
-     * Optional. Message with the giveaway that was completed, if it wasn't deleted
-     */
-    public Message giveaway_message;
-
-    /**
-     * Optional. True, if the giveaway is a Telegram Star giveaway. Otherwise, currently, the giveaway is a Telegram Premium giveaway.
-     */
-    public Boolean is_star_giveaway;
-
-}
+public record GiveawayCompleted(
+        @SerializedName("winner_count") Integer winnerCount,
+        @SerializedName("unclaimed_prize_count") Integer unclaimedPrizeCount,
+        @SerializedName("giveaway_message") Message giveawayMessage,
+        @SerializedName("is_star_giveaway") Boolean isStarGiveaway
+) {}
