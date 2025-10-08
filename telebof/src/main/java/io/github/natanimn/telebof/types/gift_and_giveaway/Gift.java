17b9b74fb205230074153ed222dd4e7781ff4e97
natanimn <natanimco@gmail.com>
2025-10-08 15:28:26 +0300

    Refactor Gift class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/Gift.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/Gift.java
@@ -2,35 +2,27 @@ package io.github.natanimn.telebof.types.gift_and_giveaway;
 
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import io.github.natanimn.telebof.types.media_and_service.Sticker;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class represents a gift that can be sent by the bot.
+ * @param id Unique identifier of the gift
+ * @param sticker The sticker that represents the gift
+ * @param starCount The number of Telegram Stars that must be paid to send the sticker
+ * @param upgradeStarCount The number of Telegram Stars that must be paid to upgrade the gift to a unique one
+ * @param totalCount The total number of the gifts of this type that can be sent; for limited gifts only
+ * @param remainingCount The number of remaining gifts of this type that can be sent; for limited gifts only
+ * @param publisherChat Optional. Information about the chat that published the gift
  * @author Natnim
  * @since July 25, 2025
- * @version 1.2.0
+ * @version 1.2.4
  */
-
-public class Gift {
-    /* Unique identifier of the gift */
-    public String id;
-
-    /* The sticker that represents the gift */
-    public Sticker sticker;
-
-    /* The number of Telegram Stars that must be paid to send the sticker */
-    public Integer star_count;
-
-    /* The number of Telegram Stars that must be paid to upgrade the gift to a unique one */
-    public Integer upgrade_star_count;
-
-    /* The total number of the gifts of this type that can be sent; for limited gifts only */
-    public Integer total_count;
-
-    /* The number of remaining gifts of this type that can be sent; for limited gifts only */
-    public Integer remaining_count;
-
-    /**
-     * Optional. Information about the chat that published the gift
-     */
-    public Chat publisher_chat;
-}
+public record Gift(
+        String id,
+        Sticker sticker,
+        @SerializedName("star_count") Integer starCount,
+        @SerializedName("upgrade_star_count") Integer upgradeStarCount,
+        @SerializedName("total_count") Integer totalCount,
+        @SerializedName("remaining_count") Integer remainingCount,
+        @SerializedName("publisher_chat") Chat publisherChat
+) {}
