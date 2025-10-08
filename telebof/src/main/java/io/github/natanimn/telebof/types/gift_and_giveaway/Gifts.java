a513416e303b54ea25778651c2b96e203ef6d1d0
natanimn <natanimco@gmail.com>
2025-10-08 15:28:58 +0300

    Refactor Gifts class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/Gifts.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/Gifts.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 /**
  * This class represent a list of gifts.
+ * @param gifts Array of {@link Gift}
  * @author Natanim
  * @since July 25, 2025
- * @version 0.6
+ * @version 1.3.0
  */
-
-public class Gifts {
-    /* Array of {@link Gift} */
-    public List<Gift> gifts;
-}
+public record Gifts(
+        List<Gift> gifts
+) {}
