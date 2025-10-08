26ed27d2c4b1c0b5847c7e69de55c807955ba751
natanimn <natanimco@gmail.com>
2025-10-08 15:30:57 +0300

    Refactor OwnedGifts class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/OwnedGifts.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/OwnedGifts.java
@@ -1,26 +1,19 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * Contains the list of gifts received and owned by a user or a chat.
+ * @param totalCount The total number of gifts owned by the user or the chat
+ * @param gifts The list of gifts
+ * @param nextOffset Optional. Offset for the next request. If empty, then there are no more results
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class OwnedGifts {
-    /**
-     * The total number of gifts owned by the user or the chat
-     */
-    public Integer total_count;
-
-    /**
-     * The list of gifts
-     */
-    public List<OwnedGift> gifts;
-
-    /**
-     * Optional. Offset for the next request. If empty, then there are no more results
-     */
-    public String next_offset;
-}
+public record OwnedGifts(
+        @SerializedName("total_count") Integer totalCount,
+        List<OwnedGift> gifts,
+        @SerializedName("next_offset") String nextOffset
+) {}
