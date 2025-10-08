60cc33b1bfd26993ef1bb1b28b7150a7ad7c217e
natanimn <natanimco@gmail.com>
2025-10-08 15:29:48 +0300

    Refactor GiveawayCreated class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCreated.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCreated.java
@@ -1,15 +1,14 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the creation of a scheduled giveaway.
- * @author Natanim 
+ * @param prizeStarCount Optional. The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class GiveawayCreated {
-    /**
-     * Optional. The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
-     */
-    public Integer prize_star_count;
-}
+public record GiveawayCreated(
+        @SerializedName("prize_star_count") Integer prizeStarCount
+) {}
