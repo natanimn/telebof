1192ecc136fb5ff5e7a4fdaa74a8b72c462abba5
natanimn <natanimco@gmail.com>
2025-10-10 09:52:34 +0300

    Refactor UniqueGiftBackdrop class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdrop.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdrop.java
@@ -1,27 +1,18 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
-
 import java.io.Serializable;
 
 /**
  * This object describes the backdrop of a unique gift.
+ * @param name Name of the backdrop
+ * @param colors Colors of the backdrop
+ * @param rarityPerMille The number of unique gifts that receive this backdrop for every 1000 gifts upgraded
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class UniqueGiftBackdrop {
-    /**
-     * Name of the backdrop
-     */
-    public String name;
-
-    /**
-     * Colors of the backdrop
-     */
-    public UniqueGiftBackdropColors colors;
-
-    /**
-     * The number of unique gifts that receive this backdrop for every 1000 gifts upgraded
-     */
-    public Integer rarity_per_mille;
-}
+public record UniqueGiftBackdrop(
+        String name,
+        UniqueGiftBackdropColors colors,
+        @com.google.gson.annotations.SerializedName("rarity_per_mille") Integer rarityPerMille
+) implements Serializable {}
