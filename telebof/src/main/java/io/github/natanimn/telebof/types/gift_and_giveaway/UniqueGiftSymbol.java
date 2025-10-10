cac7a3a09fface8d7abc129d8143137b866f7ffa
natanimn <natanimco@gmail.com>
2025-10-10 09:53:19 +0300

    Refactor UniqueGiftSymbol class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftSymbol.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftSymbol.java
@@ -1,27 +1,19 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
 import io.github.natanimn.telebof.types.media_and_service.Sticker;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes the symbol shown on the pattern of a unique gift.
+ * @param name Name of the symbol
+ * @param sticker The sticker that represents the unique gift
+ * @param rarityPerMille The number of unique gifts that receive this model for every 1000 gifts upgraded
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class UniqueGiftSymbol {
-    /**
-     * Name of the symbol
-     */
-    public String name;
-
-    /**
-     * The sticker that represents the unique gift
-     */
-    public Sticker sticker;
-
-    /**
-     * The number of unique gifts that receive this model for every 1000 gifts upgraded
-     */
-    public Integer rarity_per_mille;
-}
-
+public record UniqueGiftSymbol(
+        String name,
+        Sticker sticker,
+        @SerializedName("rarity_per_mille") Integer rarityPerMille
+) {}
