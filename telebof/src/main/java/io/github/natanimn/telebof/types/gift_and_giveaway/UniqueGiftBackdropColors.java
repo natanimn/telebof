23c3c89cee3e25444d6475f58c5e1157563eee5e
natanimn <natanimco@gmail.com>
2025-10-10 09:52:43 +0300

    Refactor UniqueGiftBackdropColors class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdropColors.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdropColors.java
@@ -1,32 +1,21 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
-
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 /**
  * This object describes the colors of the backdrop of a unique gift.
+ * @param centerColor The color in the center of the backdrop in RGB format
+ * @param edgeColor The color on the edges of the backdrop in RGB format
+ * @param symbolColor The color to be applied to the symbol in RGB format
+ * @param textColor The color for the text on the backdrop in RGB format
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class UniqueGiftBackdropColors implements Serializable {
-    /**
-     * The color in the center of the backdrop in RGB format
-     */
-    public Integer center_color;
-
-    /**
-     * The color on the edges of the backdrop in RGB format
-     */
-    public Integer edge_color;
-
-    /**
-     * The color to be applied to the symbol in RGB format
-     */
-    public Integer symbol_color;
-
-    /**
-     * The color for the text on the backdrop in RGB format
-     */
-    public Integer text_color;
-}
+public record UniqueGiftBackdropColors(
+        @SerializedName("center_color") Integer centerColor,
+        @SerializedName("edge_color") Integer edgeColor,
+        @SerializedName("symbol_color") Integer symbolColor,
+        @SerializedName("text_color") Integer textColor
+) implements Serializable {}
