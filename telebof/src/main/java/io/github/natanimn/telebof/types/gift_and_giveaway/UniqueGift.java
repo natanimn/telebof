8cbd9102ed743e87d1c163b9958cf60066d50bd2
natanimn <natanimco@gmail.com>
2025-10-10 09:52:27 +0300

    Refactor UniqueGift class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGift.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGift.java
@@ -1,47 +1,27 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
-
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes a unique gift that was upgraded from a regular gift.
+ * @param baseName Human-readable name of the regular gift from which this unique gift was upgraded
+ * @param name Unique name of the gift. This name can be used in https://t.me/nft/... links and story areas
+ * @param number Unique number of the upgraded gift among gifts upgraded from the same regular gift
+ * @param model Model of the gift
+ * @param symbol Symbol of the gift
+ * @param backdrop Backdrop of the gift
+ * @param publisherChat Optional. Information about the chat that published the gift
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class UniqueGift {
-    /**
-     * Human-readable name of the regular gift from which this unique gift was upgraded
-     */
-    public String base_name;
-
-    /**
-     * Unique name of the gift. This name can be used in https://t.me/nft/... links and story areas
-     */
-    public String name;
-
-    /**
-     * Unique number of the upgraded gift among gifts upgraded from the same regular gift
-     */
-    public Integer number;
-
-    /**
-     * Model of the gift
-     */
-    public UniqueGiftModel model;
-
-    /**
-     * Symbol of the gift
-     */
-    public UniqueGiftSymbol symbol;
-
-    /**
-     * Backdrop of the gift
-     */
-    public UniqueGiftBackdrop backdrop;
-
-    /**
-     * Optional. Information about the chat that published the gift
-     */
-    public Chat publisher_chat;
-}
+public record UniqueGift(
+        @SerializedName("base_name") String baseName,
+        String name,
+        Integer number,
+        UniqueGiftModel model,
+        UniqueGiftSymbol symbol,
+        UniqueGiftBackdrop backdrop,
+        @SerializedName("publisher_chat") Chat publisherChat
+) {}
