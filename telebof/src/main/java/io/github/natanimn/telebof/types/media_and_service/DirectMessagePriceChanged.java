ffbe818f6bcce4b1c4a9941fed1fde6cb6abf6f0
natanimn <natanimco@gmail.com>
2025-10-12 21:38:51 +0300

    Refactor DirectMessagePriceChanged class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/DirectMessagePriceChanged.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/DirectMessagePriceChanged.java
@@ -1,20 +1,16 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a service message about a change in the price of direct messages sent to a channel chat.
+ * @param areDirectMessagesEnabled True, if direct messages are enabled for the channel chat; false otherwise
+ * @param directMessageStarCount Optional. The new number of Telegram Stars that must be paid by users for each direct message sent to the channel. Does not apply to users who have been exempted by administrators. Defaults to 0.
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.3.0
  */
-public class DirectMessagePriceChanged {
-    /**
-     * True, if direct messages are enabled for the channel chat; false otherwise
-     */
-    public Boolean are_direct_messages_enabled;
-
-    /**
-     * Optional. The new number of Telegram Stars that must be paid by users for each direct message sent to the channel.
-     * Does not apply to users who have been exempted by administrators. Defaults to 0.
-     */
-    public Integer direct_message_star_count;
-}
+public record DirectMessagePriceChanged(
+        @SerializedName("are_direct_messages_enabled") Boolean areDirectMessagesEnabled,
+        @SerializedName("direct_message_star_count") Integer directMessageStarCount
+) {}
