1e814f6a0eb14b8cbe41fd674cc21db584f5075e
natanimn <natanimco@gmail.com>
2025-10-12 21:37:59 +0300

    Refactor CallbackGame class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CallbackGame.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CallbackGame.java
@@ -4,6 +4,6 @@ package io.github.natanimn.telebof.types.media_and_service;
  * A placeholder, currently holds no information. Use @BotFather to set up your game.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class CallbackGame { }
+public record CallbackGame() { }
