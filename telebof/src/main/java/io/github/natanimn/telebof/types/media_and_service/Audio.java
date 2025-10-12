aa4b413e94ea607d8bdb1a6c2852b180de31bd3e
natanimn <natanimco@gmail.com>
2025-10-12 21:37:37 +0300

    Refactor Audio class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Audio.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Audio.java
@@ -1,55 +1,30 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents an audio file to be treated as music by the Telegram clients.
- * @author Natanim 
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param performer Optional. Performer of the audio as defined by the sender or by audio tags
+ * @param title Optional. Title of the audio as defined by the sender or by audio tags
+ * @param fileName Optional. Original filename as defined by the sender
+ * @param mimeType Optional. MIME type of the file as defined by the sender
+ * @param duration Duration of the audio in seconds as defined by the sender
+ * @param fileSize Optional. File size in bytes.
+ * @param thumbnail Optional. Thumbnail of the album cover to which the music file belongs
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Audio {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Optional. Performer of the audio as defined by the sender or by audio tags
-     */
-    public String performer;
-
-    /**
-     * Optional. Title of the audio as defined by the sender or by audio tags
-     */
-    public String title;
-
-    /**
-     * Optional. Original filename as defined by the sender
-     */
-    public String file_name;
-
-    /**
-     * Optional. MIME type of the file as defined by the sender
-     */
-    public String mime_type;
-
-    /**
-     * Duration of the audio in seconds as defined by the sender
-     */
-    public Integer duration;
-
-    /**
-     * Optional. File size in bytes.
-     */
-    public Long file_size;
-
-    /**
-     * Optional. Thumbnail of the album cover to which the music file belongs
-     */
-    public PhotoSize thumbnail;
-}
+public record Audio(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        String performer,
+        String title,
+        @SerializedName("file_name") String fileName,
+        @SerializedName("mime_type") String mimeType,
+        Integer duration,
+        @SerializedName("file_size") Long fileSize,
+        PhotoSize thumbnail
+) {}
