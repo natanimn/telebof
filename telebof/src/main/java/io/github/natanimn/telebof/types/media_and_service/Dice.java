a3ae2e89dbf3839ee3ae293455b17087aaffb86a
natanimn <natanimco@gmail.com>
2025-10-12 21:38:44 +0300

    Refactor Dice class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Dice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Dice.java
@@ -1,20 +1,14 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
-
 /**
  * This object represents an animated emoji that displays a random value.
+ * @param emoji Emoji on which the dice throw animation is based
+ * @param value Value of the dice, 1-6 for “🎲”, “🎯” and “🎳” base emoji, 1-5 for “🏀” and “⚽” base emoji, 1-64 for “🎰” base emoji
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Dice {
-    /**
-     * Emoji on which the dice throw animation is based
-     */
-    public String emoji;
-
-    /**
-     * Value of the dice, 1-6 for “🎲”, “🎯” and “🎳” base emoji, 1-5 for “🏀” and “⚽” base emoji, 1-64 for “🎰” base emoji
-     */
-    public Integer value;
-}
+public record Dice(
+        String emoji,
+        Integer value
+) {}
