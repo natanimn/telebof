b0e37e6925c8b09f5157621bc03c7eceb172e823
natanimn <natanimco@gmail.com>
2025-10-12 21:38:35 +0300

    Refactor CopyTextButton class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CopyTextButton.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CopyTextButton.java
@@ -2,13 +2,11 @@ package io.github.natanimn.telebof.types.media_and_service;
 
 /**
  * This class represents an inline keyboard button that copies specified text to the clipboard.
+ * @param text The text to be copied to the clipboard; 1-256 characters
  * @author Natanim
  * @since Jul 22, 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class CopyTextButton {
-    /**
-     * The text to be copied to the clipboard; 1-256 characters
-     */
-    public String text;
-}
+public record CopyTextButton(
+        String text
+) {}
