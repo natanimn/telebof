8992e5347eb36bc1e74b2330da85a1825ad08e11
natanimn <natanimco@gmail.com>
2025-10-12 21:37:47 +0300

    Refactor Birthdate class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Birthdate.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Birthdate.java
@@ -2,24 +2,15 @@ package io.github.natanimn.telebof.types.media_and_service;
 
 /**
  * Describes the birthdate of a user.
- * @author Natanim 
+ * @param day Day of the user's birth; 1-31
+ * @param month Month of the user's birth; 1-12
+ * @param year Optional. Year of the user's birth
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class Birthdate {
-    /**
-     * Day of the user's birth; 1-31
-     */
-    public Byte day;
-
-    /**
-     * Month of the user's birth; 1-12
-     */
-    public Byte month;
-
-    /**
-     * Optional. Year of the user's birth
-     */
-    public Integer year;
-
-}
+public record Birthdate(
+        Byte day,
+        Byte month,
+        Integer year
+) {}
