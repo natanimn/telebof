717cf5aa186f217bd326c0f5d47038b7adf94eaa
natanimn <natanimco@gmail.com>
2025-10-12 21:39:01 +0300

    Refactor Document class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Document.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Document.java
@@ -1,39 +1,24 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a general file (as opposed to {@link PhotoSize}, {@link Voice} and {@link Audio}).
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param fileName Optional. Original filename as defined by the sender
+ * @param mimeType Optional. MIME type of the file as defined by the sender
+ * @param fileSize Optional. File size in bytes.
+ * @param thumbnail Optional. Document thumbnail as defined by the sender
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Document {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Optional. Original filename as defined by the sender
-     */
-    public String file_name;
-
-    /**
-     * Optional. MIME type of the file as defined by the sender
-     */
-    public String mime_type;
-
-    /**
-     * Optional. File size in bytes.
-     */
-    public Long file_size;
-
-    /**
-     * Optional. Document thumbnail as defined by the sender
-     */
-    public PhotoSize thumbnail;
-}
+public record Document(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        @SerializedName("file_name") String fileName,
+        @SerializedName("mime_type") String mimeType,
+        @SerializedName("file_size") Long fileSize,
+        PhotoSize thumbnail
+) {}
