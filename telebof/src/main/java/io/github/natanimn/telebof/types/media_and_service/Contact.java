1b6a8457d9b867e875b094f2016661cd60edb7b9
natanimn <natanimco@gmail.com>
2025-10-12 21:38:11 +0300

    Refactor Contact class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Contact.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Contact.java
@@ -1,35 +1,22 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a phone contact.
+ * @param phoneNumber Contact's phone number
+ * @param firstName Contact's first name
+ * @param lastName Optional. Contact's last name
+ * @param vcard Optional. Additional data about the contact in the form of a vCard
+ * @param userId Optional. Contact's user identifier in Telegram.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Contact {
-    /**
-     * Contact's phone number
-     */
-    public String phone_number;
-
-    /**
-     * Contact's first name
-     */
-    public String first_name;
-
-    /**
-     * Optional. Contact's last name
-     */
-    public String last_name;
-
-    /**
-     * Optional. Additional data about the contact in the form of a <a href="https://en.wikipedia.org/wiki/VCard">vCard</a>
-     */
-    public String vcard;
-
-    /**
-     * Optional. Contact's user identifier in Telegram.
-     */
-    public Long user_id;
-
-}
+public record Contact(
+        @SerializedName("phone_number") String phoneNumber,
+        @SerializedName("first_name") String firstName,
+        @SerializedName("last_name") String lastName,
+        String vcard,
+        @SerializedName("user_id") Long userId
+) {}
