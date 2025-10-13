4a8b48e2faa14afae75e1514d6b8e53500c9a1a0
natanimn <natanimco@gmail.com>
2025-10-13 21:17:05 +0300

    Refactor File class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/File.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/File.java
@@ -1,7 +1,7 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
-
 import io.github.natanimn.telebof.BotContext;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a file ready to be downloaded.
@@ -9,29 +9,17 @@ import io.github.natanimn.telebof.BotContext;
  * It is guaranteed that the link will be valid for at least 1 hour. When the link expires, a new one can be requested by calling {@link BotContext#getFile(String)}.
  * <br><br>
  * <i>The maximum file size to download is 20 MB</i>
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param filePath Optional. File path. Use {@link BotContext#downloadFile} to get the file.
+ * @param fileSize Optional. File size in bytes.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class File {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**(String)
-     * Optional. File path. Use {@link BotContext#downloadFile} to get the file.
-     */
-    public String file_path;
-
-    /**
-     * Optional. File size in bytes.
-     */
-    public Long file_size;
-
-}
+public record File(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        @SerializedName("file_path") String filePath,
+        @SerializedName("file_size") Long fileSize
+) {}
