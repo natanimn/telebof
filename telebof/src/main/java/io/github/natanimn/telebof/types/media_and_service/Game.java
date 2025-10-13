b6c3e7e65af06420de9d4151f6e6bb73400ef20f
natanimn <natanimco@gmail.com>
2025-10-13 21:17:16 +0300

    Refactor Game class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Game.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Game.java
@@ -1,45 +1,26 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
 import io.github.natanimn.telebof.BotContext;
-
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * This object represents a game. Use BotFather to create and edit games, their short names will act as unique identifiers.
+ * @param title Title of the game
+ * @param description Description of the game
+ * @param photo Photo that will be displayed in the game message in chats.
+ * @param text Optional. Brief description of the game or high scores included in the game message. Can be automatically edited to include current high scores for the game when the bot calls {@link BotContext#setGameScore}, or manually edited using {@link BotContext#editMessageText}. 0-4096 characters.
+ * @param textEntities Optional. Special entities that appear in text, such as usernames, URLs, bot commands, etc.
+ * @param animation Optional. Animation that will be displayed in the game message in chats. Upload via @BotFather
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Game {
-    /**
-     * Title of the game
-     */
-    public String title;
-
-    /**
-     * Description of the game
-     */
-    public String description;
-
-    /**
-     * Photo that will be displayed in the game message in chats.
-     */
-    public List<PhotoSize> photo;
-
-    /**
-     * Optional. Brief description of the game or high scores included in the game message.
-     * Can be automatically edited to include current high scores for the game when the bot calls {@link BotContext#setGameScore},
-     * or manually edited using {@link BotContext#editMessageText}. 0-4096 characters.
-     */
-    public String text;
-
-    /**
-     * Optional. Special entities that appear in text, such as usernames, URLs, bot commands, etc.
-     */
-    public List<MessageEntity> text_entities;
-
-    /**
-     * Optional. Animation that will be displayed in the game message in chats. Upload via @BotFather
-     */
-    public Animation animation;
-}
+public record Game(
+        String title,
+        String description,
+        List<PhotoSize> photo,
+        String text,
+        @SerializedName("text_entities") List<MessageEntity> textEntities,
+        Animation animation
+) {}
