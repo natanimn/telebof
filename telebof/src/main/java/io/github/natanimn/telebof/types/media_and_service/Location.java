6feabe7c5ec1af0fab8e3064abf15eb2bde298c2
natanimn <natanimco@gmail.com>
2025-10-13 21:18:00 +0300

    Refactor Location class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Location.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Location.java
@@ -1,42 +1,24 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a point on the map.
- * @author Natanim 
+ * @param longitude Longitude as defined by the sender
+ * @param latitude Latitude as defined by the sender
+ * @param horizontalAccuracy Optional. The radius of uncertainty for the location, measured in meters; 0-1500
+ * @param livePeriod Optional. Time relative to the message sending date, during which the location can be updated; in seconds. For active live locations only.
+ * @param heading Optional. The direction in which user is moving, in degrees; 1-360. For active live locations only.
+ * @param proximityAlertRadius Optional. The maximum distance for proximity alerts about approaching another chat member, in meters. For sent live locations only.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class Location {
-    /**
-     * Longitude as defined by the sender
-     */
-    public Double longitude;
-
-    /**
-     * Latitude as defined by the sender
-     */
-    public Double latitude;
-
-    /**
-     * Optional. The radius of uncertainty for the location, measured in meters; 0-1500
-     */
-    public Double horizontal_accuracy;
-
-    /**
-     * Optional. Time relative to the message sending date, during which the location can be updated; in seconds.
-     * For active live locations only.
-     */
-    public Integer live_period;
-
-    /**
-     * Optional. The direction in which user is moving, in degrees; 1-360. For active live locations only.
-     */
-    public Integer heading;
-
-    /**
-     * Optional. The maximum distance for proximity alerts about approaching another chat member, in meters.
-     * For sent live locations only.
-     */
-    public Integer proximity_alert_radius;
-}
+public record Location(
+        Double longitude,
+        Double latitude,
+        @SerializedName("horizontal_accuracy") Double horizontalAccuracy,
+        @SerializedName("live_period") Integer livePeriod,
+        Integer heading,
+        @SerializedName("proximity_alert_radius") Integer proximityAlertRadius
+) {}
