b5e2a2771ab687f52ca3a063b4450502b8f3f780
natanimn <natanimco@gmail.com>
2025-10-13 21:17:23 +0300

    Refactor GameHighScore class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/GameHighScore.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/GameHighScore.java
@@ -4,23 +4,15 @@ import io.github.natanimn.telebof.types.chat_and_user.User;
 
 /**
  * This object represents one row of the high scores table for a game.
+ * @param position Position in high score table for the game
+ * @param score Score
+ * @param user User
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class GameHighScore {
-    /**
-     * Position in high score table for the game
-     */
-    public Integer position;
-
-    /**
-     * Score
-     */
-    public Integer score;
-
-    /**
-     * User
-     */
-    public User user;
-}
+public record GameHighScore(
+        Integer position,
+        Integer score,
+        User user
+) {}
