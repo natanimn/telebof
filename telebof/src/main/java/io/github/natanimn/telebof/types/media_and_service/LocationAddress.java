4e391d63825622ec96f4831d5c4d4dd45a8c5b49
natanimn <natanimco@gmail.com>
2025-10-13 21:18:10 +0300

    Refactor LocationAddress class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/LocationAddress.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/LocationAddress.java
@@ -1,52 +1,68 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 /**
  * Describes the physical address of a location.
+ * @param countryCode The two-letter ISO 3166-1 alpha-2 country code of the country where the location is located
+ * @param state Optional. State of the location
+ * @param city Optional. City of the location
+ * @param street Optional. Street address of the location
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class LocationAddress implements Serializable {
-    private String country_code, state, city, street;
+public record LocationAddress(
+        @SerializedName("country_code") String countryCode,
+        String state,
+        String city,
+        String street
+) implements Serializable {
 
     /**
-     * Required
-     * @param country_code The two-letter ISO 3166-1 alpha-2 country code of the country where the location is located
+     * Creates a new LocationAddress builder
+     * @param countryCode The two-letter ISO 3166-1 alpha-2 country code of the country where the location is located
      */
-    public LocationAddress(String country_code){
-        this.country_code = country_code;
+    public static LocationAddressBuilder builder(String countryCode) {
+        return new LocationAddressBuilder(countryCode);
     }
 
     /**
-     * Optional
-     * @param state State of the location
-     * @return {@link LocationAddress}
+     * Builder class for LocationAddress
      */
-    public LocationAddress state(String state){
-        this.state = state;
-        return this;
-    }
+    public static class LocationAddressBuilder {
+        private final String countryCode;
+        private String state;
+        private String city;
+        private String street;
 
-    /**
-     * Optional
-     * @param city City of the location
-     * @return {@link LocationAddress}
-     */
-    public LocationAddress city(String city){
-        this.city = city;
-        return this;
-    }
+        public LocationAddressBuilder(String countryCode) {
+            this.countryCode = countryCode;
+        }
 
-    /**
-     * Optional
-     * @param street Street address of the location
-     * @return {@link LocationAddress}
-     */
-    public LocationAddress street(String street){
-        this.street = street;
-        return this;
-    }
+        public LocationAddressBuilder state(String state) {
+            this.state = state;
+            return this;
+        }
 
+        public LocationAddressBuilder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public LocationAddressBuilder street(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public LocationAddress build() {
+            return new LocationAddress(
+                    countryCode,
+                    state,
+                    city,
+                    street
+            );
+        }
+    }
 }
