7a30d80e52fe1e713526fc4a1ac15badb05b4cc2
natanimn <natanimco@gmail.com>
2025-10-13 21:18:34 +0300

    Refactor MessageAutoDeleteTimerChanged class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageAutoDeleteTimerChanged.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageAutoDeleteTimerChanged.java
@@ -1,15 +1,14 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a service message about a change in auto-delete timer settings.
+ * @param messageAutoDeleteTime New auto-delete time for messages in the chat; in seconds
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class MessageAutoDeleteTimerChanged {
-    /**
-     * New auto-delete time for messages in the chat; in seconds
-     */
-    public Integer message_auto_delete_time;
-
-}
+public record MessageAutoDeleteTimerChanged(
+        @SerializedName("message_auto_delete_time") Integer messageAutoDeleteTime
+) {}
