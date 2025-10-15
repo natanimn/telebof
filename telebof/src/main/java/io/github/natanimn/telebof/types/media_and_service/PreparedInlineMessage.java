347f1ddd392238021a388d87eb4bd7d0dbc57199
natanimn <natanimco@gmail.com>
2025-10-15 20:50:48 +0300

    Refactor PreparedInlineMessage class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PreparedInlineMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PreparedInlineMessage.java
@@ -1,19 +1,16 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This class represents an inline message to be sent by a user of a Mini App.
+ * @param id Unique identifier of the prepared message
+ * @param expirationDate Expiration date of the prepared message, in Unix time. Expired prepared messages can no longer be used
  * @author Natanim
  * @since jul 25, 2025
- * @version 0.6
+ * @version 1.3.0
  */
-public class PreparedInlineMessage {
-    /**
-     * Unique identifier of the prepared message
-     */
-    public String id;
-
-    /**
-     * Expiration date of the prepared message, in Unix time. Expired prepared messages can no longer be used
-     */
-    public Integer expiration_date;
-}
+public record PreparedInlineMessage(
+        String id,
+        @SerializedName("expiration_date") Integer expirationDate
+) {}
