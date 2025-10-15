b956324b090d260ffcc2edf53d16b91ed608af10
natanimn <natanimco@gmail.com>
2025-10-15 20:50:16 +0300

    Refactor MessageId class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageId.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageId.java
@@ -1,32 +1,14 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class represents a unique message identifier.
+ * @param messageId Unique message identifier. In specific instances (e.g., message containing a video sent to a big chat), the server might automatically schedule a message instead of sending it immediately. In such cases, this field will be 0 and the relevant message will be unusable until it is actually sent
  * @author Natanim
  * @since 3 March, 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class MessageId {
-
-    /**
-     * Unique message identifier. In specific instances (e.g., message containing a video sent to a big chat),
-     * the server might automatically schedule a message instead of sending it immediately.
-     * In such cases, this field will be 0 and the relevant message will be unusable until it is actually sent
-     */
-    public Integer message_id;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        MessageId messageId = (MessageId) object;
-        return Objects.equals(message_id, messageId.message_id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(message_id);
-    }
-}
+public record MessageId(
+        @SerializedName("message_id") Integer messageId
+) {}
