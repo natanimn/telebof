5e6703d5b7a74a066b4035debe2c12f180dd3a62
natanimn <natanimco@gmail.com>
2025-10-15 20:51:20 +0300

    Refactor ReactionType class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ReactionType.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ReactionType.java
@@ -1,44 +1,64 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
 import io.github.natanimn.telebof.enums.TypeReaction;
-
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 /**
  * This object describes the type of a reaction.
+ * @param type Type of the reaction
+ * @param emoji Reaction emoji. Currently, it can be one of
+ * "❤", "👍", "👎", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊",
+ * "🤡", "🥱", "🥴", "😍", "🐳", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋",
+ * "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍", "🤗", "🫡", "🎅", "🎄", "☃",
+ * "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂", "🤷", "🤷‍♀", "😡"
+ * @param customEmojiId Custom emoji identifier
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class ReactionType implements Serializable {
-    /**
-     * Type of the reaction
-     */
-    public TypeReaction type;
+public record ReactionType(
+        TypeReaction type,
+        String emoji,
+        @SerializedName("custom_emoji_id") String customEmojiId
+) implements Serializable {
 
     /**
-     * Reaction emoji. Currently, it can be one of
-     * "❤", "👍", "👎", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊",
-     * "🤡", "🥱", "🥴", "😍", "🐳", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋",
-     * "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍", "🤗", "🫡", "🎅", "🎄", "☃",
-     * "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂", "🤷", "🤷‍♀", "😡"
+     * Creates a new ReactionType builder
+     * @param type Type of the reaction
      */
-    public String emoji;
-
-    /**
-     * Custom emoji identifier
-     */
-    public String custom_emoji_id;
-
-    public ReactionType(TypeReaction reaction){
-        this.type = reaction;
+    public static ReactionTypeBuilder builder(TypeReaction type) {
+        return new ReactionTypeBuilder(type);
     }
 
-    public void emoji(String emoji){
-        this.emoji = emoji;
-    }
+    /**
+     * Builder class for ReactionType
+     */
+    public static class ReactionTypeBuilder {
+        private final TypeReaction type;
+        private String emoji;
+        private String customEmojiId;
 
-    public void customEmojiId(String custom_emoji_id){
-        this.custom_emoji_id = custom_emoji_id;
+        public ReactionTypeBuilder(TypeReaction type) {
+            this.type = type;
+        }
+
+        public ReactionTypeBuilder emoji(String emoji) {
+            this.emoji = emoji;
+            return this;
+        }
+
+        public ReactionTypeBuilder customEmojiId(String customEmojiId) {
+            this.customEmojiId = customEmojiId;
+            return this;
+        }
+
+        public ReactionType build() {
+            return new ReactionType(
+                    type,
+                    emoji,
+                    customEmojiId
+            );
+        }
     }
 }
