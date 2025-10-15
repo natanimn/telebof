5bce626b95397d37c148aa1811ef2eb78ce77258
natanimn <natanimco@gmail.com>
2025-10-15 20:51:28 +0300

    Refactor ResponseParameters class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ResponseParameters.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ResponseParameters.java
@@ -1,19 +1,16 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Describes why a request was unsuccessful.
+ * @param migrateToChatId Optional. The group has been migrated to a supergroup with the specified identifier.
+ * @param retryAfter Optional. In case of exceeding flood control, the number of seconds left to wait before the request can be repeated
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class ResponseParameters {
-    /**
-     * Optional. The group has been migrated to a supergroup with the specified identifier.
-     */
-    public Long migrate_to_chat_id;
-
-    /**
-     * Optional. In case of exceeding flood control, the number of seconds left to wait before the request can be repeated
-     */
-    public Integer retry_after;
-}
+public record ResponseParameters(
+        @SerializedName("migrate_to_chat_id") Long migrateToChatId,
+        @SerializedName("retry_after") Integer retryAfter
+) {}
