1853e140e96ad5ae054ce688dec07a477c47db02
natanimn <natanimco@gmail.com>
2025-10-15 20:51:38 +0300

    Refactor SentWebAppMessage class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/SentWebAppMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/SentWebAppMessage.java
@@ -1,14 +1,14 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Describes an inline message sent by a Web App on behalf of a user.
- * @author Natanim 
+ * @param inlineMessageId Optional. Identifier of the sent inline message. Available only if there is an inline keyboard attached to the message.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class SentWebAppMessage {
-    /**
-     * Optional. Identifier of the sent inline message. Available only if there is an inline keyboard attached to the message.
-     */
-    public String inline_message_id;
-}
+public record SentWebAppMessage(
+        @SerializedName("inline_message_id") String inlineMessageId
+) {}
