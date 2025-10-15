fd69b188bf47815483129d6848b9bd4a7c244c8e
natanimn <natanimco@gmail.com>
2025-10-15 20:50:41 +0300

    Refactor PhotoSize class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PhotoSize.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PhotoSize.java
@@ -1,35 +1,22 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents one size of a photo or a file / sticker thumbnail.
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param width Photo width
+ * @param height Photo height
+ * @param fileSize Optional. File size in bytes
  * @author Natanim
  * @since 3 March 2025
+ * @version 1.3.0
  */
-public class PhotoSize {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Photo width
-     */
-    public Integer width;
-
-    /**
-     * Photo height
-     */
-    public Integer height;
-
-    /**
-     * Optional. File size in bytes
-     */
-    public Integer file_size;
-
-}
+public record PhotoSize(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        Integer width,
+        Integer height,
+        @SerializedName("file_size") Integer fileSize
+) {}
