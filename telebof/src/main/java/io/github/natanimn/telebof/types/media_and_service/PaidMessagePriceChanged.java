1673d7cb30f5e303b2eed7fca80b43b2ca813c47
natanimn <natanimco@gmail.com>
2025-10-15 20:50:33 +0300

    Refactor PaidMessagePriceChanged class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PaidMessagePriceChanged.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PaidMessagePriceChanged.java
@@ -1,14 +1,14 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Describes a service message about a change in the price of paid messages within a chat.
+ * @param paidMessageStarCount The new number of Telegram Stars that must be paid by non-administrator users of the supergroup chat for each sent message
  * @author Natanim
  * @since 20 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class PaidMessagePriceChanged {
-    /**
-     * The new number of Telegram Stars that must be paid by non-administrator users of the supergroup chat for each sent message
-     */
-    public Integer paid_message_star_count;
-}
+public record PaidMessagePriceChanged(
+        @SerializedName("paid_message_star_count") Integer paidMessageStarCount
+) {}
