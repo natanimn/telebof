9e14948b82e5aadd44798eea50f56054b4289093
natanimn <natanimco@gmail.com>
2025-10-17 08:10:37 +0300

    Refactor VideoChatScheduled class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatScheduled.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatScheduled.java
@@ -1,14 +1,14 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a service message about a video chat scheduled in the chat.
- * @author Natanim 
+ * @param startDate Point in time (Unix timestamp) when the video chat is supposed to be started by a chat administrator
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class VideoChatScheduled {
-    /**
-     * Point in time (Unix timestamp) when the video chat is supposed to be started by a chat administrator
-     */
-    public Integer start_date;
-}
+public record VideoChatScheduled(
+        @SerializedName("start_date") Integer startDate
+) {}
