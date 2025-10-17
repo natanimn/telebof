3014df35f2a8495585e9327f6c712e16b07637e7
natanimn <natanimco@gmail.com>
2025-10-17 08:10:29 +0300

    Refactor VideoChatParticipantsInvited class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatParticipantsInvited.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatParticipantsInvited.java
@@ -1,18 +1,15 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
 import io.github.natanimn.telebof.types.chat_and_user.User;
-
 import java.util.List;
 
 /**
  * This object represents a service message about new members invited to a video chat.
+ * @param users New members that were invited to the video chat
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class VideoChatParticipantsInvited {
-    /**
-     * New members that were invited to the video chat
-     */
-    public List<User> users;
-}
+public record VideoChatParticipantsInvited(
+        List<User> users
+) {}
