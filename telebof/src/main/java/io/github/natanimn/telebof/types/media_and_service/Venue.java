154716afc884db05f4ba40f9154d9f8f83b2a6ce
natanimn <natanimco@gmail.com>
2025-10-17 08:10:04 +0300

    Refactor Venue class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Venue.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Venue.java
@@ -1,44 +1,26 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a venue.
+ * @param title Name of the venue
+ * @param address Address of the venue
+ * @param foursquareId Optional. Foursquare identifier of the venue
+ * @param foursquareType Optional. Foursquare type of the venue. (For example, “arts_entertainment/default”, “arts_entertainment/aquarium” or “food/icecream”.)
+ * @param googlePlaceId Optional. Google Places identifier of the venue
+ * @param googlePlaceType Optional. Google Places type of the venue. (See supported types.)
+ * @param location Venue location. Can't be a live location
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Venue {
-    /**
-     * Name of the venue
-     */
-    public String title;
-
-    /**
-     * Address of the venue
-     */
-    public String address;
-
-    /**
-     * Optional. Foursquare identifier of the venue
-     */
-    public String foursquare_id;
-
-    /**
-     * Optional. Foursquare type of the venue. (For example, “arts_entertainment/default”, “arts_entertainment/aquarium” or “food/icecream”.)
-     */
-    public String foursquare_type;
-
-    /**
-     * Optional. Google Places identifier of the venue
-     */
-    public String google_place_id;
-
-    /**
-     * Optional. Google Places type of the venue. (See <a href="https://developers.google.com/places/web-service/supported_types">supported types.</a>)
-     */
-    public String google_place_type;
-
-    /**
-     * Venue location. Can't be a live location
-     */
-    public Location location;
-}
+public record Venue(
+        String title,
+        String address,
+        @SerializedName("foursquare_id") String foursquareId,
+        @SerializedName("foursquare_type") String foursquareType,
+        @SerializedName("google_place_id") String googlePlaceId,
+        @SerializedName("google_place_type") String googlePlaceType,
+        Location location
+) {}
