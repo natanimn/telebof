901bf7a0fb068d777bfe298e560c63f48e0050e2
natanimn <natanimco@gmail.com>
2025-10-17 08:09:48 +0300

    Refactor SwitchInlineQueryChosenChat class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/SwitchInlineQueryChosenChat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/SwitchInlineQueryChosenChat.java
@@ -1,62 +1,77 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 /**
  * This object represents an inline button that switches the current user to inline mode in a chosen chat, with an optional default inline query.
- * @author Natanim 
+ * @param query Optional. The default inline query to be inserted in the input field. If left empty, only the bot's username will be inserted
+ * @param allowUserChats Optional. True, if private chats with users can be chosen
+ * @param allowBotChats Optional. True, if private chats with bots can be chosen
+ * @param allowGroupChats Optional. True, if group and supergroup chats can be chosen
+ * @param allowChannelChats Optional. True, if channel chats can be chosen
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class SwitchInlineQueryChosenChat implements Serializable {
+public record SwitchInlineQueryChosenChat(
+        String query,
+        @SerializedName("allow_user_chats") Boolean allowUserChats,
+        @SerializedName("allow_bot_chats") Boolean allowBotChats,
+        @SerializedName("allow_group_chats") Boolean allowGroupChats,
+        @SerializedName("allow_channel_chats") Boolean allowChannelChats
+) implements Serializable {
 
     /**
-     * Optional. The default inline query to be inserted in the input field. If left empty, only the bot's username will be inserted
+     * Creates a new SwitchInlineQueryChosenChat builder
      */
-    public String query;
-
-    /**
-     * Optional. True, if private chats with users can be chosen
-     */
-    public Boolean allow_user_chats;
-
-    /**
-     * Optional. True, if private chats with bots can be chosen
-     */
-    public Boolean allow_bot_chats;
-
-    /**
-     * Optional. True, if group and supergroup chats can be chosen
-     */
-    public Boolean allow_group_chats;
-
-    /**
-     * Optional. True, if channel chats can be chosen
-     */
-    public Boolean allow_channel_chats;
-
-    public SwitchInlineQueryChosenChat query(String query){
-        this.query = query;
-        return this;
+    public static SwitchInlineQueryChosenChatBuilder builder() {
+        return new SwitchInlineQueryChosenChatBuilder();
     }
 
-    public SwitchInlineQueryChosenChat allowUserChats(boolean allow_user_chats){
-        this.allow_user_chats = allow_user_chats;
-        return this;
-    }
+    /**
+     * Builder class for SwitchInlineQueryChosenChat
+     */
+    public static class SwitchInlineQueryChosenChatBuilder {
+        private String query;
+        private Boolean allowUserChats;
+        private Boolean allowBotChats;
+        private Boolean allowGroupChats;
+        private Boolean allowChannelChats;
 
-    public SwitchInlineQueryChosenChat allowBotChats(boolean allow_bot_chats){
-        this.allow_bot_chats = allow_bot_chats;
-        return this;
-    }
+        public SwitchInlineQueryChosenChatBuilder query(String query) {
+            this.query = query;
+            return this;
+        }
 
-    public SwitchInlineQueryChosenChat allowGroupChats(boolean allow_group_chats){
-        this.allow_group_chats = allow_group_chats;
-        return this;
-    }
+        public SwitchInlineQueryChosenChatBuilder allowUserChats(boolean allowUserChats) {
+            this.allowUserChats = allowUserChats;
+            return this;
+        }
 
-    public SwitchInlineQueryChosenChat allowChannelChats(boolean allow_channel_chats){
-        this.allow_channel_chats = allow_channel_chats;
-        return this;
+        public SwitchInlineQueryChosenChatBuilder allowBotChats(boolean allowBotChats) {
+            this.allowBotChats = allowBotChats;
+            return this;
+        }
+
+        public SwitchInlineQueryChosenChatBuilder allowGroupChats(boolean allowGroupChats) {
+            this.allowGroupChats = allowGroupChats;
+            return this;
+        }
+
+        public SwitchInlineQueryChosenChatBuilder allowChannelChats(boolean allowChannelChats) {
+            this.allowChannelChats = allowChannelChats;
+            return this;
+        }
+
+        public SwitchInlineQueryChosenChat build() {
+            return new SwitchInlineQueryChosenChat(
+                    query,
+                    allowUserChats,
+                    allowBotChats,
+                    allowGroupChats,
+                    allowChannelChats
+            );
+        }
     }
 }
