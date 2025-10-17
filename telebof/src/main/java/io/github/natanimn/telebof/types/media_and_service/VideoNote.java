196bbb43a20403c92f1103671d976fe48f35483c
natanimn <natanimco@gmail.com>
2025-10-17 08:10:59 +0300

    Refactor VideoNote class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoNote.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoNote.java
@@ -1,42 +1,25 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
- * This object represents a <a href="https://telegram.org/blog/video-messages-and-telescope">video message</a>
- * (available in Telegram apps as of <a href="https://telegram.org/blog/video-messages-and-telescope">v.4.0</a>).
- * @author Natanim 
+ * This object represents a video message
+ * (available in Telegram apps as of v.4.0).
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param length Video width and height (diameter of the video message) as defined by the sender
+ * @param duration Duration of the video in seconds as defined by the sender
+ * @param fileSize Optional. File size in bytes
+ * @param thumbnail Optional. Video thumbnail
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class VideoNote {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Video width and height (diameter of the video message) as defined by the sender
-     */
-    public Integer length;
-
-    /**
-     * Duration of the video in seconds as defined by the sender
-     */
-    public Integer duration;
-
-    /**
-     * Optional. File size in bytes
-     */
-    public Integer file_size;
-
-    /**
-     * Optional. Video thumbnail
-     */
-    public PhotoSize thumbnail;
-
-}
+public record VideoNote(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        Integer length,
+        Integer duration,
+        @SerializedName("file_size") Integer fileSize,
+        PhotoSize thumbnail
+) {}
