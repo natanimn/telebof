ba7b63b78aa5addbbcf5bdb3df5fc6f08c710582
natanimn <natanimco@gmail.com>
2025-10-17 08:10:50 +0300

    Refactor VideoChatStarted class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatStarted.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatStarted.java
@@ -2,8 +2,8 @@ package io.github.natanimn.telebof.types.media_and_service;
 
 /**
  * This object represents a service message about a video chat started in the chat. Currently holds no information.
- * @author Natanim 
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class VideoChatStarted { }
+public record VideoChatStarted() {}
