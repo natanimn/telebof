ae54bb42e1ff9d1d78ce4cf1c09e747a3be9d4d4
natanimn <natanimco@gmail.com>
2025-10-17 08:09:31 +0300

    Refactor Sticker class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Sticker.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Sticker.java
@@ -1,90 +1,43 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
 import io.github.natanimn.telebof.enums.StickerType;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class represents a sticker.
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param type Type of the sticker. The type of the sticker is independent of its format, which is determined by the fields is_animated and is_video.
+ * @param width Sticker width
+ * @param height Sticker height
+ * @param isAnimated True, if the sticker is animated
+ * @param isVideo True, if the sticker is video sticker
+ * @param thumbnail Sticker thumbnail in the .WEBP or .JPG format
+ * @param emoji Emoji associated with the sticker
+ * @param setName Name of the sticker set to which the sticker belongs
+ * @param premiumAnimation For premium regular stickers, premium animation for the sticker
+ * @param maskPosition For mask stickers, the position where the mask should be placed
+ * @param customEmojiId For custom emoji stickers, unique identifier of the custom emoji
+ * @param needsRepainting True, if the sticker must be repainted to a text color in messages, the color of the Telegram Premium badge in emoji status, white color on chat photos, or another appropriate color in other places
+ * @param fileSize File size in bytes
  * @author Natanim
  * @since 3 March 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class Sticker {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Type of the sticker.
-     * The type of the sticker is independent of its format, which is determined by the fields is_animated and is_video.
-     */
-    public StickerType type;
-
-    /**
-     * Sticker width
-     */
-    public Integer width;
-
-    /**
-     * Sticker height
-     */
-    public Integer height;
-
-    /**
-     * True, if the sticker is <a href="https://telegram.org/blog/animated-stickers">animated</a>
-     */
-    public Boolean is_animated;
-
-    /**
-     * True, if the sticker is <a href="https://telegram.org/blog/video-stickers-better-reactions">video sticker</a>
-     */
-    public Boolean is_video;
-
-    /**
-     * Sticker thumbnail in the .WEBP or .JPG format
-     */
-    public PhotoSize thumbnail;
-
-    /**
-     * Emoji associated with the sticker
-     */
-    public String emoji;
-
-    /**
-     * Name of the sticker set to which the sticker belongs
-     */
-    public String setName;
-
-    /**
-     * For premium regular stickers, premium animation for the sticker
-     */
-    public File premium_animation;
-
-    /**
-     * For mask stickers, the position where the mask should be placed
-     */
-    public MaskPosition mask_position;
-
-    /**
-     *  For custom emoji stickers, unique identifier of the custom emoji
-     */
-    public String custom_emoji_id;
-
-    /**
-     * True, if the sticker must be repainted to a text color in messages, the color of the Telegram Premium badge in emoji status,
-     * white color on chat photos, or another appropriate color in other places
-     */
-    public Boolean needs_repainting;
-
-    /**
-     * File size in bytes
-     */
-    public Integer file_size;
-
-}
+public record Sticker(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        StickerType type,
+        Integer width,
+        Integer height,
+        @SerializedName("is_animated") Boolean isAnimated,
+        @SerializedName("is_video") Boolean isVideo,
+        PhotoSize thumbnail,
+        String emoji,
+        @SerializedName("set_name") String setName,
+        @SerializedName("premium_animation") File premiumAnimation,
+        @SerializedName("mask_position") MaskPosition maskPosition,
+        @SerializedName("custom_emoji_id") String customEmojiId,
+        @SerializedName("needs_repainting") Boolean needsRepainting,
+        @SerializedName("file_size") Integer fileSize
+) {}
