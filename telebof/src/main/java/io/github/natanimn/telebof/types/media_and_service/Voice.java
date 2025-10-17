915af885862de04ddc0a11874d29a820646a3436
natanimn <natanimco@gmail.com>
2025-10-17 08:11:09 +0300

    Refactor Voice class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Voice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Voice.java
@@ -1,36 +1,22 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * This object represents a voice note.
- * @author Natanim 
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param mimeType Optional. MIME type of the file as defined by the sender
+ * @param duration Duration of the audio in seconds as defined by the sender
+ * @param fileSize Optional. File size in bytes.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class Voice {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Optional. MIME type of the file as defined by the sender
-     */
-    public String mime_type;
-
-    /**
-     * Duration of the audio in seconds as defined by the sender
-     */
-    public Integer duration;
-
-    /**
-     * Optional. File size in bytes.
-     */
-    public Long file_size;
-
-}
+public record Voice(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        @SerializedName("mime_type") String mimeType,
+        Integer duration,
+        @SerializedName("file_size") Long fileSize
+) {}
