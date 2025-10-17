37c8ca1783ae614ac478cc3712373eb56606a92d
natanimn <natanimco@gmail.com>
2025-10-17 08:10:13 +0300

    Refactor Video class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Video.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Video.java
@@ -1,85 +1,34 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
-import java.util.Objects;
 
 /**
- * @author Natanim 
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param width Video width as defined by the sender
+ * @param height Video height as defined by the sender
+ * @param duration Duration of the video in seconds as defined by the sender
+ * @param fileName Original filename as defined by the sender
+ * @param mimeType MIME type of the file as defined by the sender
+ * @param fileSize File size in bytes.
+ * @param thumbnail Video thumbnail
+ * @param startTimestamp Timestamp in seconds from which the video will play in the message
+ * @param cover Available sizes of the cover of the video in the message
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class Video{
-
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * Video width as defined by the sender
-     */
-    public Integer width;
-
-    /**
-     * Video height as defined by the sender
-     */
-    public Integer height;
-
-    /**
-     * Duration of the video in seconds as defined by the sender
-     */
-    public Integer duration;
-
-    /**
-     * Original filename as defined by the sender
-     */
-    public String file_name;
-
-    /**
-     *  MIME type of the file as defined by the sender
-     */
-    public String mime_type;
-
-    /**
-     * File size in bytes.
-     */
-    public Integer file_size;
-
-    /**
-     * Video thumbnail
-     */
-    public PhotoSize thumbnail;
-
-    /**
-     * Timestamp in seconds from which the video will play in the message
-     */
-    public Integer start_timestamp;
-
-    /**
-     * Available sizes of the cover of the video in the message
-     */
-    public List<PhotoSize> cover;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Video video = (Video) object;
-        return Objects.equals(file_id, video.file_id) && Objects.equals(file_unique_id, video.file_unique_id) &&
-                Objects.equals(file_name, video.file_name) && Objects.equals(mime_type, video.mime_type) &&
-                Objects.equals(width, video.width) && Objects.equals(height, video.height) &&
-                Objects.equals(duration, video.duration) && Objects.equals(file_size, video.file_size) &&
-                Objects.equals(thumbnail, video.thumbnail);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(file_id, file_unique_id, file_name, mime_type, width, height, duration, file_size, thumbnail);
-    }
-}
+public record Video(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        Integer width,
+        Integer height,
+        Integer duration,
+        @SerializedName("file_name") String fileName,
+        @SerializedName("mime_type") String mimeType,
+        @SerializedName("file_size") Integer fileSize,
+        PhotoSize thumbnail,
+        @SerializedName("start_timestamp") Integer startTimestamp,
+        List<PhotoSize> cover
+) {}
