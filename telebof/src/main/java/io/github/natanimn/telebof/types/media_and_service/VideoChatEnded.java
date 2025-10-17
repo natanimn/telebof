dc680dde6108eb3c60508c62f815c25e046b9ee6
natanimn <natanimco@gmail.com>
2025-10-17 08:10:22 +0300

    Refactor VideoChatEnded class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatEnded.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatEnded.java
@@ -2,13 +2,11 @@ package io.github.natanimn.telebof.types.media_and_service;
 
 /**
  * This object represents a service message about a video chat ended in the chat.
- * @author Natanim 
+ * @param duration Video chat duration in seconds
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class VideoChatEnded {
-    /**
-     * Video chat duration in seconds
-     */
-    public Integer duration;
-}
+public record VideoChatEnded(
+        Integer duration
+) {}
