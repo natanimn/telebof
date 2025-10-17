ef4f7fe2b241cb832d3b1dc594eff40040f9a561
natanimn <natanimco@gmail.com>
2025-10-17 08:09:56 +0300

    Refactor TextQuote class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/TextQuote.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/TextQuote.java
@@ -1,32 +1,21 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * This object contains information about the quoted part of a message that is replied to by the given message.
- * @author Natanim 
+ * @param text Text of the quoted part of a message that is replied to by the given message
+ * @param entities Optional. Special entities that appear in the quote. Currently, only bold, italic, underline, strikethrough, spoiler, and custom_emoji entities are kept in quotes.
+ * @param position Approximate quote position in the original message in UTF-16 code units as specified by the sender
+ * @param isManual Optional. True, if the quote was chosen manually by the message sender. Otherwise, the quote was added automatically by the server.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class TextQuote {
-    /**
-     * Text of the quoted part of a message that is replied to by the given message
-     */
-    public String text;
-
-    /**
-     * Optional. Special entities that appear in the quote. Currently, only bold, italic, underline, strikethrough, spoiler, and custom_emoji entities are kept in quotes.
-     */
-    public List<MessageEntity> entities;
-
-    /**
-     * Approximate quote position in the original message in UTF-16 code units as specified by the sender
-     */
-    public Integer position;
-
-    /**
-     * Optional. True, if the quote was chosen manually by the message sender. Otherwise, the quote was added automatically by the server.
-     */
-    public Boolean is_manual;
-
-}
+public record TextQuote(
+        String text,
+        List<MessageEntity> entities,
+        Integer position,
+        @SerializedName("is_manual") Boolean isManual
+) {}
