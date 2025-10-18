c8b150cefee301055fa4e812b4eac9f4fcf72e9e
natanimn <natanimco@gmail.com>
2025-10-18 14:53:19 +0300

    Refactor AffiliateInfo class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/AffiliateInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/AffiliateInfo.java
@@ -2,38 +2,23 @@ package io.github.natanimn.telebof.types.payments;
 
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import io.github.natanimn.telebof.types.chat_and_user.User;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class Contains information about the affiliate that received a commission via this transaction.
+ * @param affiliateUser The bot or the user that received an affiliate commission if it was received by a bot or a user
+ * @param affiliateChat The chat that received an affiliate commission if it was received by a chat
+ * @param commissionPerMille The number of Telegram Stars received by the affiliate for each 1000 Telegram Stars received by the bot from referred users
+ * @param amount Integer amount of Telegram Stars received by the affiliate from the transaction, rounded to 0; can be negative for refunds
+ * @param nanostarAmount The number of 1/1000000000 shares of Telegram Stars received by the affiliate; from -999999999 to 999999999; can be negative for refunds
  * @author Natanim
  * @since July 3, 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class AffiliateInfo {
-    /**
-     * The bot or the user that received an affiliate commission if it was received by a bot or a user
-     */
-    public User affiliate_user;
-
-    /**
-     * The chat that received an affiliate commission if it was received by a chat
-     */
-    public Chat affiliate_chat;
-
-    /**
-     * The number of Telegram Stars received by the affiliate for each 1000 Telegram Stars received by the bot from referred users
-     */
-    public Integer commission_per_mille;
-
-    /**
-     * Integer amount of Telegram Stars received by the affiliate from the transaction, rounded to 0; can be negative for refunds
-     */
-    public Integer amount;
-
-    /**
-     * The number of 1/1000000000 shares of Telegram Stars received by the affiliate; from -999999999 to 999999999;
-     * can be negative for refunds
-     */
-    public Integer nanostar_amount;
-
-}
+public record AffiliateInfo(
+        @SerializedName("affiliate_user") User affiliateUser,
+        @SerializedName("affiliate_chat") Chat affiliateChat,
+        @SerializedName("commission_per_mille") Integer commissionPerMille,
+        Integer amount,
+        @SerializedName("nanostar_amount") Integer nanostarAmount
+) {}
