d345f7ebd929827cf1238c1c81b90036fb2f5a75
natanimn <natanimco@gmail.com>
2025-10-18 14:54:12 +0300

    Refactor PaidMediaInfo class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/PaidMediaInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/PaidMediaInfo.java
@@ -1,21 +1,17 @@
 package io.github.natanimn.telebof.types.payments;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * Describes the paid media added to a message.
+ * @param starCount The number of Telegram Stars that must be paid to buy access to the media
+ * @param paidMedia Information about the paid media
  * @author Natanim
  * @since 3 March 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class PaidMediaInfo {
-    /**
-     * The number of Telegram Stars that must be paid to buy access to the media
-     */
-    public Integer star_count;
-
-    /**
-     * Information about the paid media
-     */
-    public List<PaidMedia> paid_media;
-}
+public record PaidMediaInfo(
+        @SerializedName("star_count") Integer starCount,
+        @SerializedName("paid_media") List<PaidMedia> paidMedia
+) {}
