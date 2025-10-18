70a88ced00029b95557cc19d3ecd7c4240ff3df3
natanimn <natanimco@gmail.com>
2025-10-18 14:55:00 +0300

    Refactor RefundedPayment class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/RefundedPayment.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/RefundedPayment.java
@@ -1,64 +1,22 @@
 package io.github.natanimn.telebof.types.payments;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class contains basic information about a refunded payment.
- * @author Natanim Negash
+ * @param currency Three-letter ISO 4217 currency code, or “XTR” for payments in Telegram Stars. Currently, always “XTR”
+ * @param totalAmount Total refunded price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45, total_amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies)
+ * @param invoicePayload Bot-specified invoice payload
+ * @param telegramPaymentChargeId Telegram payment identifier
+ * @param providerPaymentChargeId Provider payment identifier
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.7
+ * @version 1.3.0
  */
-
-public class RefundedPayment {
-    /**
-     * Three-letter ISO 4217 <a href="https://core.telegram.org/bots/payments#supported-currencies">currency code</a>,
-     * or “XTR” for payments in <a href="https://t.me/BotNews/90">Telegram Stars.</a> Currently, always “XTR”
-     */
-    public String currency;
-
-    /**
-     * Total refunded price in the smallest units of the currency (integer, not float/double).
-     * For example, for a price of US$ 1.45, total_amount = 145.
-     * See the exp parameter in <a href="https://core.telegram.org/bots/payments/currencies.json">currencies.json</a>, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies)
-     */
-
-    public Integer total_amount;
-
-    /**
-     * Bot-specified invoice payload
-     */
-    public String invoice_payload;
-
-    /**
-     * Telegram payment identifier
-     */
-    public String telegram_payment_charge_id;
-
-    /**
-     * Provider payment identifier
-     */
-    public String provider_payment_charge_id;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        RefundedPayment that = (RefundedPayment) object;
-        return  Objects.equals(currency, that.currency) &&
-                Objects.equals(invoice_payload, that.invoice_payload) &&
-                Objects.equals(telegram_payment_charge_id, that.telegram_payment_charge_id) &&
-                Objects.equals(provider_payment_charge_id, that.provider_payment_charge_id) &&
-                Objects.equals(total_amount, that.total_amount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(
-                currency,
-                invoice_payload,
-                telegram_payment_charge_id,
-                provider_payment_charge_id,
-                total_amount
-        );
-    }
-}
+public record RefundedPayment(
+        String currency,
+        @SerializedName("total_amount") Integer totalAmount,
+        @SerializedName("invoice_payload") String invoicePayload,
+        @SerializedName("telegram_payment_charge_id") String telegramPaymentChargeId,
+        @SerializedName("provider_payment_charge_id") String providerPaymentChargeId
+) {}
