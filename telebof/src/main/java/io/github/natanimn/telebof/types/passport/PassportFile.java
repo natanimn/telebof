b670ee6e50d5c9c98f930623b7d6cf2d0a27c360
natanimn <natanimco@gmail.com>
2025-10-18 14:54:30 +0300

    Refactor PassportFile class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/passport/PassportFile.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/passport/PassportFile.java
@@ -1,7 +1,7 @@
 package io.github.natanimn.telebof.types.passport;
 
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * <p>
@@ -12,45 +12,17 @@ import java.util.Objects;
  *<br>
  * This class represents a file uploaded to Telegram Passport.
  * Currently, all Telegram Passport files are in JPEG format when decrypted and don't exceed 10MB.
+ * @param fileId Identifier for this file, which can be used to download or reuse the file
+ * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+ * @param fileSize File size in bytes
+ * @param fileDate Unix time when the file was uploaded
  * @author Natanim
  * @since 3 March 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class PassportFile  implements Serializable {
-    /**
-     * Identifier for this file, which can be used to download or reuse the file
-     */
-    public String file_id;
-
-    /**
-     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
-     * Can't be used to download or reuse the file.
-     */
-    public String file_unique_id;
-
-    /**
-     * File size in bytes
-     */
-    public Integer file_size;
-
-    /**
-     * Unix time when the file was uploaded
-     */
-    public Integer file_date;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        PassportFile that = (PassportFile) object;
-        return  Objects.equals(file_id, that.file_id) &&
-                Objects.equals(file_unique_id, that.file_unique_id) &&
-                Objects.equals(file_size, that.file_size) &&
-                Objects.equals(file_date, that.file_date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(file_id, file_unique_id, file_size, file_date);
-    }
-}
+public record PassportFile(
+        @SerializedName("file_id") String fileId,
+        @SerializedName("file_unique_id") String fileUniqueId,
+        @SerializedName("file_size") Integer fileSize,
+        @SerializedName("file_date") Integer fileDate
+) {}
