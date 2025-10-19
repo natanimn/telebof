6c253a5d736de0335c57feedf5098576c0c6cd82
natanimn <natanimco@gmail.com>
2025-10-19 12:23:23 +0300

    Refactor StarAmount class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/StarAmount.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/StarAmount.java
@@ -1,21 +1,16 @@
 package io.github.natanimn.telebof.types.payments;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Describes an amount of Telegram Stars.
+ * @param amount Integer amount of Telegram Stars, rounded to 0; can be negative
+ * @param nanostarAmount Optional. The number of 1/1000000000 shares of Telegram Stars; from -999999999 to 999999999; can be negative if and only if amount is non-positive
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class StarAmount {
-    /**
-     * Integer amount of Telegram Stars, rounded to 0; can be negative
-     */
-    public Integer amount;
-
-    /**
-     * Optional.
-     * The number of 1/1000000000 shares of Telegram Stars; from -999999999 to 999999999; can be negative if and only if amount is non-positive
-     */
-    public Integer nanostar_amount;
-
-}
+public record StarAmount(
+        Integer amount,
+        @SerializedName("nanostar_amount") Integer nanostarAmount
+) {}
