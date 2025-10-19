f189a634e7d10f8fe9c8b33e80e84aeaeb61fabb
natanimn <natanimco@gmail.com>
2025-10-19 12:23:17 +0300

    Refactor ShippingOption class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/ShippingOption.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/ShippingOption.java
@@ -5,23 +5,15 @@ import java.util.List;
 
 /**
  * This object represents one shipping option.
+ * @param id Shipping option identifier
+ * @param title Option title
+ * @param prices List of price portions
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class ShippingOption implements Serializable {
-    private String id, title;
-    private List<LabeledPrice> prices;
-
-    /**
-     * Required
-     * @param id Shipping option identifier
-     * @param title Option title
-     * @param prices List of price portions
-     */
-    public ShippingOption(String id, String title, List<LabeledPrice> prices){
-        this.id = id;
-        this.title = title;
-        this.prices = prices;
-    }
-}
+public record ShippingOption(
+        String id,
+        String title,
+        List<LabeledPrice> prices
+) implements Serializable {}
