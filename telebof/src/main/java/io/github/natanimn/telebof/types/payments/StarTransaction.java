e4b8e75b7eb9b749c0164bfc97f9c858d23d6b50
natanimn <natanimco@gmail.com>
2025-10-19 12:23:30 +0300

    Refactor StarTransaction class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/StarTransaction.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/StarTransaction.java
@@ -1,65 +1,27 @@
 package io.github.natanimn.telebof.types.payments;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes a Telegram Star transaction.
  * Note that if the buyer initiates a chargeback with the payment provider from whom they acquired Stars (e.g., Apple, Google)
  * following this transaction, the refunded Stars will be deducted from the bot's balance.
  * This is outside of Telegram's control.
+ * @param id Unique identifier of the transaction. Coincides with the identifier of the original transaction for refund transactions. Coincides with SuccessfulPayment.telegram_payment_charge_id for successful incoming payments from users.
+ * @param amount Integer amount of Telegram Stars transferred by the transaction
+ * @param date Date the transaction was created in Unix time
+ * @param source Source of an incoming transaction (e.g., a user purchasing goods or services, Fragment refunding a failed withdrawal). Only for incoming transactions
+ * @param receiver Receiver of an outgoing transaction (e.g., a user for a purchase refund, Fragment for a withdrawal). Only for outgoing transactions
+ * @param nanostarAmount The number of 1/1000000000 shares of Telegram Stars transferred by the transaction; from 0 to 999999999
  * @author Natanim
  * @since March 3, 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class StarTransaction {
-    /**
-     * Unique identifier of the transaction. Coincides with the identifier of the original transaction for refund transactions.
-     * Coincides with SuccessfulPayment.telegram_payment_charge_id for successful incoming payments from users.
-     */
-    public String id;
-
-    /**
-     * Integer amount of Telegram Stars transferred by the transaction
-     */
-    public Integer amount;
-
-    /**
-     * Date the transaction was created in Unix time
-     */
-    public Integer date;
-
-    /**
-     * Source of an incoming transaction (e.g., a user purchasing goods or services, Fragment refunding a failed withdrawal).
-     * Only for incoming transactions
-     */
-    public TransactionPartner source;
-
-    /**
-     * Receiver of an outgoing transaction (e.g., a user for a purchase refund, Fragment for a withdrawal).
-     * Only for outgoing transactions
-     */
-    public TransactionPartner receiver;
-
-    /**
-     * The number of 1/1000000000 shares of Telegram Stars transferred by the transaction; from 0 to 999999999
-     */
-    public Integer nanostar_amount;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        StarTransaction that = (StarTransaction) object;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(amount, that.amount) &&
-                Objects.equals(date, that.date) &&
-                Objects.equals(source, that.source) &&
-                Objects.equals(receiver, that.receiver) &&
-                Objects.equals(nanostar_amount, that.nanostar_amount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, amount, date, source, receiver, nanostar_amount);
-    }
-}
+public record StarTransaction(
+        String id,
+        Integer amount,
+        Integer date,
+        TransactionPartner source,
+        TransactionPartner receiver,
+        @SerializedName("nanostar_amount") Integer nanostarAmount
+) {}
