4db8f5c4066aaa4feb5947e254d45d37bfdf0aa7
natanimn <natanimco@gmail.com>
2025-10-20 23:48:11 +0300

    Refactor StoryAreaTypeLink class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaTypeLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaTypeLink.java
@@ -2,20 +2,15 @@ package io.github.natanimn.telebof.types.story;
 
 /**
  * Describes a story area pointing to an HTTP or tg:// link. Currently, a story can have up to 3 link areas.
+ * @param url HTTP or tg:// URL to be opened when the area is clicked
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class StoryAreaTypeLink implements StoryAreaType {
-    private String type;
-    private String url;
-
-    /**
-     * Required
-     * @param url HTTP or tg:// URL to be opened when the area is clicked
-     */
-    public StoryAreaTypeLink(String url) {
-        this.type = "link";
-        this.url = url;
+public record StoryAreaTypeLink(
+        String url
+) implements StoryAreaType {
+    public String type() {
+        return "link";
     }
 }
