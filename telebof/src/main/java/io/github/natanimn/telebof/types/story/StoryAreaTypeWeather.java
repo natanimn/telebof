87c8fbdda298277e6f6930b4d870a390f60be75d
natanimn <natanimco@gmail.com>
2025-10-20 23:48:42 +0300

    Refactor StoryAreaTypeWeather class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaTypeWeather.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaTypeWeather.java
@@ -1,35 +1,55 @@
 package io.github.natanimn.telebof.types.story;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Describes a story area containing weather information. Currently, a story can have up to 3 weather areas.
+ * @param temperature Temperature, in degree Celsius
+ * @param emoji Emoji representing the weather
+ * @param backgroundColor Optional. A color of the area background in the ARGB format
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class StoryAreaTypeWeather implements StoryAreaType {
-    private String type;
-    private Double temperature;
-    private String emoji;
-    private Integer background_color;
+public record StoryAreaTypeWeather(
+        Double temperature,
+        String emoji,
+        @SerializedName("background_color") Integer backgroundColor
+) implements StoryAreaType {
 
-    /**
-     * Required
-     * @param temperature Temperature, in degree Celsius
-     * @param emoji Emoji representing the weather
-     */
-    public StoryAreaTypeWeather(double temperature, String emoji) {
-        this.type = "weather";
-        this.temperature = temperature;
-        this.emoji = emoji;
+    public String type() {
+        return "weather";
     }
 
     /**
-     * Optional
-     * @param background_color A color of the area background in the ARGB format
-     * @return {@link StoryAreaTypeWeather}
+     * Creates a new StoryAreaTypeWeather builder
+     * @param temperature Temperature, in degree Celsius
+     * @param emoji Emoji representing the weather
      */
-    public StoryAreaTypeWeather backgroundColor(Integer background_color) {
-        this.background_color = background_color;
-        return this;
+    public static StoryAreaTypeWeatherBuilder builder(double temperature, String emoji) {
+        return new StoryAreaTypeWeatherBuilder(temperature, emoji);
+    }
+
+    /**
+     * Builder class for StoryAreaTypeWeather
+     */
+    public static class StoryAreaTypeWeatherBuilder {
+        private final double temperature;
+        private final String emoji;
+        private Integer backgroundColor;
+
+        public StoryAreaTypeWeatherBuilder(double temperature, String emoji) {
+            this.temperature = temperature;
+            this.emoji = emoji;
+        }
+
+        public StoryAreaTypeWeatherBuilder backgroundColor(Integer backgroundColor) {
+            this.backgroundColor = backgroundColor;
+            return this;
+        }
+
+        public StoryAreaTypeWeather build() {
+            return new StoryAreaTypeWeather(temperature, emoji, backgroundColor);
+        }
     }
 }
