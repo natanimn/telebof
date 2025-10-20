65329ce6506b393d849558578c5f7b474c04190f
natanimn <natanimco@gmail.com>
2025-10-20 23:47:34 +0300

    Refactor StoryArea class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryArea.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryArea.java
@@ -4,21 +4,13 @@ import java.io.Serializable;
 
 /**
  * Describes a clickable area on a story media.
+ * @param position Position of the area
+ * @param type Type of the area
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class StoryArea implements Serializable {
-    private StoryAreaPosition position;
-    private StoryAreaType type;
-
-    /**
-     * Required
-     * @param position Position of the area
-     * @param type Type of the area
-     */
-    public StoryArea(StoryAreaPosition position, StoryAreaType type) {
-        this.position = position;
-        this.type = type;
-    }
-}
+public record StoryArea(
+        StoryAreaPosition position,
+        StoryAreaType type
+) implements Serializable {}
