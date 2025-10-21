7728bb0c0a804da4477b5b16777402ec7cf8b24c
natanimn <natanimco@gmail.com>
2025-10-21 13:42:59 +0300

    Refactor SuggestedPostDeclined class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostDeclined.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostDeclined.java
@@ -1,21 +1,17 @@
 package io.github.natanimn.telebof.types.suggested;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the rejection of a suggested post.
+ * @param suggestedPostMessage Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+ * @param comment Optional. Comment with which the post was declined
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  */
-public class SuggestedPostDeclined {
-    /**
-     * Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
-     */
-    public Message suggested_post_message;
-
-    /**
-     * Optional. Comment with which the post was declined
-     */
-    public String comment;
-}
+public record SuggestedPostDeclined(
+        @SerializedName("suggested_post_message") Message suggestedPostMessage,
+        String comment
+) {}
