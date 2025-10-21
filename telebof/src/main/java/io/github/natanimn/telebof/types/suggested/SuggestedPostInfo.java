42cadebe4b16448b34abe06b20f18426144494ae
natanimn <natanimco@gmail.com>
2025-10-21 13:43:13 +0300

    Refactor SuggestedPostInfo class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostInfo.java
@@ -1,25 +1,18 @@
 package io.github.natanimn.telebof.types.suggested;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Contains information about a suggested post.
+ * @param state State of the suggested post. Currently, it can be one of "pending", "approved", "declined".
+ * @param price Optional. Proposed price of the post. If the field is omitted, then the post is unpaid.
+ * @param sendDate Optional. Proposed send date of the post. If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user or administrator who approves it.
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  */
-public class SuggestedPostInfo {
-    /**
-     * State of the suggested post. Currently, it can be one of “pending”, “approved”, “declined”.
-     */
-    public String state;
-
-    /**
-     * Optional. Proposed price of the post. If the field is omitted, then the post is unpaid.
-     */
-    public SuggestedPostPrice price;
-
-    /**
-     * Optional. Proposed send date of the post.
-     * If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user or administrator who approves it.
-     */
-    public Integer send_date;
-}
+public record SuggestedPostInfo(
+        String state,
+        SuggestedPostPrice price,
+        @SerializedName("send_date") Integer sendDate
+) {}
