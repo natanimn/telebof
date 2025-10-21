e72fb4e405ef179ba2c3be279971da5971b1903a
natanimn <natanimco@gmail.com>
2025-10-21 13:43:31 +0300

    Refactor SuggestedPostParameters class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostParameters.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostParameters.java
@@ -4,29 +4,43 @@ import java.io.Serializable;
 
 /**
  * Contains parameters of a post that is being suggested by the bot.
+ * @param price Optional. Proposed price for the post. If the field is omitted, then the post is unpaid.
+ * @param date Optional. Proposed send date of the post. If specified, then the date must be between 300 second and 2678400 seconds (30 days) in the future. If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user who approves it.
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  */
-public class SuggestedPostParameters implements Serializable {
-    private SuggestedPostPrice price;
-    private Integer date;
+public record SuggestedPostParameters(
+        SuggestedPostPrice price,
+        Integer date
+) implements Serializable {
 
     /**
-     * Optional
-     * @param price Optional. Proposed price for the post. If the field is omitted, then the post is unpaid.
+     * Creates a new SuggestedPostParameters builder
      */
-    public void setPrice(SuggestedPostPrice price) {
-        this.price = price;
+    public static SuggestedPostParametersBuilder builder() {
+        return new SuggestedPostParametersBuilder();
     }
 
     /**
-     * Optional
-     * @param date Optional. Proposed send date of the post.
-     *             If specified, then the date must be between 300 second and 2678400 seconds (30 days) in the future.
-     *             If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user who approves it.
+     * Builder class for SuggestedPostParameters
      */
-    public void setDate(Integer date) {
-        this.date = date;
+    public static class SuggestedPostParametersBuilder {
+        private SuggestedPostPrice price;
+        private Integer date;
+
+        public SuggestedPostParametersBuilder price(SuggestedPostPrice price) {
+            this.price = price;
+            return this;
+        }
+
+        public SuggestedPostParametersBuilder date(Integer date) {
+            this.date = date;
+            return this;
+        }
+
+        public SuggestedPostParameters build() {
+            return new SuggestedPostParameters(price, date);
+        }
     }
 }
