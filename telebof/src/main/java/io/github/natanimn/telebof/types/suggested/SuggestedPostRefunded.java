fb49c98bb3bfad010b260888f72b50ebc2394cf4
natanimn <natanimco@gmail.com>
2025-10-21 13:43:53 +0300

    Refactor SuggestedPostRefunded class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostRefunded.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostRefunded.java
@@ -1,21 +1,17 @@
 package io.github.natanimn.telebof.types.suggested;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about a payment refund for a suggested post.
+ * @param suggestedPostMessage Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+ * @param reason Reason for the refund. Currently, one of "post_deleted" if the post was deleted within 24 hours of being posted or removed from scheduled messages without being posted, or "payment_refunded" if the payer refunded their payment.
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  */
-public class SuggestedPostRefunded {
-    /**
-     * Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
-     */
-    public Message suggested_post_message;
-
-    /**
-     * Reason for the refund. Currently, one of "post_deleted" if the post was deleted within 24 hours of being posted or removed from scheduled messages without being posted, or "payment_refunded" if the payer refunded their payment.
-     */
-    public String reason;
-}
+public record SuggestedPostRefunded(
+        @SerializedName("suggested_post_message") Message suggestedPostMessage,
+        String reason
+) {}
