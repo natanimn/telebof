f1b7af0b21165d74ac0dc803f4f33f5446cd9f8b
natanimn <natanimco@gmail.com>
2025-10-21 13:42:50 +0300

    Refactor SuggestedPostApproved class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApproved.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApproved.java
@@ -1,26 +1,19 @@
 package io.github.natanimn.telebof.types.suggested;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the approval of a suggested post.
+ * @param suggestedPostMessage Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+ * @param price Optional. Amount paid for the post
+ * @param sendDate Date when the post will be published
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  */
-public class SuggestedPostApproved {
-    /**
-     * Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
-     */
-    public Message suggested_post_message;
-
-    /**
-     * Optional. Amount paid for the post
-     */
-    public SuggestedPostPrice price;
-
-    /**
-     * Date when the post will be published
-     */
-    public Integer send_date;
-}
+public record SuggestedPostApproved(
+        @SerializedName("suggested_post_message") Message suggestedPostMessage,
+        SuggestedPostPrice price,
+        @SerializedName("send_date") Integer sendDate
+) {}
