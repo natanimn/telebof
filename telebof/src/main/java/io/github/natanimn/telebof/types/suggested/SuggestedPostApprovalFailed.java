9bc54443ee12a04590809a6b6153fbf98b2cb9fe
natanimn <natanimco@gmail.com>
2025-10-21 13:42:39 +0300

    Refactor SuggestedPostApprovalFailed class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApprovalFailed.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApprovalFailed.java
@@ -1,22 +1,18 @@
 package io.github.natanimn.telebof.types.suggested;
 
 import io.github.natanimn.telebof.types.updates.Message;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the failed approval of a suggested post.
  * Currently, only caused by insufficient user funds at the time of approval.
+ * @param suggestedPostMessage Optional. Message containing the suggested post whose approval has failed. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+ * @param price Expected price of the post
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  */
-public class SuggestedPostApprovalFailed {
-    /**
-     * Optional. Message containing the suggested post whose approval has failed. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
-     */
-    public Message suggested_post_message;
-
-    /**
-     * Expected price of the post
-     */
-    public SuggestedPostPrice price;
-}
+public record SuggestedPostApprovalFailed(
+        @SerializedName("suggested_post_message") Message suggestedPostMessage,
+        SuggestedPostPrice price
+) {}
