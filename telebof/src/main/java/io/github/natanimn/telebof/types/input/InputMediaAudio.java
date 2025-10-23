5b380e9e7c870960f18dc8b26d194229ef4b613d
natanimn <natanimco@gmail.com>
2025-10-23 20:21:46 +0300

    Refactor InputMediaAudio class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaAudio.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaAudio.java
@@ -1,77 +1,146 @@
 package io.github.natanimn.telebof.types.input;
 
+import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
+import com.google.gson.annotations.SerializedName;
 import java.io.File;
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Represents an audio file to be treated as music to be sent.
- * @author Natanim 
+ * @param media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), or pass an HTTP URL for Telegram to get a file from the Internet.
+ * @param thumbnail Optional. Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side.
+ * @param performer Optional. Performer of the audio
+ * @param title Optional. Title of the audio
+ * @param duration Optional. Duration of the audio in seconds
+ * @param hasSpoiler Optional. Pass True if the audio needs to be covered with a spoiler animation
+ * @param caption Optional. Caption of the audio to be sent, 0-1024 characters after entities parsing
+ * @param parseMode Optional. Mode for parsing entities in the audio caption
+ * @param captionEntities Optional. List of special entities that appear in the caption
+ * @param files List of files to be uploaded
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class InputMediaAudio extends InputMediaBuilder<InputMediaAudio> implements Serializable {
-    private String thumbnail, performer, title;
-    private Integer duration;
-    private Boolean has_spoiler;
+public record InputMediaAudio(
+        String media,
+        String thumbnail,
+        String performer,
+        String title,
+        Integer duration,
+        @SerializedName("has_spoiler") Boolean hasSpoiler,
+        String caption,
+        @SerializedName("parse_mode") String parseMode,
+        @SerializedName("caption_entities") List<MessageEntity> captionEntities,
+        List<File> files
+) implements InputMediaInt{
 
-    /**
-     * Required
-     * @param media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), or
-     *              pass an HTTP URL for Telegram to get a file from the Internet.
-     */
-    public InputMediaAudio(String media) {
-        super("audio", media);
+    @Override
+    public String type() {
+        return "audio";
+    }
+
+    @Override
+    public boolean hasFile() {
+        return files != null && !files.isEmpty();
+    }
+
+    @Override
+    public List<File> getFiles() {
+        return files;
     }
 
     /**
-     * Required
-     * @param media File to send. Pass an object of {@link File} to upload from your local machine.
+     * Creates a new InputMediaAudio builder
+     * @param media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), or pass an HTTP URL for Telegram to get a file from the Internet.
      */
-    public InputMediaAudio(File media) {
-        super("audio", media);
+    public static InputMediaAudioBuilder builder(String media) {
+        return new InputMediaAudioBuilder(media);
     }
 
     /**
-     * Optional
-     * @param thumbnail humbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side.
-     *                  The thumbnail should be in JPEG format and less than 200 kB in size.
-     *                  A thumbnail's width and height should not exceed 320.
-     * @return {@link InputMediaAudio}
+     * Creates a new InputMediaAudio builder
+     * @param media File to send. Pass an object of File to upload from your local machine.
      */
-    public InputMediaAudio thumbnail(File thumbnail) {
-        this.thumbnail = "attach://"+thumbnail.getName();
-        setHasFile(true);
-        addFiles(thumbnail);
-        return this;
+    public static InputMediaAudioBuilder builder(File media) {
+        return new InputMediaAudioBuilder(media);
     }
 
     /**
-     * Optional
-     * @param performer Performer of the audio
-     * @return {@link InputMediaAudio}
+     * Builder class for InputMediaAudio
      */
-    public InputMediaAudio performer(String performer) {
-        this.performer = performer;
-        return this;
-    }
+    public static class InputMediaAudioBuilder {
+        private String media;
+        private String thumbnail;
+        private String performer;
+        private String title;
+        private Integer duration;
+        private Boolean hasSpoiler;
+        private String caption;
+        private String parseMode;
+        private List<MessageEntity> captionEntities;
+        private List<File> files = new ArrayList<>();
 
-    /**
-     * Optional
-     * @param title Title of the audio
-     * @return {@link InputMediaAudio}
-     */
-    public InputMediaAudio title(String title) {
-        this.title = title;
-        return this;
-    }
+        public InputMediaAudioBuilder(String media) {
+            this.media = media;
+        }
 
-    /**
-     * Optional
-     * @param duration Duration of the audio in seconds
-     * @return {@link InputMediaAudio}
-     */
-    public InputMediaAudio duration(int duration) {
-        this.duration = duration;
-        return this;
+        public InputMediaAudioBuilder(File media) {
+            this.media = "attach://" + media.getName();
+            this.files.add(media);
+        }
+
+        public InputMediaAudioBuilder thumbnail(File thumbnail) {
+            this.thumbnail = "attach://" + thumbnail.getName();
+            this.files.add(thumbnail);
+            return this;
+        }
+
+        public InputMediaAudioBuilder thumbnail(String thumbnail) {
+            this.thumbnail = thumbnail;
+            return this;
+        }
+
+        public InputMediaAudioBuilder performer(String performer) {
+            this.performer = performer;
+            return this;
+        }
+
+        public InputMediaAudioBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public InputMediaAudioBuilder duration(Integer duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public InputMediaAudioBuilder hasSpoiler(Boolean hasSpoiler) {
+            this.hasSpoiler = hasSpoiler;
+            return this;
+        }
+
+        public InputMediaAudioBuilder caption(String caption) {
+            this.caption = caption;
+            return this;
+        }
+
+        public InputMediaAudioBuilder parseMode(String parseMode) {
+            this.parseMode = parseMode;
+            return this;
+        }
+
+        public InputMediaAudioBuilder captionEntities(List<MessageEntity> captionEntities) {
+            this.captionEntities = captionEntities;
+            return this;
+        }
+
+        public InputMediaAudio build() {
+            return new InputMediaAudio(
+                    media, thumbnail, performer, title, duration, hasSpoiler,
+                    caption, parseMode, captionEntities, files
+            );
+        }
     }
 }
