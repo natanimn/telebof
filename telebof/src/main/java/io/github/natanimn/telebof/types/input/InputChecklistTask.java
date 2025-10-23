fc7989a66a26440b6341782095198a5b1f1df716
natanimn <natanimco@gmail.com>
2025-10-23 20:20:58 +0300

    Refactor InputChecklistTask class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputChecklistTask.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputChecklistTask.java
@@ -2,49 +2,62 @@ package io.github.natanimn.telebof.types.input;
 
 import io.github.natanimn.telebof.enums.ParseMode;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
-
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * This object represents a task to add to a checklist.
+ * @param id Unique identifier of the task; must be positive and unique among all task identifiers currently present in the checklist
+ * @param text Text of the task; 1-100 characters after entities parsing
+ * @param parseMode Optional. Mode for parsing entities in the text.
+ * @param textEntities Optional. List of special entities that appear in the text, which can be specified instead of parse_mode. Currently, only bold, italic, underline, strikethrough, spoiler, and custom_emoji entities are allowed.
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.3.0
  */
-public class InputChecklistTask implements Serializable {
-    private Integer id;
-    private String text;
-    private ParseMode parse_mode;
-    private MessageEntity[] text_entities;
+public record InputChecklistTask(
+        Integer id,
+        String text,
+        @SerializedName("parse_mode") ParseMode parseMode,
+        @SerializedName("text_entities") MessageEntity[] textEntities
+) implements Serializable {
+
     /**
-     * Required
+     * Creates a new InputChecklistTask builder
      * @param id Unique identifier of the task; must be positive and unique among all task identifiers currently present in the checklist
      * @param text Text of the task; 1-100 characters after entities parsing
      */
-    public InputChecklistTask(int id, String text){
-        this.id = id;
-        this.text = text;
-
+    public static InputChecklistTaskBuilder builder(int id, String text) {
+        return new InputChecklistTaskBuilder(id, text);
     }
 
     /**
-     * Optional
-     * @param parse_mode Mode for parsing entities in the text.
-     * @return {@link InputChecklistTask}
+     * Builder class for InputChecklistTask
      */
-    public InputChecklistTask parseMode(ParseMode parse_mode){
-        this.parse_mode = parse_mode;
-        return this;
-    }
+    public static class InputChecklistTaskBuilder {
+        private final int id;
+        private final String text;
+        private ParseMode parseMode;
+        private MessageEntity[] textEntities;
 
-    /**
-     * Optional
-     * @param text_entities ist of special entities that appear in the text, which can be specified instead of parse_mode.
-     *                      Currently, only bold, italic, underline, strikethrough, spoiler, and custom_emoji entities are allowed.
-     * @return {@link InputChecklistTask}
-     */
-    public InputChecklistTask textEntities(MessageEntity[] text_entities){
-        this.text_entities = text_entities;
-        return this;
+        public InputChecklistTaskBuilder(int id, String text) {
+            this.id = id;
+            this.text = text;
+        }
+
+        public InputChecklistTaskBuilder parseMode(ParseMode parseMode) {
+            this.parseMode = parseMode;
+            return this;
+        }
+
+        public InputChecklistTaskBuilder textEntities(MessageEntity[] textEntities) {
+            this.textEntities = textEntities;
+            return this;
+        }
+
+        public InputChecklistTask build() {
+            return new InputChecklistTask(id, text, parseMode, textEntities);
+        }
     }
 }
