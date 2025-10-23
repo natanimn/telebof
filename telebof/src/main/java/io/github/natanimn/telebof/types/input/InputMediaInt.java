f8a6fd76115feb603d89cee103c3a92933ad759a
natanimn <natanimco@gmail.com>
2025-10-23 20:22:56 +0300

    Added InputMediaInt interface

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaInt.java
@@ -0,0 +1,21 @@
+package io.github.natanimn.telebof.types.input;
+
+import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
+import java.io.File;
+import java.util.List;
+
+/**
+ * Interface for input media types
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 1.3.0
+ */
+public interface InputMediaInt extends InputMedia {
+    String type();
+    String media();
+    String caption();
+    String parseMode();
+    MessageEntity[] captionEntities();
+    List<File> getFiles();
+    boolean hasFile();
+}
