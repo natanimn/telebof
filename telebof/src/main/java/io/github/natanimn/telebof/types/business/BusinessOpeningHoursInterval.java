81dd7540900a5da3922bad287aa731cdf1939ad0
natanimn <natanimco@gmail.com>
2025-10-27 15:25:59 +0300

    Refactor BusinessOpeningHoursInterval class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHoursInterval.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHoursInterval.java
@@ -6,28 +6,14 @@ import com.google.gson.annotations.SerializedName;
  * Describes the opening hours of a business.
  * @author Natanim
  * @since 3 March 2025
- * @version 1.3.0
+ * @version 1.2.4
+ * @param openingMinute The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 * 24 * 60
+ * @param closingMinute The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 * 24 * 60
  */
-public class BusinessOpeningHoursInterval {
+public record BusinessOpeningHoursInterval(
     @SerializedName("opening_minute")
-    private Integer openingMinute;
+    Integer openingMinute,
 
     @SerializedName("closing_minute")
-    private Integer closingMinute;
-
-    /**
-     * Gets the minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open.
-     * @return The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 * 24 * 60
-     */
-    public Integer getOpeningMinute() {
-        return openingMinute;
-    }
-
-    /**
-     * Gets the minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open.
-     * @return The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 * 24 * 60
-     */
-    public Integer getClosingMinute() {
-        return closingMinute;
-    }
-}
+    Integer closingMinute
+){}
