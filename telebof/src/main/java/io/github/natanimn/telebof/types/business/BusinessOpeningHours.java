b986e63062544eb3e4dc0aa3086f2057fb648ebd
natanimn <natanimco@gmail.com>
2025-10-27 15:25:48 +0300

    Refactor BusinessOpeningHours class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHours.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHours.java
@@ -3,33 +3,20 @@ package io.github.natanimn.telebof.types.business;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Describes the opening hours of a business.
  * @author Natanim
  * @since 3 March 2025
- * @version 1.3.0
+ * @version 1.2.4
+ * @param timeZoneName Unique name of the time zone for which the opening hours are defined
+ * @param openingHours List of time intervals describing business opening hours
  */
-public class BusinessOpeningHours {
-    @SerializedName("time_zone_name")
-    private String timeZoneName;
+public record BusinessOpeningHours(
+     @SerializedName("time_zone_name")
+     String timeZoneName,
 
-    @SerializedName("opening_hours")
-    private List<BusinessOpeningHoursInterval> openingHours;
-
-    /**
-     * Gets the unique name of the time zone for which the opening hours are defined.
-     * @return Unique name of the time zone for which the opening hours are defined
-     */
-    public String getTimeZoneName() {
-        return timeZoneName;
-    }
-
-    /**
-     * Gets the list of time intervals describing business opening hours.
-     * @return List of time intervals describing business opening hours
-     */
-    public List<BusinessOpeningHoursInterval> getOpeningHours() {
-        return openingHours;
-    }
-}
+     @SerializedName("opening_hours")
+     List<BusinessOpeningHoursInterval> openingHours
+){ }
