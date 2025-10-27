659c2cb46483054b6861960495c58d4c0fc630f3
natanimn <natanimco@gmail.com>
2025-10-27 15:26:35 +0300

    Refactor ChatBoost class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoost.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoost.java
@@ -4,51 +4,23 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object contains information about a chat boost.
+ * @param boostId Unique identifier of the boost
+ * @param addDate Point in time (Unix timestamp) when the chat was boosted
+ * @param expirationData Point in time (Unix timestamp) when the boost will automatically expire, unless the booster's Telegram Premium subscription is prolonged
+ * @param source Source of the added boost
  * @author Natanim
  * @since 3 March 2025
- * @version 1.3.0
+ * @version 1.2.4
  */
-public class ChatBoost {
-    @SerializedName("boost_id")
-    private String boostId;
+public record ChatBoost(
+        @SerializedName("boost_id")
+        String boostId,
 
-    @SerializedName("add_date")
-    private Integer addDate;
+        @SerializedName("add_date")
+        Integer addDate,
 
-    @SerializedName("expiration_data")
-    private Integer expirationData;
+        @SerializedName("expiration_data")
+        Integer expirationData,
 
-    private ChatBoostSource source;
-
-    /**
-     * Gets the unique identifier of the boost.
-     * @return Unique identifier of the boost
-     */
-    public String getBoostId() {
-        return boostId;
-    }
-
-    /**
-     * Gets the point in time (Unix timestamp) when the chat was boosted.
-     * @return Point in time (Unix timestamp) when the chat was boosted
-     */
-    public Integer getAddDate() {
-        return addDate;
-    }
-
-    /**
-     * Gets the point in time (Unix timestamp) when the boost will automatically expire, unless the booster's Telegram Premium subscription is prolonged.
-     * @return Point in time (Unix timestamp) when the boost will automatically expire, unless the booster's Telegram Premium subscription is prolonged
-     */
-    public Integer getExpirationData() {
-        return expirationData;
-    }
-
-    /**
-     * Gets the source of the added boost.
-     * @return Source of the added boost
-     */
-    public ChatBoostSource getSource() {
-        return source;
-    }
-}
+        ChatBoostSource source
+){}
