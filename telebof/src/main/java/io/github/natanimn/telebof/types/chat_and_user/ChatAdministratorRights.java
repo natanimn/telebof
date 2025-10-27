c64d1b81a39230b1e994b3bf402232eb44d429a8
natanimn <natanimco@gmail.com>
2025-10-27 09:38:10 +0300

    Refactor added getter and setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatAdministratorRights.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatAdministratorRights.java
@@ -5,172 +5,250 @@ import java.io.Serializable;
 
 /**
  * Represents the rights of an administrator in a chat.
- * @param isAnonymous True, if the user's presence in the chat is hidden
- * @param canManageChat True, if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members, report spam messages, ignore slow mode, and send messages to the chat without paying Telegram Stars. Implied by any other administrator privilege.
- * @param canDeleteMessages True, if the administrator can delete messages of other user
- * @param canManageVideoChats True, if the administrator can manage video chats
- * @param canRestrictMembers True, if the administrator can restrict, ban or unban chat members, or access supergroup statistics
- * @param canPromoteMembers True, if the administrator can add new administrators with a subset of their own privileges or demote administrators that they have promoted, directly or indirectly (promoted by administrators that were appointed by the user)
- * @param canChangeInfo True, if the user is allowed to change the chat title, photo and other settings
- * @param canInviteUsers True, if the user is allowed to invite new users to the chat
- * @param canPostMessages True, if the administrator can post messages in the channel, approve suggested posts, or access channel statistics; for channels only
- * @param canEditMessages True, if the administrator can edit messages of other users and can pin messages; for channels only
- * @param canPinMessages True, if the user is allowed to pin messages; for groups and supergroups only
- * @param canManageTopics True, if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
- * @param canPostStories True, if the administrator can post stories to the chat
- * @param canEditStories True, if the administrator can edit stories posted by other users, post stories to the chat page, pin chat stories, and access the chat's story archive
- * @param canDeleteStories True, if the administrator can delete stories posted by other users
- * @param canManageDirectMessages True, if the administrator can manage direct messages of the channel and decline suggested posts; for channels only
  * @author Natanim
  * @since 3 March 2025
- * @version 1.2.4
+ * @version 1.3.0
  */
-public record ChatAdministratorRights(
-        @SerializedName("is_anonymous") Boolean isAnonymous,
-        @SerializedName("can_manage_chat") Boolean canManageChat,
-        @SerializedName("can_delete_messages") Boolean canDeleteMessages,
-        @SerializedName("can_manage_video_chats") Boolean canManageVideoChats,
-        @SerializedName("can_restrict_members") Boolean canRestrictMembers,
-        @SerializedName("can_promote_members") Boolean canPromoteMembers,
-        @SerializedName("can_change_info") Boolean canChangeInfo,
-        @SerializedName("can_invite_users") Boolean canInviteUsers,
-        @SerializedName("can_post_messages") Boolean canPostMessages,
-        @SerializedName("can_edit_messages") Boolean canEditMessages,
-        @SerializedName("can_pin_messages") Boolean canPinMessages,
-        @SerializedName("can_manage_topics") Boolean canManageTopics,
-        @SerializedName("can_post_stories") Boolean canPostStories,
-        @SerializedName("can_edit_stories") Boolean canEditStories,
-        @SerializedName("can_delete_stories") Boolean canDeleteStories,
-        @SerializedName("can_manage_direct_messages") Boolean canManageDirectMessages
-) implements Serializable {
+public class ChatAdministratorRights implements Serializable {
+    @SerializedName("is_anonymous")
+    private Boolean isAnonymous;
 
+    @SerializedName("can_manage_chat")
+    private Boolean canManageChat;
+
+    @SerializedName("can_delete_messages")
+    private Boolean canDeleteMessages;
+
+    @SerializedName("can_manage_video_chats")
+    private Boolean canManageVideoChats;
+
+    @SerializedName("can_restrict_members")
+    private Boolean canRestrictMembers;
+
+    @SerializedName("can_promote_members")
+    private Boolean canPromoteMembers;
+
+    @SerializedName("can_change_info")
+    private Boolean canChangeInfo;
+
+    @SerializedName("can_invite_users")
+    private Boolean canInviteUsers;
+
+    @SerializedName("can_post_messages")
+    private Boolean canPostMessages;
+
+    @SerializedName("can_edit_messages")
+    private Boolean canEditMessages;
+
+    @SerializedName("can_pin_messages")
+    private Boolean canPinMessages;
+
+    @SerializedName("can_manage_topics")
+    private Boolean canManageTopics;
+
+    @SerializedName("can_post_stories")
+    private Boolean canPostStories;
+
+    @SerializedName("can_edit_stories")
+    private Boolean canEditStories;
+
+    @SerializedName("can_delete_stories")
+    private Boolean canDeleteStories;
+
+    @SerializedName("can_manage_direct_messages")
+    private Boolean canManageDirectMessages;
+
+    // Setters
+    public void setIsAnonymous(Boolean isAnonymous) {
+        this.isAnonymous = isAnonymous;
+    }
+
+    public void setCanManageChat(Boolean canManageChat) {
+        this.canManageChat = canManageChat;
+    }
+
+    public void setCanDeleteMessages(Boolean canDeleteMessages) {
+        this.canDeleteMessages = canDeleteMessages;
+    }
+
+    public void setCanManageVideoChats(Boolean canManageVideoChats) {
+        this.canManageVideoChats = canManageVideoChats;
+    }
+
+    public void setCanRestrictMembers(Boolean canRestrictMembers) {
+        this.canRestrictMembers = canRestrictMembers;
+    }
+
+    public void setCanPromoteMembers(Boolean canPromoteMembers) {
+        this.canPromoteMembers = canPromoteMembers;
+    }
+
+    public void setCanChangeInfo(Boolean canChangeInfo) {
+        this.canChangeInfo = canChangeInfo;
+    }
+
+    public void setCanInviteUsers(Boolean canInviteUsers) {
+        this.canInviteUsers = canInviteUsers;
+    }
+
+    public void setCanPostMessages(Boolean canPostMessages) {
+        this.canPostMessages = canPostMessages;
+    }
+
+    public void setCanEditMessages(Boolean canEditMessages) {
+        this.canEditMessages = canEditMessages;
+    }
+
+    public void setCanPinMessages(Boolean canPinMessages) {
+        this.canPinMessages = canPinMessages;
+    }
+
+    public void setCanManageTopics(Boolean canManageTopics) {
+        this.canManageTopics = canManageTopics;
+    }
+
+    public void setCanPostStories(Boolean canPostStories) {
+        this.canPostStories = canPostStories;
+    }
+
+    public void setCanEditStories(Boolean canEditStories) {
+        this.canEditStories = canEditStories;
+    }
+
+    public void setCanDeleteStories(Boolean canDeleteStories) {
+        this.canDeleteStories = canDeleteStories;
+    }
+
+    public void setCanManageDirectMessages(Boolean canManageDirectMessages) {
+        this.canManageDirectMessages = canManageDirectMessages;
+    }
+
+    // Getters
     /**
-     * Creates a new ChatAdministratorRights builder
+     * Gets whether the user's presence in the chat is hidden.
+     * @return True, if the user's presence in the chat is hidden
      */
-    public static ChatAdministratorRightsBuilder builder() {
-        return new ChatAdministratorRightsBuilder();
+    public Boolean getIsAnonymous() {
+        return isAnonymous;
     }
 
     /**
-     * Builder class for ChatAdministratorRights
+     * Gets whether the administrator can access the chat event log, get boost list, see hidden supergroup and channel members, report spam messages, ignore slow mode, and send messages to the chat without paying Telegram Stars.
+     * @return True, if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members, report spam messages, ignore slow mode, and send messages to the chat without paying Telegram Stars. Implied by any other administrator privilege.
      */
-    public static class ChatAdministratorRightsBuilder {
-        private Boolean isAnonymous;
-        private Boolean canManageChat;
-        private Boolean canDeleteMessages;
-        private Boolean canManageVideoChats;
-        private Boolean canRestrictMembers;
-        private Boolean canPromoteMembers;
-        private Boolean canChangeInfo;
-        private Boolean canInviteUsers;
-        private Boolean canPostMessages;
-        private Boolean canEditMessages;
-        private Boolean canPinMessages;
-        private Boolean canManageTopics;
-        private Boolean canPostStories;
-        private Boolean canEditStories;
-        private Boolean canDeleteStories;
-        private Boolean canManageDirectMessages;
+    public Boolean getCanManageChat() {
+        return canManageChat;
+    }
 
-        public ChatAdministratorRightsBuilder isAnonymous(boolean isAnonymous) {
-            this.isAnonymous = isAnonymous;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can delete messages of other users.
+     * @return True, if the administrator can delete messages of other users
+     */
+    public Boolean getCanDeleteMessages() {
+        return canDeleteMessages;
+    }
 
-        public ChatAdministratorRightsBuilder canManageChat(boolean canManageChat) {
-            this.canManageChat = canManageChat;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can manage video chats.
+     * @return True, if the administrator can manage video chats
+     */
+    public Boolean getCanManageVideoChats() {
+        return canManageVideoChats;
+    }
 
-        public ChatAdministratorRightsBuilder canDeleteMessages(boolean canDeleteMessages) {
-            this.canDeleteMessages = canDeleteMessages;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can restrict, ban or unban chat members, or access supergroup statistics.
+     * @return True, if the administrator can restrict, ban or unban chat members, or access supergroup statistics
+     */
+    public Boolean getCanRestrictMembers() {
+        return canRestrictMembers;
+    }
 
-        public ChatAdministratorRightsBuilder canManageVideoChats(boolean canManageVideoChats) {
-            this.canManageVideoChats = canManageVideoChats;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can add new administrators with a subset of their own privileges or demote administrators.
+     * @return True, if the administrator can add new administrators with a subset of their own privileges or demote administrators that they have promoted, directly or indirectly (promoted by administrators that were appointed by the user)
+     */
+    public Boolean getCanPromoteMembers() {
+        return canPromoteMembers;
+    }
 
-        public ChatAdministratorRightsBuilder canRestrictMembers(boolean canRestrictMembers) {
-            this.canRestrictMembers = canRestrictMembers;
-            return this;
-        }
+    /**
+     * Gets whether the user is allowed to change the chat title, photo and other settings.
+     * @return True, if the user is allowed to change the chat title, photo and other settings
+     */
+    public Boolean getCanChangeInfo() {
+        return canChangeInfo;
+    }
 
-        public ChatAdministratorRightsBuilder canPromoteMembers(boolean canPromoteMembers) {
-            this.canPromoteMembers = canPromoteMembers;
-            return this;
-        }
+    /**
+     * Gets whether the user is allowed to invite new users to the chat.
+     * @return True, if the user is allowed to invite new users to the chat
+     */
+    public Boolean getCanInviteUsers() {
+        return canInviteUsers;
+    }
 
-        public ChatAdministratorRightsBuilder canChangeInfo(boolean canChangeInfo) {
-            this.canChangeInfo = canChangeInfo;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can post messages in the channel, approve suggested posts, or access channel statistics.
+     * @return True, if the administrator can post messages in the channel, approve suggested posts, or access channel statistics; for channels only
+     */
+    public Boolean getCanPostMessages() {
+        return canPostMessages;
+    }
 
-        public ChatAdministratorRightsBuilder canInviteUsers(boolean canInviteUsers) {
-            this.canInviteUsers = canInviteUsers;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can edit messages of other users and can pin messages.
+     * @return True, if the administrator can edit messages of other users and can pin messages; for channels only
+     */
+    public Boolean getCanEditMessages() {
+        return canEditMessages;
+    }
 
-        public ChatAdministratorRightsBuilder canPostMessages(boolean canPostMessages) {
-            this.canPostMessages = canPostMessages;
-            return this;
-        }
+    /**
+     * Gets whether the user is allowed to pin messages.
+     * @return True, if the user is allowed to pin messages; for groups and supergroups only
+     */
+    public Boolean getCanPinMessages() {
+        return canPinMessages;
+    }
 
-        public ChatAdministratorRightsBuilder canEditMessages(boolean canEditMessages) {
-            this.canEditMessages = canEditMessages;
-            return this;
-        }
+    /**
+     * Gets whether the user is allowed to create, rename, close, and reopen forum topics.
+     * @return True, if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
+     */
+    public Boolean getCanManageTopics() {
+        return canManageTopics;
+    }
 
-        public ChatAdministratorRightsBuilder canPinMessages(boolean canPinMessages) {
-            this.canPinMessages = canPinMessages;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can post stories to the chat.
+     * @return True, if the administrator can post stories to the chat
+     */
+    public Boolean getCanPostStories() {
+        return canPostStories;
+    }
 
-        public ChatAdministratorRightsBuilder canManageTopics(boolean canManageTopics) {
-            this.canManageTopics = canManageTopics;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can edit stories posted by other users, post stories to the chat page, pin chat stories, and access the chat's story archive.
+     * @return True, if the administrator can edit stories posted by other users, post stories to the chat page, pin chat stories, and access the chat's story archive
+     */
+    public Boolean getCanEditStories() {
+        return canEditStories;
+    }
 
-        public ChatAdministratorRightsBuilder canPostStories(boolean canPostStories) {
-            this.canPostStories = canPostStories;
-            return this;
-        }
+    /**
+     * Gets whether the administrator can delete stories posted by other users.
+     * @return True, if the administrator can delete stories posted by other users
+     */
+    public Boolean getCanDeleteStories() {
+        return canDeleteStories;
+    }
 
-        public ChatAdministratorRightsBuilder canEditStories(boolean canEditStories) {
-            this.canEditStories = canEditStories;
-            return this;
-        }
-
-        public ChatAdministratorRightsBuilder canDeleteStories(boolean canDeleteStories) {
-            this.canDeleteStories = canDeleteStories;
-            return this;
-        }
-
-        public ChatAdministratorRightsBuilder canManageDirectMessages(boolean canManageDirectMessages) {
-            this.canManageDirectMessages = canManageDirectMessages;
-            return this;
-        }
-
-        public ChatAdministratorRights build() {
-            return new ChatAdministratorRights(
-                    isAnonymous,
-                    canManageChat,
-                    canDeleteMessages,
-                    canManageVideoChats,
-                    canRestrictMembers,
-                    canPromoteMembers,
-                    canChangeInfo,
-                    canInviteUsers,
-                    canPostMessages,
-                    canEditMessages,
-                    canPinMessages,
-                    canManageTopics,
-                    canPostStories,
-                    canEditStories,
-                    canDeleteStories,
-                    canManageDirectMessages
-            );
-        }
+    /**
+     * Gets whether the administrator can manage direct messages of the channel and decline suggested posts.
+     * @return True, if the administrator can manage direct messages of the channel and decline suggested posts; for channels only
+     */
+    public Boolean getCanManageDirectMessages() {
+        return canManageDirectMessages;
     }
 }
