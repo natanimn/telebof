3b90dae88764be39189607e96cdc7e5d27df4d40
natanimn <natanimco@gmail.com>
2025-10-27 15:26:44 +0300

    Refactor ChatBoostAdded class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoostAdded.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoostAdded.java
@@ -6,17 +6,7 @@ import com.google.gson.annotations.SerializedName;
  * This object represents a service message about a user boosting a chat.
  * @author Natanim
  * @since 3 March 2025
- * @version 1.3.0
+ * @version 1.2.4
+ * @param boostCount Number of boosts added by the user
  */
-public class ChatBoostAdded {
-    @SerializedName("boost_count")
-    private Integer boostCount;
-
-    /**
-     * Gets the number of boosts added by the user.
-     * @return Number of boosts added by the user
-     */
-    public Integer getBoostCount() {
-        return boostCount;
-    }
-}
+public record ChatBoostAdded(@SerializedName("boost_count") Integer boostCount) { }
