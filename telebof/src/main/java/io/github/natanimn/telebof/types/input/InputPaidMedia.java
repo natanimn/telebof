911b3b54613e4692518f2628c3d70bd392a16382
natanimn <natanimco@gmail.com>
2025-11-04 16:28:32 +0300

    Added field type()

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPaidMedia.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPaidMedia.java
@@ -4,4 +4,6 @@ package io.github.natanimn.telebof.types.input;
  * This object describes the paid media to be sent
  * @author Natanim
  */
-public interface InputPaidMedia extends InputMedia { }
+public interface InputPaidMedia extends InputMedia {
+    String type();
+}
--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContent.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContent.java
@@ -6,4 +6,6 @@ package io.github.natanimn.telebof.types.input;
  * @since 19 August 2025
  * @version 1.0.0
  */
-public interface InputStoryContent extends InputMedia{ }
+public interface InputStoryContent extends InputMedia{
+    String type();
+}
