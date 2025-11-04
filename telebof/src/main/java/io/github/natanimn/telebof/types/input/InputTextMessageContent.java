9b76ad03f05f5a5b8c47c6bcd220ce700f421131
natanimn <natanimco@gmail.com>
2025-11-04 16:28:02 +0300

    Refactor InputTextMessageContent class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputTextMessageContent.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputTextMessageContent.java
@@ -3,57 +3,66 @@ package io.github.natanimn.telebof.types.input;
 import io.github.natanimn.telebof.enums.ParseMode;
 import io.github.natanimn.telebof.types.media_and_service.LinkPreviewOptions;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
-
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
  * Represents the content of a text message to be sent as the result of an inline query.
- * @author Natanim 
+ * @param messageText Text of the message to be sent, 1-4096 characters
+ * @param parseMode Optional. Mode for parsing entities in the message text
+ * @param entities Optional. List of special entities that appear in message text, which can be specified instead of parseMode
+ * @param linkPreviewOptions Optional. Link preview generation options for the message
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class InputTextMessageContent implements InputMessageContent {
-    private String message_text;
-    private ParseMode parse_mode;
-    private List<MessageEntity> entities;
-    private LinkPreviewOptions link_preview_options;
+public record InputTextMessageContent(
+        @SerializedName("message_text") String messageText,
+        @SerializedName("parse_mode") ParseMode parseMode,
+        List<MessageEntity> entities,
+        @SerializedName("link_preview_options") LinkPreviewOptions linkPreviewOptions
+) implements InputMessageContent {
 
     /**
-     * Required
+     * Creates a new InputTextMessageContent builder
      * @param text Text of the message to be sent, 1-4096 characters
      */
-    public InputTextMessageContent(String text) {
-        this.message_text = text;
+    public static InputTextMessageContentBuilder builder(String text) {
+        return new InputTextMessageContentBuilder(text);
     }
 
     /**
-     * Optional
-     * @param parse_mode Mode for parsing entities in the message text
-     * @return {@link InputTextMessageContent}
+     * Builder class for InputTextMessageContent
      */
-    public InputTextMessageContent parseMode(ParseMode parse_mode) {
-        this.parse_mode = parse_mode;
-        return this;
-    }
+    public static class InputTextMessageContentBuilder {
+        private final String messageText;
+        private ParseMode parseMode;
+        private List<MessageEntity> entities;
+        private LinkPreviewOptions linkPreviewOptions;
 
-    /**
-     * Optional
-     * @param entities  List of special entities that appear in message text, which can be specified instead of {@link #parseMode}
-     * @return {@link InputTextMessageContent}
-     */
-    public InputTextMessageContent entities(MessageEntity[] entities) {
-        this.entities = List.of(entities);
-        return this;
-    }
+        public InputTextMessageContentBuilder(String messageText) {
+            this.messageText = messageText;
+        }
 
-    /**
-     * Optional
-     * @param link_preview_options Link preview generation options for the message
-     * @return {@link InputTextMessageContent}
-     */
-    public InputTextMessageContent linkPreviewOptions(LinkPreviewOptions link_preview_options){
-        this.link_preview_options = link_preview_options;
-        return this;
-    }
+        public InputTextMessageContentBuilder parseMode(ParseMode parseMode) {
+            this.parseMode = parseMode;
+            return this;
+        }
 
+        public InputTextMessageContentBuilder entities(List<MessageEntity> entities) {
+            this.entities = entities;
+            return this;
+        }
+
+        public InputTextMessageContentBuilder linkPreviewOptions(LinkPreviewOptions linkPreviewOptions) {
+            this.linkPreviewOptions = linkPreviewOptions;
+            return this;
+        }
+
+        public InputTextMessageContent build() {
+            return new InputTextMessageContent(
+                    messageText, parseMode, entities, linkPreviewOptions
+            );
+        }
+    }
 }
