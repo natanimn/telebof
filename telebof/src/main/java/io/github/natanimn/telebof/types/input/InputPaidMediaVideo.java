43d2796df87999c467b4f25bae066b57408b7fe6
natanimn <natanimco@gmail.com>
2025-11-04 16:26:34 +0300

    Refactor InputPaidMediaVideo class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPaidMediaVideo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPaidMediaVideo.java
@@ -1,143 +1,143 @@
 package io.github.natanimn.telebof.types.input;
 
+import com.google.gson.annotations.SerializedName;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * The paid media to send is a video.
- * @author Natanim 
+ * @param media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), or pass an HTTP URL for Telegram to get a file from the Internet.
+ * @param thumbnail Optional. Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side.
+ * @param width Optional. Video width
+ * @param height Optional. Video height
+ * @param duration Optional. Video duration in seconds
+ * @param supportsStreaming Optional. Pass True if the uploaded video is suitable for streaming
+ * @param cover Optional. Cover for the video in the message.
+ * @param startTimestamp Optional. Start timestamp for the video in the message
+ * @param files List of files to be uploaded
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9.4
+ * @version 1.3.0
  */
-public class InputPaidMediaVideo implements InputPaidMedia{
-    private String type;
-    private String media;
-    private transient boolean isFile, hasThumbnailFile;
-    private String thumbnail;
-    private Integer width, height, duration;
-    private Boolean supports_streaming;
-    private String cover;
-    private Integer start_timestamp;
+public record InputPaidMediaVideo(
+        String media,
+        String thumbnail,
+        Integer width,
+        Integer height,
+        Integer duration,
+        @SerializedName("supports_streaming") Boolean supportsStreaming,
+        String cover,
+        @SerializedName("start_timestamp") Integer startTimestamp,
+        List<File> files
+) implements InputPaidMedia {
 
-    private transient Boolean hasFile;
-    private transient List<File> files = new ArrayList<>();
-
-    /**
-     * Required
-     * @param media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), or
-     *              pass an HTTP URL for Telegram to get a file from the Internet.
-     */
-    public InputPaidMediaVideo(String media){
-        this.type = "video";
-        this.media = media;
-        this.hasFile = false;
+    @Override
+    public String type() {
+        return "video";
     }
 
-    /**
-     * Required
-     * @param media File to send. Pass an object of {@link File} to upload from your local machine.
-     */
-    public InputPaidMediaVideo(File media){
-        this.type = "video";
-        this.media = "attach://"+media.getName();
-        this.hasFile = true;
-        this.files.add(media);
-    }
-
-    /**
-     * Optional
-     * @param thumbnail Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side.
-     *                  The thumbnail should be in JPEG format and less than 200 kB in size.
-     *                  A thumbnail's width and height should not exceed 320.
-     * @return {@link InputPaidMediaVideo}
-     */
-    public InputPaidMediaVideo thumbnail(File thumbnail) {
-        this.thumbnail = "attach://"+thumbnail.getName();
-        this.hasFile = true;
-        this.files.add(thumbnail);
-        return this;
-
-    }
-
-    /**
-     * Optional
-     * @param width Video width
-     * @return {@link InputPaidMediaVideo}
-     */
-    public InputPaidMediaVideo width(Integer width) {
-        this.width = width;
-        return this;
-    }
-
-
-    /**
-     * Optional
-     * @param height Video height
-     * @return {@link InputPaidMediaVideo}
-     */
-    public InputPaidMediaVideo height(Integer height) {
-        this.height = height;
-        return this;
-    }
-
-    /**
-     * Optional
-     * @param duration Video duration in seconds
-     * @return {@link InputPaidMediaVideo}
-     */
-    public InputPaidMediaVideo duration(Integer duration) {
-        this.duration = duration;
-        return this;
-    }
-
-    /**
-     * Optional
-     * @param supports_streaming Pass True if the uploaded video is suitable for streaming
-     * @return {@link InputPaidMediaVideo}
-     */
-    public InputPaidMediaVideo supportsStreaming(Boolean supports_streaming) {
-        this.supports_streaming = supports_streaming;
-        return this;
-    }
-
-    /**
-     * Optional
-     * @param cover Cover for the video in the message.
-     * @return {@link InputMediaVideo}
-     */
-    public InputPaidMediaVideo cover(File cover){
-        this.cover = "attach://"+cover.getName();
-        this.hasFile = true;
-        this.files.add(cover);
-        return this;
-    }
-
-    /**
-     * Optional
-     * @param start_timestamp Start timestamp for the video in the message
-     * @return {@link  InputMediaVideo}
-     */
-    public InputPaidMediaVideo starTimestamp(int start_timestamp){
-        this.start_timestamp = start_timestamp;
-        return this;
-    }
-
-    /**
-     * For internal use only
-     * @return boolean
-     */
     @Override
     public boolean hasFile() {
-        return hasFile;
+        return files != null && !files.isEmpty();
     }
 
-    /**
-     * For internal use only
-     * @return list of files
-     */
     @Override
     public List<File> getFiles() {
         return files;
+    }
+
+    /**
+     * Creates a new InputPaidMediaVideo builder
+     * @param media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), or pass an HTTP URL for Telegram to get a file from the Internet.
+     */
+    public static InputPaidMediaVideoBuilder builder(String media) {
+        return new InputPaidMediaVideoBuilder(media);
+    }
+
+    /**
+     * Creates a new InputPaidMediaVideo builder
+     * @param media File to send. Pass an object of File to upload from your local machine.
+     */
+    public static InputPaidMediaVideoBuilder builder(File media) {
+        return new InputPaidMediaVideoBuilder(media);
+    }
+
+    /**
+     * Builder class for InputPaidMediaVideo
+     */
+    public static class InputPaidMediaVideoBuilder {
+        private String media;
+        private String thumbnail;
+        private Integer width;
+        private Integer height;
+        private Integer duration;
+        private Boolean supportsStreaming;
+        private String cover;
+        private Integer startTimestamp;
+        private List<File> files = new ArrayList<>();
+
+        public InputPaidMediaVideoBuilder(String media) {
+            this.media = media;
+        }
+
+        public InputPaidMediaVideoBuilder(File media) {
+            this.media = "attach://" + media.getName();
+            this.files.add(media);
+        }
+
+        public InputPaidMediaVideoBuilder thumbnail(File thumbnail) {
+            this.thumbnail = "attach://" + thumbnail.getName();
+            this.files.add(thumbnail);
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder thumbnail(String thumbnail) {
+            this.thumbnail = thumbnail;
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder width(Integer width) {
+            this.width = width;
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder height(Integer height) {
+            this.height = height;
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder duration(Integer duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder supportsStreaming(Boolean supportsStreaming) {
+            this.supportsStreaming = supportsStreaming;
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder cover(File cover) {
+            this.cover = "attach://" + cover.getName();
+            this.files.add(cover);
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder cover(String cover) {
+            this.cover = cover;
+            return this;
+        }
+
+        public InputPaidMediaVideoBuilder startTimestamp(Integer startTimestamp) {
+            this.startTimestamp = startTimestamp;
+            return this;
+        }
+
+        public InputPaidMediaVideo build() {
+            return new InputPaidMediaVideo(
+                    media, thumbnail, width, height, duration, supportsStreaming,
+                    cover, startTimestamp, files
+            );
+        }
     }
 }
