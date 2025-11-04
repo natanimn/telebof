990aebce65df8bfc948f3a32d297a93599dd2292
natanimn <natanimco@gmail.com>
2025-11-04 16:36:01 +0300

    Refactor BusinessConnection class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/BusinessConnection.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/BusinessConnection.java
@@ -2,74 +2,25 @@ package io.github.natanimn.telebof.types.updates;
 
 import io.github.natanimn.telebof.types.business.BusinessBotRights;
 import io.github.natanimn.telebof.types.chat_and_user.User;
-
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes the connection of the bot with a business account.
+ * @param id Unique identifier of the business connection
+ * @param user Business account user that created the business connection
+ * @param userChatId Identifier of a private chat with the user who created the business connection
+ * @param date Date the connection was established in Unix time
+ * @param rights Optional. Rights of the business bot
+ * @param isEnabled True, if the connection is active
  * @author Natanim
  * @since 3 March 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class BusinessConnection implements TelegramUpdate{
-    /**
-     * Unique identifier of the business connection
-     */
-    public String id;
-
-    /**
-     * Business account user that created the business connection
-     */
-    public User user;
-
-    /**
-     * Identifier of a private chat with the user who created the business connection
-     */
-    public Long user_Chat_id;
-
-    /**
-     * Date the connection was established in Unix time
-     */
-    public Integer date;
-
-    /**
-     * Optional.
-     * Rights of the business bot
-     */
-    public BusinessBotRights rights;
-
-    /**
-     * True, if the connection is active
-     */
-    public Boolean is_enabled;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        BusinessConnection that = (BusinessConnection) object;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(user, that.user) &&
-                Objects.equals(user_Chat_id, that.user_Chat_id) &&
-                Objects.equals(date, that.date) &&
-                Objects.equals(rights, that.rights) &&
-                Objects.equals(is_enabled, that.is_enabled);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, user, user_Chat_id, date, rights, is_enabled);
-    }
-
-    @Override
-    public String toString() {
-        return "BusinessConnection{" +
-                "id='" + id + '\'' +
-                ", user=" + user +
-                ", user_Chat_id=" + user_Chat_id +
-                ", date=" + date +
-                ", rights=" + rights +
-                ", is_enabled=" + is_enabled +
-                '}';
-    }
-}
+public record BusinessConnection(
+        String id,
+        User user,
+        @SerializedName("user_chat_id") Long userChatId,
+        Integer date,
+        BusinessBotRights rights,
+        @SerializedName("is_enabled") Boolean isEnabled
+) implements TelegramUpdate {}
