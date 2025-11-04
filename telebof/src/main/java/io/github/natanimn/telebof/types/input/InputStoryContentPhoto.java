fd917836e15053c6bb4a4d949376e0256c0396ca
natanimn <natanimco@gmail.com>
2025-11-04 16:27:21 +0300

    Refactor InputStoryContentPhoto class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContentPhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContentPhoto.java
@@ -5,39 +5,37 @@ import java.util.List;
 
 /**
  * Describes a photo to post as a story.
+ * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
+ * @param file The file object for internal use
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
-public class InputStoryContentPhoto implements InputStoryContent{
-    transient private File file;
-    private String type, photo;
+public record InputStoryContentPhoto(
+        String photo,
+        File file
+) implements InputStoryContent {
 
-    /**
-     * Required
-     * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
-     */
-    public InputStoryContentPhoto(File photo){
-        this.photo = "attach://"+photo.getName();
-        this.type  = "photo";
-        this.file  = photo;
+    @Override
+    public String type() {
+        return "photo";
     }
 
-    /**
-     * Used only for internal purpose
-     * @return boolean
-     */
     @Override
     public boolean hasFile() {
         return true;
     }
 
-    /**
-     * Used only for internal purpose
-     * @return List of files
-     */
     @Override
     public List<File> getFiles() {
         return List.of(file);
+    }
+
+    /**
+     * Creates a new InputStoryContentPhoto
+     * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
+     */
+    public static InputStoryContentPhoto fromFile(File photo) {
+        return new InputStoryContentPhoto("attach://" + photo.getName(), photo);
     }
 }
