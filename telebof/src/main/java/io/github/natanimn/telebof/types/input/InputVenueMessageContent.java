c4df8daa4f9bcbbf2e5c1f06118d5228506c7dcd
natanimn <natanimco@gmail.com>
2025-11-04 16:37:41 +0300

    Refactor InputVenueMessageContent class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputVenueMessageContent.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputVenueMessageContent.java
@@ -1,87 +1,88 @@
 package io.github.natanimn.telebof.types.input;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents the content of a venue message to be sent as the result of an inline query.
- * @author Natanim 
+ * @param latitude Latitude of the venue in degrees
+ * @param longitude Longitude of the venue in degrees
+ * @param title Name of the venue
+ * @param address Address of the venue
+ * @param foursquareId Optional. Foursquare identifier of the venue, if known
+ * @param foursquareType Optional. Foursquare type of the venue, if known. (For example, "arts_entertainment/default", "arts_entertainment/aquarium" or "food/icecream".)
+ * @param googlePlaceId Optional. Google Places identifier of the venue
+ * @param googlePlaceType Optional. Google Places type of the venue.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class InputVenueMessageContent implements InputMessageContent{
-    private double latitude, longitude;
-    private String address, title, foursquare_id, foursquare_type, google_place_id, google_place_type;
+public record InputVenueMessageContent(
+        Double latitude,
+        Double longitude,
+        String title,
+        String address,
+        @SerializedName("foursquare_id") String foursquareId,
+        @SerializedName("foursquare_type") String foursquareType,
+        @SerializedName("google_place_id") String googlePlaceId,
+        @SerializedName("google_place_type") String googlePlaceType
+) implements InputMessageContent {
 
     /**
-     * Required
+     * Creates a new InputVenueMessageContent builder
      * @param latitude Latitude of the venue in degrees
      * @param longitude Longitude of the venue in degrees
      * @param title Name of the venue
      * @param address Address of the venue
      */
-    public InputVenueMessageContent(double latitude, double longitude, String title, String address){
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.title = title;
-        this.address = address;
+    public static InputVenueMessageContentBuilder builder(double latitude, double longitude, String title, String address) {
+        return new InputVenueMessageContentBuilder(latitude, longitude, title, address);
     }
 
     /**
-     * Optional
-     * @param foursquare_id Foursquare identifier of the venue, if known
-     * @return {@link InputVenueMessageContent}
+     * Builder class for InputVenueMessageContent
      */
-    public InputVenueMessageContent foursquareId(String foursquare_id) {
-        this.foursquare_id = foursquare_id;
-        return this;
-    }
+    public static class InputVenueMessageContentBuilder {
+        private final double latitude;
+        private final double longitude;
+        private final String title;
+        private final String address;
+        private String foursquareId;
+        private String foursquareType;
+        private String googlePlaceId;
+        private String googlePlaceType;
 
-    /**
-     * Optional
-     * @param foursquare_type Foursquare type of the venue, if known. (For example, “arts_entertainment/default”, “arts_entertainment/aquarium” or “food/icecream”.)
-     * @return {@link InputVenueMessageContent}
-     */
-    public InputVenueMessageContent foursquareType(String foursquare_type) {
-        this.foursquare_type = foursquare_type;
-        return this;
-    }
+        public InputVenueMessageContentBuilder(double latitude, double longitude, String title, String address) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.title = title;
+            this.address = address;
+        }
 
-    /**
-     * Optional
-     * @param google_place_id Google Places identifier of the venue
-     * @return {@link InputVenueMessageContent}
-     */
-    public InputVenueMessageContent googlePlaceId(String google_place_id) {
-        this.google_place_id = google_place_id;
-        return this;
-    }
+        public InputVenueMessageContentBuilder foursquareId(String foursquareId) {
+            this.foursquareId = foursquareId;
+            return this;
+        }
 
-    /**
-     * Optional
-     * @param google_place_type Google Places type of the venue.
-     * @see <a href="https://developers.google.com/places/web-service/supported_types">Supported types</a>
-     * @return {@link InputVenueMessageContent}
-     */
-    public InputVenueMessageContent googlePlaceType(String google_place_type) {
-        this.google_place_type = google_place_type;
-        return this;
-    }
+        public InputVenueMessageContentBuilder foursquareType(String foursquareType) {
+            this.foursquareType = foursquareType;
+            return this;
+        }
 
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        InputVenueMessageContent that = (InputVenueMessageContent) object;
-        return Double.compare(latitude, that.latitude) == 0 && Double.compare(longitude, that.longitude) == 0 &&
-                Objects.equals(address, that.address) && Objects.equals(title, that.title) &&
-                Objects.equals(foursquare_id, that.foursquare_id) &&
-                Objects.equals(foursquare_type, that.foursquare_type) &&
-                Objects.equals(google_place_id, that.google_place_id) &&
-                Objects.equals(google_place_type, that.google_place_type);
-    }
+        public InputVenueMessageContentBuilder googlePlaceId(String googlePlaceId) {
+            this.googlePlaceId = googlePlaceId;
+            return this;
+        }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(latitude, longitude, address, title, foursquare_id, foursquare_type, google_place_id, google_place_type);
+        public InputVenueMessageContentBuilder googlePlaceType(String googlePlaceType) {
+            this.googlePlaceType = googlePlaceType;
+            return this;
+        }
+
+        public InputVenueMessageContent build() {
+            return new InputVenueMessageContent(
+                    latitude, longitude, title, address,
+                    foursquareId, foursquareType, googlePlaceId, googlePlaceType
+            );
+        }
     }
 }
