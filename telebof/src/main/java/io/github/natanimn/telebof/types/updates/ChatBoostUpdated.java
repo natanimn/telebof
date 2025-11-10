374245612923d1f0ccbb978132fbff11c52f820f
natanimn <natanimco@gmail.com>
2025-11-10 21:34:12 +0300

    Refactor ChatBoostUpdated class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/ChatBoostUpdated.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/ChatBoostUpdated.java
@@ -3,43 +3,15 @@ package io.github.natanimn.telebof.types.updates;
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import io.github.natanimn.telebof.types.chat_and_user.ChatBoost;
 
-import java.util.Objects;
-
 /**
  * This object represents a boost added to a chat or changed.
+ * @param chat Chat which was boosted
+ * @param boost Information about the chat boost
  * @author Natanim
  * @since 3 March 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class ChatBoostUpdated implements TelegramUpdate {
-    /**
-     * Chat which was boosted
-     */
-    public Chat chat;
-
-    /**
-     * Information about the chat boost
-     */
-    public ChatBoost boost;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ChatBoostUpdated that = (ChatBoostUpdated) object;
-        return Objects.equals(chat, that.chat) && Objects.equals(boost, that.boost);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(chat, boost);
-    }
-
-    @Override
-    public String toString() {
-        return "ChatBoostUpdated{" +
-                "chat=" + chat +
-                ", boost=" + boost +
-                '}';
-    }
-}
+public record ChatBoostUpdated(
+        Chat chat,
+        ChatBoost boost
+) implements TelegramUpdate {}
