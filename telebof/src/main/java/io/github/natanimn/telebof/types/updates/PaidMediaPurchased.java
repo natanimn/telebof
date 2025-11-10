95eb44cfda47a8355d3ea13d8c0b0f4befb0c1e4
natanimn <natanimco@gmail.com>
2025-11-10 21:35:11 +0300

    Refactor PaidMediaPurchased class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/PaidMediaPurchased.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/PaidMediaPurchased.java
@@ -1,37 +1,17 @@
 package io.github.natanimn.telebof.types.updates;
 
 import io.github.natanimn.telebof.types.chat_and_user.User;
-
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class contains information about a paid media purchase.
+ * @param from User who purchased the media
+ * @param paidMediaPayload Bot-specified paid media payload
  * @author Natanim
  * @since 17 April 2025
- * @version 0.7
+ * @version 1.3.0
  */
-
-public class PaidMediaPurchased implements TelegramUpdate {
-    /**
-     * User who purchased the media
-     */
-    public User from;
-
-    /**
-     * Bot-specified paid media payload
-     */
-    public String paid_media_payload;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        PaidMediaPurchased that = (PaidMediaPurchased) o;
-        return  Objects.equals(from, that.from) &&
-                Objects.equals(paid_media_payload, that.paid_media_payload);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(from, paid_media_payload);
-    }
-}
+public record PaidMediaPurchased(
+        User from,
+        @SerializedName("paid_media_payload") String paidMediaPayload
+) implements TelegramUpdate {}
