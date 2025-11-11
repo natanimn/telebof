cc8f9903d27fafd58acd5f43f6bc0f337d3f3e7d
natanimn <natanimco@gmail.com>
2025-11-11 20:19:59 +0300

    Refactor PollAnswer class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/PollAnswer.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/PollAnswer.java
@@ -2,58 +2,22 @@ package io.github.natanimn.telebof.types.updates;
 
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import io.github.natanimn.telebof.types.chat_and_user.User;
-
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * This object represents an answer of a user in a non-anonymous poll.
+ * @param pollId Unique poll identifier
+ * @param user The user that changed the answer to the poll, if the voter isn't anonymous
+ * @param optionIds 0-based identifiers of chosen answer options. May be empty if the vote was retracted.
+ * @param voterChat The chat that changed the answer to the poll, if the voter is anonymous
  * @author Natanim
  * @since 3 March 2025
- * @version 0.7
+ * @version 1.3.0
  */
-public class PollAnswer implements TelegramUpdate{
-    /**
-     * Unique poll identifier
-     */
-    public String poll_id;
-
-    /**
-     * The user that changed the answer to the poll, if the voter isn't anonymous
-     */
-    public User user;
-
-    /**
-     * 0-based identifiers of chosen answer options. May be empty if the vote was retracted.
-     */
-    public List<Integer> options_id;
-
-    /**
-     * The chat that changed the answer to the poll, if the voter is anonymous
-     */
-    public Chat voter_chat;
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        PollAnswer that = (PollAnswer) object;
-        return Objects.equals(poll_id, that.poll_id) && Objects.equals(user, that.user) &&
-                Objects.equals(options_id, that.options_id) && Objects.equals(voter_chat, that.voter_chat);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(poll_id, user, options_id, voter_chat);
-    }
-
-    @Override
-    public String toString() {
-        return "PollAnswer{" +
-                "poll_id='" + poll_id + '\'' +
-                ", user=" + user +
-                ", options_id=" + options_id +
-                ", voter_chat=" + voter_chat +
-                '}';
-    }
-}
+public record PollAnswer(
+        @SerializedName("poll_id") String pollId,
+        User user,
+        @SerializedName("option_ids") List<Integer> optionIds,
+        @SerializedName("voter_chat") Chat voterChat
+) implements TelegramUpdate {}
