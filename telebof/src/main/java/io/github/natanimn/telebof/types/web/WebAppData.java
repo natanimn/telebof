7d57ceec7bc7e8e56ecd89a093885e564521fe75
natanimn <natanimco@gmail.com>
2025-11-11 20:20:45 +0300

    Refactor WebAppData class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/web/WebAppData.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/web/WebAppData.java
@@ -1,19 +1,17 @@
 package io.github.natanimn.telebof.types.web;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Describes data sent from a <a href="https://core.telegram.org/bots/webapps">Web App</a> to the bot.
- * @author Natanim 
+ *
+ * @param data The data. Be aware that a bad client can send arbitrary data in this field.
+ * @param buttonText Text of the web_app keyboard button from which the Web App was opened. Be aware that a bad client can send arbitrary data in this field.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
-public class WebAppData {
-    /**
-     * The data. Be aware that a bad client can send arbitrary data in this field.
-     */
-    public String data;
-
-    /**
-     * Text of the web_app keyboard button from which the Web App was opened. Be aware that a bad client can send arbitrary data in this field.
-     */
-    public String button_text;
-}
+public record WebAppData(
+        String data,
+        @SerializedName("button_text") String buttonText
+) {}
