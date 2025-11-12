7907e69043dbd43f2b63ad339aac2813a9c86bea
natanimn <natanimco@gmail.com>
2025-11-12 21:33:32 +0300

    Refactor BusinessOpeningHoursInterval class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHoursInterval.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHoursInterval.java
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName;
  * Describes the opening hours of a business.
  * @author Natanim
  * @since 3 March 2025
- * @version 1.2.4
+ * @version 1.3.0
  * @param openingMinute The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 * 24 * 60
  * @param closingMinute The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 * 24 * 60
  */
