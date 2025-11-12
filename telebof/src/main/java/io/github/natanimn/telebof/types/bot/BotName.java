2a9fe6292f815130ae89c2950d4e30dd4b6b3c2d
natanimn <natanimco@gmail.com>
2025-11-12 21:32:04 +0300

    Refactor BotName class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotName.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotName.java
@@ -5,7 +5,7 @@ package io.github.natanimn.telebof.types.bot;
  * This object represents the bot's name.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  * @param name The bot's name
  */
 public record BotName(String name) { }
