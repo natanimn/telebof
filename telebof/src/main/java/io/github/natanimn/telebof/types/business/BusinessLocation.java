7333594fb6d6a4b278e619d1337c1adc22be3426
natanimn <natanimco@gmail.com>
2025-11-12 21:33:10 +0300

    Refactor BusinessLocation class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessLocation.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessLocation.java
@@ -6,7 +6,7 @@ import io.github.natanimn.telebof.types.media_and_service.Location;
  * Contains information about the location of a Telegram Business account.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  * @param address Address of the business
  * @param location Location of the business
  */
