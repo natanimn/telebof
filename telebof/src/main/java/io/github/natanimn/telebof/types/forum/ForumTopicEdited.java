59ef0ea618f457ca3868d0d96c9dbab44d7c89df
natanimn <natanimco@gmail.com>
2025-11-16 18:49:59 +0300

    Refactor ForumTopicEdited class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/forum/ForumTopicEdited.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/forum/ForumTopicEdited.java
@@ -8,7 +8,7 @@ import com.google.gson.annotations.SerializedName;
  * @param iconCustomEmojiId Optional. New identifier of the custom emoji shown as the topic icon, if it was edited; an empty string if the icon was removed
  * @author Natanim
  * @since 3 March 2025
- * @version 1.2.4
+ * @version 1.3.0
  */
 public record ForumTopicEdited(
         String name,
