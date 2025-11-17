614152d160592bfe3728c6d37aba551cbcd6f799
natanimn <natanimco@gmail.com>
2025-11-17 16:50:50 +0300

    Converted snake case method parameter names into camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPollOption.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPollOption.java
@@ -2,57 +2,50 @@ package io.github.natanimn.telebof.types.input;
 
 import io.github.natanimn.telebof.enums.ParseMode;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
-import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * This object contains information about one answer option in a poll to be sent.
- * @param text Option text, 1-100 characters
- * @param textParseMode Optional. Mode for parsing entities in the text
- * @param textEntities Optional. A JSON-serialized list of special entities that appear in the poll option text. It can be specified instead of textParseMode
  * @author Natanim
  * @since 3 March 2025
- * @version 1.3.0
+ * @version 1.4.0
  */
-public record InputPollOption(
-        String text,
-        @SerializedName("text_parse_mode") ParseMode textParseMode,
-        @SerializedName("text_entities") MessageEntity[] textEntities
-) implements Serializable {
+public class InputPollOption implements Serializable {
+    private String text;
+    private ParseMode text_parse_mode;
+    private MessageEntity[] text_entities;
 
     /**
-     * Creates a new InputPollOption builder
+     * Required
      * @param text Option text, 1-100 characters
      */
-    public static InputPollOptionBuilder builder(String text) {
-        return new InputPollOptionBuilder(text);
+    public InputPollOption(String text){
+        this.text = text;
     }
 
     /**
-     * Builder class for InputPollOption
+     * Optional
+     * @param textParseMode Mode for parsing entities in the text
+     * @return {@link InputPollOption}
      */
-    public static class InputPollOptionBuilder {
-        private final String text;
-        private ParseMode textParseMode;
-        private MessageEntity[] textEntities;
-
-        public InputPollOptionBuilder(String text) {
-            this.text = text;
-        }
-
-        public InputPollOptionBuilder textParseMode(ParseMode textParseMode) {
-            this.textParseMode = textParseMode;
-            return this;
-        }
-
-        public InputPollOptionBuilder textEntities(MessageEntity[] textEntities) {
-            this.textEntities = textEntities;
-            return this;
-        }
-
-        public InputPollOption build() {
-            return new InputPollOption(text, textParseMode, textEntities);
-        }
+    public InputPollOption textParseMode(ParseMode textParseMode){
+        this.text_parse_mode = textParseMode;
+        return this;
     }
+
+    /**
+     * Optional
+     * @param textEntities  A JSON-serialized list of special entities that appear in the poll option text.
+     *                      It can be specified instead of {@link #textParseMode(ParseMode)}
+     * @return {@link InputPollOption}
+     */
+    public InputPollOption textEntities(MessageEntity[] textEntities){
+        this.text_entities = textEntities;
+        return this;
+    }
+
 }
