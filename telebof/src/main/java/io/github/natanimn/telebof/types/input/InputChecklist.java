948b33f3b1094ed35da7b9c33add32e5f03a56ba
natanimn <natanimco@gmail.com>
2025-11-17 16:48:41 +0300

    Refactor parameters case parse_mode, title_entities, others_can_add_tasks, others_can_mark_tasks_as_done  class into camelcase

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputChecklist.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputChecklist.java
@@ -2,80 +2,70 @@ package io.github.natanimn.telebof.types.input;
 
 import io.github.natanimn.telebof.enums.ParseMode;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
-import com.google.gson.annotations.SerializedName;
-import java.io.Serializable;
-import java.util.List;
 
+import java.io.Serializable;
 /**
  * This object represents a checklist to create.
- * @param title Title of the checklist; 1-255 characters after entities parsing
- * @param tasks Array of 1-30 tasks in the checklist
- * @param parseMode Optional. Mode for parsing entities in the title
- * @param titleEntities Optional. A JSON-serialized list of special entities that appear in the title. It can be specified instead of parseMode
- * @param othersCanAddTasks Optional. Pass True if other users can add tasks to the checklist
- * @param othersCanMarkTasksAsDone Optional. Pass True if other users can mark tasks as done or not done in the checklist
  * @author Natanim
  * @since 1 September 2025
  * @version 1.3.0
  */
-public record InputChecklist(
-        String title,
-        List<InputChecklistTask> tasks,
-        @SerializedName("parse_mode") ParseMode parseMode,
-        @SerializedName("title_entities") List<MessageEntity> titleEntities,
-        @SerializedName("others_can_add_tasks") Boolean othersCanAddTasks,
-        @SerializedName("others_can_mark_tasks_as_done") Boolean othersCanMarkTasksAsDone
-) implements Serializable {
+public class InputChecklist implements Serializable {
+    private String title;
+    private ParseMode parse_mode;
+    private MessageEntity[] title_entities;
+    private InputChecklistTask[] tasks;
+    private Boolean others_can_add_tasks;
+    private Boolean others_can_mark_tasks_as_done;
 
     /**
-     * Creates a new InputChecklist builder
+     * Required
      * @param title Title of the checklist; 1-255 characters after entities parsing
      * @param tasks Array of 1-30 tasks in the checklist
      */
-    public static InputChecklistBuilder builder(String title, List<InputChecklistTask> tasks) {
-        return new InputChecklistBuilder(title, tasks);
+    public InputChecklist(String title, InputChecklistTask[] tasks) {
+        this.title = title;
+        this.tasks = tasks;
     }
 
     /**
-     * Builder class for InputChecklist
+     * Optional
+     * @param parseMode Mode for parsing entities in the title
+     * @return {@link InputChecklist}
      */
-    public static class InputChecklistBuilder {
-        private final String title;
-        private final List<InputChecklistTask> tasks;
-        private ParseMode parseMode;
-        private List<MessageEntity> titleEntities;
-        private Boolean othersCanAddTasks;
-        private Boolean othersCanMarkTasksAsDone;
+    public InputChecklist parseMode(ParseMode parseMode) {
+        this.parse_mode = parseMode;
+        return this;
+    }
 
-        public InputChecklistBuilder(String title, List<InputChecklistTask> tasks) {
-            this.title = title;
-            this.tasks = tasks;
-        }
+    /**
+     * Optional
+     * @param titleEntities A JSON-serialized list of special entities that appear in the title.
+     *                       It can be specified instead of {@link #parseMode(ParseMode)}
+     * @return {@link InputChecklist}
+     */
+    public InputChecklist titleEntities(MessageEntity[] titleEntities) {
+        this.title_entities = titleEntities;
+        return this;
+    }
 
-        public InputChecklistBuilder parseMode(ParseMode parseMode) {
-            this.parseMode = parseMode;
-            return this;
-        }
+    /**
+     * Optional
+     * @param othersCanAddTasks Pass True if other users can add tasks to the checklist
+     * @return {@link InputChecklist}
+     */
+    public InputChecklist othersCanAddTasks(Boolean othersCanAddTasks) {
+        this.others_can_add_tasks = othersCanAddTasks;
+        return this;
+    }
 
-        public InputChecklistBuilder titleEntities(List<MessageEntity> titleEntities) {
-            this.titleEntities = titleEntities;
-            return this;
-        }
-
-        public InputChecklistBuilder othersCanAddTasks(Boolean othersCanAddTasks) {
-            this.othersCanAddTasks = othersCanAddTasks;
-            return this;
-        }
-
-        public InputChecklistBuilder othersCanMarkTasksAsDone(Boolean othersCanMarkTasksAsDone) {
-            this.othersCanMarkTasksAsDone = othersCanMarkTasksAsDone;
-            return this;
-        }
-
-        public InputChecklist build() {
-            return new InputChecklist(
-                    title, tasks, parseMode, titleEntities, othersCanAddTasks, othersCanMarkTasksAsDone
-            );
-        }
+    /**
+     * Optional
+     * @param othersCanMarkTasksAsDone Pass True if other users can mark tasks as done or not done in the checklist
+     * @return {@link InputChecklist}
+     */
+    public InputChecklist othersCanMarkTasksAsDone(Boolean othersCanMarkTasksAsDone) {
+        this.others_can_mark_tasks_as_done = othersCanMarkTasksAsDone;
+        return this;
     }
 }
