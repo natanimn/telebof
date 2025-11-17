f326b483228914a13dc27fc8e6b756bc424f8565
natanimn <natanimco@gmail.com>
2025-11-17 16:47:16 +0300

    Refactor DirectMessagesTopic class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/DirectMessagesTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/DirectMessagesTopic.java
@@ -1,5 +1,6 @@
 package io.github.natanimn.telebof.types;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.natanimn.telebof.types.chat_and_user.User;
 
 /**
@@ -7,15 +8,11 @@ import io.github.natanimn.telebof.types.chat_and_user.User;
  * @author Natanim
  * @since 3 September 2025
  * @version 1.2.0
+ * @param topicId Unique identifier of the topic.
+ * @param user Optional. Information about the user that created the topic. Currently, it is always present
  */
-public class DirectMessagesTopic {
-    /**
-     * Unique identifier of the topic.
-     */
-    public Long topic_id;
 
-    /**
-     * Optional. Information about the user that created the topic. Currently, it is always present
-     */
-    public User user;
-}
+public record DirectMessagesTopic(
+    @SerializedName("topic_id") Long topicId,
+    User user
+) { }
