6a2dbb3a1a0cdbf16b8d92ed6a28257cd3d173cc
natanimn <natanimco@gmail.com>
2025-11-18 12:30:30 +0300

    Refactor BackgroundFill class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/BackgroundFill.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/BackgroundFill.java
@@ -1,43 +1,28 @@
 package io.github.natanimn.telebof.types;
 
 import io.github.natanimn.telebof.enums.BackgroundFillType;
+import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 /**
  * This object describes the way a background is filled based on the selected colors
- * @author Natanim 
+ *
+ * @param type Type of the background fill
+ * @param color The color of the background fill in the RGB24 format
+ * @param topColor Top color of the gradient in the RGB24 format
+ * @param bottomColor Bottom color of the gradient in the RGB24 format
+ * @param rotationAngle Clockwise rotation angle of the background fill in degrees; 0-359
+ * @param colors A list of the 3 or 4 base colors that are used to generate the freeform gradient in the RGB24 format
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
-public class BackgroundFill {
-    /**
-     * Type of the background fill,
-     */
-    public BackgroundFillType type;
-
-    /**
-     * The color of the background fill in the RGB24 format
-     */
-    public Integer color;
-
-    /**
-     * Top color of the gradient in the RGB24 format
-     */
-    public Integer top_color;
-
-    /**
-     * Bottom color of the gradient in the RGB24 format
-     */
-    public Integer bottom_color;
-
-    /**
-     * Clockwise rotation angle of the background fill in degrees; 0-359
-     */
-    public Integer rotation_angle;
-
-    /**
-     * A list of the 3 or 4 base colors that are used to generate the freeform gradient in the RGB24 format
-     */
-    public List<Integer> colors;
-}
+public record BackgroundFill(
+        BackgroundFillType type,
+        Integer color,
+        @SerializedName("top_color") Integer topColor,
+        @SerializedName("bottom_color") Integer bottomColor,
+        @SerializedName("rotation_angle") Integer rotationAngle,
+        List<Integer> colors
+) {}
