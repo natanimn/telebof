884553ef6f5562e26fc4b24b8cb0fe153235a525
natanimn <natanimco@gmail.com>
2025-11-20 18:38:41 +0300

    Refactor InputStoryContentPhoto class into a record

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContentPhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContentPhoto.java
@@ -5,37 +5,54 @@ import java.util.List;
 
 /**
  * Describes a photo to post as a story.
- * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
- * @param file The file object for internal use
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
+ * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
  */
-public record InputStoryContentPhoto(
-        String photo,
-        File file
-) implements InputStoryContent {
-
+public record InputStoryContentPhoto(String photo, File file) implements InputStoryContent{
     @Override
     public String type() {
         return "photo";
     }
 
+    /**
+     * Used only for internal purpose
+     * @return boolean
+     */
     @Override
     public boolean hasFile() {
         return true;
     }
 
+    /**
+     * Used only for internal purpose
+     * @return List of files
+     */
     @Override
     public List<File> getFiles() {
         return List.of(file);
     }
 
+    public static InputStoryContentVideo.InputStoryContentVideoBuilder builder(File video) {
+        return new InputStoryContentVideo.InputStoryContentVideoBuilder(video);
+    }
+
     /**
-     * Creates a new InputStoryContentPhoto
-     * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
+     * Builder class for InputStoryContentPhoto
      */
-    public static InputStoryContentPhoto fromFile(File photo) {
-        return new InputStoryContentPhoto("attach://" + photo.getName(), photo);
+    public static class InputStoryContentPhotoBuilder {
+        private final File photo;
+
+        public InputStoryContentPhotoBuilder(File photo) {
+            this.photo = photo;
+        }
+
+        public InputStoryContentPhoto build() {
+            return new InputStoryContentPhoto(
+                    "attach://" + photo.getName(),
+                    photo
+            );
+        }
     }
 }
