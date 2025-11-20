404e5bd888eb1b8e26ccd2d2634fa7dfb43f2dec
natanimn <natanimco@gmail.com>
2025-11-20 18:38:58 +0300

    Bump into v1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContent.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContent.java
@@ -4,7 +4,7 @@ package io.github.natanimn.telebof.types.input;
  * This object describes the content of a story to post.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
 public interface InputStoryContent extends InputMedia{
     String type();
