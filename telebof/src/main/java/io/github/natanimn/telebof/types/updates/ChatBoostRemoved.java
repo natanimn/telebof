364fd6fe986cd90d536873ba0d3808a38b8d175d
natanimn <natanimco@gmail.com>
2025-11-21 13:58:21 +0300

    Replaced snake case fields with camelCase and added getter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/ChatBoostRemoved.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/ChatBoostRemoved.java
@@ -4,19 +4,76 @@ import io.github.natanimn.telebof.types.chat_and_user.ChatBoostSource;
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 /**
  * This object represents a boost removed from a chat.
- * @param chat Chat which was boosted
- * @param boostId Unique identifier of the boost
- * @param removedDate Point in time (Unix timestamp) when the boost was removed
- * @param source Source of the removed boost
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ChatBoostRemoved(
-        Chat chat,
-        @SerializedName("boost_id") String boostId,
-        @SerializedName("removed_date") Integer removedDate,
-        ChatBoostSource source
-) implements TelegramUpdate {}
+public class ChatBoostRemoved implements TelegramUpdate {
+    /**
+     * Chat which was boosted
+     */
+    private Chat chat;
+
+    /**
+     * Unique identifier of the boost
+     */
+    @SerializedName("boost_id")
+    private String boostId;
+
+    /**
+     * Point in time (Unix timestamp) when the boost was removed
+     */
+    @SerializedName("removed_date")
+    private Integer removedDate;
+
+    /**
+     * Source of the removed boost
+     */
+    private ChatBoostSource source;
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public String getBoostId() {
+        return boostId;
+    }
+
+    public Integer getRemovedDate() {
+        return removedDate;
+    }
+
+    public ChatBoostSource getSource() {
+        return source;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ChatBoostRemoved that = (ChatBoostRemoved) object;
+        return Objects.equals(chat, that.chat) &&
+                Objects.equals(boostId, that.boostId) &&
+                Objects.equals(removedDate, that.removedDate) &&
+                Objects.equals(source, that.source);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chat, boostId, removedDate, source);
+    }
+
+    @Override
+    public String toString() {
+        return "ChatBoostRemoved{" +
+                "chat=" + chat +
+                ", boostId='" + boostId + '\'' +
+                ", removedDate=" + removedDate +
+                ", source=" + source +
+                '}';
+    }
+}
