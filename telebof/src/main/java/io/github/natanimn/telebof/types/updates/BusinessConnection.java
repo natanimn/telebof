2ada67c2a2a140940326a87056d66f876f4853a1
natanimn <natanimco@gmail.com>
2025-11-21 13:56:31 +0300

    Replaced snake case fields with camelCase and added getter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/BusinessConnection.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/BusinessConnection.java
@@ -4,23 +4,98 @@ import io.github.natanimn.telebof.types.business.BusinessBotRights;
 import io.github.natanimn.telebof.types.chat_and_user.User;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 /**
  * Describes the connection of the bot with a business account.
- * @param id Unique identifier of the business connection
- * @param user Business account user that created the business connection
- * @param userChatId Identifier of a private chat with the user who created the business connection
- * @param date Date the connection was established in Unix time
- * @param rights Optional. Rights of the business bot
- * @param isEnabled True, if the connection is active
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record BusinessConnection(
-        String id,
-        User user,
-        @SerializedName("user_chat_id") Long userChatId,
-        Integer date,
-        BusinessBotRights rights,
-        @SerializedName("is_enabled") Boolean isEnabled
-) implements TelegramUpdate {}
+public class BusinessConnection implements TelegramUpdate {
+    /**
+     * Unique identifier of the business connection
+     */
+    private String id;
+
+    /**
+     * Business account user that created the business connection
+     */
+    private User user;
+
+    /**
+     * Identifier of a private chat with the user who created the business connection
+     */
+    @SerializedName("user_chat_id")
+    private Long userChatId;
+
+    /**
+     * Date the connection was established in Unix time
+     */
+    private Integer date;
+
+    /**
+     * Optional. Rights of the business bot
+     */
+    private BusinessBotRights rights;
+
+    /**
+     * True, if the connection is active
+     */
+    @SerializedName("is_enabled")
+    private Boolean isEnabled;
+
+    public String getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Long getUserChatId() {
+        return userChatId;
+    }
+
+    public Integer getDate() {
+        return date;
+    }
+
+    public BusinessBotRights getRights() {
+        return rights;
+    }
+
+    public Boolean getIsEnabled() {
+        return isEnabled;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        BusinessConnection that = (BusinessConnection) object;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(user, that.user) &&
+                Objects.equals(userChatId, that.userChatId) &&
+                Objects.equals(date, that.date) &&
+                Objects.equals(rights, that.rights) &&
+                Objects.equals(isEnabled, that.isEnabled);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, userChatId, date, rights, isEnabled);
+    }
+
+    @Override
+    public String toString() {
+        return "BusinessConnection{" +
+                "id='" + id + '\'' +
+                ", user=" + user +
+                ", userChatId=" + userChatId +
+                ", date=" + date +
+                ", rights=" + rights +
+                ", isEnabled=" + isEnabled +
+                '}';
+    }
+}
