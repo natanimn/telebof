8241e3a8e02c5e92bf32a2600c48bbe8fc03830d
natanimn <natanimco@gmail.com>
2025-11-21 13:58:55 +0300

    Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/ChatBoostUpdated.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/ChatBoostUpdated.java
@@ -3,15 +3,52 @@ package io.github.natanimn.telebof.types.updates;
 import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import io.github.natanimn.telebof.types.chat_and_user.ChatBoost;
 
+import java.util.Objects;
+
 /**
  * This object represents a boost added to a chat or changed.
- * @param chat Chat which was boosted
- * @param boost Information about the chat boost
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ChatBoostUpdated(
-        Chat chat,
-        ChatBoost boost
-) implements TelegramUpdate {}
+public class ChatBoostUpdated implements TelegramUpdate {
+    /**
+     * Chat which was boosted
+     */
+    private Chat chat;
+
+    /**
+     * Information about the chat boost
+     */
+    private ChatBoost boost;
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public ChatBoost getBoost() {
+        return boost;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ChatBoostUpdated that = (ChatBoostUpdated) object;
+        return Objects.equals(chat, that.chat) &&
+                Objects.equals(boost, that.boost);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chat, boost);
+    }
+
+    @Override
+    public String toString() {
+        return "ChatBoostUpdated{" +
+                "chat=" + chat +
+                ", boost=" + boost +
+                '}';
+    }
+}
