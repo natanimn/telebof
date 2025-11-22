46b43b0d68f40c56ef62d02a2fa92c7ae17abbc2
natanimn <natanimco@gmail.com>
2025-11-22 08:56:54 +0300

    Added getter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotDescription.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotDescription.java
@@ -2,9 +2,17 @@ package io.github.natanimn.telebof.types.bot;
 
 /**
  * This object represents the bot's description.
- * @param description The bot's description
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record BotDescription(String description) {}
+public class BotDescription {
+    /**
+     * The bot's description
+     */
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+}
