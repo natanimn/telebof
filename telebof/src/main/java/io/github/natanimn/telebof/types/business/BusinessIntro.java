307cb9e88a803e5f00455b0f357b08fb4f63f91b
natanimn <natanimco@gmail.com>
2025-11-22 08:58:16 +0300

    Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessIntro.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessIntro.java
@@ -4,11 +4,35 @@ import io.github.natanimn.telebof.types.media_and_service.Sticker;
 
 /**
  * Contains information about the start page settings of a Telegram Business account.
- * @param title Title text of the business intro
- * @param message Message text of the business intro
- * @param sticker Sticker of the business intro
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record BusinessIntro(String title, String message, Sticker sticker){ }
+public class BusinessIntro {
+    /**
+     * Title text of the business intro
+     */
+    private String title;
+
+    /**
+     * Message text of the business intro
+     */
+    private String message;
+
+    /**
+     * Sticker of the business intro
+     */
+    private Sticker sticker;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Sticker getSticker() {
+        return sticker;
+    }
+}
