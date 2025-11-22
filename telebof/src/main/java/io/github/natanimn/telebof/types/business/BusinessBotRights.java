94653106c7c980544e49ddec3091897772009c1b
natanimn <natanimco@gmail.com>
2025-11-22 08:57:52 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessBotRights.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessBotRights.java
@@ -4,64 +4,148 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents the rights of a business bot.
- * @param canReply Optional. True, if the bot can send and edit messages in the  chats that had incoming messages in the last 24 hours
- * @param canReadMessages Optional. True, if the bot can mark incoming  messages as read
- * @param canDeleteSentMessages Optional. True, if the bot can delete messages sent by the bot
- * @param canDeleteAllMessages Optional. True, if the bot can delete all  messages in managed chats
- * @param canEditName Optional. True, if the bot can edit the first and last name of the business account
- * @param canEditBio Optional. True, if the bot can edit the bio of the business account
- * @param canEditProfilePhoto True, if the bot can edit the profile photo of the business account
- * @param canEditUsername True, if the bot can edit the username of the business account
- * @param canChangeGiftSettings True, if the bot can change the privacy settings pertaining to gifts for the business account
- * @param canViewGiftsAndStars True, if the bot can view gifts and the amount of Telegram Stars owned by the business account
- * @param canConvertGiftsToStars True, if the bot can convert regular gifts owned by the business account to Telegram Stars
- * @param canTransferAndUpgradeGifts True, if the bot can transfer and upgrade gifts owned by the business account
- * @param canTransferStars True, if the bot can transfer Telegram Stars received by the business account to its own account, or use them to upgrade and transfer gifts
- * @param canManageStories True, if the bot can post, edit and delete stories on behalf of the business account
  * @author Natanim
  * @since 19 August 20025
  * @version 1.3.0
  */
-public record BusinessBotRights(
+public class BusinessBotRights {
+    /**
+     * Optional. True, if the bot can send and edit messages in the  chats that had incoming messages in the last 24 hours
+     */
     @SerializedName("can_reply")
-     Boolean canReply,
+    private Boolean canReply;
 
+    /**
+     * Optional. True, if the bot can mark incoming  messages as read
+     */
     @SerializedName("can_read_messages")
-     Boolean canReadMessages,
+    private Boolean canReadMessages;
 
+    /**
+     * Optional. True, if the bot can delete messages sent by the bot
+     */
     @SerializedName("can_delete_sent_messages")
-     Boolean canDeleteSentMessages,
+    private Boolean canDeleteSentMessages;
 
+    /**
+     * Optional. True, if the bot can delete all  messages in managed chats
+     */
     @SerializedName("can_delete_all_messages")
-     Boolean canDeleteAllMessages,
+    private Boolean canDeleteAllMessages;
 
+    /**
+     * Optional. True, if the bot can edit the first and last name of the business account
+     */
     @SerializedName("can_edit_name")
-     Boolean canEditName,
+    private Boolean canEditName;
 
+    /**
+     * Optional. True, if the bot can edit the bio of the business account
+     */
     @SerializedName("can_edit_bio")
-     Boolean canEditBio,
+    private Boolean canEditBio;
 
+    /**
+     * True, if the bot can edit the profile photo of the business account
+     */
     @SerializedName("can_edit_profile_photo")
-     Boolean canEditProfilePhoto,
+    private Boolean canEditProfilePhoto;
 
+    /**
+     * True, if the bot can edit the username of the business account
+     */
     @SerializedName("can_edit_username")
-     Boolean canEditUsername,
+    private Boolean canEditUsername;
 
+    /**
+     * True, if the bot can change the privacy settings pertaining to gifts for the business account
+     */
     @SerializedName("can_change_gift_settings")
-     Boolean canChangeGiftSettings,
+    private Boolean canChangeGiftSettings;
 
+    /**
+     * True, if the bot can view gifts and the amount of Telegram Stars owned by the business account
+     */
     @SerializedName("can_view_gifts_and_stars")
-     Boolean canViewGiftsAndStars,
+    private Boolean canViewGiftsAndStars;
 
+    /**
+     * True, if the bot can convert regular gifts owned by the business account to Telegram Stars
+     */
     @SerializedName("can_convert_gifts_to_stars")
-     Boolean canConvertGiftsToStars,
+    private Boolean canConvertGiftsToStars;
 
+    /**
+     * True, if the bot can transfer and upgrade gifts owned by the business account
+     */
     @SerializedName("can_transfer_and_upgrade_gifts")
-     Boolean canTransferAndUpgradeGifts,
+    private Boolean canTransferAndUpgradeGifts;
 
+    /**
+     * True, if the bot can transfer Telegram Stars received by the business account to its own account, or use them to upgrade and transfer gifts
+     */
     @SerializedName("can_transfer_stars")
-     Boolean canTransferStars,
+    private Boolean canTransferStars;
 
+    /**
+     * True, if the bot can post, edit and delete stories on behalf of the business account
+     */
     @SerializedName("can_manage_stories")
-     Boolean canManageStories
-    ){ }
+    private Boolean canManageStories;
+
+    public Boolean getCanReply() {
+        return canReply;
+    }
+
+    public Boolean getCanReadMessages() {
+        return canReadMessages;
+    }
+
+    public Boolean getCanDeleteSentMessages() {
+        return canDeleteSentMessages;
+    }
+
+    public Boolean getCanDeleteAllMessages() {
+        return canDeleteAllMessages;
+    }
+
+    public Boolean getCanEditName() {
+        return canEditName;
+    }
+
+    public Boolean getCanEditBio() {
+        return canEditBio;
+    }
+
+    public Boolean getCanEditProfilePhoto() {
+        return canEditProfilePhoto;
+    }
+
+    public Boolean getCanEditUsername() {
+        return canEditUsername;
+    }
+
+    public Boolean getCanChangeGiftSettings() {
+        return canChangeGiftSettings;
+    }
+
+    public Boolean getCanViewGiftsAndStars() {
+        return canViewGiftsAndStars;
+    }
+
+    public Boolean getCanConvertGiftsToStars() {
+        return canConvertGiftsToStars;
+    }
+
+    public Boolean getCanTransferAndUpgradeGifts() {
+        return canTransferAndUpgradeGifts;
+    }
+
+    public Boolean getCanTransferStars() {
+        return canTransferStars;
+    }
+
+    public Boolean getCanManageStories() {
+        return canManageStories;
+    }
+}
