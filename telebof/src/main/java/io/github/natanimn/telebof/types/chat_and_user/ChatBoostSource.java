9fb35ce84b2e319d5c923adfa8c6ce344d4f5540
natanimn <natanimco@gmail.com>
2025-11-23 18:03:57 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoostSource.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatBoostSource.java
@@ -4,27 +4,57 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes the source of a chat boost.
- * @param source Source of the boost, one of “premium”, "gift_code", "giveaway"
- * @param giveawayMessageId Identifier of a message in the chat with the giveaway; the message could have been deleted already.
- *                          May be 0 if the message isn't sent yet.
- * @param prizeStarCount The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
- * @param user User
- * @param isUnclaimed True, if the giveaway was completed, but there was no user to win the prize
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ChatBoostSource(
-    String source,
+public class ChatBoostSource {
+    /**
+     * Source of the boost, one of “premium”, "gift_code", "giveaway"
+     */
+    private String source;
 
+    /**
+     * Identifier of a message in the chat with the giveaway; the message could have been deleted already.
+     * May be 0 if the message isn't sent yet.
+     */
     @SerializedName("giveaway_message_id")
-    Integer giveawayMessageId,
+    private Integer giveawayMessageId;
 
+    /**
+     * The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
+     */
     @SerializedName("prize_star_count")
-    Integer prizeStarCount,
+    private Integer prizeStarCount;
 
-    User user,
+    /**
+     * User
+     */
+    private User user;
 
+    /**
+     * True, if the giveaway was completed, but there was no user to win the prize
+     */
     @SerializedName("is_unclaimed")
-    Boolean isUnclaimed
-){ }
+    private Boolean isUnclaimed;
+
+    public String getSource() {
+        return source;
+    }
+
+    public Integer getGiveawayMessageId() {
+        return giveawayMessageId;
+    }
+
+    public Integer getPrizeStarCount() {
+        return prizeStarCount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Boolean getIsUnclaimed() {
+        return isUnclaimed;
+    }
+}
