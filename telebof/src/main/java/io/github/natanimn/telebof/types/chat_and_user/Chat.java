63008932df52da2fab71e4385d1c0a695b9758ee
natanimn <natanimco@gmail.com>
2025-11-23 18:02:23 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/Chat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/Chat.java
@@ -5,36 +5,84 @@ import io.github.natanimn.telebof.enums.ChatType;
 
 /**
  * Represents a chat.
- * @param id Unique identifier for this chat.
- * @param type Type of the chat, can be either {@link ChatType#PRIVATE}, {@link ChatType#GROUP}, {@link ChatType#SUPERGROUP} or {@link ChatType#CHANNEL}
- * @param title Title, for supergroups, channels and group chats
- * @param username Username, for private chats, supergroups and channels if available
- * @param firstName First name of the other party in a private chat
- * @param lastName Last name of the other party in a private chat
- * @param isForum True, if the supergroup chat is a forum
- * @param isDirectMessages True, if the chat is the direct messages chat of a channel
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Chat(
-     long id,
+public class Chat {
+    /**
+     * Unique identifier for this chat.
+     */
+    private long id;
 
-     ChatType type,
+    /**
+     * Type of the chat, can be either {@link ChatType#PRIVATE}, {@link ChatType#GROUP}, {@link ChatType#SUPERGROUP} or {@link ChatType#CHANNEL}
+     */
+    private ChatType type;
 
-     String title,
+    /**
+     * Title, for supergroups, channels and group chats
+     */
+    private String title;
 
-     String username,
+    /**
+     * Username, for private chats, supergroups and channels if available
+     */
+    private String username;
 
-     @SerializedName("first_name")
-     String firstName,
+    /**
+     * First name of the other party in a private chat
+     */
+    @SerializedName("first_name")
+    private String firstName;
 
-     @SerializedName("last_name")
-     String lastName,
+    /**
+     * Last name of the other party in a private chat
+     */
+    @SerializedName("last_name")
+    private String lastName;
 
-     @SerializedName("is_forum")
-     Boolean isForum,
+    /**
+     * True, if the supergroup chat is a forum
+     */
+    @SerializedName("is_forum")
+    private Boolean isForum;
 
-     @SerializedName("is_direct_messages")
-     Boolean isDirectMessages
-){}
+    /**
+     * True, if the chat is the direct messages chat of a channel
+     */
+    @SerializedName("is_direct_messages")
+    private Boolean isDirectMessages;
+
+    public long getId() {
+        return id;
+    }
+
+    public ChatType getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Boolean getIsForum() {
+        return isForum;
+    }
+
+    public Boolean getIsDirectMessages() {
+        return isDirectMessages;
+    }
+}
