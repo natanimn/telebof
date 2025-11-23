d408f39ef8c0ae5bea29648b0a4156f3bf80a7a1
natanimn <natanimco@gmail.com>
2025-11-23 18:01:37 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHours.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/business/BusinessOpeningHours.java
@@ -1,22 +1,32 @@
 package io.github.natanimn.telebof.types.business;
 
 import com.google.gson.annotations.SerializedName;
-
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Describes the opening hours of a business.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
- * @param timeZoneName Unique name of the time zone for which the opening hours are defined
- * @param openingHours List of time intervals describing business opening hours
  */
-public record BusinessOpeningHours(
-     @SerializedName("time_zone_name")
-     String timeZoneName,
+public class BusinessOpeningHours {
+    /**
+     * Unique name of the time zone for which the opening hours are defined
+     */
+    @SerializedName("time_zone_name")
+    private String timeZoneName;
 
-     @SerializedName("opening_hours")
-     List<BusinessOpeningHoursInterval> openingHours
-){ }
+    /**
+     * List of time intervals describing business opening hours
+     */
+    @SerializedName("opening_hours")
+    private List<BusinessOpeningHoursInterval> openingHours;
+
+    public String getTimeZoneName() {
+        return timeZoneName;
+    }
+
+    public List<BusinessOpeningHoursInterval> getOpeningHours() {
+        return openingHours;
+    }
+}
