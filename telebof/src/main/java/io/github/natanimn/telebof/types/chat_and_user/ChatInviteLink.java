666fe11bc01ee5605bb1b56e0839f85dc76bf334
natanimn <natanimco@gmail.com>
2025-11-23 18:04:33 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatInviteLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatInviteLink.java
@@ -4,31 +4,116 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents an invite link for a chat.
- * @param inviteLink The invite link. If the link was created by another chat administrator, then the second part of the link will be replaced with "...".
- * @param name Optional. Invite link name
- * @param creator Creator of the link
- * @param expireDate Optional. Point in time (Unix timestamp) when the link will expire or has been expired
- * @param memberLimit Optional. The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
- * @param pendingJoinRequestCount Optional. Number of pending join requests created using this link
- * @param subscriptionPeriod Optional. The number of seconds the subscription will be active for before the next payment
- * @param subscriptionPrice Optional. The amount of Telegram Stars a user must pay initially and after each subsequent subscription period to be a member of the chat using the link
- * @param isPrimary True, if the link is primary
- * @param isRevoked True, if the link is revoked
- * @param createsJoinRequest True, if users joining the chat via the link need to be approved by chat administrators
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ChatInviteLink(
-        @SerializedName("invite_link") String inviteLink,
-        String name,
-        User creator,
-        @SerializedName("expire_date") Integer expireDate,
-        @SerializedName("member_limit") Integer memberLimit,
-        @SerializedName("pending_join_request_count") Integer pendingJoinRequestCount,
-        @SerializedName("subscription_period") Integer subscriptionPeriod,
-        @SerializedName("subscription_price") Integer subscriptionPrice,
-        @SerializedName("is_primary") Boolean isPrimary,
-        @SerializedName("is_revoked") Boolean isRevoked,
-        @SerializedName("creates_join_request") Boolean createsJoinRequest
-) {}
+public class ChatInviteLink {
+    /**
+     * The invite link. If the link was created by another chat administrator, then the second part of the link will be replaced with "...".
+     */
+    @SerializedName("invite_link")
+    private String inviteLink;
+
+    /**
+     * Optional. Invite link name
+     */
+    private String name;
+
+    /**
+     * Creator of the link
+     */
+    private User creator;
+
+    /**
+     * Optional. Point in time (Unix timestamp) when the link will expire or has been expired
+     */
+    @SerializedName("expire_date")
+    private Integer expireDate;
+
+    /**
+     * Optional. The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
+     */
+    @SerializedName("member_limit")
+    private Integer memberLimit;
+
+    /**
+     * Optional. Number of pending join requests created using this link
+     */
+    @SerializedName("pending_join_request_count")
+    private Integer pendingJoinRequestCount;
+
+    /**
+     * Optional. The number of seconds the subscription will be active for before the next payment
+     */
+    @SerializedName("subscription_period")
+    private Integer subscriptionPeriod;
+
+    /**
+     * Optional. The amount of Telegram Stars a user must pay initially and after each subsequent subscription period to be a member of the chat using the link
+     */
+    @SerializedName("subscription_price")
+    private Integer subscriptionPrice;
+
+    /**
+     * True, if the link is primary
+     */
+    @SerializedName("is_primary")
+    private Boolean isPrimary;
+
+    /**
+     * True, if the link is revoked
+     */
+    @SerializedName("is_revoked")
+    private Boolean isRevoked;
+
+    /**
+     * True, if users joining the chat via the link need to be approved by chat administrators
+     */
+    @SerializedName("creates_join_request")
+    private Boolean createsJoinRequest;
+
+    public String getInviteLink() {
+        return inviteLink;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public Integer getExpireDate() {
+        return expireDate;
+    }
+
+    public Integer getMemberLimit() {
+        return memberLimit;
+    }
+
+    public Integer getPendingJoinRequestCount() {
+        return pendingJoinRequestCount;
+    }
+
+    public Integer getSubscriptionPeriod() {
+        return subscriptionPeriod;
+    }
+
+    public Integer getSubscriptionPrice() {
+        return subscriptionPrice;
+    }
+
+    public Boolean getIsPrimary() {
+        return isPrimary;
+    }
+
+    public Boolean getIsRevoked() {
+        return isRevoked;
+    }
+
+    public Boolean getCreatesJoinRequest() {
+        return createsJoinRequest;
+    }
+}
