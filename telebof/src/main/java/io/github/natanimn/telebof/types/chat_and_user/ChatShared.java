721f2eb6325b76cbeeff4ac97cc577df1bb42f13
natanimn <natanimco@gmail.com>
2025-11-24 09:38:25 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatShared.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/ChatShared.java
@@ -3,23 +3,58 @@ package io.github.natanimn.telebof.types.chat_and_user;
 import io.github.natanimn.telebof.types.media_and_service.PhotoSize;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * This object contains information about a chat that was shared with the bot using a {@link io.github.natanimn.telebof.types.keyboard.KeyboardButtonRequestChat} button.
- * @param requestId Identifier of the request
- * @param chatId Identifier of the shared chat.
- * @param title Optional. Title of the chat, if the title was requested by the bot.
- * @param username Optional. Username of the chat, if the username was requested by the bot and available.
- * @param photo Optional. Available sizes of the chat photo, if the photo was requested by the bot
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ChatShared(
-        @SerializedName("request_id") Integer requestId,
-        @SerializedName("chat_id") Long chatId,
-        String title,
-        String username,
-        List<PhotoSize> photo
-) { }
+public class ChatShared {
+    /**
+     * Identifier of the request
+     */
+    @SerializedName("request_id")
+    private Integer requestId;
+
+    /**
+     * Identifier of the shared chat.
+     */
+    @SerializedName("chat_id")
+    private Long chatId;
+
+    /**
+     * Optional. Title of the chat, if the title was requested by the bot.
+     */
+    private String title;
+
+    /**
+     * Optional. Username of the chat, if the username was requested by the bot and available.
+     */
+    private String username;
+
+    /**
+     * Optional. Available sizes of the chat photo, if the photo was requested by the bot
+     */
+    private List<PhotoSize> photo;
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<PhotoSize> getPhoto() {
+        return photo;
+    }
+}
