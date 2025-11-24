dbde5eea5dc0da4398eb90c78c77dbd034c52225
natanimn <natanimco@gmail.com>
2025-11-24 09:37:02 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Animation.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Animation.java
@@ -4,27 +4,94 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents an animation file (GIF or H.264/MPEG-4 AVC video without sound).
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param fileName Optional. Original animation filename as defined by the sender
- * @param mimeType Optional. MIME type of the file as defined by the sender
- * @param width Video width as defined by the sender
- * @param height Video height as defined by the sender
- * @param duration Duration of the video in seconds as defined by the sender
- * @param fileSize Optional. File size in bytes.
- * @param thumbnail Optional. Animation thumbnail as defined by the sender
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Animation(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        @SerializedName("file_name") String fileName,
-        @SerializedName("mime_type") String mimeType,
-        Integer width,
-        Integer height,
-        Integer duration,
-        @SerializedName("file_size") Long fileSize,
-        PhotoSize thumbnail
-) {}
+public class Animation {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Optional. Original animation filename as defined by the sender
+     */
+    @SerializedName("file_name")
+    private String fileName;
+
+    /**
+     * Optional. MIME type of the file as defined by the sender
+     */
+    @SerializedName("mime_type")
+    private String mimeType;
+
+    /**
+     * Video width as defined by the sender
+     */
+    private Integer width;
+
+    /**
+     * Video height as defined by the sender
+     */
+    private Integer height;
+
+    /**
+     * Duration of the video in seconds as defined by the sender
+     */
+    private Integer duration;
+
+    /**
+     * Optional. File size in bytes.
+     */
+    @SerializedName("file_size")
+    private Long fileSize;
+
+    /**
+     * Optional. Animation thumbnail as defined by the sender
+     */
+    private PhotoSize thumbnail;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public PhotoSize getThumbnail() {
+        return thumbnail;
+    }
+}
