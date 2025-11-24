9e8c98340c80bd9d7dffb82ce20773bcd1af770e
natanimn <natanimco@gmail.com>
2025-11-24 09:37:28 +0300

    Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Birthdate.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Birthdate.java
@@ -2,15 +2,35 @@ package io.github.natanimn.telebof.types.media_and_service;
 
 /**
  * Describes the birthdate of a user.
- * @param day Day of the user's birth; 1-31
- * @param month Month of the user's birth; 1-12
- * @param year Optional. Year of the user's birth
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Birthdate(
-        Byte day,
-        Byte month,
-        Integer year
-) {}
+public class Birthdate {
+    /**
+     * Day of the user's birth; 1-31
+     */
+    private Byte day;
+
+    /**
+     * Month of the user's birth; 1-12
+     */
+    private Byte month;
+
+    /**
+     * Optional. Year of the user's birth
+     */
+    private Integer year;
+
+    public Byte getDay() {
+        return day;
+    }
+
+    public Byte getMonth() {
+        return month;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+}
