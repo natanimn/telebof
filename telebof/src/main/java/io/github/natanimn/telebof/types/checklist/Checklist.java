b51a7ed9fd750ae738626cc9466d2a07362f1f47
natanimn <natanimco@gmail.com>
2025-11-24 09:38:38 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/checklist/Checklist.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/checklist/Checklist.java
@@ -6,19 +6,56 @@ import java.util.List;
 
 /**
  * This object represents a checklist.
- * @param title Title of the checklist
- * @param titleEntities Optional. Special entities that appear in the checklist title
- * @param tasks List of tasks in the checklist
- * @param othersCanAddTasks Optional. True, if users other than the creator of the list can add tasks to the list
- * @param othersCanMarkTasksAsDone Optional. True, if users other than the creator of the list can mark tasks as done or not done
  * @author Natanim
  * @since 1 September 2025
  * @version 1.3.0
  */
-public record Checklist(
-        String title,
-        @SerializedName("title_entities") List<MessageEntity> titleEntities,
-        List<ChecklistTask> tasks,
-        @SerializedName("others_can_add_tasks") Boolean othersCanAddTasks,
-        @SerializedName("others_can_mark_tasks_as_done") Boolean othersCanMarkTasksAsDone
-) {}
+public class Checklist {
+    /**
+     * Title of the checklist
+     */
+    private String title;
+
+    /**
+     * Optional. Special entities that appear in the checklist title
+     */
+    @SerializedName("title_entities")
+    private List<MessageEntity> titleEntities;
+
+    /**
+     * List of tasks in the checklist
+     */
+    private List<ChecklistTask> tasks;
+
+    /**
+     * Optional. True, if users other than the creator of the list can add tasks to the list
+     */
+    @SerializedName("others_can_add_tasks")
+    private Boolean othersCanAddTasks;
+
+    /**
+     * Optional. True, if users other than the creator of the list can mark tasks as done or not done
+     */
+    @SerializedName("others_can_mark_tasks_as_done")
+    private Boolean othersCanMarkTasksAsDone;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<MessageEntity> getTitleEntities() {
+        return titleEntities;
+    }
+
+    public List<ChecklistTask> getTasks() {
+        return tasks;
+    }
+
+    public Boolean getOthersCanAddTasks() {
+        return othersCanAddTasks;
+    }
+
+    public Boolean getOthersCanMarkTasksAsDone() {
+        return othersCanMarkTasksAsDone;
+    }
+}
