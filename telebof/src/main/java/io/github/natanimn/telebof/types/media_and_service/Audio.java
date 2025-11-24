6a1287ed23d9f0f5d1f9eda16ace8454d340f740
natanimn <natanimco@gmail.com>
2025-11-24 09:37:17 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Audio.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Audio.java
@@ -4,27 +4,94 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents an audio file to be treated as music by the Telegram clients.
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param performer Optional. Performer of the audio as defined by the sender or by audio tags
- * @param title Optional. Title of the audio as defined by the sender or by audio tags
- * @param fileName Optional. Original filename as defined by the sender
- * @param mimeType Optional. MIME type of the file as defined by the sender
- * @param duration Duration of the audio in seconds as defined by the sender
- * @param fileSize Optional. File size in bytes.
- * @param thumbnail Optional. Thumbnail of the album cover to which the music file belongs
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Audio(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        String performer,
-        String title,
-        @SerializedName("file_name") String fileName,
-        @SerializedName("mime_type") String mimeType,
-        Integer duration,
-        @SerializedName("file_size") Long fileSize,
-        PhotoSize thumbnail
-) {}
+public class Audio {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Optional. Performer of the audio as defined by the sender or by audio tags
+     */
+    private String performer;
+
+    /**
+     * Optional. Title of the audio as defined by the sender or by audio tags
+     */
+    private String title;
+
+    /**
+     * Optional. Original filename as defined by the sender
+     */
+    @SerializedName("file_name")
+    private String fileName;
+
+    /**
+     * Optional. MIME type of the file as defined by the sender
+     */
+    @SerializedName("mime_type")
+    private String mimeType;
+
+    /**
+     * Duration of the audio in seconds as defined by the sender
+     */
+    private Integer duration;
+
+    /**
+     * Optional. File size in bytes.
+     */
+    @SerializedName("file_size")
+    private Long fileSize;
+
+    /**
+     * Optional. Thumbnail of the album cover to which the music file belongs
+     */
+    private PhotoSize thumbnail;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public String getPerformer() {
+        return performer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public PhotoSize getThumbnail() {
+        return thumbnail;
+    }
+}
