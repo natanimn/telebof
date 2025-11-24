ca142223ce4a80ed0b1eb2f517be6a71a1cf0ead
natanimn <natanimco@gmail.com>
2025-11-24 09:37:10 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/BackgroundFill.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/BackgroundFill.java
@@ -7,22 +7,65 @@ import java.util.List;
 
 /**
  * This object describes the way a background is filled based on the selected colors
- *
- * @param type Type of the background fill
- * @param color The color of the background fill in the RGB24 format
- * @param topColor Top color of the gradient in the RGB24 format
- * @param bottomColor Bottom color of the gradient in the RGB24 format
- * @param rotationAngle Clockwise rotation angle of the background fill in degrees; 0-359
- * @param colors A list of the 3 or 4 base colors that are used to generate the freeform gradient in the RGB24 format
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record BackgroundFill(
-        BackgroundFillType type,
-        Integer color,
-        @SerializedName("top_color") Integer topColor,
-        @SerializedName("bottom_color") Integer bottomColor,
-        @SerializedName("rotation_angle") Integer rotationAngle,
-        List<Integer> colors
-) {}
+public class BackgroundFill {
+    /**
+     * Type of the background fill
+     */
+    private BackgroundFillType type;
+
+    /**
+     * The color of the background fill in the RGB24 format
+     */
+    private Integer color;
+
+    /**
+     * Top color of the gradient in the RGB24 format
+     */
+    @SerializedName("top_color")
+    private Integer topColor;
+
+    /**
+     * Bottom color of the gradient in the RGB24 format
+     */
+    @SerializedName("bottom_color")
+    private Integer bottomColor;
+
+    /**
+     * Clockwise rotation angle of the background fill in degrees; 0-359
+     */
+    @SerializedName("rotation_angle")
+    private Integer rotationAngle;
+
+    /**
+     * A list of the 3 or 4 base colors that are used to generate the freeform gradient in the RGB24 format
+     */
+    private List<Integer> colors;
+
+    public BackgroundFillType getType() {
+        return type;
+    }
+
+    public Integer getColor() {
+        return color;
+    }
+
+    public Integer getTopColor() {
+        return topColor;
+    }
+
+    public Integer getBottomColor() {
+        return bottomColor;
+    }
+
+    public Integer getRotationAngle() {
+        return rotationAngle;
+    }
+
+    public List<Integer> getColors() {
+        return colors;
+    }
+}
