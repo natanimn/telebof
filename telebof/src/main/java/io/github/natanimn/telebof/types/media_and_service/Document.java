0dffe58b85a65dd68da0f456fabad680cc782f81
natanimn <natanimco@gmail.com>
2025-11-26 00:02:49 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Document.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Document.java
@@ -4,21 +4,67 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a general file (as opposed to {@link PhotoSize}, {@link Voice} and {@link Audio}).
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param fileName Optional. Original filename as defined by the sender
- * @param mimeType Optional. MIME type of the file as defined by the sender
- * @param fileSize Optional. File size in bytes.
- * @param thumbnail Optional. Document thumbnail as defined by the sender
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Document(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        @SerializedName("file_name") String fileName,
-        @SerializedName("mime_type") String mimeType,
-        @SerializedName("file_size") Long fileSize,
-        PhotoSize thumbnail
-) {}
+public class Document {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Optional. Original filename as defined by the sender
+     */
+    @SerializedName("file_name")
+    private String fileName;
+
+    /**
+     * Optional. MIME type of the file as defined by the sender
+     */
+    @SerializedName("mime_type")
+    private String mimeType;
+
+    /**
+     * Optional. File size in bytes.
+     */
+    @SerializedName("file_size")
+    private Long fileSize;
+
+    /**
+     * Optional. Document thumbnail as defined by the sender
+     */
+    private PhotoSize thumbnail;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public PhotoSize getThumbnail() {
+        return thumbnail;
+    }
+}
