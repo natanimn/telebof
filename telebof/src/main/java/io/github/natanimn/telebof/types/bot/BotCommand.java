c8acbcf807996002b523ff6111a665f5b98a7281
natanimn <natanimco@gmail.com>
2025-11-26 00:02:02 +0300

    Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotCommand.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotCommand.java
@@ -4,15 +4,33 @@ import java.io.Serializable;
 
 /**
  * This object represents a bot command.
- *
- * @param command Text of the command; 1-32 characters. Can contain only lowercase English letters, digits and underscores.
- * @param description  Description of the command; 1-256 characters.
- *
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record BotCommand(
-        String command,
-        String description
-) implements Serializable {}
+public class BotCommand implements Serializable {
+    /**
+     * Text of the command; 1-32 characters. Can contain only lowercase English letters, digits and underscores.
+     */
+    private String command;
+
+    /**
+     * Description of the command; 1-256 characters.
+     */
+    private String description;
+
+    public BotCommand(String command, String description) {
+        this.command = command;
+        this.description = description;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+}
