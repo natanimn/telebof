439e097a31266160774885c4f60b563626ac1476
natanimn <natanimco@gmail.com>
2025-11-26 00:02:33 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTasksDone.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/checklist/ChecklistTasksDone.java
@@ -6,15 +6,38 @@ import java.util.List;
 
 /**
  * This object represents a service message about checklist tasks marked as done or not done.
- * @param checklistMessage Optional. Message containing the checklist whose tasks were marked as done or not done. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
- * @param markedAsDoneTaskIds Optional. Identifiers of the tasks that were marked as done
- * @param markedAsNotDoneTaskIds Optional. Identifiers of the tasks that were marked as not done
  * @author Natanim
  * @since 1 September 2025
  * @version 1.3.0
  */
-public record ChecklistTasksDone(
-        @SerializedName("checklist_message") Message checklistMessage,
-        @SerializedName("marked_as_done_task_ids") List<Integer> markedAsDoneTaskIds,
-        @SerializedName("marked_as_not_done_task_ids") List<Integer> markedAsNotDoneTaskIds
-) {}
+public class ChecklistTasksDone {
+    /**
+     * Optional. Message containing the checklist whose tasks were marked as done or not done. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+     */
+    @SerializedName("checklist_message")
+    private Message checklistMessage;
+
+    /**
+     * Optional. Identifiers of the tasks that were marked as done
+     */
+    @SerializedName("marked_as_done_task_ids")
+    private List<Integer> markedAsDoneTaskIds;
+
+    /**
+     * Optional. Identifiers of the tasks that were marked as not done
+     */
+    @SerializedName("marked_as_not_done_task_ids")
+    private List<Integer> markedAsNotDoneTaskIds;
+
+    public Message getChecklistMessage() {
+        return checklistMessage;
+    }
+
+    public List<Integer> getMarkedAsDoneTaskIds() {
+        return markedAsDoneTaskIds;
+    }
+
+    public List<Integer> getMarkedAsNotDoneTaskIds() {
+        return markedAsNotDoneTaskIds;
+    }
+}
