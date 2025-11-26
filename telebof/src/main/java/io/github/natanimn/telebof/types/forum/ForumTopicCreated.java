2469c936f14b995bdfdc0f698fc2001d193f7ef7
natanimn <natanimco@gmail.com>
2025-11-26 18:13:29 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/forum/ForumTopicCreated.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/forum/ForumTopicCreated.java
@@ -4,15 +4,37 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about a new forum topic created in the chat.
- * @param name Name of the topic
- * @param iconCustomEmojiId Color of the topic icon in RGB format
- * @param iconColor Optional. Unique identifier of the custom emoji shown as the topic icon
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ForumTopicCreated(
-        String name,
-        @SerializedName("icon_custom_emoji_id") String iconCustomEmojiId,
-        @SerializedName("icon_color") Integer iconColor
-) {}
+public class ForumTopicCreated {
+    /**
+     * Name of the topic
+     */
+    private String name;
+
+    /**
+     * Color of the topic icon in RGB format
+     */
+    @SerializedName("icon_custom_emoji_id")
+    private String iconCustomEmojiId;
+
+    /**
+     * Optional. Unique identifier of the custom emoji shown as the topic icon
+     */
+    @SerializedName("icon_color")
+    private Integer iconColor;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIconCustomEmojiId() {
+        return iconCustomEmojiId;
+    }
+
+    public Integer getIconColor() {
+        return iconColor;
+    }
+}
