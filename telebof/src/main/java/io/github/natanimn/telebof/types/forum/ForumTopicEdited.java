bbf1ad183bc2b2b747258d9d4844df83d63a6e69
natanimn <natanimco@gmail.com>
2025-11-26 18:13:37 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/forum/ForumTopicEdited.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/forum/ForumTopicEdited.java
@@ -4,13 +4,27 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about an edited forum topic.
- * @param name Optional. New name of the topic, if it was edited
- * @param iconCustomEmojiId Optional. New identifier of the custom emoji shown as the topic icon, if it was edited; an empty string if the icon was removed
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ForumTopicEdited(
-        String name,
-        @SerializedName("icon_custom_emoji_id") String iconCustomEmojiId
-) {}
+public class ForumTopicEdited {
+    /**
+     * Optional. New name of the topic, if it was edited
+     */
+    private String name;
+
+    /**
+     * Optional. New identifier of the custom emoji shown as the topic icon, if it was edited; an empty string if the icon was removed
+     */
+    @SerializedName("icon_custom_emoji_id")
+    private String iconCustomEmojiId;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIconCustomEmojiId() {
+        return iconCustomEmojiId;
+    }
+}
