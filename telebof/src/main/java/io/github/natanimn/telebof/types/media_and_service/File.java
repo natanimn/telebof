95e56505d54e76f697fabcb22e1e7c5b07f454e4
natanimn <natanimco@gmail.com>
2025-11-26 18:13:12 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/File.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/File.java
@@ -9,17 +9,48 @@ import com.google.gson.annotations.SerializedName;
  * It is guaranteed that the link will be valid for at least 1 hour. When the link expires, a new one can be requested by calling {@link BotContext#getFile(String)}.
  * <br><br>
  * <i>The maximum file size to download is 20 MB</i>
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param filePath Optional. File path. Use {@link BotContext#downloadFile} to get the file.
- * @param fileSize Optional. File size in bytes.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record File(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        @SerializedName("file_path") String filePath,
-        @SerializedName("file_size") Long fileSize
-) {}
+public class File {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Optional. File path. Use {@link BotContext#downloadFile} to get the file.
+     */
+    @SerializedName("file_path")
+    private String filePath;
+
+    /**
+     * Optional. File size in bytes.
+     */
+    @SerializedName("file_size")
+    private Long fileSize;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+}
