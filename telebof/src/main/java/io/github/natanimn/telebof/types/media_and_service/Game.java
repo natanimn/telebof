b5d2c40a0ae1a9df8a93b36e409147d3f07b87a5
natanimn <natanimco@gmail.com>
2025-11-26 18:13:47 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Game.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Game.java
@@ -6,21 +6,63 @@ import java.util.List;
 
 /**
  * This object represents a game. Use BotFather to create and edit games, their short names will act as unique identifiers.
- * @param title Title of the game
- * @param description Description of the game
- * @param photo Photo that will be displayed in the game message in chats.
- * @param text Optional. Brief description of the game or high scores included in the game message. Can be automatically edited to include current high scores for the game when the bot calls {@link BotContext#setGameScore}, or manually edited using {@link BotContext#editMessageText}. 0-4096 characters.
- * @param textEntities Optional. Special entities that appear in text, such as usernames, URLs, bot commands, etc.
- * @param animation Optional. Animation that will be displayed in the game message in chats. Upload via @BotFather
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Game(
-        String title,
-        String description,
-        List<PhotoSize> photo,
-        String text,
-        @SerializedName("text_entities") List<MessageEntity> textEntities,
-        Animation animation
-) {}
+public class Game {
+    /**
+     * Title of the game
+     */
+    private String title;
+
+    /**
+     * Description of the game
+     */
+    private String description;
+
+    /**
+     * Photo that will be displayed in the game message in chats.
+     */
+    private List<PhotoSize> photo;
+
+    /**
+     * Optional. Brief description of the game or high scores included in the game message. Can be automatically edited to include current high scores for the game when the bot calls {@link BotContext#setGameScore}, or manually edited using {@link BotContext#editMessageText}. 0-4096 characters.
+     */
+    private String text;
+
+    /**
+     * Optional. Special entities that appear in text, such as usernames, URLs, bot commands, etc.
+     */
+    @SerializedName("text_entities")
+    private List<MessageEntity> textEntities;
+
+    /**
+     * Optional. Animation that will be displayed in the game message in chats. Upload via @BotFather
+     */
+    private Animation animation;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<PhotoSize> getPhoto() {
+        return photo;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public List<MessageEntity> getTextEntities() {
+        return textEntities;
+    }
+
+    public Animation getAnimation() {
+        return animation;
+    }
+}
