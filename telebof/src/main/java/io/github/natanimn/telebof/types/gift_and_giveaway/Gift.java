8748244e0ccfc5b58b375ff3d4c3a78e8311e438
natanimn <natanimco@gmail.com>
2025-11-26 18:13:58 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/Gift.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/Gift.java
@@ -6,23 +6,76 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This class represents a gift that can be sent by the bot.
- * @param id Unique identifier of the gift
- * @param sticker The sticker that represents the gift
- * @param starCount The number of Telegram Stars that must be paid to send the sticker
- * @param upgradeStarCount The number of Telegram Stars that must be paid to upgrade the gift to a unique one
- * @param totalCount The total number of the gifts of this type that can be sent; for limited gifts only
- * @param remainingCount The number of remaining gifts of this type that can be sent; for limited gifts only
- * @param publisherChat Optional. Information about the chat that published the gift
  * @author Natnim
  * @since July 25, 2025
- * @version 1.2.4
+ * @version 1.3.0
  */
-public record Gift(
-        String id,
-        Sticker sticker,
-        @SerializedName("star_count") Integer starCount,
-        @SerializedName("upgrade_star_count") Integer upgradeStarCount,
-        @SerializedName("total_count") Integer totalCount,
-        @SerializedName("remaining_count") Integer remainingCount,
-        @SerializedName("publisher_chat") Chat publisherChat
-) {}
+public class Gift {
+    /**
+     * Unique identifier of the gift
+     */
+    private String id;
+
+    /**
+     * The sticker that represents the gift
+     */
+    private Sticker sticker;
+
+    /**
+     * The number of Telegram Stars that must be paid to send the sticker
+     */
+    @SerializedName("star_count")
+    private Integer starCount;
+
+    /**
+     * The number of Telegram Stars that must be paid to upgrade the gift to a unique one
+     */
+    @SerializedName("upgrade_star_count")
+    private Integer upgradeStarCount;
+
+    /**
+     * The total number of the gifts of this type that can be sent; for limited gifts only
+     */
+    @SerializedName("total_count")
+    private Integer totalCount;
+
+    /**
+     * The number of remaining gifts of this type that can be sent; for limited gifts only
+     */
+    @SerializedName("remaining_count")
+    private Integer remainingCount;
+
+    /**
+     * Optional. Information about the chat that published the gift
+     */
+    @SerializedName("publisher_chat")
+    private Chat publisherChat;
+
+    public String getId() {
+        return id;
+    }
+
+    public Sticker getSticker() {
+        return sticker;
+    }
+
+    public Integer getStarCount() {
+        return starCount;
+    }
+
+    public Integer getUpgradeStarCount() {
+        return upgradeStarCount;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public Integer getRemainingCount() {
+        return remainingCount;
+    }
+
+    public Chat getPublisherChat() {
+        return publisherChat;
+    }
+}
