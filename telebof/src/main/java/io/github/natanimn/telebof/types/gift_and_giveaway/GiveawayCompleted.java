0686de55b88d059dababa0293209c934e4f3b339
natanimn <natanimco@gmail.com>
2025-11-29 23:16:41 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCompleted.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCompleted.java
@@ -5,17 +5,48 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the completion of a giveaway without public winners.
- * @param winnerCount Number of winners in the giveaway
- * @param unclaimedPrizeCount Optional. Number of undistributed prizes
- * @param giveawayMessage Optional. Message with the giveaway that was completed, if it wasn't deleted
- * @param isStarGiveaway Optional. True, if the giveaway is a Telegram Star giveaway. Otherwise, currently, the giveaway is a Telegram Premium giveaway.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record GiveawayCompleted(
-        @SerializedName("winner_count") Integer winnerCount,
-        @SerializedName("unclaimed_prize_count") Integer unclaimedPrizeCount,
-        @SerializedName("giveaway_message") Message giveawayMessage,
-        @SerializedName("is_star_giveaway") Boolean isStarGiveaway
-) {}
+public class GiveawayCompleted {
+    /**
+     * Number of winners in the giveaway
+     */
+    @SerializedName("winner_count")
+    private Integer winnerCount;
+
+    /**
+     * Optional. Number of undistributed prizes
+     */
+    @SerializedName("unclaimed_prize_count")
+    private Integer unclaimedPrizeCount;
+
+    /**
+     * Optional. Message with the giveaway that was completed, if it wasn't deleted
+     */
+    @SerializedName("giveaway_message")
+    private Message giveawayMessage;
+
+    /**
+     * Optional. True, if the giveaway is a Telegram Star giveaway. Otherwise, currently, the giveaway is a Telegram Premium giveaway.
+     */
+    @SerializedName("is_star_giveaway")
+    private Boolean isStarGiveaway;
+
+    public Integer getWinnerCount() {
+        return winnerCount;
+    }
+
+    public Integer getUnclaimedPrizeCount() {
+        return unclaimedPrizeCount;
+    }
+
+    public Message getGiveawayMessage() {
+        return giveawayMessage;
+    }
+
+    public Boolean getIsStarGiveaway() {
+        return isStarGiveaway;
+    }
+}
