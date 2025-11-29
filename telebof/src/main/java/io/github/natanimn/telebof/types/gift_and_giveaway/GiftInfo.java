0d9aabaa0fb94596bd4a8ed01b7f2300299f177e
natanimn <natanimco@gmail.com>
2025-11-29 23:16:12 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiftInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiftInfo.java
@@ -6,25 +6,85 @@ import java.util.List;
 
 /**
  * Describes a service message about a regular gift that was sent or received.
- * @param gift Information about the gift
- * @param ownedGiftId Optional. Unique identifier of the received gift for the bot; only present for gifts received on behalf of business accounts
- * @param convertStarCount Optional. Number of Telegram Stars that can be claimed by the receiver by converting the gift; omitted if conversion to Telegram Stars is impossible
- * @param prepaidUpgradeStarCount Optional. Number of Telegram Stars that were prepaid by the sender for the ability to upgrade the gift
- * @param canBeUpgraded Optional. True, if the gift can be upgraded to a unique gift
- * @param text Optional. Text of the message that was added to the gift
- * @param entities Optional. Special entities that appear in the text
- * @param isPrivate Optional. True, if the sender and gift text are shown only to the gift receiver; otherwise, everyone will be able to see them
  * @author Natanim
  * @since 19 August 2025
- * @version 1.2.4
+ * @version 1.3.0
  */
-public record GiftInfo(
-        Gift gift,
-        @SerializedName("owned_gift_id") String ownedGiftId,
-        @SerializedName("convert_star_count") Integer convertStarCount,
-        @SerializedName("prepaid_upgrade_star_count") Integer prepaidUpgradeStarCount,
-        @SerializedName("can_be_upgraded") Boolean canBeUpgraded,
-        String text,
-        List<MessageEntity> entities,
-        @SerializedName("is_private") Boolean isPrivate
-) {}
+public class GiftInfo {
+    /**
+     * Information about the gift
+     */
+    private Gift gift;
+
+    /**
+     * Optional. Unique identifier of the received gift for the bot; only present for gifts received on behalf of business accounts
+     */
+    @SerializedName("owned_gift_id")
+    private String ownedGiftId;
+
+    /**
+     * Optional. Number of Telegram Stars that can be claimed by the receiver by converting the gift; omitted if conversion to Telegram Stars is impossible
+     */
+    @SerializedName("convert_star_count")
+    private Integer convertStarCount;
+
+    /**
+     * Optional. Number of Telegram Stars that were prepaid by the sender for the ability to upgrade the gift
+     */
+    @SerializedName("prepaid_upgrade_star_count")
+    private Integer prepaidUpgradeStarCount;
+
+    /**
+     * Optional. True, if the gift can be upgraded to a unique gift
+     */
+    @SerializedName("can_be_upgraded")
+    private Boolean canBeUpgraded;
+
+    /**
+     * Optional. Text of the message that was added to the gift
+     */
+    private String text;
+
+    /**
+     * Optional. Special entities that appear in the text
+     */
+    private List<MessageEntity> entities;
+
+    /**
+     * Optional. True, if the sender and gift text are shown only to the gift receiver; otherwise, everyone will be able to see them
+     */
+    @SerializedName("is_private")
+    private Boolean isPrivate;
+
+    public Gift getGift() {
+        return gift;
+    }
+
+    public String getOwnedGiftId() {
+        return ownedGiftId;
+    }
+
+    public Integer getConvertStarCount() {
+        return convertStarCount;
+    }
+
+    public Integer getPrepaidUpgradeStarCount() {
+        return prepaidUpgradeStarCount;
+    }
+
+    public Boolean getCanBeUpgraded() {
+        return canBeUpgraded;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public List<MessageEntity> getEntities() {
+        return entities;
+    }
+
+    public Boolean getIsPrivate() {
+        return isPrivate;
+    }
+}
