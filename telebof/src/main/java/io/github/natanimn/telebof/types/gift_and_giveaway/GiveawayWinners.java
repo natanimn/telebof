7c174e7be1fe0c362d9b8c92ddaf7d8f5371210c
natanimn <natanimco@gmail.com>
2025-11-30 16:27:19 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayWinners.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayWinners.java
@@ -7,33 +7,126 @@ import java.util.List;
 
 /**
  * This object represents a message about the completion of a giveaway with public winners.
- * @param chat The chat that created the giveaway
- * @param giveawayMessageId Identifier of the message with the giveaway in the chat
- * @param winnersSelectionDate Point in time (Unix timestamp) when winners of the giveaway were selected
- * @param winnerCount Total number of winners in the giveaway
- * @param winners List of up to 100 winners of the giveaway
- * @param additionalChatCount Optional. The number of other chats the user had to join in order to be eligible for the giveaway
- * @param premiumSubscriptionMonthCount Optional. The number of months the Telegram Premium subscription won from the giveaway will be active for; for Telegram Premium giveaways only
- * @param unclaimedPrizeCount Optional. Number of undistributed prizes
- * @param prizeStarCount Optional. The number of Telegram Stars that were split between giveaway winners; for Telegram Star giveaways only
- * @param onlyNewMembers Optional. True, if only users who had joined the chats after the giveaway started were eligible to win
- * @param wasRefunded Optional. True, if the giveaway was canceled because the payment for it was refunded
- * @param prizeDescription Optional. Description of additional giveaway prize
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record GiveawayWinners(
-        Chat chat,
-        @SerializedName("giveaway_message_id") Integer giveawayMessageId,
-        @SerializedName("winners_selection_date") Integer winnersSelectionDate,
-        @SerializedName("winner_count") Integer winnerCount,
-        List<User> winners,
-        @SerializedName("additional_chat_count") Integer additionalChatCount,
-        @SerializedName("premium_subscription_month_count") Integer premiumSubscriptionMonthCount,
-        @SerializedName("unclaimed_prize_count") Integer unclaimedPrizeCount,
-        @SerializedName("prize_star_count") Integer prizeStarCount,
-        @SerializedName("only_new_members") Boolean onlyNewMembers,
-        @SerializedName("was_refunded") Boolean wasRefunded,
-        @SerializedName("prize_description") String prizeDescription
-) {}
+public class GiveawayWinners {
+    /**
+     * The chat that created the giveaway
+     */
+    private Chat chat;
+
+    /**
+     * Identifier of the message with the giveaway in the chat
+     */
+    @SerializedName("giveaway_message_id")
+    private Integer giveawayMessageId;
+
+    /**
+     * Point in time (Unix timestamp) when winners of the giveaway were selected
+     */
+    @SerializedName("winners_selection_date")
+    private Integer winnersSelectionDate;
+
+    /**
+     * Total number of winners in the giveaway
+     */
+    @SerializedName("winner_count")
+    private Integer winnerCount;
+
+    /**
+     * List of up to 100 winners of the giveaway
+     */
+    private List<User> winners;
+
+    /**
+     * Optional. The number of other chats the user had to join in order to be eligible for the giveaway
+     */
+    @SerializedName("additional_chat_count")
+    private Integer additionalChatCount;
+
+    /**
+     * Optional. The number of months the Telegram Premium subscription won from the giveaway will be active for; for Telegram Premium giveaways only
+     */
+    @SerializedName("premium_subscription_month_count")
+    private Integer premiumSubscriptionMonthCount;
+
+    /**
+     * Optional. Number of undistributed prizes
+     */
+    @SerializedName("unclaimed_prize_count")
+    private Integer unclaimedPrizeCount;
+
+    /**
+     * Optional. The number of Telegram Stars that were split between giveaway winners; for Telegram Star giveaways only
+     */
+    @SerializedName("prize_star_count")
+    private Integer prizeStarCount;
+
+    /**
+     * Optional. True, if only users who had joined the chats after the giveaway started were eligible to win
+     */
+    @SerializedName("only_new_members")
+    private Boolean onlyNewMembers;
+
+    /**
+     * Optional. True, if the giveaway was canceled because the payment for it was refunded
+     */
+    @SerializedName("was_refunded")
+    private Boolean wasRefunded;
+
+    /**
+     * Optional. Description of additional giveaway prize
+     */
+    @SerializedName("prize_description")
+    private String prizeDescription;
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public Integer getGiveawayMessageId() {
+        return giveawayMessageId;
+    }
+
+    public Integer getWinnersSelectionDate() {
+        return winnersSelectionDate;
+    }
+
+    public Integer getWinnerCount() {
+        return winnerCount;
+    }
+
+    public List<User> getWinners() {
+        return winners;
+    }
+
+    public Integer getAdditionalChatCount() {
+        return additionalChatCount;
+    }
+
+    public Integer getPremiumSubscriptionMonthCount() {
+        return premiumSubscriptionMonthCount;
+    }
+
+    public Integer getUnclaimedPrizeCount() {
+        return unclaimedPrizeCount;
+    }
+
+    public Integer getPrizeStarCount() {
+        return prizeStarCount;
+    }
+
+    public Boolean getOnlyNewMembers() {
+        return onlyNewMembers;
+    }
+
+    public Boolean getWasRefunded() {
+        return wasRefunded;
+    }
+
+    public String getPrizeDescription() {
+        return prizeDescription;
+    }
+}
