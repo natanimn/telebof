fdc5ab49a17e4ad089f86ca1305473d16c985272
natanimn <natanimco@gmail.com>
2025-11-30 16:28:07 +0300

    Replaced snake_case fields with camelCase and added setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryMediaResult.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryMediaResult.java
@@ -4,6 +4,7 @@ import io.github.natanimn.telebof.enums.ParseMode;
 import io.github.natanimn.telebof.types.keyboard.InlineKeyboardMarkup;
 import io.github.natanimn.telebof.types.input.InputTextMessageContent;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
+import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
@@ -11,14 +12,21 @@ import java.util.List;
  * Common field builder for InlineQueryResult those are media
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 @SuppressWarnings("unchecked")
-class InlineQueryMediaResult<T>  implements InlineQueryResult {
-    public String type, id, caption, parse_mode;
-    public List<MessageEntity> caption_entities;
-    public InputTextMessageContent input_message_content;
-    public InlineKeyboardMarkup reply_markup;
+class InlineQueryMediaResult<T> implements InlineQueryResult {
+    private String type;
+    private String id;
+    private String caption;
+    @SerializedName("parse_mode")
+    private String parseMode;
+    @SerializedName("caption_entities")
+    private List<MessageEntity> captionEntities;
+    @SerializedName("input_message_content")
+    private InputTextMessageContent inputMessageContent;
+    @SerializedName("reply_markup")
+    private InlineKeyboardMarkup replyMarkup;
 
     T thisT = (T) this;
 
@@ -34,21 +42,21 @@ class InlineQueryMediaResult<T>  implements InlineQueryResult {
 
     /**
      * Optional
-     * @param input_message_content Content of the message to be sent instead of the media(photo, audio, voice, etc)
+     * @param inputMessageContent Content of the message to be sent instead of the media(photo, audio, voice, etc)
      * @return {@link T}
      */
-    public T inputMessageContent(InputTextMessageContent input_message_content) {
-        this.input_message_content = input_message_content;
+    public T setInputMessageContent(InputTextMessageContent inputMessageContent) {
+        this.inputMessageContent = inputMessageContent;
         return thisT;
     }
 
     /**
      * Optional
-     * @param keyboard {@link InlineKeyboardMarkup Inline keyboard} attached to the message
+     * @param replyMarkup {@link InlineKeyboardMarkup Inline keyboard} attached to the message
      * @return {@link T}
      */
-    public T replyMarkup(InlineKeyboardMarkup keyboard) {
-        this.reply_markup = keyboard;
+    public T setReplyMarkup(InlineKeyboardMarkup replyMarkup) {
+        this.replyMarkup = replyMarkup;
         return thisT;
     }
 
@@ -57,30 +65,28 @@ class InlineQueryMediaResult<T>  implements InlineQueryResult {
      * @param caption Caption of the photo to be sent, 0-1024 characters after entities parsing
      * @return {@link T}
      */
-    public T caption(String caption) {
+    public T setCaption(String caption) {
         this.caption = caption;
         return thisT;
     }
 
-
     /**
      * Optional
-     * @param caption_entities List of special entities that appear in the caption, which can be specified instead of {@link #parseMode}
+     * @param captionEntities List of special entities that appear in the caption, which can be specified instead of {@link #setParseMode}
      * @return {@link T}
      */
-    public T captionEntities(MessageEntity[] caption_entities) {
-        this.caption_entities = List.of(caption_entities);
+    public T setCaptionEntities(MessageEntity[] captionEntities) {
+        this.captionEntities = List.of(captionEntities);
         return thisT;
     }
 
     /**
      * Optional
-     * @param parse_mode Mode for parsing entities in the caption
+     * @param parseMode Mode for parsing entities in the caption
      * @return {@link T}
      */
-    public T parseMode(ParseMode parse_mode) {
-        this.parse_mode = parse_mode.name().toLowerCase();
+    public T setParseMode(ParseMode parseMode) {
+        this.parseMode = parseMode.name().toLowerCase();
         return thisT;
     }
-
 }
