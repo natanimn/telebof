878b3b72101f0f5b689312d4737479f68366fed7
natanimn <natanimco@gmail.com>
2025-11-30 16:31:38 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedMpeg4Gif.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedMpeg4Gif.java
@@ -1,26 +1,30 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents a link to a video animation (H.264/MPEG-4 AVC video without sound) stored on the Telegram servers.
  * By default, this animated MPEG-4 file will be sent by the user with an optional caption.
- * Alternatively, you can use {@link #inputMessageContent} to send a message with the specified content instead of the animation.
- * @author Natanim 
+ * Alternatively, you can use {@link io.github.natanimn.telebof.types.input.InputTextMessageContent} to send a message with the specified content instead of the animation.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultCachedMpeg4Gif extends InlineQueryMediaResult<InlineQueryResultCachedMpeg4Gif> {
-    private String mpeg4_file_id;
+    @SerializedName("mpeg4_file_id")
+    private String mpeg4FileId;
     private String title;
-    private Boolean show_caption_above_media;
+    @SerializedName("show_caption_above_media")
+    private Boolean showCaptionAboveMedia;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param mpeg4_file_id A valid file identifier for the MPEG4 file
+     * @param mpeg4FileId A valid file identifier for the MPEG4 file
      */
-    public InlineQueryResultCachedMpeg4Gif(String id, String mpeg4_file_id) {
+    public InlineQueryResultCachedMpeg4Gif(String id, String mpeg4FileId) {
         super("mpeg4", id);
-        this.mpeg4_file_id = mpeg4_file_id;
+        this.mpeg4FileId = mpeg4FileId;
     }
 
     /**
@@ -28,19 +32,18 @@ public class InlineQueryResultCachedMpeg4Gif extends InlineQueryMediaResult<Inli
      * @param title Title for the result
      * @return {@link InlineQueryResultCachedMpeg4Gif}
      */
-    public InlineQueryResultCachedMpeg4Gif title(String title) {
+    public InlineQueryResultCachedMpeg4Gif setTitle(String title) {
         this.title = title;
         return this;
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media
      * @return {@link InlineQueryResultCachedMpeg4Gif}
      */
-    public InlineQueryResultCachedMpeg4Gif showCaptionAboveMedia(boolean show_caption_above_media){
-        this.show_caption_above_media = show_caption_above_media;
+    public InlineQueryResultCachedMpeg4Gif setShowCaptionAboveMedia(Boolean showCaptionAboveMedia) {
+        this.showCaptionAboveMedia = showCaptionAboveMedia;
         return this;
     }
-
 }
