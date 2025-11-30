5f7eaf70bd1ed422dca95592e818c68f4fb187ec
natanimn <natanimco@gmail.com>
2025-11-30 16:30:22 +0300

    Replaced snake_case fields with camelCase

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedAudio.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedAudio.java
@@ -1,22 +1,25 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents a link to an MP3 audio file stored on the Telegram servers. By default, this audio file will be sent by the user.
- * Alternatively, you can use {@link #inputMessageContent} to send a message with the specified content instead of the audio.
+ * Alternatively, you can use {@link io.github.natanimn.telebof.types.input.InputMessageContent} to send a message with the specified content instead of the audio.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultCachedAudio extends InlineQueryMediaResult<InlineQueryResultCachedAudio>{
-    private String audio_file_id;
+    @SerializedName("audio_file_id")
+    private String audioFileId;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param audio_file_id A valid file identifier for the audio file
+     * @param audioFileId A valid file identifier for the audio file
      */
-    public InlineQueryResultCachedAudio(String id, String audio_file_id) {
+    public InlineQueryResultCachedAudio(String id, String audioFileId) {
         super("audio", id);
-        this.audio_file_id = audio_file_id ;
+        this.audioFileId = audioFileId ;
     }
 }
