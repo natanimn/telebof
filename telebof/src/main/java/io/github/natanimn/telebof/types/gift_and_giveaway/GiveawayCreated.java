0d9f66741ad7441dc3a353e087bcad6a6f452f69
natanimn <natanimco@gmail.com>
2025-11-30 16:27:00 +0300

    Replaced snake_case fields with camelCase and added getter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCreated.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/GiveawayCreated.java
@@ -4,11 +4,18 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the creation of a scheduled giveaway.
- * @param prizeStarCount Optional. The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record GiveawayCreated(
-        @SerializedName("prize_star_count") Integer prizeStarCount
-) {}
+public class GiveawayCreated {
+    /**
+     * Optional. The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
+     */
+    @SerializedName("prize_star_count")
+    private Integer prizeStarCount;
+
+    public Integer getPrizeStarCount() {
+        return prizeStarCount;
+    }
+}
