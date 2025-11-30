0096b43087b3ce0a6607915cdeff1849838501d4
natanimn <natanimco@gmail.com>
2025-11-30 16:31:13 +0300

    Replaced snake_case fields with camelCase and added setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedGif.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedGif.java
@@ -1,25 +1,29 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents a link to an animated GIF file stored on the Telegram servers. By default, this animated GIF file will be sent by the user with an optional caption.
  * Alternatively, you can use {@link #inputMessageContent} to send a message with specified content instead of the animation.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultCachedGif extends InlineQueryMediaResult<InlineQueryResultCachedGif> {
-    private String gif_file_id;
+    @SerializedName("gif_file_id")
+    private String gifFileId;
     private String title;
-    private Boolean show_caption_above_media;
+    @SerializedName("show_caption_above_media")
+    private Boolean showCaptionAboveMedia;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param gif_file_id A valid file identifier for the GIF file
+     * @param gifFileId A valid file identifier for the GIF file
      */
-    public InlineQueryResultCachedGif(String id, String gif_file_id) {
+    public InlineQueryResultCachedGif(String id, String gifFileId) {
         super("gif", id);
-        this.gif_file_id = gif_file_id;
+        this.gifFileId = gifFileId;
     }
 
     /**
@@ -27,19 +31,18 @@ public class InlineQueryResultCachedGif extends InlineQueryMediaResult<InlineQue
      * @param title Title for the result
      * @return {@link InlineQueryResultCachedGif}
      */
-    public InlineQueryResultCachedGif title(String title) {
+    public InlineQueryResultCachedGif setTitle(String title) {
         this.title = title;
         return this;
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media
      * @return {@link InlineQueryResultCachedGif}
      */
-    public InlineQueryResultCachedGif showCaptionAboveMedia(boolean show_caption_above_media){
-        this.show_caption_above_media = show_caption_above_media;
+    public InlineQueryResultCachedGif setShowCaptionAboveMedia(Boolean showCaptionAboveMedia) {
+        this.showCaptionAboveMedia = showCaptionAboveMedia;
         return this;
     }
-
 }
