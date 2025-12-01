83dde0294984568b28a75063e72f88bd11ea7226
natanimn <natanimco@gmail.com>
2025-12-01 23:21:21 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultGame.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultGame.java
@@ -1,37 +1,40 @@
 package io.github.natanimn.telebof.types.inline;
 
 import io.github.natanimn.telebof.types.keyboard.InlineKeyboardMarkup;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a <a href="https://core.telegram.org/bots/api#games">Game.</a>
- * @author Natanim 
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultGame implements InlineQueryResult {
     private String id;
     private String type;
-    private String game_short_name;
-    private InlineKeyboardMarkup reply_markup;
+    @SerializedName("game_short_name")
+    private String gameShortName;
+    @SerializedName("reply_markup")
+    private InlineKeyboardMarkup replyMarkup;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param game_short_name Short name of the game
+     * @param gameShortName Short name of the game
      */
-    public InlineQueryResultGame(String id, String game_short_name) {
+    public InlineQueryResultGame(String id, String gameShortName) {
         this.type = "game";
         this.id = id;
-        this.game_short_name = game_short_name;
+        this.gameShortName = gameShortName;
     }
 
     /**
      * Optional
-     * @param keyboard {@link InlineKeyboardMarkup Inline keyboard} attached to the message
+     * @param replyMarkup {@link InlineKeyboardMarkup Inline keyboard} attached to the message
      * @return {@link InlineQueryResultGame}
      */
-    public InlineQueryResultGame replyMarkup(InlineKeyboardMarkup keyboard) {
-        this.reply_markup = keyboard;
+    public InlineQueryResultGame setReplyMarkup(InlineKeyboardMarkup replyMarkup) {
+        this.replyMarkup = replyMarkup;
         return this;
     }
 }
