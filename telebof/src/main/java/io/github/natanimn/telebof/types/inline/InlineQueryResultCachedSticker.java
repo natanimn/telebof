8f0312fd61459902a9602ef094cde2f3f6ae5978
natanimn <natanimco@gmail.com>
2025-12-01 23:20:24 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedSticker.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedSticker.java
@@ -2,50 +2,53 @@ package io.github.natanimn.telebof.types.inline;
 
 import io.github.natanimn.telebof.types.keyboard.InlineKeyboardMarkup;
 import io.github.natanimn.telebof.types.input.InputTextMessageContent;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a link to a sticker stored on the Telegram servers. By default, this sticker will be sent by the user.
- * Alternatively, you can use {@link #inputMessageContent} to send a message with the specified content instead of the sticker.
- * @author Natanim 
+ * Alternatively, you can use {@link #setInputMessageContent} to send a message with the specified content instead of the sticker.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultCachedSticker implements InlineQueryResult {
-    private String sticker_file_id;
+    @SerializedName("sticker_file_id")
+    private String stickerFileId;
     private String type;
     private String id;
-    private InputTextMessageContent input_message_content;
-    private InlineKeyboardMarkup reply_markup;
+    @SerializedName("input_message_content")
+    private InputTextMessageContent inputMessageContent;
+    @SerializedName("reply_markup")
+    private InlineKeyboardMarkup replyMarkup;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param sticker_file_id A valid file identifier of the sticker
+     * @param stickerFileId A valid file identifier of the sticker
      */
-    public InlineQueryResultCachedSticker(String id, String sticker_file_id) {
+    public InlineQueryResultCachedSticker(String id, String stickerFileId) {
         this.type = "sticker";
         this.id = id;
-        this.sticker_file_id = sticker_file_id;
+        this.stickerFileId = stickerFileId;
     }
 
     /**
      * Optional
-     * @param input_message_content Content of the message to be sent instead of the sticker
+     * @param inputMessageContent Content of the message to be sent instead of the sticker
      * @return {@link InlineQueryResultCachedSticker}
      */
-    public InlineQueryResultCachedSticker inputMessageContent(InputTextMessageContent input_message_content) {
-        this.input_message_content = input_message_content;
+    public InlineQueryResultCachedSticker setInputMessageContent(InputTextMessageContent inputMessageContent) {
+        this.inputMessageContent = inputMessageContent;
         return this;
     }
 
     /**
      * Optional
-     * @param keyboard {@link InlineKeyboardMarkup Inline keyboard} attached to the message
+     * @param replyMarkup {@link InlineKeyboardMarkup Inline keyboard} attached to the message
      * @return {@link InlineQueryResultCachedSticker}
      */
-    public InlineQueryResultCachedSticker replyMarkup(InlineKeyboardMarkup keyboard) {
-       this.reply_markup = keyboard;
-       return this;
+    public InlineQueryResultCachedSticker setReplyMarkup(InlineKeyboardMarkup replyMarkup) {
+        this.replyMarkup = replyMarkup;
+        return this;
     }
-
 }
