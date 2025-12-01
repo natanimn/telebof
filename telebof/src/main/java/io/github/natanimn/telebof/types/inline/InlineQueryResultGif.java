9d08774a7031705e34ba79b787cdccbce2ca2b96
natanimn <natanimco@gmail.com>
2025-12-01 23:21:34 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultGif.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultGif.java
@@ -1,83 +1,91 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a link to an animated GIF file. By default, this animated GIF file will be sent by the user with optional caption.
- * Alternatively, you can use {@link InlineQueryMediaResult#inputMessageContent} to send a message with the specified content instead of the animation.
- * @author Natanim 
+ * Alternatively, you can use {@link InlineQueryMediaResult#setInputMessageContent} to send a message with the specified content instead of the animation.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultGif extends InlineQueryMediaResult<InlineQueryResultGif> {
-    private String gif_url;
-    private String thumbnail_mime_type;
-    private Integer gif_width;
-    private Integer gif_height;
-    private Integer gif_duration;
-    private Boolean show_caption_above_media;
-    private String thumbnail_url;
+    @SerializedName("gif_url")
+    private String gifUrl;
+    @SerializedName("thumbnail_mime_type")
+    private String thumbnailMimeType;
+    @SerializedName("gif_width")
+    private Integer gifWidth;
+    @SerializedName("gif_height")
+    private Integer gifHeight;
+    @SerializedName("gif_duration")
+    private Integer gifDuration;
+    @SerializedName("show_caption_above_media")
+    private Boolean showCaptionAboveMedia;
+    @SerializedName("thumbnail_url")
+    private String thumbnailUrl;
     private String title;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param gif_url A valid URL for the GIF file
-     * @param thumbnail_url URL of the static (JPEG or GIF) or animated (MPEG4) thumbnail for the result
+     * @param gifUrl A valid URL for the GIF file
+     * @param thumbnailUrl URL of the static (JPEG or GIF) or animated (MPEG4) thumbnail for the result
      */
-    public InlineQueryResultGif(String id, String gif_url, String thumbnail_url) {
+    public InlineQueryResultGif(String id, String gifUrl, String thumbnailUrl) {
         super("gif", id);
-        this.gif_url = gif_url;
-        this.thumbnail_url = thumbnail_url;
+        this.gifUrl = gifUrl;
+        this.thumbnailUrl = thumbnailUrl;
     }
 
     /**
      * Optional
-     * @param gif_width Width of the GIF
+     * @param gifWidth Width of the GIF
      * @return {@link InlineQueryResultGif}
      */
-    public InlineQueryResultGif gifWidth(int gif_width) {
-        this.gif_width = gif_width;
+    public InlineQueryResultGif setGifWidth(Integer gifWidth) {
+        this.gifWidth = gifWidth;
         return this;
     }
 
     /**
      * Optional
-     * @param gif_height Height of the GIF
+     * @param gifHeight Height of the GIF
      * @return {@link InlineQueryResultGif}
      */
-    public InlineQueryResultGif gifHeight(int gif_height) {
-        this.gif_height = gif_height;
+    public InlineQueryResultGif setGifHeight(Integer gifHeight) {
+        this.gifHeight = gifHeight;
         return this;
     }
 
     /**
      * Optional
-     * @param gif_duration Duration of the GIF in seconds
+     * @param gifDuration Duration of the GIF in seconds
      * @return {@link InlineQueryResultGif}
      */
-    public InlineQueryResultGif gifDuration(int gif_duration) {
-        this.gif_duration = gif_duration;
+    public InlineQueryResultGif setGifDuration(Integer gifDuration) {
+        this.gifDuration = gifDuration;
         return this;
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media
-     * @return @link InlineQueryResultGif}
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media
+     * @return {@link InlineQueryResultGif}
      */
-    public InlineQueryResultGif showCaptionAboveMedia(boolean show_caption_above_media){
-        this.show_caption_above_media = show_caption_above_media;
+    public InlineQueryResultGif setShowCaptionAboveMedia(Boolean showCaptionAboveMedia) {
+        this.showCaptionAboveMedia = showCaptionAboveMedia;
         return this;
     }
 
     /**
      * Optional
-     * @param thumbnail_mime_type MIME type of the thumbnail, must be one of “image/jpeg”, “image/gif”, or “video/mp4”.
+     * @param thumbnailMimeType MIME type of the thumbnail, must be one of “image/jpeg”, “image/gif”, or “video/mp4”.
      *                            Defaults to “image/jpeg”
      * @return {@link InlineQueryResultGif}
      */
-    public InlineQueryResultGif thumbnailMimeType(String thumbnail_mime_type) {
-        this.thumbnail_mime_type = thumbnail_mime_type;
+    public InlineQueryResultGif setThumbnailMimeType(String thumbnailMimeType) {
+        this.thumbnailMimeType = thumbnailMimeType;
         return this;
     }
 
@@ -86,7 +94,7 @@ public class InlineQueryResultGif extends InlineQueryMediaResult<InlineQueryResu
      * @param title Title for the result
      * @return {@link InlineQueryResultGif}
      */
-    public InlineQueryResultGif title(String title){
+    public InlineQueryResultGif setTitle(String title) {
         this.title = title;
         return this;
     }
