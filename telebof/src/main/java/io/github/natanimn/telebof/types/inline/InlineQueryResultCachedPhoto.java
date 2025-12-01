8af437484cfa81e2e9cbc247f59721df3fe60ce4
natanimn <natanimco@gmail.com>
2025-12-01 23:20:15 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedPhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedPhoto.java
@@ -1,27 +1,30 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a link to a photo stored on the Telegram servers. By default, this photo will be sent by the user with an optional caption.
- * Alternatively, you can use {@link #inputMessageContent} to send a message with the specified content instead of the photo.
- * @author Natanim 
+ * Alternatively, you can use {@link #setInputMessageContent} to send a message with the specified content instead of the photo.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultCachedPhoto extends InlineQueryMediaResult<InlineQueryResultCachedPhoto> {
-    private String photo_file_id;
+    @SerializedName("photo_file_id")
+    private String photoFileId;
     private String title;
     private String description;
-    private Boolean show_caption_above_media;
+    @SerializedName("show_caption_above_media")
+    private Boolean showCaptionAboveMedia;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param photo_file_id A valid file identifier of the photo
+     * @param photoFileId A valid file identifier of the photo
      */
-    public InlineQueryResultCachedPhoto(String id, String photo_file_id) {
+    public InlineQueryResultCachedPhoto(String id, String photoFileId) {
         super("photo", id);
-        this.photo_file_id = photo_file_id;
+        this.photoFileId = photoFileId;
     }
 
     /**
@@ -29,7 +32,7 @@ public class InlineQueryResultCachedPhoto extends InlineQueryMediaResult<InlineQ
      * @param title Title for the result
      * @return {@link InlineQueryResultCachedPhoto}
      */
-    public InlineQueryResultCachedPhoto title(String title) {
+    public InlineQueryResultCachedPhoto setTitle(String title) {
         this.title = title;
         return this;
     }
@@ -39,18 +42,18 @@ public class InlineQueryResultCachedPhoto extends InlineQueryMediaResult<InlineQ
      * @param description Short description of the result
      * @return {@link InlineQueryResultCachedPhoto}
      */
-    public InlineQueryResultCachedPhoto description(String description) {
+    public InlineQueryResultCachedPhoto setDescription(String description) {
         this.description = description;
         return this;
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media
      * @return {@link InlineQueryResultCachedPhoto}
      */
-    public InlineQueryResultCachedPhoto showCaptionAboveMedia(boolean show_caption_above_media){
-        this.show_caption_above_media = show_caption_above_media;
+    public InlineQueryResultCachedPhoto setShowCaptionAboveMedia(Boolean showCaptionAboveMedia) {
+        this.showCaptionAboveMedia = showCaptionAboveMedia;
         return this;
     }
 }
