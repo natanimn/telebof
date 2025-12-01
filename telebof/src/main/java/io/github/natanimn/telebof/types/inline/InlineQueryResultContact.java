23513e1df24dae3b1a458acaa6608d5423b122a3
natanimn <natanimco@gmail.com>
2025-12-01 23:21:02 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultContact.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultContact.java
@@ -2,47 +2,56 @@ package io.github.natanimn.telebof.types.inline;
 
 import io.github.natanimn.telebof.types.keyboard.InlineKeyboardMarkup;
 import io.github.natanimn.telebof.types.input.InputTextMessageContent;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a contact with a phone number. By default, this contact will be sent by the user.
- * Alternatively, you can use {@link #inputMessageContent} to send a message with the specified content instead of the contact
- * @author Natanim 
+ * Alternatively, you can use {@link #setInputMessageContent} to send a message with the specified content instead of the contact
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultContact implements InlineQueryResult {
     private String type;
     private String id;
-    private String phone_number;
-    private String first_name;
-    private String last_name;
+    @SerializedName("phone_number")
+    private String phoneNumber;
+    @SerializedName("first_name")
+    private String firstName;
+    @SerializedName("last_name")
+    private String lastName;
     private String vcard;
-    private String thumbnail_url;
-    private Integer thumbnail_width;
-    private Integer thumbnail_height;
-    private InlineKeyboardMarkup reply_markup;
-    private InputTextMessageContent input_message_content;
+    @SerializedName("thumbnail_url")
+    private String thumbnailUrl;
+    @SerializedName("thumbnail_width")
+    private Integer thumbnailWidth;
+    @SerializedName("thumbnail_height")
+    private Integer thumbnailHeight;
+    @SerializedName("reply_markup")
+    private InlineKeyboardMarkup replyMarkup;
+    @SerializedName("input_message_content")
+    private InputTextMessageContent inputMessageContent;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 Bytes
-     * @param phone_number Contact's phone number
-     * @param first_name Contact's first name
+     * @param phoneNumber Contact's phone number
+     * @param firstName Contact's first name
      */
-    public InlineQueryResultContact(String id, String phone_number, String first_name) {
+    public InlineQueryResultContact(String id, String phoneNumber, String firstName) {
         this.type = "contact";
         this.id = id;
-        this.phone_number = phone_number;
-        this.first_name = first_name;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
     }
 
     /**
      * Optional
-     * @param last_name Contact's last name
+     * @param lastName Contact's last name
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact lastName(String last_name) {
-        this.last_name = last_name;
+    public InlineQueryResultContact setLastName(String lastName) {
+        this.lastName = lastName;
         return this;
     }
 
@@ -51,59 +60,58 @@ public class InlineQueryResultContact implements InlineQueryResult {
      * @param vcard Additional data about the contact in the form of a <a href="https://en.wikipedia.org/wiki/VCard">vCard</a>, 0-2048 bytes
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact vcard(String vcard) {
+    public InlineQueryResultContact setVcard(String vcard) {
         this.vcard = vcard;
         return this;
     }
 
     /**
      * Optional
-     * @param input_message_content Content of the message to be sent instead of the contact
+     * @param inputMessageContent Content of the message to be sent instead of the contact
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact inputMessageContent(InputTextMessageContent input_message_content) {
-        this.input_message_content = input_message_content;
+    public InlineQueryResultContact setInputMessageContent(InputTextMessageContent inputMessageContent) {
+        this.inputMessageContent = inputMessageContent;
         return this;
     }
 
     /**
      * Optional
-     * @param keyboard {@link InlineKeyboardMarkup Inline keyboard} attached to the message
+     * @param replyMarkup {@link InlineKeyboardMarkup Inline keyboard} attached to the message
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact replyMarkup(InlineKeyboardMarkup keyboard) {
-        this.reply_markup = keyboard;
+    public InlineQueryResultContact setReplyMarkup(InlineKeyboardMarkup replyMarkup) {
+        this.replyMarkup = replyMarkup;
         return this;
     }
 
     /**
      * Optional
-     * @param thumbnail_url Url of the thumbnail for the result
+     * @param thumbnailUrl Url of the thumbnail for the result
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact thumbnailUrl(String thumbnail_url) {
-        this.thumbnail_url = thumbnail_url;
+    public InlineQueryResultContact setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
         return this;
     }
 
     /**
      * Optional
-     * @param thumbnail_width Thumbnail width
+     * @param thumbnailWidth Thumbnail width
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact thumbnailWidth(int thumbnail_width) {
-        this.thumbnail_width = thumbnail_width;
+    public InlineQueryResultContact setThumbnailWidth(Integer thumbnailWidth) {
+        this.thumbnailWidth = thumbnailWidth;
         return this;
     }
 
     /**
      * Optional
-     * @param thumbnail_height Thumbnail height
+     * @param thumbnailHeight Thumbnail height
      * @return {@link InlineQueryResultContact}
      */
-    public InlineQueryResultContact thumbnailHeight(int thumbnail_height) {
-        this.thumbnail_height = thumbnail_height;
+    public InlineQueryResultContact setThumbnailHeight(Integer thumbnailHeight) {
+        this.thumbnailHeight = thumbnailHeight;
         return this;
     }
-
 }
