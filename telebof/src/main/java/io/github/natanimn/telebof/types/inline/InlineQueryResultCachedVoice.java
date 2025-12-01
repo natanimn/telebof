3d09ce63d5b9441b832514a0ad3322edad9d6180
natanimn <natanimco@gmail.com>
2025-12-01 23:20:50 +0300

    Replaced snake_case fields with camelCase and added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedVoice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultCachedVoice.java
@@ -1,26 +1,28 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents a link to a voice message stored on the Telegram servers. By default, this voice message will be sent by the user.
- * Alternatively, you can use {@link #inputMessageContent} to send a message with the specified content instead of the voice message.
- * @author Natanim 
+ * Alternatively, you can use {@link #setInputMessageContent} to send a message with the specified content instead of the voice message.
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultCachedVoice extends InlineQueryMediaResult<InlineQueryResultCachedVoice> {
-    private String voice_file_id;
+    @SerializedName("voice_file_id")
+    private String voiceFileId;
     private String title;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param voice_file_id A valid file identifier for the voice message
+     * @param voiceFileId A valid file identifier for the voice message
      * @param title Voice message title
      */
-    public InlineQueryResultCachedVoice(String id, String voice_file_id, String title) {
+    public InlineQueryResultCachedVoice(String id, String voiceFileId, String title) {
         super("voice", id);
-        this.voice_file_id = voice_file_id;
+        this.voiceFileId = voiceFileId;
         this.title = title;
     }
-
 }
