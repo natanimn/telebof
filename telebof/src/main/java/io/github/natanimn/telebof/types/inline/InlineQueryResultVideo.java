cc2f8bf01458ed675e494e671a811d33a2cd651f
natanimn <natanimco@gmail.com>
2025-12-03 03:32:49 +0300

    Replaced snake_case fields with camelCase and added setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultVideo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultVideo.java
@@ -1,73 +1,80 @@
 package io.github.natanimn.telebof.types.inline;
 
 import io.github.natanimn.telebof.types.input.InputTextMessageContent;
-
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 /**
  * Represents a link to a page containing an embedded video player or a video file.
  * By default, this video file will be sent by the user with an optional caption.
- * Alternatively, you can use {@link InlineQueryMediaResult#inputMessageContent} to send a message with the specified content instead of the video.
+ * Alternatively, you can use {@link io.github.natanimn.telebof.types.input.InputMessageContent} to send a message with the specified content instead of the video.
  * If an InlineQueryResultVideo message contains an embedded video (e.g., YouTube),
- * you must replace its content using {@link InlineQueryMediaResult#inputMessageContent}.
+ * you must replace its content using {@link io.github.natanimn.telebof.types.input.InputMessageContent}.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultVideo extends InlineQueryMediaResult<InlineQueryResultVideo> implements Serializable {
-    private String video_url;
+    @SerializedName("video_url")
+    private String videoUrl;
     private String description;
-    private String mime_type;
-    private String thumbnail_url;
-    private Integer video_width;
-    private Integer video_height;
-    private Integer video_duration;
-    private Boolean show_caption_above_media;
+    @SerializedName("mime_type")
+    private String mimeType;
+    @SerializedName("thumbnail_url")
+    private String thumbnailUrl;
+    @SerializedName("video_width")
+    private Integer videoWidth;
+    @SerializedName("video_height")
+    private Integer videoHeight;
+    @SerializedName("video_duration")
+    private Integer videoDuration;
+    @SerializedName("show_caption_above_media")
+    private Boolean showCaptionAboveMedia;
     private String title;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param video_url A valid URL for the embedded video player or video file
-     * @param mime_type MIME type of the content of the video URL, “text/html” or “video/mp4”
-     * @param thumbnail_url URL of the thumbnail (JPEG only) for the video
+     * @param videoUrl A valid URL for the embedded video player or video file
+     * @param mimeType MIME type of the content of the video URL, “text/html” or “video/mp4”
+     * @param thumbnailUrl URL of the thumbnail (JPEG only) for the video
      * @param title Title for the result
      */
-    public InlineQueryResultVideo(String id, String video_url, String mime_type, String thumbnail_url, String title) {
+    public InlineQueryResultVideo(String id, String videoUrl, String mimeType, String thumbnailUrl, String title) {
         super("video", id);
-        this.video_url = video_url;
-        this.mime_type = mime_type;
-        this.thumbnail_url = thumbnail_url;
+        this.videoUrl = videoUrl;
+        this.mimeType = mimeType;
+        this.thumbnailUrl = thumbnailUrl;
         this.title = title;
     }
 
     /**
      * Optional
-     * @param video_width Video width
+     * @param videoWidth Video width
      * @return {@link InlineQueryResultVideo}
      */
-    public InlineQueryResultVideo videoWidth(int video_width) {
-        this.video_width = video_width;
+    public InlineQueryResultVideo setVideoWidth(Integer videoWidth) {
+        this.videoWidth = videoWidth;
         return this;
     }
 
     /**
      * Optional
-     * @param video_height Video width
+     * @param videoHeight Video height
      * @return {@link InlineQueryResultVideo}
      */
-    public InlineQueryResultVideo videoHeight(int video_height) {
-        this.video_height = video_height;
+    public InlineQueryResultVideo setVideoHeight(Integer videoHeight) {
+        this.videoHeight = videoHeight;
         return this;
     }
 
     /**
      * Optional
-     * @param video_duration Video duration in seconds
+     * @param videoDuration Video duration in seconds
      * @return {@link InlineQueryResultVideo}
      */
-    public InlineQueryResultVideo videoDuration(int video_duration) {
-        this.video_duration = video_duration;
+    public InlineQueryResultVideo setVideoDuration(Integer videoDuration) {
+        this.videoDuration = videoDuration;
         return this;
     }
 
@@ -76,29 +83,18 @@ public class InlineQueryResultVideo extends InlineQueryMediaResult<InlineQueryRe
      * @param description Short description of the result
      * @return {@link InlineQueryResultVideo}
      */
-    public InlineQueryResultVideo description(String description) {
+    public InlineQueryResultVideo setDescription(String description) {
         this.description = description;
         return this;
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media
      * @return {@link InlineQueryResultVideo}
      */
-    public InlineQueryResultVideo showCaptionAboveMedia(boolean show_caption_above_media){
-        this.show_caption_above_media = show_caption_above_media;
+    public InlineQueryResultVideo setShowCaptionAboveMedia(Boolean showCaptionAboveMedia) {
+        this.showCaptionAboveMedia = showCaptionAboveMedia;
         return this;
-    }
-
-    /**
-     * Optional
-     * @param input_message_content Content of the message to be sent instead of the video.
-     *                              This field is required if InlineQueryResultVideo is used to send an HTML-page as a result (e.g., a YouTube video).
-     * @return {@link InlineQueryResultVideo}
-     */
-    @Override
-    public InlineQueryResultVideo inputMessageContent(InputTextMessageContent input_message_content) {
-        return super.inputMessageContent(input_message_content);
     }
 }
