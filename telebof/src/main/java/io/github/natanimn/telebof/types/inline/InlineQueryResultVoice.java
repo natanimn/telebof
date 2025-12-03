8df620a29c1a31841c5d3d5653a8e40ef98b17b7
natanimn <natanimco@gmail.com>
2025-12-03 03:32:57 +0300

    Replaced snake_case fields with camelCase and added setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultVoice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/inline/InlineQueryResultVoice.java
@@ -1,39 +1,41 @@
 package io.github.natanimn.telebof.types.inline;
 
+import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 /**
  * Represents a link to a voice recording in an .OGG container encoded with OPUS. By default, this voice recording will be sent by the user.
- * Alternatively, you can use {@link InlineQueryMediaResult#inputMessageContent} to send a message with the specified content instead of the the voice message.
+ * Alternatively, you can use {@link InlineQueryMediaResult#inputMessageContent} to send a message with the specified content instead of the voice message.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineQueryResultVoice extends InlineQueryMediaResult<InlineQueryResultVoice> implements Serializable {
-    private String voice_url;
-    private Integer voice_duration;
+    @SerializedName("voice_url")
+    private String voiceUrl;
+    @SerializedName("voice_duration")
+    private Integer voiceDuration;
     private String title;
 
     /**
      * Required
      * @param id Unique identifier for this result, 1-64 bytes
-     * @param voice_url A valid URL for the voice recording
+     * @param voiceUrl A valid URL for the voice recording
      * @param title Recording title
      */
-    public InlineQueryResultVoice(String id, String voice_url, String title) {
+    public InlineQueryResultVoice(String id, String voiceUrl, String title) {
         super("voice", id);
-        this.voice_url = voice_url;
+        this.voiceUrl = voiceUrl;
         this.title = title;
     }
 
     /**
      * Optional
-     * @param voice_duration Recording duration in seconds
+     * @param voiceDuration Recording duration in seconds
      * @return {@link InlineQueryResultVoice}
      */
-    public InlineQueryResultVoice voiceDuration(int voice_duration) {
-        this.voice_duration = voice_duration;
+    public InlineQueryResultVoice setVoiceDuration(Integer voiceDuration) {
+        this.voiceDuration = voiceDuration;
         return this;
     }
-
 }
