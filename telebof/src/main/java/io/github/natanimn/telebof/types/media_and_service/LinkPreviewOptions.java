6a27c5ccacf82cd1af50700f3c22ca028c078d20
natanimn <natanimco@gmail.com>
2025-12-04 23:05:22 +0300

    Replaced snake case fields with camelCase and added getter and setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/LinkPreviewOptions.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/LinkPreviewOptions.java
@@ -4,73 +4,82 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes the options used for link preview generation.
- * @param isDisabled Optional. True, if the link preview is disabled
- * @param preferSmallMedia Optional. URL to use for the link preview. If empty, then the first URL found in the message text will be used
- * @param preferLargeMedia Optional. True, if the media in the link preview is supposed to be enlarged; ignored if the URL isn't explicitly specified or media size change isn't supported for the preview
- * @param showAboveText Optional. True, if the link preview must be shown above the message text; otherwise, the link preview will be shown below the message text
- * @param url Optional. URL to use for the link preview. If empty, then the first URL found in the message text will be used
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record LinkPreviewOptions(
-        @SerializedName("is_disabled") Boolean isDisabled,
-        @SerializedName("prefer_small_media") Boolean preferSmallMedia,
-        @SerializedName("prefer_large_media") Boolean preferLargeMedia,
-        @SerializedName("show_above_text") Boolean showAboveText,
-        String url
-) {
+public class LinkPreviewOptions {
+    /**
+     * Optional. True, if the link preview is disabled
+     */
+    @SerializedName("is_disabled")
+    private Boolean isDisabled;
 
     /**
-     * Creates a new LinkPreviewOptions builder
+     * Optional. URL to use for the link preview. If empty, then the first URL found in the message text will be used
      */
-    public static LinkPreviewOptionsBuilder builder() {
-        return new LinkPreviewOptionsBuilder();
+    @SerializedName("prefer_small_media")
+    private Boolean preferSmallMedia;
+
+    /**
+     * Optional. True, if the media in the link preview is supposed to be enlarged; ignored if the URL isn't explicitly specified or media size change isn't supported for the preview
+     */
+    @SerializedName("prefer_large_media")
+    private Boolean preferLargeMedia;
+
+    /**
+     * Optional. True, if the link preview must be shown above the message text; otherwise, the link preview will be shown below the message text
+     */
+    @SerializedName("show_above_text")
+    private Boolean showAboveText;
+
+    /**
+     * Optional. URL to use for the link preview. If empty, then the first URL found in the message text will be used
+     */
+    private String url;
+
+    public Boolean getIsDisabled() {
+        return isDisabled;
     }
 
-    /**
-     * Builder class for LinkPreviewOptions
-     */
-    public static class LinkPreviewOptionsBuilder {
-        private Boolean isDisabled;
-        private Boolean preferSmallMedia;
-        private Boolean preferLargeMedia;
-        private Boolean showAboveText;
-        private String url;
+    public LinkPreviewOptions setIsDisabled(Boolean isDisabled) {
+        this.isDisabled = isDisabled;
+        return this;
+    }
 
-        public LinkPreviewOptionsBuilder isDisabled(boolean isDisabled) {
-            this.isDisabled = isDisabled;
-            return this;
-        }
+    public Boolean getPreferSmallMedia() {
+        return preferSmallMedia;
+    }
 
-        public LinkPreviewOptionsBuilder preferSmallMedia(boolean preferSmallMedia) {
-            this.preferSmallMedia = preferSmallMedia;
-            return this;
-        }
+    public LinkPreviewOptions setPreferSmallMedia(Boolean preferSmallMedia) {
+        this.preferSmallMedia = preferSmallMedia;
+        return this;
+    }
 
-        public LinkPreviewOptionsBuilder preferLargeMedia(boolean preferLargeMedia) {
-            this.preferLargeMedia = preferLargeMedia;
-            return this;
-        }
+    public Boolean getPreferLargeMedia() {
+        return preferLargeMedia;
+    }
 
-        public LinkPreviewOptionsBuilder showAboveText(boolean showAboveText) {
-            this.showAboveText = showAboveText;
-            return this;
-        }
+    public LinkPreviewOptions setPreferLargeMedia(Boolean preferLargeMedia) {
+        this.preferLargeMedia = preferLargeMedia;
+        return this;
+    }
 
-        public LinkPreviewOptionsBuilder url(String url) {
-            this.url = url;
-            return this;
-        }
+    public Boolean getShowAboveText() {
+        return showAboveText;
+    }
 
-        public LinkPreviewOptions build() {
-            return new LinkPreviewOptions(
-                    isDisabled,
-                    preferSmallMedia,
-                    preferLargeMedia,
-                    showAboveText,
-                    url
-            );
-        }
+    public LinkPreviewOptions setShowAboveText(Boolean showAboveText) {
+        this.showAboveText = showAboveText;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public LinkPreviewOptions setUrl(String url) {
+        this.url = url;
+        return this;
     }
 }
