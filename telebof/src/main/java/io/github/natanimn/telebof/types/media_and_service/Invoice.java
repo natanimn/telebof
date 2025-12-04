18f6d0fe829093e4b200f1f87d00074288ab2c2f
natanimn <natanimco@gmail.com>
2025-12-04 23:01:52 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Invoice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Invoice.java
@@ -4,19 +4,55 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object contains basic information about an invoice.
- * @param title Product name
- * @param description Product description
- * @param startParameter Unique bot deep-linking parameter that can be used to generate this invoice
- * @param currency Three-letter ISO 4217 currency code, or “XTR” for payments in Telegram Stars
- * @param totalAmount Total price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Invoice(
-        String title,
-        String description,
-        @SerializedName("start_parameter") String startParameter,
-        String currency,
-        @SerializedName("total_amount") Integer totalAmount
-) {}
+public class Invoice {
+    /**
+     * Product name
+     */
+    private String title;
+
+    /**
+     * Product description
+     */
+    private String description;
+
+    /**
+     * Unique bot deep-linking parameter that can be used to generate this invoice
+     */
+    @SerializedName("start_parameter")
+    private String startParameter;
+
+    /**
+     * Three-letter ISO 4217 currency code, or “XTR” for payments in Telegram Stars
+     */
+    private String currency;
+
+    /**
+     * Total price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
+     */
+    @SerializedName("total_amount")
+    private Integer totalAmount;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStartParameter() {
+        return startParameter;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+}
