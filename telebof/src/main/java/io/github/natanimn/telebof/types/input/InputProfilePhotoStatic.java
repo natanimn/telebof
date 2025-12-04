29c0dd67b61a1718cc5a188900f93828fd218cab
natanimn <natanimco@gmail.com>
2025-12-04 23:01:39 +0300

    v1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputProfilePhotoStatic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputProfilePhotoStatic.java
@@ -7,11 +7,11 @@ import java.util.List;
  * A static profile photo in the .JPG format.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  */
 public class InputProfilePhotoStatic implements InputProfilePhoto {
     private String photo, type;
-    transient private File file;
+    private File file;
 
     /**
      * Required
