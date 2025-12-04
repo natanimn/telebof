385648510d7225fc273d29789392229e7a318045
natanimn <natanimco@gmail.com>
2025-12-04 23:05:54 +0300

    Replaced snake_case fields with camelCase and added setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Location.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Location.java
@@ -4,21 +4,95 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a point on the map.
- * @param longitude Longitude as defined by the sender
- * @param latitude Latitude as defined by the sender
- * @param horizontalAccuracy Optional. The radius of uncertainty for the location, measured in meters; 0-1500
- * @param livePeriod Optional. Time relative to the message sending date, during which the location can be updated; in seconds. For active live locations only.
- * @param heading Optional. The direction in which user is moving, in degrees; 1-360. For active live locations only.
- * @param proximityAlertRadius Optional. The maximum distance for proximity alerts about approaching another chat member, in meters. For sent live locations only.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Location(
-        Double longitude,
-        Double latitude,
-        @SerializedName("horizontal_accuracy") Double horizontalAccuracy,
-        @SerializedName("live_period") Integer livePeriod,
-        Integer heading,
-        @SerializedName("proximity_alert_radius") Integer proximityAlertRadius
-) {}
+public class Location {
+    /**
+     * Longitude as defined by the sender
+     */
+    private Double longitude;
+
+    /**
+     * Latitude as defined by the sender
+     */
+    private Double latitude;
+
+    /**
+     * Optional. The radius of uncertainty for the location, measured in meters; 0-1500
+     */
+    @SerializedName("horizontal_accuracy")
+    private Double horizontalAccuracy;
+
+    /**
+     * Optional. Time relative to the message sending date, during which the location can be updated; in seconds. For active live locations only.
+     */
+    @SerializedName("live_period")
+    private Integer livePeriod;
+
+    /**
+     * Optional. The direction in which user is moving, in degrees; 1-360. For active live locations only.
+     */
+    private Integer heading;
+
+    /**
+     * Optional. The maximum distance for proximity alerts about approaching another chat member, in meters. For sent live locations only.
+     */
+    @SerializedName("proximity_alert_radius")
+    private Integer proximityAlertRadius;
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public Location setLongitude(Double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Location setLatitude(Double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+
+    public Double getHorizontalAccuracy() {
+        return horizontalAccuracy;
+    }
+
+    public Location setHorizontalAccuracy(Double horizontalAccuracy) {
+        this.horizontalAccuracy = horizontalAccuracy;
+        return this;
+    }
+
+    public Integer getLivePeriod() {
+        return livePeriod;
+    }
+
+    public Location setLivePeriod(Integer livePeriod) {
+        this.livePeriod = livePeriod;
+        return this;
+    }
+
+    public Integer getHeading() {
+        return heading;
+    }
+
+    public Location setHeading(Integer heading) {
+        this.heading = heading;
+        return this;
+    }
+
+    public Integer getProximityAlertRadius() {
+        return proximityAlertRadius;
+    }
+
+    public Location setProximityAlertRadius(Integer proximityAlertRadius) {
+        this.proximityAlertRadius = proximityAlertRadius;
+        return this;
+    }
+}
