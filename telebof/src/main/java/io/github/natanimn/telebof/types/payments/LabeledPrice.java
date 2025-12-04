88eeaa753d5e62f43479d83532faf095ff5c9184
natanimn <natanimco@gmail.com>
2025-12-04 23:05:02 +0300

    Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/LabeledPrice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/LabeledPrice.java
@@ -4,13 +4,31 @@ import java.io.Serializable;
 
 /**
  * This object represents a portion of the price for goods or services.
- * @param label Portion label
- * @param amount Price of the product in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record LabeledPrice(
-        String label,
-        Integer amount
-) implements Serializable {}
+public class LabeledPrice implements Serializable {
+    /**
+     * Portion label
+     */
+    private final String label;
+
+    /**
+     * Price of the product in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
+     */
+    private final Integer amount;
+
+    public LabeledPrice(String label, Integer amount) {
+        this.label = label;
+        this.amount = amount;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+}
