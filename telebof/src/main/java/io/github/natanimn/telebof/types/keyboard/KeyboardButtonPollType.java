8bc9a2f1859b76ce4caa2dcc0af4eba33ee66352
natanimn <natanimco@gmail.com>
2025-12-04 23:04:40 +0300

    Added setter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/KeyboardButtonPollType.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/KeyboardButtonPollType.java
@@ -17,11 +17,12 @@ public class KeyboardButtonPollType implements Serializable {
 
     /**
      * Optional
-     * @param type If quiz is passed, the user will be allowed to create only polls in the quiz mode.
-     *             If regular is passed, only regular polls will be allowed.
+     * @param type If {@link PollType#QUIZ} is passed, the user will be allowed to create only polls in the quiz mode.
+     *             If {@link PollType#REGULAR} is passed, only regular polls will be allowed.
      *             Otherwise, the user will be allowed to create a poll of any type.
      */
-    public KeyboardButtonPollType(PollType type) {
+    public KeyboardButtonPollType setType(PollType type) {
         this.type = type;
+        return this;
     }
 }
