dc3c61badd4fc6de086fcd0eca283da38eb43dab
natanimn <natanimco@gmail.com>
2025-12-05 12:09:01 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PhotoSize.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PhotoSize.java
@@ -4,19 +4,56 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents one size of a photo or a file / sticker thumbnail.
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param width Photo width
- * @param height Photo height
- * @param fileSize Optional. File size in bytes
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record PhotoSize(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        Integer width,
-        Integer height,
-        @SerializedName("file_size") Integer fileSize
-) {}
+public class PhotoSize {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Photo width
+     */
+    private Integer width;
+
+    /**
+     * Photo height
+     */
+    private Integer height;
+
+    /**
+     * Optional. File size in bytes
+     */
+    @SerializedName("file_size")
+    private Integer fileSize;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+}
