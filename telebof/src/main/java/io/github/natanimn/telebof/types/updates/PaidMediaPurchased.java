b71a1a747f87bb9dd3bebca183e823faee5e2318
natanimn <natanimco@gmail.com>
2025-12-05 12:08:47 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/PaidMediaPurchased.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/PaidMediaPurchased.java
@@ -2,16 +2,39 @@ package io.github.natanimn.telebof.types.updates;
 
 import io.github.natanimn.telebof.types.chat_and_user.User;
 import com.google.gson.annotations.SerializedName;
+import java.util.Objects;
 
 /**
  * This class contains information about a paid media purchase.
- * @param from User who purchased the media
- * @param paidMediaPayload Bot-specified paid media payload
  * @author Natanim
  * @since 17 April 2025
  * @version 1.3.0
  */
-public record PaidMediaPurchased(
-        User from,
-        @SerializedName("paid_media_payload") String paidMediaPayload
-) implements TelegramUpdate {}
+public class PaidMediaPurchased implements TelegramUpdate {
+    /**
+     * User who purchased the media
+     */
+    private User from;
+
+    /**
+     * Bot-specified paid media payload
+     */
+    @SerializedName("paid_media_payload")
+    private String paidMediaPayload;
+
+    public User getFrom() {
+        return from;
+    }
+
+    public String getPaidMediaPayload() {
+        return paidMediaPayload;
+    }
+
+    @Override
+    public String toString() {
+        return "PaidMediaPurchased{" +
+                "from=" + from +
+                ", paidMediaPayload='" + paidMediaPayload + '\'' +
+                '}';
+    }
+}
