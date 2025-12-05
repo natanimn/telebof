ff9a9952ddd76631e840f22d59b4d8b36d995bd8
natanimn <natanimco@gmail.com>
2025-12-05 12:06:40 +0300

    Replaced snake case fields with camelCase and added getter and setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MaskPosition.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MaskPosition.java
@@ -5,64 +5,66 @@ import java.io.Serializable;
 
 /**
  * This object describes the position on faces where a mask should be placed by default.
- * @param point The part of the face relative to which the mask should be placed. One of “forehead”, “eyes”, “mouth”, or “chin”.
- * @param xShift Shift by X-axis measured in widths of the mask scaled to the face size, from left to right. For example, choosing -1.0 will place mask just to the left of the default mask position.
- * @param yShift Shift by Y-axis measured in heights of the mask scaled to the face size, from top to bottom. For example, 1.0 will place the mask just below the default mask position.
- * @param scale Mask scaling coefficient. For example, 2.0 means double size.
  * @author Natanim Negash
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record MaskPosition(
-        String point,
-        @SerializedName("x_shift") Double xShift,
-        @SerializedName("y_shift") Double yShift,
-        Double scale
-) implements Serializable {
+public class MaskPosition implements Serializable {
+    /**
+     * The part of the face relative to which the mask should be placed. One of “forehead”, “eyes”, “mouth”, or “chin”.
+     */
+    private String point;
 
     /**
-     * Creates a new MaskPosition builder
+     * Shift by X-axis measured in widths of the mask scaled to the face size, from left to right. For example, choosing -1.0 will place mask just to the left of the default mask position.
      */
-    public static MaskPositionBuilder builder() {
-        return new MaskPositionBuilder();
+    @SerializedName("x_shift")
+    private Double xShift;
+
+    /**
+     * Shift by Y-axis measured in heights of the mask scaled to the face size, from top to bottom. For example, 1.0 will place the mask just below the default mask position.
+     */
+    @SerializedName("y_shift")
+    private Double yShift;
+
+    /**
+     * Mask scaling coefficient. For example, 2.0 means double size.
+     */
+    private Double scale;
+
+    public String getPoint() {
+        return point;
     }
 
-    /**
-     * Builder class for MaskPosition
-     */
-    public static class MaskPositionBuilder {
-        private String point;
-        private Double xShift;
-        private Double yShift;
-        private Double scale;
+    public MaskPosition setPoint(String point) {
+        this.point = point;
+        return this;
+    }
 
-        public MaskPositionBuilder point(String point) {
-            this.point = point;
-            return this;
-        }
+    public Double getXShift() {
+        return xShift;
+    }
 
-        public MaskPositionBuilder xShift(double xShift) {
-            this.xShift = xShift;
-            return this;
-        }
+    public MaskPosition setXShift(Double xShift) {
+        this.xShift = xShift;
+        return this;
+    }
 
-        public MaskPositionBuilder yShift(double yShift) {
-            this.yShift = yShift;
-            return this;
-        }
+    public Double getYShift() {
+        return yShift;
+    }
 
-        public MaskPositionBuilder scale(double scale) {
-            this.scale = scale;
-            return this;
-        }
+    public MaskPosition setYShift(Double yShift) {
+        this.yShift = yShift;
+        return this;
+    }
 
-        public MaskPosition build() {
-            return new MaskPosition(
-                    point,
-                    xShift,
-                    yShift,
-                    scale
-            );
-        }
+    public Double getScale() {
+        return scale;
+    }
+
+    public MaskPosition setScale(Double scale) {
+        this.scale = scale;
+        return this;
     }
 }
