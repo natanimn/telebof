257c9492e8683c5d75c59c34b54e57f7a6e0d480
natanimn <natanimco@gmail.com>
2025-12-06 18:46:39 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/SharedUser.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/SharedUser.java
@@ -6,19 +6,56 @@ import java.util.List;
 
 /**
  * This object contains information about a user that was shared with the bot using a {@link io.github.natanimn.telebof.types.keyboard.KeyboardButtonRequestUsers} button.
- * @param userId Identifier of the shared user.
- * @param firstName Optional. First name of the user, if the name was requested by the bot
- * @param lastName Optional. Last name of the user, if the name was requested by the bot
- * @param username Optional. Username of the user, if the username was requested by the bot
- * @param photo Optional. Available sizes of the chat photo, if the photo was requested by the bot
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record SharedUser(
-        @SerializedName("user_id") Long userId,
-        @SerializedName("first_name") String firstName,
-        @SerializedName("last_name") String lastName,
-        String username,
-        List<PhotoSize> photo
-) { }
+public class SharedUser {
+    /**
+     * Identifier of the shared user.
+     */
+    @SerializedName("user_id")
+    private Long userId;
+
+    /**
+     * Optional. First name of the user, if the name was requested by the bot
+     */
+    @SerializedName("first_name")
+    private String firstName;
+
+    /**
+     * Optional. Last name of the user, if the name was requested by the bot
+     */
+    @SerializedName("last_name")
+    private String lastName;
+
+    /**
+     * Optional. Username of the user, if the username was requested by the bot
+     */
+    private String username;
+
+    /**
+     * Optional. Available sizes of the chat photo, if the photo was requested by the bot
+     */
+    private List<PhotoSize> photo;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<PhotoSize> getPhoto() {
+        return photo;
+    }
+}
