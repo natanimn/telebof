c62ad7111f4fff926d4eab616b8b3664b6f73af5
natanimn <natanimco@gmail.com>
2025-12-06 18:45:25 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/PollAnswer.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/PollAnswer.java
@@ -4,20 +4,61 @@ import io.github.natanimn.telebof.types.chat_and_user.Chat;
 import io.github.natanimn.telebof.types.chat_and_user.User;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * This object represents an answer of a user in a non-anonymous poll.
- * @param pollId Unique poll identifier
- * @param user The user that changed the answer to the poll, if the voter isn't anonymous
- * @param optionIds 0-based identifiers of chosen answer options. May be empty if the vote was retracted.
- * @param voterChat The chat that changed the answer to the poll, if the voter is anonymous
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record PollAnswer(
-        @SerializedName("poll_id") String pollId,
-        User user,
-        @SerializedName("option_ids") List<Integer> optionIds,
-        @SerializedName("voter_chat") Chat voterChat
-) implements TelegramUpdate {}
+public class PollAnswer implements TelegramUpdate {
+    /**
+     * Unique poll identifier
+     */
+    @SerializedName("poll_id")
+    private String pollId;
+
+    /**
+     * The user that changed the answer to the poll, if the voter isn't anonymous
+     */
+    private User user;
+
+    /**
+     * 0-based identifiers of chosen answer options. May be empty if the vote was retracted.
+     */
+    @SerializedName("option_ids")
+    private List<Integer> optionIds;
+
+    /**
+     * The chat that changed the answer to the poll, if the voter is anonymous
+     */
+    @SerializedName("voter_chat")
+    private Chat voterChat;
+
+    public String getPollId() {
+        return pollId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public List<Integer> getOptionIds() {
+        return optionIds;
+    }
+
+    public Chat getVoterChat() {
+        return voterChat;
+    }
+
+    @Override
+    public String toString() {
+        return "PollAnswer{" +
+                "pollId='" + pollId + '\'' +
+                ", user=" + user +
+                ", optionIds=" + optionIds +
+                ", voterChat=" + voterChat +
+                '}';
+    }
+}
