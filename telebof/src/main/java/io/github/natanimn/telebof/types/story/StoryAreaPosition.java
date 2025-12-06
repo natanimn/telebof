2822ccd4d2591ab6af72e7a7b36e690fb199c588
natanimn <natanimco@gmail.com>
2025-12-06 18:47:34 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaPosition.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaPosition.java
@@ -4,21 +4,68 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes the position of a clickable area within a story.
- * @param xPercentage The abscissa of the area's center, as a percentage of the media width
- * @param yPercentage The ordinate of the area's center, as a percentage of the media height
- * @param widthPercentage The width of the area's rectangle, as a percentage of the media width
- * @param heightPercentage The height of the area's rectangle, as a percentage of the media height
- * @param rotationAngle The clockwise rotation angle of the rectangle, in degrees; 0-360
- * @param cornerRadiusPercentage The radius of the rectangle corner rounding, as a percentage of the media width
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record StoryAreaPosition(
-        @SerializedName("x_percentage") Double xPercentage,
-        @SerializedName("y_percentage") Double yPercentage,
-        @SerializedName("width_percentage") Double widthPercentage,
-        @SerializedName("height_percentage") Double heightPercentage,
-        @SerializedName("rotation_angle") Double rotationAngle,
-        @SerializedName("corner_radius_percentage") Double cornerRadiusPercentage
-) {}
+public class StoryAreaPosition {
+    /**
+     * The abscissa of the area's center, as a percentage of the media width
+     */
+    @SerializedName("x_percentage")
+    private Double xPercentage;
+
+    /**
+     * The ordinate of the area's center, as a percentage of the media height
+     */
+    @SerializedName("y_percentage")
+    private Double yPercentage;
+
+    /**
+     * The width of the area's rectangle, as a percentage of the media width
+     */
+    @SerializedName("width_percentage")
+    private Double widthPercentage;
+
+    /**
+     * The height of the area's rectangle, as a percentage of the media height
+     */
+    @SerializedName("height_percentage")
+    private Double heightPercentage;
+
+    /**
+     * The clockwise rotation angle of the rectangle, in degrees; 0-360
+     */
+    @SerializedName("rotation_angle")
+    private Double rotationAngle;
+
+    /**
+     * The radius of the rectangle corner rounding, as a percentage of the media width
+     */
+    @SerializedName("corner_radius_percentage")
+    private Double cornerRadiusPercentage;
+
+    public Double getXPercentage() {
+        return xPercentage;
+    }
+
+    public Double getYPercentage() {
+        return yPercentage;
+    }
+
+    public Double getWidthPercentage() {
+        return widthPercentage;
+    }
+
+    public Double getHeightPercentage() {
+        return heightPercentage;
+    }
+
+    public Double getRotationAngle() {
+        return rotationAngle;
+    }
+
+    public Double getCornerRadiusPercentage() {
+        return cornerRadiusPercentage;
+    }
+}
