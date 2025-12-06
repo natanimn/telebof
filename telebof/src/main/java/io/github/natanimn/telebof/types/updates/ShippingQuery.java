2839766b72b48d67641608f1192c646986f660f7
natanimn <natanimco@gmail.com>
2025-12-06 18:46:55 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/ShippingQuery.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/ShippingQuery.java
@@ -3,21 +3,60 @@ package io.github.natanimn.telebof.types.updates;
 import io.github.natanimn.telebof.types.chat_and_user.User;
 import io.github.natanimn.telebof.types.payments.ShippingAddress;
 import com.google.gson.annotations.SerializedName;
+import java.util.Objects;
 
 /**
- * This record contains information about an incoming shipping query.
- *
- * @param id Unique query identifier
- * @param from User who sent the query
- * @param invoicePayload Bot-specified invoice payload
- * @param shippingAddress User specified shipping address
+ * This class contains information about an incoming shipping query.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ShippingQuery(
-        String id,
-        User from,
-        @SerializedName("invoice_payload") String invoicePayload,
-        @SerializedName("shipping_address") ShippingAddress shippingAddress
-) implements TelegramUpdate {}
+public class ShippingQuery implements TelegramUpdate {
+    /**
+     * Unique query identifier
+     */
+    private String id;
+
+    /**
+     * User who sent the query
+     */
+    private User from;
+
+    /**
+     * Bot-specified invoice payload
+     */
+    @SerializedName("invoice_payload")
+    private String invoicePayload;
+
+    /**
+     * User specified shipping address
+     */
+    @SerializedName("shipping_address")
+    private ShippingAddress shippingAddress;
+
+    public String getId() {
+        return id;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public String getInvoicePayload() {
+        return invoicePayload;
+    }
+
+    public ShippingAddress getShippingAddress() {
+        return shippingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "ShippingQuery{" +
+                "id='" + id + '\'' +
+                ", from=" + from +
+                ", invoicePayload='" + invoicePayload + '\'' +
+                ", shippingAddress=" + shippingAddress +
+                '}';
+    }
+}
