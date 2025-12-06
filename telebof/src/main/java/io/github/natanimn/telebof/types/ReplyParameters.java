22a4585210bd29d9b5f5671a179a53ed41eabd09
natanimn <natanimco@gmail.com>
2025-12-06 18:46:17 +0300

    Replaced snake_case fields with camelCase and added setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/ReplyParameters.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/ReplyParameters.java
@@ -2,6 +2,7 @@ package io.github.natanimn.telebof.types;
 
 import io.github.natanimn.telebof.enums.ParseMode;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
+import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -9,47 +10,62 @@ import java.util.Objects;
 
 /**
  * Describes reply parameters for the message that is being sent.
- * @author Natanim 
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
 public class ReplyParameters implements Serializable {
-    private int message_id;
-    private Object chat_id;
-    private Boolean allow_sending_without_reply;
+    @SerializedName("message_id")
+    private int messageId;
+
+    @SerializedName("chat_id")
+    private Object chatId;
+
+    @SerializedName("allow_sending_without_reply")
+    private Boolean allowSendingWithoutReply;
+
     private String quote;
-    private ParseMode quote_parse_mode;
-    private MessageEntity[] quote_entities;
-    private Integer quote_position, checklist_task_id;
+
+    @SerializedName("quote_parse_mode")
+    private ParseMode quoteParseMode;
+
+    @SerializedName("quote_entities")
+    private MessageEntity[] quoteEntities;
+
+    @SerializedName("quote_position")
+    private Integer quotePosition;
+
+    @SerializedName("checklist_task_id")
+    private Integer checklistTaskId;
 
     /**
      * Required
-     * @param message_id Identifier of the message that will be replied to in the current chat, or in the chat chat_id if it is specified
+     * @param messageId Identifier of the message that will be replied to in the current chat, or in the chat chatId if it is specified
      */
-    public ReplyParameters(int message_id){
-        this.message_id = message_id;
+    public ReplyParameters(int messageId) {
+        this.messageId = messageId;
     }
 
     /**
      * Optional
-     * @param chat_id If the message to be replied to is from a different chat, unique identifier for the chat or username of the channel (in the format @channelusername).
-     *                Not supported for messages sent on behalf of a business account.
+     * @param chatId If the message to be replied to is from a different chat, unique identifier for the chat or username of the channel (in the format @channelusername).
+     *               Not supported for messages sent on behalf of a business account.
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters chatId(Object chat_id){
-        this.chat_id = chat_id;
+    public ReplyParameters setChatId(Object chatId) {
+        this.chatId = chatId;
         return this;
     }
 
     /**
      * Optional
-     * @param allow_sending_without_reply Pass True if the message should be sent even if the specified message to be replied to is not found.
-     *                                    Always False for replies in another chat or forum topic.
-     *                                    Always True for messages sent on behalf of a business account.
+     * @param allowSendingWithoutReply Pass True if the message should be sent even if the specified message to be replied to is not found.
+     *                                 Always False for replies in another chat or forum topic.
+     *                                 Always True for messages sent on behalf of a business account.
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters allowSendingWithoutReply(boolean allow_sending_without_reply){
-        this.allow_sending_without_reply = allow_sending_without_reply;
+    public ReplyParameters setAllowSendingWithoutReply(Boolean allowSendingWithoutReply) {
+        this.allowSendingWithoutReply = allowSendingWithoutReply;
         return this;
     }
 
@@ -60,66 +76,48 @@ public class ReplyParameters implements Serializable {
      *              The message will fail to send if the quote isn't found in the original message.
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters quote(String quote){
+    public ReplyParameters setQuote(String quote) {
         this.quote = quote;
         return this;
     }
 
     /**
      * Optional
-     * @param quote_parse_mode Mode for parsing entities in the quote.
+     * @param quoteParseMode Mode for parsing entities in the quote.
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters quoteParseMode(ParseMode quote_parse_mode){
-        this.quote_parse_mode = quote_parse_mode;
+    public ReplyParameters setQuoteParseMode(ParseMode quoteParseMode) {
+        this.quoteParseMode = quoteParseMode;
         return this;
     }
 
     /**
      * Optional
-     * @param quote_entities  A JSON-serialized list of special entities that appear in the quote. It can be specified instead of {@link #quoteParseMode(ParseMode)}.
+     * @param quoteEntities A JSON-serialized list of special entities that appear in the quote. It can be specified instead of {@link #setQuoteParseMode(ParseMode)}.
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters quoteEntities(MessageEntity[] quote_entities){
-        this.quote_entities = quote_entities;
+    public ReplyParameters setQuoteEntities(MessageEntity[] quoteEntities) {
+        this.quoteEntities = quoteEntities;
         return this;
     }
 
     /**
      * Optional
-     * @param quote_position Position of the quote in the original message in UTF-16 code units
+     * @param quotePosition Position of the quote in the original message in UTF-16 code units
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters quotePosition(Integer quote_position){
-        this.quote_position = quote_position;
+    public ReplyParameters setQuotePosition(Integer quotePosition) {
+        this.quotePosition = quotePosition;
         return this;
     }
 
     /**
      * Optional
-     * @param checklist_task_id Identifier of the specific checklist task to be replied to
+     * @param checklistTaskId Identifier of the specific checklist task to be replied to
      * @return {@link ReplyParameters}
      */
-    public ReplyParameters checklistTaskId(Integer checklist_task_id){
-        this.checklist_task_id = checklist_task_id;
+    public ReplyParameters setChecklistTaskId(Integer checklistTaskId) {
+        this.checklistTaskId = checklistTaskId;
         return this;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ReplyParameters that = (ReplyParameters) object;
-        return message_id == that.message_id && Objects.equals(chat_id, that.chat_id) &&
-                Objects.equals(allow_sending_without_reply, that.allow_sending_without_reply) &&
-                Objects.equals(quote, that.quote) && quote_parse_mode == that.quote_parse_mode &&
-                Arrays.equals(quote_entities, that.quote_entities) && Objects.equals(quote_position, that.quote_position);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(message_id, chat_id, allow_sending_without_reply, quote, quote_parse_mode, quote_position);
-        result = 31 * result + Arrays.hashCode(quote_entities);
-        return result;
     }
 }
