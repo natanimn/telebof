95a77679d27eee51f360ab431efb1142c8af47e3
natanimn <natanimco@gmail.com>
2025-12-06 18:45:49 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/PreCheckoutQuery.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/PreCheckoutQuery.java
@@ -3,26 +3,92 @@ package io.github.natanimn.telebof.types.updates;
 import io.github.natanimn.telebof.types.chat_and_user.User;
 import io.github.natanimn.telebof.types.payments.OrderInfo;
 import com.google.gson.annotations.SerializedName;
+import java.util.Objects;
 
 /**
  * This class contains information about an incoming pre-checkout query.
- * @param id Unique query identifier
- * @param from User who sent the query
- * @param currency Three-letter ISO 4217 currency code, or "XTR" for payments in Telegram Stars
- * @param totalAmount Total price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
- * @param invoicePayload Bot-specified invoice payload
- * @param shippingOptionId Identifier of the shipping option chosen by the user
- * @param orderInfo Order information provided by the user
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record PreCheckoutQuery(
-        String id,
-        User from,
-        String currency,
-        @SerializedName("total_amount") Integer totalAmount,
-        @SerializedName("invoice_payload") String invoicePayload,
-        @SerializedName("shipping_option_id") String shippingOptionId,
-        @SerializedName("order_info") OrderInfo orderInfo
-) implements TelegramUpdate {}
+public class PreCheckoutQuery implements TelegramUpdate {
+    /**
+     * Unique query identifier
+     */
+    private String id;
+
+    /**
+     * User who sent the query
+     */
+    private User from;
+
+    /**
+     * Three-letter ISO 4217 currency code, or "XTR" for payments in Telegram Stars
+     */
+    private String currency;
+
+    /**
+     * Total price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
+     */
+    @SerializedName("total_amount")
+    private Integer totalAmount;
+
+    /**
+     * Bot-specified invoice payload
+     */
+    @SerializedName("invoice_payload")
+    private String invoicePayload;
+
+    /**
+     * Identifier of the shipping option chosen by the user
+     */
+    @SerializedName("shipping_option_id")
+    private String shippingOptionId;
+
+    /**
+     * Order information provided by the user
+     */
+    @SerializedName("order_info")
+    private OrderInfo orderInfo;
+
+    public String getId() {
+        return id;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public String getInvoicePayload() {
+        return invoicePayload;
+    }
+
+    public String getShippingOptionId() {
+        return shippingOptionId;
+    }
+
+    public OrderInfo getOrderInfo() {
+        return orderInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "PreCheckoutQuery{" +
+                "id='" + id + '\'' +
+                ", from=" + from +
+                ", currency='" + currency + '\'' +
+                ", totalAmount=" + totalAmount +
+                ", invoicePayload='" + invoicePayload + '\'' +
+                ", shippingOptionId='" + shippingOptionId + '\'' +
+                ", orderInfo=" + orderInfo +
+                '}';
+    }
+}
