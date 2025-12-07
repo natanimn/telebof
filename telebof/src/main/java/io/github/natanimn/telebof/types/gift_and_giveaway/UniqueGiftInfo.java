8b01056f9b291a3ee03683789390f62e4f12346c
natanimn <natanimco@gmail.com>
2025-12-07 15:11:02 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftInfo.java
@@ -4,21 +4,66 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes a service message about a unique gift that was sent or received.
- * @param gift Information about the gift
- * @param origin Origin of the gift. Currently, either "upgrade" for gifts upgraded from regular gifts, "transfer" for gifts transferred from other users or channels, or "resale" for gifts bought from other users
- * @param lastResaleStarCount Optional. For gifts bought from other users, the price paid for the gift
- * @param ownedGiftId Optional. Unique identifier of the received gift for the bot; only present for gifts received on behalf of business accounts
- * @param transferStarCount Optional. Number of Telegram Stars that must be paid to transfer the gift; omitted if the bot cannot transfer the gift
- * @param nextTransferDate Optional. Point in time (Unix timestamp) when the gift can be transferred. If it is in the past, then the gift can be transferred now
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record UniqueGiftInfo(
-        UniqueGift gift,
-        String origin,
-        @SerializedName("last_resale_star_count") Integer lastResaleStarCount,
-        @SerializedName("owned_gift_id") String ownedGiftId,
-        @SerializedName("transfer_star_count") Integer transferStarCount,
-        @SerializedName("next_transfer_date") Integer nextTransferDate
-) {}
+public class UniqueGiftInfo {
+    /**
+     * Information about the gift
+     */
+    private UniqueGift gift;
+
+    /**
+     * Origin of the gift. Currently, either "upgrade" for gifts upgraded from regular gifts, "transfer" for gifts transferred from other users or channels, or "resale" for gifts bought from other users
+     */
+    private String origin;
+
+    /**
+     * Optional. For gifts bought from other users, the price paid for the gift
+     */
+    @SerializedName("last_resale_star_count")
+    private Integer lastResaleStarCount;
+
+    /**
+     * Optional. Unique identifier of the received gift for the bot; only present for gifts received on behalf of business accounts
+     */
+    @SerializedName("owned_gift_id")
+    private String ownedGiftId;
+
+    /**
+     * Optional. Number of Telegram Stars that must be paid to transfer the gift; omitted if the bot cannot transfer the gift
+     */
+    @SerializedName("transfer_star_count")
+    private Integer transferStarCount;
+
+    /**
+     * Optional. Point in time (Unix timestamp) when the gift can be transferred. If it is in the past, then the gift can be transferred now
+     */
+    @SerializedName("next_transfer_date")
+    private Integer nextTransferDate;
+
+    public UniqueGift getGift() {
+        return gift;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public Integer getLastResaleStarCount() {
+        return lastResaleStarCount;
+    }
+
+    public String getOwnedGiftId() {
+        return ownedGiftId;
+    }
+
+    public Integer getTransferStarCount() {
+        return transferStarCount;
+    }
+
+    public Integer getNextTransferDate() {
+        return nextTransferDate;
+    }
+}
