b55be2a8880499ceb9d4ba4fb585138d03ae7ac1
natanimn <natanimco@gmail.com>
2025-12-07 15:10:47 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdropColors.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdropColors.java
@@ -5,17 +5,57 @@ import java.io.Serializable;
 
 /**
  * This object describes the colors of the backdrop of a unique gift.
- * @param centerColor The color in the center of the backdrop in RGB format
- * @param edgeColor The color on the edges of the backdrop in RGB format
- * @param symbolColor The color to be applied to the symbol in RGB format
- * @param textColor The color for the text on the backdrop in RGB format
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record UniqueGiftBackdropColors(
-        @SerializedName("center_color") Integer centerColor,
-        @SerializedName("edge_color") Integer edgeColor,
-        @SerializedName("symbol_color") Integer symbolColor,
-        @SerializedName("text_color") Integer textColor
-) implements Serializable {}
+public class UniqueGiftBackdropColors implements Serializable {
+    /**
+     * The color in the center of the backdrop in RGB format
+     */
+    @SerializedName("center_color")
+    private Integer centerColor;
+
+    /**
+     * The color on the edges of the backdrop in RGB format
+     */
+    @SerializedName("edge_color")
+    private Integer edgeColor;
+
+    /**
+     * The color to be applied to the symbol in RGB format
+     */
+    @SerializedName("symbol_color")
+    private Integer symbolColor;
+
+    /**
+     * The color for the text on the backdrop in RGB format
+     */
+    @SerializedName("text_color")
+    private Integer textColor;
+
+    public UniqueGiftBackdropColors(){}
+
+    public UniqueGiftBackdropColors(Integer centerColor, Integer edgeColor, Integer symbolColor, Integer textColor) {
+        this.centerColor = centerColor;
+        this.edgeColor = edgeColor;
+        this.symbolColor = symbolColor;
+        this.textColor = textColor;
+    }
+
+    public Integer getCenterColor() {
+        return centerColor;
+    }
+
+    public Integer getEdgeColor() {
+        return edgeColor;
+    }
+
+    public Integer getSymbolColor() {
+        return symbolColor;
+    }
+
+    public Integer getTextColor() {
+        return textColor;
+    }
+}
