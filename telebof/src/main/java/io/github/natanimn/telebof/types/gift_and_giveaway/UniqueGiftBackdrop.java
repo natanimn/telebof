229ebee67cfae5f51d582abfb2056f9b8bd335cc
natanimn <natanimco@gmail.com>
2025-12-07 15:10:18 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdrop.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftBackdrop.java
@@ -1,18 +1,47 @@
 package io.github.natanimn.telebof.types.gift_and_giveaway;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 /**
  * This object describes the backdrop of a unique gift.
- * @param name Name of the backdrop
- * @param colors Colors of the backdrop
- * @param rarityPerMille The number of unique gifts that receive this backdrop for every 1000 gifts upgraded
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record UniqueGiftBackdrop(
-        String name,
-        UniqueGiftBackdropColors colors,
-        @com.google.gson.annotations.SerializedName("rarity_per_mille") Integer rarityPerMille
-) implements Serializable {}
+public class UniqueGiftBackdrop implements Serializable {
+    /**
+     * Name of the backdrop
+     */
+    private String name;
+
+    /**
+     * Colors of the backdrop
+     */
+    private UniqueGiftBackdropColors colors;
+
+    /**
+     * The number of unique gifts that receive this backdrop for every 1000 gifts upgraded
+     */
+    @SerializedName("rarity_per_mille")
+    private Integer rarityPerMille;
+
+    public UniqueGiftBackdrop(String name, UniqueGiftBackdropColors colors, Integer rarityPerMille) {
+        this.name = name;
+        this.colors = colors;
+        this.rarityPerMille = rarityPerMille;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UniqueGiftBackdropColors getColors() {
+        return colors;
+    }
+
+    public Integer getRarityPerMille() {
+        return rarityPerMille;
+    }
+}
