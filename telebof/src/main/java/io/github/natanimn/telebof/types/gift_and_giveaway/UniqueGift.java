0adbb2d9f2aec61d963f5fd8bed640a8092e440a
natanimn <natanimco@gmail.com>
2025-12-07 15:09:54 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGift.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGift.java
@@ -5,23 +5,73 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes a unique gift that was upgraded from a regular gift.
- * @param baseName Human-readable name of the regular gift from which this unique gift was upgraded
- * @param name Unique name of the gift. This name can be used in https://t.me/nft/... links and story areas
- * @param number Unique number of the upgraded gift among gifts upgraded from the same regular gift
- * @param model Model of the gift
- * @param symbol Symbol of the gift
- * @param backdrop Backdrop of the gift
- * @param publisherChat Optional. Information about the chat that published the gift
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record UniqueGift(
-        @SerializedName("base_name") String baseName,
-        String name,
-        Integer number,
-        UniqueGiftModel model,
-        UniqueGiftSymbol symbol,
-        UniqueGiftBackdrop backdrop,
-        @SerializedName("publisher_chat") Chat publisherChat
-) {}
+public class UniqueGift {
+    /**
+     * Human-readable name of the regular gift from which this unique gift was upgraded
+     */
+    @SerializedName("base_name")
+    private String baseName;
+
+    /**
+     * Unique name of the gift. This name can be used in https://t.me/nft/... links and story areas
+     */
+    private String name;
+
+    /**
+     * Unique number of the upgraded gift among gifts upgraded from the same regular gift
+     */
+    private Integer number;
+
+    /**
+     * Model of the gift
+     */
+    private UniqueGiftModel model;
+
+    /**
+     * Symbol of the gift
+     */
+    private UniqueGiftSymbol symbol;
+
+    /**
+     * Backdrop of the gift
+     */
+    private UniqueGiftBackdrop backdrop;
+
+    /**
+     * Optional. Information about the chat that published the gift
+     */
+    @SerializedName("publisher_chat")
+    private Chat publisherChat;
+
+    public String getBaseName() {
+        return baseName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public UniqueGiftModel getModel() {
+        return model;
+    }
+
+    public UniqueGiftSymbol getSymbol() {
+        return symbol;
+    }
+
+    public UniqueGiftBackdrop getBackdrop() {
+        return backdrop;
+    }
+
+    public Chat getPublisherChat() {
+        return publisherChat;
+    }
+}
