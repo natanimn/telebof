4009284f2225d5696808e0022cdc0499dbc9a9b1
natanimn <natanimco@gmail.com>
2025-12-07 15:11:17 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftSymbol.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/gift_and_giveaway/UniqueGiftSymbol.java
@@ -5,15 +5,36 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes the symbol shown on the pattern of a unique gift.
- * @param name Name of the symbol
- * @param sticker The sticker that represents the unique gift
- * @param rarityPerMille The number of unique gifts that receive this model for every 1000 gifts upgraded
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record UniqueGiftSymbol(
-        String name,
-        Sticker sticker,
-        @SerializedName("rarity_per_mille") Integer rarityPerMille
-) {}
+public class UniqueGiftSymbol {
+    /**
+     * Name of the symbol
+     */
+    private String name;
+
+    /**
+     * The sticker that represents the unique gift
+     */
+    private Sticker sticker;
+
+    /**
+     * The number of unique gifts that receive this model for every 1000 gifts upgraded
+     */
+    @SerializedName("rarity_per_mille")
+    private Integer rarityPerMille;
+
+    public String getName() {
+        return name;
+    }
+
+    public Sticker getSticker() {
+        return sticker;
+    }
+
+    public Integer getRarityPerMille() {
+        return rarityPerMille;
+    }
+}
