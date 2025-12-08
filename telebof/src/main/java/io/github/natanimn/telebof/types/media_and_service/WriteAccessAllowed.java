93e9cbb270e915c053d71af01b09fee88f1d6c73
natanimn <natanimco@gmail.com>
2025-12-08 09:59:32 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/WriteAccessAllowed.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/WriteAccessAllowed.java
@@ -5,15 +5,38 @@ import com.google.gson.annotations.SerializedName;
 /**
  * This object represents a service message about a user allowing a bot to write messages after adding it to the attachment menu,
  * launching a Web App from a link, or accepting an explicit request from a Web App sent by the method requestWriteAccess.
- * @param webAppName Optional. Name of the Web App, if the access was granted when the Web App was launched from a link
- * @param fromRequest Optional. True, if the access was granted after the user accepted an explicit request from a Web App sent by the method requestWriteAccess.
- * @param fromAttachmentMenu Optional. True, if the access was granted when the bot was added to the attachment or side menu
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record WriteAccessAllowed(
-        @SerializedName("web_app_name") String webAppName,
-        @SerializedName("from_request") Boolean fromRequest,
-        @SerializedName("from_attachment_menu") Boolean fromAttachmentMenu
-) {}
+public class WriteAccessAllowed {
+    /**
+     * Optional. Name of the Web App, if the access was granted when the Web App was launched from a link
+     */
+    @SerializedName("web_app_name")
+    private String webAppName;
+
+    /**
+     * Optional. True, if the access was granted after the user accepted an explicit request from a Web App sent by the method requestWriteAccess.
+     */
+    @SerializedName("from_request")
+    private Boolean fromRequest;
+
+    /**
+     * Optional. True, if the access was granted when the bot was added to the attachment or side menu
+     */
+    @SerializedName("from_attachment_menu")
+    private Boolean fromAttachmentMenu;
+
+    public String getWebAppName() {
+        return webAppName;
+    }
+
+    public Boolean getFromRequest() {
+        return fromRequest;
+    }
+
+    public Boolean getFromAttachmentMenu() {
+        return fromAttachmentMenu;
+    }
+}
