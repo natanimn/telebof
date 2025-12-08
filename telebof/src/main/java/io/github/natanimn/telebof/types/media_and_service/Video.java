818abc503f584aeedfb6dcfddf4d3463fb5e0aab
natanimn <natanimco@gmail.com>
2025-12-08 10:00:19 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Video.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Video.java
@@ -4,31 +4,114 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 /**
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param width Video width as defined by the sender
- * @param height Video height as defined by the sender
- * @param duration Duration of the video in seconds as defined by the sender
- * @param fileName Original filename as defined by the sender
- * @param mimeType MIME type of the file as defined by the sender
- * @param fileSize File size in bytes.
- * @param thumbnail Video thumbnail
- * @param startTimestamp Timestamp in seconds from which the video will play in the message
- * @param cover Available sizes of the cover of the video in the message
+ * This object represents a video file.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Video(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        Integer width,
-        Integer height,
-        Integer duration,
-        @SerializedName("file_name") String fileName,
-        @SerializedName("mime_type") String mimeType,
-        @SerializedName("file_size") Integer fileSize,
-        PhotoSize thumbnail,
-        @SerializedName("start_timestamp") Integer startTimestamp,
-        List<PhotoSize> cover
-) {}
+public class Video {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Video width as defined by the sender
+     */
+    private Integer width;
+
+    /**
+     * Video height as defined by the sender
+     */
+    private Integer height;
+
+    /**
+     * Duration of the video in seconds as defined by the sender
+     */
+    private Integer duration;
+
+    /**
+     * Original filename as defined by the sender
+     */
+    @SerializedName("file_name")
+    private String fileName;
+
+    /**
+     * MIME type of the file as defined by the sender
+     */
+    @SerializedName("mime_type")
+    private String mimeType;
+
+    /**
+     * File size in bytes.
+     */
+    @SerializedName("file_size")
+    private Integer fileSize;
+
+    /**
+     * Video thumbnail
+     */
+    private PhotoSize thumbnail;
+
+    /**
+     * Timestamp in seconds from which the video will play in the message
+     */
+    @SerializedName("start_timestamp")
+    private Integer startTimestamp;
+
+    /**
+     * Available sizes of the cover of the video in the message
+     */
+    private List<PhotoSize> cover;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+
+    public PhotoSize getThumbnail() {
+        return thumbnail;
+    }
+
+    public Integer getStartTimestamp() {
+        return startTimestamp;
+    }
+
+    public List<PhotoSize> getCover() {
+        return cover;
+    }
+}
