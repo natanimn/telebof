113fd90b3ae0460d4379a3f2c1ae9725946f7e3a
natanimn <natanimco@gmail.com>
2025-12-08 10:00:09 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoNote.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoNote.java
@@ -5,21 +5,65 @@ import com.google.gson.annotations.SerializedName;
 /**
  * This object represents a video message
  * (available in Telegram apps as of v.4.0).
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param length Video width and height (diameter of the video message) as defined by the sender
- * @param duration Duration of the video in seconds as defined by the sender
- * @param fileSize Optional. File size in bytes
- * @param thumbnail Optional. Video thumbnail
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record VideoNote(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        Integer length,
-        Integer duration,
-        @SerializedName("file_size") Integer fileSize,
-        PhotoSize thumbnail
-) {}
+public class VideoNote {
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * Video width and height (diameter of the video message) as defined by the sender
+     */
+    private Integer length;
+
+    /**
+     * Duration of the video in seconds as defined by the sender
+     */
+    private Integer duration;
+
+    /**
+     * Optional. File size in bytes
+     */
+    @SerializedName("file_size")
+    private Integer fileSize;
+
+    /**
+     * Optional. Video thumbnail
+     */
+    private PhotoSize thumbnail;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+
+    public PhotoSize getThumbnail() {
+        return thumbnail;
+    }
+}
