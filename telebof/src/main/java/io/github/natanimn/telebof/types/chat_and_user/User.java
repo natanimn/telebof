fa091edad83049e24ca91d5271e505915a3ea635
natanimn <natanimco@gmail.com>
2025-12-09 23:10:24 +0300

    Replaced snake_case fields with camelCase and added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/User.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/User.java
@@ -6,38 +6,138 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a Telegram user or bot.
- * @param id Unique identifier for this user or bot.
- * @param isBot True, if this user is a bot
- * @param firstName User's or bot's first name
- * @param lastName Optional. User's or bot's last name
- * @param languageCode Optional. IETF language tag of the user's language
- * @param username Optional. User's or bot's username
- * @param isPremium Optional. True, if this user is a Telegram Premium user
- * @param addedToAttachmentMenu Optional. True, if this user added the bot to the attachment menu
- * @param canJoinGroups Optional. True, if the bot can be invited to groups. Returned only in {@link BotContext#getMe}.
- * @param canReadAllGroupMessages Optional. True, if privacy mode is disabled for the bot. Returned only in getMe.
- * @param supportsInlineQueries Optional. True, if the bot supports inline queries. Returned only in {@link BotContext#getMe}.
- * @param canConnectToBusiness Optional. True, if the bot can be connected to a Telegram Business account to receive its messages. Returned only in {@link BotContext#getMe}.
- * @param hasMainWebApp Optional. True, if the bot has a main Web App. Returned only in {@link BotContext#getMe}.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record User(
-        Long id,
-        @SerializedName("is_bot") Boolean isBot,
-        @SerializedName("first_name") String firstName,
-        @SerializedName("last_name") String lastName,
-        @SerializedName("language_code") String languageCode,
-        String username,
-        @SerializedName("is_premium") Boolean isPremium,
-        @SerializedName("added_to_attachment_menu") Boolean addedToAttachmentMenu,
-        @SerializedName("can_join_groups") Boolean canJoinGroups,
-        @SerializedName("can_read_all_group_messages") Boolean canReadAllGroupMessages,
-        @SerializedName("supports_inline_queries") Boolean supportsInlineQueries,
-        @SerializedName("can_connect_to_business") Boolean canConnectToBusiness,
-        @SerializedName("has_main_web_app") Boolean hasMainWebApp
-) {
+public class User {
+    /**
+     * Unique identifier for this user or bot.
+     */
+    private Long id;
+
+    /**
+     * True, if this user is a bot
+     */
+    @SerializedName("is_bot")
+    private Boolean isBot;
+
+    /**
+     * User's or bot's first name
+     */
+    @SerializedName("first_name")
+    private String firstName;
+
+    /**
+     * Optional. User's or bot's last name
+     */
+    @SerializedName("last_name")
+    private String lastName;
+
+    /**
+     * Optional. IETF language tag of the user's language
+     */
+    @SerializedName("language_code")
+    private String languageCode;
+
+    /**
+     * Optional. User's or bot's username
+     */
+    private String username;
+
+    /**
+     * Optional. True, if this user is a Telegram Premium user
+     */
+    @SerializedName("is_premium")
+    private Boolean isPremium;
+
+    /**
+     * Optional. True, if this user added the bot to the attachment menu
+     */
+    @SerializedName("added_to_attachment_menu")
+    private Boolean addedToAttachmentMenu;
+
+    /**
+     * Optional. True, if the bot can be invited to groups. Returned only in {@link BotContext#getMe}.
+     */
+    @SerializedName("can_join_groups")
+    private Boolean canJoinGroups;
+
+    /**
+     * Optional. True, if privacy mode is disabled for the bot. Returned only in getMe.
+     */
+    @SerializedName("can_read_all_group_messages")
+    private Boolean canReadAllGroupMessages;
+
+    /**
+     * Optional. True, if the bot supports inline queries. Returned only in {@link BotContext#getMe}.
+     */
+    @SerializedName("supports_inline_queries")
+    private Boolean supportsInlineQueries;
+
+    /**
+     * Optional. True, if the bot can be connected to a Telegram Business account to receive its messages. Returned only in {@link BotContext#getMe}.
+     */
+    @SerializedName("can_connect_to_business")
+    private Boolean canConnectToBusiness;
+
+    /**
+     * Optional. True, if the bot has a main Web App. Returned only in {@link BotContext#getMe}.
+     */
+    @SerializedName("has_main_web_app")
+    private Boolean hasMainWebApp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Boolean getIsBot() {
+        return isBot;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Boolean getIsPremium() {
+        return isPremium;
+    }
+
+    public Boolean getAddedToAttachmentMenu() {
+        return addedToAttachmentMenu;
+    }
+
+    public Boolean getCanJoinGroups() {
+        return canJoinGroups;
+    }
+
+    public Boolean getCanReadAllGroupMessages() {
+        return canReadAllGroupMessages;
+    }
+
+    public Boolean getSupportsInlineQueries() {
+        return supportsInlineQueries;
+    }
+
+    public Boolean getCanConnectToBusiness() {
+        return canConnectToBusiness;
+    }
+
+    public Boolean getHasMainWebApp() {
+        return hasMainWebApp;
+    }
 
     public String mention() {
         // Do not forget to set parseMode to ParseMode.HTML
