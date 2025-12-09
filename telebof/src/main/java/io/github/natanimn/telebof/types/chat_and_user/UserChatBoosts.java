00ecc03485e26a8c66fc02d0c33942e2646a1d5e
natanimn <natanimco@gmail.com>
2025-12-09 23:10:11 +0300

    Added getter method

--- telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/UserChatBoosts.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/chat_and_user/UserChatBoosts.java
@@ -1,13 +1,20 @@
 package io.github.natanimn.telebof.types.chat_and_user;
 
 import java.util.List;
-import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a list of boosts added to a chat by a user.
- * @param boosts The list of boosts added to the chat by the user
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record UserChatBoosts(List<ChatBoost> boosts) {}
+public class UserChatBoosts {
+    /**
+     * The list of boosts added to the chat by the user
+     */
+    private List<ChatBoost> boosts;
+
+    public List<ChatBoost> getBoosts() {
+        return boosts;
+    }
+}
