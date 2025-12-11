8ba44cf33edefbfabe0ee7b80c6385d763e5cbf7
natanimn <natanimco@gmail.com>
2025-12-11 17:46:42 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/answer/AnswerInlineQuery.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/answer/AnswerInlineQuery.java
@@ -6,56 +6,54 @@ import io.github.natanimn.telebof.requests.Api;
 import io.github.natanimn.telebof.types.inline.InlineQueryResult;
 import io.github.natanimn.telebof.types.inline.InlineQueryResultsButton;
 
-import java.util.List;
-
 /**
  * AnswerInlineQuery clas. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#answerInlineQuery
  */
 public class AnswerInlineQuery extends AbstractBaseRequest<AnswerInlineQuery, Boolean> {
     /**
      * Required
-     * @param inline_query_id inline query id
+     * @param inlineQueryId inline query id
      * @param results results
      * @param api api
      */
-    public AnswerInlineQuery(String inline_query_id, InlineQueryResult[] results, Api api) {
+    public AnswerInlineQuery(String inlineQueryId, InlineQueryResult[] results, Api api) {
         super(api, "answerInlineQuery");
-        add("inline_query_id", inline_query_id);
-        add("results", List.of(results));
+        add("inline_query_id", inlineQueryId);
+        add("results", results);
     }
 
     /**
      * Optional
-     * @param cache_time The maximum amount of time in seconds that the result of the inline query may be cached on the server. Defaults to 300.
+     * @param cacheTime The maximum amount of time in seconds that the result of the inline query may be cached on the server. Defaults to 300.
      * @return {@link AnswerInlineQuery}
      */
-    public AnswerInlineQuery cacheTime(int cache_time) {
-        return add("cache_time", cache_time);
+    public AnswerInlineQuery cacheTime(int cacheTime) {
+        return add("cache_time", cacheTime);
     }
 
     /**
      * Optional
-     * @param is_personal Pass True if results may be cached on the server side only for the user that sent the query.
+     * @param isPersonal Pass True if results may be cached on the server side only for the user that sent the query.
      *                    By default, results may be returned to any user who sends the same query.
      * @return {@link AnswerInlineQuery}
      */
-    public AnswerInlineQuery isPersonal(boolean is_personal) {
-        return add("is_personal", is_personal);
+    public AnswerInlineQuery isPersonal(boolean isPersonal) {
+        return add("is_personal", isPersonal);
     }
 
     /**
      * Optional
-     * @param next_offset Pass the offset that a client should send in the next query with the same text to receive more results.
+     * @param nextOffset Pass the offset that a client should send in the next query with the same text to receive more results.
      *                    Pass an empty string if there are no more results or if you don't support pagination.
      *                    Offset length can't exceed 64 bytes.
      * @return {@link AnswerInlineQuery}
      */
-    public AnswerInlineQuery nextOffset(String next_offset) {
-        return add("next_offset", next_offset);
+    public AnswerInlineQuery nextOffset(String nextOffset) {
+        return add("next_offset", nextOffset);
     }
 
     /**
