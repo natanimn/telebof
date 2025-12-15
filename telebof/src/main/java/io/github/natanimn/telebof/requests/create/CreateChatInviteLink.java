b26e8aa7474cd5897391b1f6bc4a82a63cb72578
natanimn <natanimco@gmail.com>
2025-12-15 22:02:04 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateChatInviteLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateChatInviteLink.java
@@ -9,17 +9,17 @@ import io.github.natanimn.telebof.types.chat_and_user.ChatInviteLink;
  * CreateChatInviteLink class
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#createChatInviteLink
  */
 public class CreateChatInviteLink extends AbstractBaseRequest<CreateChatInviteLink, ChatInviteLink> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public CreateChatInviteLink(Object chat_id, Api api) {
-        super(chat_id, api, "createChatInviteLink", ChatInviteLink.class);
+    public CreateChatInviteLink(Object chatId, Api api) {
+        super(chatId, api, "createChatInviteLink", ChatInviteLink.class);
     }
 
     /**
@@ -33,30 +33,30 @@ public class CreateChatInviteLink extends AbstractBaseRequest<CreateChatInviteLi
 
     /**
      * optional
-     * @param expire_date Point in time (Unix timestamp) when the link will expire
+     * @param expireDate Point in time (Unix timestamp) when the link will expire
      * @return {@link CreateChatInviteLink}
      */
-    public CreateChatInviteLink expireDate(int expire_date) {
-        return add("expire_date", expire_date);
+    public CreateChatInviteLink expireDate(int expireDate) {
+        return add("expire_date", expireDate);
     }
 
     /**
      * Optional
-     * @param member_limit The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
+     * @param memberLimit The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
      * @return {@link CreateChatInviteLink}
      */
-    public CreateChatInviteLink memberLimit(int member_limit) {
-        return add("member_limit", member_limit);
+    public CreateChatInviteLink memberLimit(int memberLimit) {
+        return add("member_limit", memberLimit);
     }
 
     /**
      * Optional
-     * @param creates_join_request True, if users joining the chat via the link need to be approved by chat administrators.
+     * @param createsJoinRequest True, if users joining the chat via the link need to be approved by chat administrators.
      *                             If True, member_limit can't be specified
      * @return {@link CreateChatInviteLink}
      */
-    public CreateChatInviteLink createsJoinRequest(boolean creates_join_request) {
-        return add("creates_join_request", creates_join_request);
+    public CreateChatInviteLink createsJoinRequest(boolean createsJoinRequest) {
+        return add("creates_join_request", createsJoinRequest);
     }
 
 }
