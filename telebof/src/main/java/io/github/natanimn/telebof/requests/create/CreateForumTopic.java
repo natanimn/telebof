546f6b89cb231870bdf5f783f647b882f268572d
natanimn <natanimco@gmail.com>
2025-12-15 22:02:57 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateForumTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateForumTopic.java
@@ -9,40 +9,40 @@ import io.github.natanimn.telebof.types.forum.ForumTopic;
  * CreateForumTopic class. Returns {@link ForumTopic} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#createForumTopic
  */
 public class CreateForumTopic extends AbstractBaseRequest<CreateForumTopic, ForumTopic> {
 
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param name name
      * @param api api
      */
-    public CreateForumTopic(Object chat_id, String name, Api api) {
-        super(chat_id, api, "createForumTopic", ForumTopic.class);
+    public CreateForumTopic(Object chatId, String name, Api api) {
+        super(chatId, api, "createForumTopic", ForumTopic.class);
         add("name", name);
     }
 
     /**
      * Optional
-     * @param icon_color Color of the topic icon in RGB format.
+     * @param iconColor Color of the topic icon in RGB format.
      *                   Currently, must be one of 7322096 (0x6FB9F0), 16766590 (0xFFD67E), 13338331 (0xCB86DB), 9367192 (0x8EEE98), 16749490 (0xFF93B2), or 16478047 (0xFB6F5F)
      * @return {@link CreateForumTopic}
      */
-    public CreateForumTopic iconColor(int icon_color) {
-        return add("icon_color", icon_color);
+    public CreateForumTopic iconColor(int iconColor) {
+        return add("icon_color", iconColor);
     }
 
     /**
      * Optional
-     * @param icon_custom_emoji_id Unique identifier of the custom emoji shown as the topic icon.
+     * @param iconCustomEmojiId Unique identifier of the custom emoji shown as the topic icon.
      *                             Use {@link BotContext#getForumTopicIconStickers} to get all allowed custom emoji identifiers.
      * @return {@link CreateForumTopic}
      */
-    public CreateForumTopic iconCustomEmojiId(String icon_custom_emoji_id) {
-        return add("icon_custom_emoji_id", icon_custom_emoji_id);
+    public CreateForumTopic iconCustomEmojiId(String iconCustomEmojiId) {
+        return add("icon_custom_emoji_id", iconCustomEmojiId);
     }
 
 }
