93a95712ef915caad123c4c466244a4da8315e94
natanimn <natanimco@gmail.com>
2025-12-15 22:03:06 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateInvoiceLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateInvoiceLink.java
@@ -10,7 +10,7 @@ import java.util.List;
  * CreateInvoiceLink class. Returns the created invoice link as String on success.
  * @author Natanim
  * @since March 3, 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#createInvoiceLink
  */
 public class CreateInvoiceLink extends AbstractBaseRequest<CreateInvoiceLink, String> {
@@ -35,99 +35,99 @@ public class CreateInvoiceLink extends AbstractBaseRequest<CreateInvoiceLink, St
 
     /**
      * Optional
-     * @param provider_token Payment provider token, obtained via @BotFather. Pass an empty string for payments in Telegram Stars.
+     * @param providerToken Payment provider token, obtained via @BotFather. Pass an empty string for payments in Telegram Stars.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink providerToken(String provider_token){
-        return add("provider_token", provider_token);
+    public CreateInvoiceLink providerToken(String providerToken){
+        return add("provider_token", providerToken);
     }
 
     /**
      * Optional
-     * @param max_tip_amount The maximum accepted amount for tips in the smallest units of the currency (integer, not float/double).
+     * @param maxTipAmount The maximum accepted amount for tips in the smallest units of the currency (integer, not float/double).
      *                       For example, for a maximum tip of US$ 1.45 pass max_tip_amount = 145.
      *                       See the exp parameter in <a href="https://core.telegram.org/bots/payments/currencies.json">currencies.json</a>,
      *                       it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
      *                       Defaults to 0. Not supported for payments in Telegram Stars.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink maxTipAmount(int max_tip_amount) {
-        return add("max_tip_amount", max_tip_amount);
+    public CreateInvoiceLink maxTipAmount(int maxTipAmount) {
+        return add("max_tip_amount", maxTipAmount);
     }
 
     /**
      * Optional
-     * @param suggested_tip_amounts A JSON-serialized array of suggested amounts of tips in the smallest units of the currency (integer, not float/double).
+     * @param suggestedTipAmounts A JSON-serialized array of suggested amounts of tips in the smallest units of the currency (integer, not float/double).
      *                              At most 4 suggested tip amounts can be specified.
      *                              The suggested tip amounts must be positive, passed in a strictly increased order and must not exceed max_tip_amount.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink suggestedTipAmounts(int[] suggested_tip_amounts) {
-        return add("suggested_tip_amounts", List.of(suggested_tip_amounts));
+    public CreateInvoiceLink suggestedTipAmounts(Integer[] suggestedTipAmounts) {
+        return add("suggested_tip_amounts", suggestedTipAmounts);
     }
 
     /**
      * Optional
-     * @param provider_data JSON-serialized data about the invoice, which will be shared with the payment provider.
+     * @param providerData JSON-serialized data about the invoice, which will be shared with the payment provider.
      *                      A detailed description of required fields should be provided by the payment provider.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink providerData(String provider_data) {
-        return add("provider_data", provider_data);
+    public CreateInvoiceLink providerData(String providerData) {
+        return add("provider_data", providerData);
     }
 
     /**
      * Optional
-     * @param photo_url URL of the product photo for the invoice. Can be a photo of the goods or a marketing image for a service.
+     * @param photoUrl URL of the product photo for the invoice. Can be a photo of the goods or a marketing image for a service.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink photoUrl(String photo_url) {
-        return add("photo_url", photo_url);
+    public CreateInvoiceLink photoUrl(String photoUrl) {
+        return add("photo_url", photoUrl);
     }
 
     /**
      * Optional
-     * @param photo_size Photo size in bytes
+     * @param photoSize Photo size in bytes
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink photoSize(int photo_size) {
-        return add("photo_size", photo_size);
+    public CreateInvoiceLink photoSize(int photoSize) {
+        return add("photo_size", photoSize);
     }
 
     /**
      * Optional
-     * @param photo_width Photo width
+     * @param photoWidth Photo width
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink photoWidth(int photo_width) {
-        return add("photo_width", photo_width);
+    public CreateInvoiceLink photoWidth(int photoWidth) {
+        return add("photo_width", photoWidth);
     }
 
     /**
      * Optional
-     * @param photo_height Photo height
+     * @param photoHeight Photo height
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink photoHeight(int photo_height) {
-        return add("photo_height", photo_height);
+    public CreateInvoiceLink photoHeight(int photoHeight) {
+        return add("photo_height", photoHeight);
     }
 
     /**
      * Optional
-     * @param need_name Pass True if you require the user's full name to complete the order. Ignored for payments in Telegram Stars.
+     * @param needName Pass True if you require the user's full name to complete the order. Ignored for payments in Telegram Stars.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink needName(boolean need_name) {
-        return add("need_name", need_name);
+    public CreateInvoiceLink needName(boolean needName) {
+        return add("need_name", needName);
     }
 
     /**
      * Optional
-     * @param need_phone_number Pass True if you require the user's phone number to complete the order. Ignored for payments in Telegram Stars.
+     * @param needPhoneNumber Pass True if you require the user's phone number to complete the order. Ignored for payments in Telegram Stars.
      * @return {@link CreateInvoiceLink}
      */
-    public CreateInvoiceLink needPhoneNumber(boolean need_phone_number) {
-        return add("need_phone_number", need_phone_number);
+    public CreateInvoiceLink needPhoneNumber(boolean needPhoneNumber) {
+        return add("need_phone_number", needPhoneNumber);
     }
 
     /**
