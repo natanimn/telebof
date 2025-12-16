551d07c6f3bf5c8244e32c56ee5122e101bdba0c
natanimn <natanimco@gmail.com>
2025-12-16 14:22:36 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateNewStickerSet.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/create/CreateNewStickerSet.java
@@ -12,42 +12,43 @@ import java.util.List;
  * CreateNewStickerSet class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
+ * @version 1.3.0
  * @see BotContext#createNewStickerSet
  */
 public class CreateNewStickerSet extends AbstractBaseRequest<CreateNewStickerSet, Boolean> {
     /**
      * Required
-     * @param user_id user id
+     * @param userId user id
      * @param name name
      * @param title title
      * @param stickers stickers
      * @param api api
      */
-    public CreateNewStickerSet(long user_id, String name, String title, InputSticker[] stickers, Api api) {
+    public CreateNewStickerSet(long userId, String name, String title, InputSticker[] stickers, Api api) {
         super(api, "createNewStickerSet");
-        add("user_id", user_id);
+        add("user_id", userId);
         add("name", name);
         add("title", title);
-        add("stickers", List.of(stickers));
+        add("stickers", stickers);
     }
 
     /**
      * Optional
-     * @param sticker_type Type of stickers in the set
+     * @param stickerType Type of stickers in the set
      * @return {@link CreateNewStickerSet}
      */
-    public CreateNewStickerSet stickerType(StickerType sticker_type) {
-        return add("sticker_type", sticker_type);
+    public CreateNewStickerSet stickerType(StickerType stickerType) {
+        return add("sticker_type", stickerType);
     }
 
     /**
      * Optional
-     * @param needs_repainting Pass True if stickers in the sticker set must be repainted to the color of text when used in messages,
+     * @param needsRepainting Pass True if stickers in the sticker set must be repainted to the color of text when used in messages,
      *                         the accent color if used as emoji status, white on chat photos, or another appropriate color based on context; for custom emoji sticker sets only
      * @return {@link CreateNewStickerSet}
      */
-    public CreateNewStickerSet needsRepainting(boolean needs_repainting) {
-        return add("needs_repainting", needs_repainting);
+    public CreateNewStickerSet needsRepainting(boolean needsRepainting) {
+        return add("needs_repainting", needsRepainting);
     }
 
 }
