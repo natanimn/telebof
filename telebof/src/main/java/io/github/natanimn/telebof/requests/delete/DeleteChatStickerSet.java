35f3bf6a357b7bf9adf4f2bc83a0f941ba7a5033
natanimn <natanimco@gmail.com>
2025-12-16 14:23:09 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteChatStickerSet.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteChatStickerSet.java
@@ -8,17 +8,17 @@ import io.github.natanimn.telebof.BotContext;
  * DeleteChatStickerSet class. Returns True on sucess.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#deleteChatStickerSet
  */
 public class DeleteChatStickerSet extends AbstractBaseRequest<DeleteChatStickerSet, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public DeleteChatStickerSet(Object chat_id, Api api) {
-        super(chat_id, api, "deleteChatStickerSet", Boolean.class);
+    public DeleteChatStickerSet(Object chatId, Api api) {
+        super(chatId, api, "deleteChatStickerSet", Boolean.class);
     }
 
 }
