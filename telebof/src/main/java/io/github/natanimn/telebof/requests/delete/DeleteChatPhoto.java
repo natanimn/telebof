25e6137e675033033034046f9f0c8c64204cb679
natanimn <natanimco@gmail.com>
2025-12-16 14:22:52 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteChatPhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteChatPhoto.java
@@ -8,17 +8,17 @@ import io.github.natanimn.telebof.BotContext;
  * DeleteChatPhoto class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#deleteChatPhoto
  */
 public class DeleteChatPhoto extends AbstractBaseRequest<DeleteChatPhoto, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public DeleteChatPhoto(Object chat_id, Api api) {
-        super(chat_id, api, "deleteChatPhoto", Boolean.class);
+    public DeleteChatPhoto(Object chatId, Api api) {
+        super(chatId, api, "deleteChatPhoto", Boolean.class);
     }
 
 }
