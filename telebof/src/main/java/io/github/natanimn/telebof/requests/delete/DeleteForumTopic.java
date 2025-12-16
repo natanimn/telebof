2de22d40c7c79cd788d401ef22ede99197a09e2d
natanimn <natanimco@gmail.com>
2025-12-16 14:23:24 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteForumTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteForumTopic.java
@@ -8,18 +8,18 @@ import io.github.natanimn.telebof.requests.Api;
  * DeleteForumTopic class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#deleteForumTopic
  */
 public class DeleteForumTopic extends AbstractBaseRequest<DeleteForumTopic, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_thread_id message thread id
+     * @param chatId chat id
+     * @param messageThreadId message thread id
      * @param api api
      */
-    public DeleteForumTopic(Object chat_id, int message_thread_id, Api api) {
-        super(chat_id, api, "deleteForumTopic", Boolean.class);
-        add("message_thread_id", message_thread_id);
+    public DeleteForumTopic(Object chatId, int messageThreadId, Api api) {
+        super(chatId, api, "deleteForumTopic", Boolean.class);
+        add("message_thread_id", messageThreadId);
     }
 }
