fd2ca84c9677655aa8bb20d575b314f5d0401f28
natanimn <natanimco@gmail.com>
2025-12-17 17:13:30 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteStory.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteStory.java
@@ -7,19 +7,19 @@ import io.github.natanimn.telebof.requests.Api;
  * DeleteStory class. Returns True on success.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  * @see io.github.natanimn.telebof.BotContext#deleteStory
  */
 public class DeleteStory extends AbstractBaseRequest<DeleteStory, Boolean> {
     /**
      * Required
-     * @param business_connection_id Unique identifier of the business connection
-     * @param story_id Unique identifier of the story to delete
+     * @param businessConnectionId Unique identifier of the business connection
+     * @param storyId Unique identifier of the story to delete
      * @param api api
      */
-    public DeleteStory(String business_connection_id, Integer story_id, Api api) {
+    public DeleteStory(String businessConnectionId, int storyId, Api api) {
         super(api, "deleteStory", Boolean.class);
-        add("business_connection_id", business_connection_id);
-        add("story_id", story_id);
+        add("business_connection_id", businessConnectionId);
+        add("story_id", storyId);
     }
 }
