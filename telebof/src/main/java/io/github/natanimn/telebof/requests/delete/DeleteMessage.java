a394bdc49d9803f15454eea262865ca3113838c2
natanimn <natanimco@gmail.com>
2025-12-17 17:13:10 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteMessage.java
@@ -8,19 +8,19 @@ import io.github.natanimn.telebof.requests.Api;
  * DeleteMessage class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#deleteMessage
  */
 public class DeleteMessage extends AbstractBaseRequest<DeleteMessage, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_id message id
+     * @param chatId chat id
+     * @param messageId message id
      * @param api api
      */
-    public DeleteMessage(Object chat_id, int message_id, Api api) {
-        super(chat_id, api, "deleteMessage", Boolean.class);
-        add("message_id", message_id);
+    public DeleteMessage(Object chatId, int messageId, Api api) {
+        super(chatId, api, "deleteMessage", Boolean.class);
+        add("message_id", messageId);
     }
 
 }
