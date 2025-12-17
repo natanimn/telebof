e8cfaac6f7f80fd689619b83794cd87e4f795134
natanimn <natanimco@gmail.com>
2025-12-17 17:13:23 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteMyCommands.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/delete/DeleteMyCommands.java
@@ -9,7 +9,7 @@ import io.github.natanimn.telebof.types.bot.BotCommandScope;
  * DeleteMyCommands class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#deleteMyCommands
  */
 public class DeleteMyCommands extends AbstractBaseRequest<DeleteMyCommands, Boolean> {
@@ -23,22 +23,22 @@ public class DeleteMyCommands extends AbstractBaseRequest<DeleteMyCommands, Bool
 
     /**
      * Optional
-     * @param bot_command_scope A JSON-serialized object, describing scope of users for which the commands are relevant.
+     * @param scope A JSON-serialized object, describing scope of users for which the commands are relevant.
      *                         Defaults to {@link io.github.natanimn.telebof.types.bot.BotCommandScopeDefault}.
      * @return {@link DeleteMyCommands}
      */
-    public DeleteMyCommands scope(BotCommandScope bot_command_scope) {
-        return add("scope", bot_command_scope);
+    public DeleteMyCommands scope(BotCommandScope scope) {
+        return add("scope", scope);
     }
 
     /**
      * Optional
-     * @param language_code A two-letter ISO 639-1 language code.
+     * @param languageCode A two-letter ISO 639-1 language code.
      *                      If empty, commands will be applied to all users from the given scope, for whose language there are no dedicated commands
      * @return {@link DeleteMyCommands}
      */
-    public DeleteMyCommands languageCode(String language_code) {
-        return add("language_code", language_code);
+    public DeleteMyCommands languageCode(String languageCode) {
+        return add("language_code", languageCode);
     }
 
 }
