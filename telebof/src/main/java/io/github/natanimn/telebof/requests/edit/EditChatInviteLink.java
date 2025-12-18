b59836dfd91810e6a11f9d68210306028fc2462e
natanimn <natanimco@gmail.com>
2025-12-18 14:52:14 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditChatInviteLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditChatInviteLink.java
@@ -9,19 +9,19 @@ import io.github.natanimn.telebof.types.chat_and_user.ChatInviteLink;
  * EditChatInviteLink class
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#editChatInviteLink
  */
 public class EditChatInviteLink extends AbstractBaseRequest<EditChatInviteLink, ChatInviteLink> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param invite_link invite link
+     * @param chatId chat id
+     * @param inviteLink invite link
      * @param api api
      */
-    public EditChatInviteLink(Object chat_id, String invite_link, Api api) {
-        super(chat_id, api, "editChatInviteLink", ChatInviteLink.class);
-        add("invite_link", invite_link);
+    public EditChatInviteLink(Object chatId, String inviteLink, Api api) {
+        super(chatId, api, "editChatInviteLink", ChatInviteLink.class);
+        add("invite_link", inviteLink);
     }
 
     /**
@@ -35,30 +35,30 @@ public class EditChatInviteLink extends AbstractBaseRequest<EditChatInviteLink, 
 
     /**
      * optional
-     * @param expire_date Point in time (Unix timestamp) when the link will expire
+     * @param expireDate Point in time (Unix timestamp) when the link will expire
      * @return {@link EditChatInviteLink}
      */
-    public EditChatInviteLink expireDate(int expire_date) {
-        return add("expire_date", expire_date);
+    public EditChatInviteLink expireDate(int expireDate) {
+        return add("expire_date", expireDate);
     }
 
     /**
      * Optional
-     * @param member_limit The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
+     * @param memberLimit The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
      * @return {@link EditChatInviteLink}
      */
-    public EditChatInviteLink memberLimit(int member_limit) {
-        return add("member_limit", member_limit);
+    public EditChatInviteLink memberLimit(int memberLimit) {
+        return add("member_limit", memberLimit);
     }
 
     /**
      * Optional
-     * @param creates_join_request True, if users joining the chat via the link need to be approved by chat administrators.
+     * @param createsJoinRequest True, if users joining the chat via the link need to be approved by chat administrators.
      *                             If True, member_limit can't be specified
      * @return {@link EditChatInviteLink}
      */
-    public EditChatInviteLink createsJoinRequest(boolean creates_join_request) {
-        return add("creates_join_request", creates_join_request);
+    public EditChatInviteLink createsJoinRequest(boolean createsJoinRequest) {
+        return add("creates_join_request", createsJoinRequest);
     }
 
 }
