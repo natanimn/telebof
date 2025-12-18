f5ac22f8ac15ac2b4ab0a897d892a11c3aa0f60c
natanimn <natanimco@gmail.com>
2025-12-18 14:52:43 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditGeneralForumTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditGeneralForumTopic.java
@@ -8,18 +8,18 @@ import io.github.natanimn.telebof.requests.Api;
  * EditGeneralForumTopic class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#editGeneralForumTopic
  */
 public class EditGeneralForumTopic extends AbstractBaseRequest<EditGeneralForumTopic, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param name name
      * @param api api
      */
-    public EditGeneralForumTopic(Object chat_id, String name, Api api) {
-        super(chat_id, api, "editGeneralForumTopic", Boolean.class);
+    public EditGeneralForumTopic(Object chatId, String name, Api api) {
+        super(chatId, api, "editGeneralForumTopic", Boolean.class);
         add("name", name);
     }
 
