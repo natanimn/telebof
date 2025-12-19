06851de78f8bc242a923c2219976191d0d0f628e
natanimn <natanimco@gmail.com>
2025-12-19 08:49:26 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditMessageChecklist.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditMessageChecklist.java
@@ -11,32 +11,32 @@ import io.github.natanimn.telebof.types.updates.Message;
  * EditMessageChecklist class. Returns {@link Message} on success.
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.3.0
  * @see BotContext#editMessageChecklist
  */
 public class EditMessageChecklist extends AbstractBaseRequest<EditMessageChecklist, Message> {
 
     /**
      * Required
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message will be sent
-     * @param chat_id Unique identifier for the target chat
-     * @param message_id Unique identifier for the target message
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message will be sent
+     * @param chatId Unique identifier for the target chat
+     * @param messageId Unique identifier for the target message
      * @param checklist A JSON-serialized object for the new checklist
      * @param api api
      */
-    public EditMessageChecklist(String business_connection_id, Object chat_id, Integer message_id, InputChecklist checklist, Api api) {
-        super(chat_id, api, "editMessageChecklist", Message.class);
-        add("business_connection_id", business_connection_id);
-        add("message_id", message_id);
+    public EditMessageChecklist(String businessConnectionId, Object chatId, Integer messageId, InputChecklist checklist, Api api) {
+        super(chatId, api, "editMessageChecklist", Message.class);
+        add("business_connection_id", businessConnectionId);
+        add("message_id", messageId);
         add("checklist", checklist);
     }
 
     /**
      * Optional
-     * @param reply_markup A JSON-serialized object for the new inline keyboard for the message
+     * @param replyMarkup A JSON-serialized object for the new inline keyboard for the message
      * @return {@link EditMessageChecklist}
      */
-    public EditMessageChecklist replyMarkup(InlineKeyboardMarkup reply_markup) {
-        return add("reply_markup", reply_markup);
+    public EditMessageChecklist replyMarkup(InlineKeyboardMarkup replyMarkup) {
+        return add("reply_markup", replyMarkup);
     }
 }
