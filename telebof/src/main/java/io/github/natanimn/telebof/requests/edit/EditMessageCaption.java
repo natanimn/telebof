e413588780d8fe6edaf0ac6b44a16733c89d6fa0
natanimn <natanimco@gmail.com>
2025-12-19 08:49:16 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditMessageCaption.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/edit/EditMessageCaption.java
@@ -8,8 +8,6 @@ import io.github.natanimn.telebof.types.keyboard.InlineKeyboardMarkup;
 import io.github.natanimn.telebof.types.updates.Message;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
 
-import java.util.List;
-
 /**
  * EditMessageCaption class.
  * @author Natanim
@@ -19,69 +17,69 @@ import java.util.List;
 public class EditMessageCaption extends AbstractBaseRequest<EditMessageCaption, Object> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_id message id
+     * @param chatId chat id
+     * @param messageId message id
      * @param api api
      * @see BotContext#editMessageCaption(Object, int)
      */
-    public EditMessageCaption(Object chat_id, int message_id, Api api) {
-        super(chat_id, api, "editMessageCaption", Message.class);
-        add("message_id", message_id);
+    public EditMessageCaption(Object chatId, int messageId, Api api) {
+        super(chatId, api, "editMessageCaption", Message.class);
+        add("message_id", messageId);
     }
 
     /**
      * Required
-     * @param inline_message_id inline message id
+     * @param inlineMessageId inline message id
      * @param api api
      */
-    public EditMessageCaption(String inline_message_id, Api api) {
+    public EditMessageCaption(String inlineMessageId, Api api) {
         super(api, "editMessageCaption", Boolean.class);
-        add("inline_message_id", inline_message_id);
+        add("inline_message_id", inlineMessageId);
     }
 
     /**
      * Optional
-     * @param parse_mode Mode for parsing entities in the message caption.
+     * @param parseMode Mode for parsing entities in the message caption.
      * @return {@link EditMessageCaption}
      */
-    public EditMessageCaption parseMode(ParseMode parse_mode) {
-        return add("parse_mode",  parse_mode);
+    public EditMessageCaption parseMode(ParseMode parseMode) {
+        return add("parse_mode",  parseMode);
     }
 
     /**
      * Optional
-     * @param caption_entities A JSON-serialized list of special entities that appear in the caption, which can be specified instead of parse_mode
+     * @param captionEntities A JSON-serialized list of special entities that appear in the caption, which can be specified instead of parse_mode
      * @return {@link EditMessageCaption}
      */
-    public EditMessageCaption entities(MessageEntity[] caption_entities) {
-        return add("caption_entities", List.of(caption_entities));
+    public EditMessageCaption captionEntities(MessageEntity[] captionEntities) {
+        return add("caption_entities", captionEntities);
     }
 
     /**
      * Optional
-     * @param reply_markup A JSON-serialized object for an inline keyboard.
+     * @param replyMarkup A JSON-serialized object for an inline keyboard.
      * @return {@link EditMessageCaption}
      */
-    public EditMessageCaption replyMarkup(InlineKeyboardMarkup reply_markup) {
-        return add("reply_markup", reply_markup);
+    public EditMessageCaption replyMarkup(InlineKeyboardMarkup replyMarkup) {
+        return add("reply_markup", replyMarkup);
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media.
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media.
      *                                Supported only for animation, photo and video messages.
      * @return {@link EditMessageCaption}
      */
-    public EditMessageCaption showCaptionAboveMedia(boolean show_caption_above_media){
-        return add("show_caption_above_media", show_caption_above_media);
+    public EditMessageCaption showCaptionAboveMedia(boolean showCaptionAboveMedia){
+        return add("show_caption_above_media", showCaptionAboveMedia);
     }
 
     /**
      * Optional
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message to be edited was sent
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message to be edited was sent
      * @return {@link EditMessageCaption}
      */
-    public EditMessageCaption businessConnectionId(String business_connection_id){
-        return add("business_connection_id", business_connection_id);
+    public EditMessageCaption businessConnectionId(String businessConnectionId){
+        return add("business_connection_id", businessConnectionId);
     }
 }
