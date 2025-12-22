1f40fd06ccce24a716fc53789c2c7ce31cdae7e5
natanimn <natanimco@gmail.com>
2025-12-22 09:34:25 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetChatAdministrators.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetChatAdministrators.java
@@ -13,7 +13,7 @@ import java.util.List;
  * GetChatAdministrators class. Returns an array of {@link ChatMember} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#getChatAdministrators
  */
 public class GetChatAdministrators extends AbstractBaseRequest<GetChatAdministrators, List<ChatMember>> {
@@ -22,11 +22,11 @@ public class GetChatAdministrators extends AbstractBaseRequest<GetChatAdministra
 
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public GetChatAdministrators(Object chat_id, Api api){
-        super(chat_id, api, "getChatAdministrators", responseType);
+    public GetChatAdministrators(Object chatId, Api api){
+        super(chatId, api, "getChatAdministrators", responseType);
     }
 
 }
