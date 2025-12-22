be967ba084740584c0540913b148bb060db71b8c
natanimn <natanimco@gmail.com>
2025-12-22 09:34:38 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetChatMemberCount.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetChatMemberCount.java
@@ -8,17 +8,17 @@ import io.github.natanimn.telebof.requests.Api;
  * GetChatMemberCount class. Returns an Integer on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#getChatMemberCount
  */
 public class GetChatMemberCount extends AbstractBaseRequest<GetChatMemberCount, Integer> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public GetChatMemberCount(Object chat_id, Api api) {
-        super(chat_id, api, "getChatMemberCount", Integer.class);
+    public GetChatMemberCount(Object chatId, Api api) {
+        super(chatId, api, "getChatMemberCount", Integer.class);
     }
 
 }
