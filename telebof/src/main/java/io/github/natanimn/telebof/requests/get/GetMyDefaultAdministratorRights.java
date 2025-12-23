4eee1d4664bda45be432550f83136922d86d4809
natanimn <natanimco@gmail.com>
2025-12-23 19:51:19 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetMyDefaultAdministratorRights.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetMyDefaultAdministratorRights.java
@@ -9,7 +9,7 @@ import io.github.natanimn.telebof.types.chat_and_user.ChatAdministratorRights;
  * GetMyDefaultAdministratorRights class. Returns {@link ChatAdministratorRights} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#getMyDefaultAdministratorRights
  */
 public class GetMyDefaultAdministratorRights extends AbstractBaseRequest<GetMyDefaultAdministratorRights, ChatAdministratorRights> {
@@ -23,12 +23,12 @@ public class GetMyDefaultAdministratorRights extends AbstractBaseRequest<GetMyDe
 
     /**
      * Optional
-     * @param for_channel Pass True to get default administrator rights of the bot in channels.
+     * @param forChannel Pass True to get default administrator rights of the bot in channels.
      *                    Otherwise, default administrator rights of the bot for groups and supergroups will be returned.
      * @return {@link GetMyDefaultAdministratorRights}
      */
-    public GetMyDefaultAdministratorRights forChannel(boolean for_channel) {
-        return add("for_channel", for_channel);
+    public GetMyDefaultAdministratorRights forChannel(boolean forChannel) {
+        return add("for_channel", forChannel);
     }
 
 }
