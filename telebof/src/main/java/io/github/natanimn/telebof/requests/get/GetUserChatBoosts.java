41d1d378f7b83e5e84dd3e416a10a10da4157cb4
natanimn <natanimco@gmail.com>
2025-12-24 05:59:06 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetUserChatBoosts.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/get/GetUserChatBoosts.java
@@ -9,19 +9,19 @@ import io.github.natanimn.telebof.types.chat_and_user.UserChatBoosts;
  * GetUserChatBoosts class. Returns {@link UserChatBoosts} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#getUserChatBoosts
  */
 public class GetUserChatBoosts extends AbstractBaseRequest<GetUserChatBoosts, UserChatBoosts> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param user_id user id
+     * @param chatId chat id
+     * @param userId user id
      * @param api api
      */
-    public GetUserChatBoosts(Object chat_id, long user_id, Api api){
-        super(chat_id, api, "getUserChatBoosts", UserChatBoosts.class);
-        add("user_id", user_id);
+    public GetUserChatBoosts(Object chatId, long userId, Api api){
+        super(chatId, api, "getUserChatBoosts", UserChatBoosts.class);
+        add("user_id", userId);
     }
 
 }
