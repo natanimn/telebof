4c266bd3400a9a724b62d2243631cbe266f035d0
natanimn <natanimco@gmail.com>
2025-12-25 08:58:55 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/pin_and_unpin/UnpinChatMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/pin_and_unpin/UnpinChatMessage.java
@@ -8,35 +8,35 @@ import io.github.natanimn.telebof.BotContext;
  * UnpinChatMessage class. Returns true on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#unpinChatMessage
  */
 public class UnpinChatMessage extends AbstractBaseRequest<UnpinChatMessage, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public UnpinChatMessage(Object chat_id, Api api) {
-        super(chat_id, api, "unpinChatMessage", Boolean.class);
+    public UnpinChatMessage(Object chatId, Api api) {
+        super(chatId, api, "unpinChatMessage", Boolean.class);
     }
 
     /**
      * Optional
-     * @param message_id Identifier of the message to pin_and_unpin. Required if {@link #businessConnectionId} is specified.
+     * @param messageId Identifier of the message to pin_and_unpin. Required if {@link #businessConnectionId} is specified.
      *                   If not specified, the most recent pinned message (by sending date) will be unpinned.
      * @return {@link UnpinChatMessage}
      */
-    public UnpinChatMessage messageId(int message_id) {
-        return add("message_id", message_id);
+    public UnpinChatMessage messageId(int messageId) {
+        return add("message_id", messageId);
     }
 
     /**
      * Optional
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message will be unpinned
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message will be unpinned
      * @return {@link UnpinChatMessage}
      */
-    public UnpinChatMessage businessConnectionId(String business_connection_id) {
-        return add("business_connection_id", business_connection_id);
+    public UnpinChatMessage businessConnectionId(String businessConnectionId) {
+        return add("business_connection_id", businessConnectionId);
     }
 }
