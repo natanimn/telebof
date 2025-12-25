4451229ca15452d1da5112d6adeaf0695fbb5505
natanimn <natanimco@gmail.com>
2025-12-25 08:58:39 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/pin_and_unpin/UnpinAllForumTopicMessages.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/pin_and_unpin/UnpinAllForumTopicMessages.java
@@ -8,19 +8,19 @@ import io.github.natanimn.telebof.BotContext;
  * UnpinAllForumTopicMessages class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#unpinAllForumTopicMessages
  */
 public class UnpinAllForumTopicMessages extends AbstractBaseRequest<UnpinAllForumTopicMessages, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_thread_id message thread id
+     * @param chatId chat id
+     * @param messageThreadId message thread id
      * @param api api
      */
-    public UnpinAllForumTopicMessages(Object chat_id, int message_thread_id, Api api) {
-        super(chat_id, api, "unpinAllForumTopicMessages", Boolean.class);
-        add("message_thread_id", message_thread_id);
+    public UnpinAllForumTopicMessages(Object chatId, int messageThreadId, Api api) {
+        super(chatId, api, "unpinAllForumTopicMessages", Boolean.class);
+        add("message_thread_id", messageThreadId);
     }
 
 }
