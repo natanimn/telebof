57f240be13f694210bbb32a9b9e552d8fb9fe1b6
natanimn <natanimco@gmail.com>
2025-12-25 08:58:19 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/pin_and_unpin/PinChatMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/pin_and_unpin/PinChatMessage.java
@@ -8,38 +8,38 @@ import io.github.natanimn.telebof.requests.Api;
  * PinChatMessage class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#pinChatMessage
  */
 public class PinChatMessage extends AbstractBaseRequest<PinChatMessage, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_id message id
+     * @param chatId chat id
+     * @param messageId message id
      * @param api api
      */
-    public PinChatMessage(Object chat_id, int message_id, Api api) {
-        super(chat_id, api, "pinChatMessage", Boolean.class);
-        add("message_id", message_id);
+    public PinChatMessage(Object chatId, int messageId, Api api) {
+        super(chatId, api, "pinChatMessage", Boolean.class);
+        add("message_id", messageId);
     }
 
     /**
      * Optional
-     * @param disable_notification Pass True if it is not necessary to send a notification to all chat members about the new pinned message.
+     * @param disableNotification Pass True if it is not necessary to send a notification to all chat members about the new pinned message.
      *                             Notifications are always disabled in channels and private chats.
      * @return {@link PinChatMessage}
      */
-    public PinChatMessage disableNotification(boolean disable_notification) {
-        return add("disable_notification", disable_notification);
+    public PinChatMessage disableNotification(boolean disableNotification) {
+        return add("disable_notification", disableNotification);
     }
 
     /**
      * Optional
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message will be pinned
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message will be pinned
      * @return {@link PinChatMessage}
      */
-    public PinChatMessage businessConnectionId(String business_connection_id) {
-        return add("business_connection_id", business_connection_id);
+    public PinChatMessage businessConnectionId(String businessConnectionId) {
+        return add("business_connection_id", businessConnectionId);
     }
 
 }
