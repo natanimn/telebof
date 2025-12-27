a4eba966cc90a798eca24c9e8d3846085b1b79a7
natanimn <natanimco@gmail.com>
2025-12-27 10:20:51 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendChecklist.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendChecklist.java
@@ -12,66 +12,66 @@ import io.github.natanimn.telebof.types.ReplyParameters;
  * SendChecklist class. Returns {@link Message} on success.
  * @author Natanim
  * @since 1 September 2025
- * @version 1.1.0
+ * @version 1.3.0
  * @see BotContext#sendChecklist
  */
 public class SendChecklist extends AbstractBaseRequest<SendChecklist, Message> {
 
     /**
      * Required
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message will be sent
-     * @param chat_id Unique identifier for the target chat
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message will be sent
+     * @param chatId Unique identifier for the target chat
      * @param checklist A JSON-serialized object for the checklist to send
      * @param api api
      */
-    public SendChecklist(String business_connection_id, Object chat_id, InputChecklist checklist, Api api) {
-        super(chat_id, api, "sendChecklist", Message.class);
-        add("business_connection_id", business_connection_id);
+    public SendChecklist(String businessConnectionId, Object chatId, InputChecklist checklist, Api api) {
+        super(chatId, api, "sendChecklist", Message.class);
+        add("business_connection_id", businessConnectionId);
         add("checklist", checklist);
     }
 
     /**
      * Optional
-     * @param disable_notification Sends the message silently. Users will receive a notification with no sound.
+     * @param disableNotification Sends the message silently. Users will receive a notification with no sound.
      * @return {@link SendChecklist}
      */
-    public SendChecklist disableNotification(boolean disable_notification) {
-        return add("disable_notification", disable_notification);
+    public SendChecklist disableNotification(boolean disableNotification) {
+        return add("disable_notification", disableNotification);
     }
 
     /**
      * Optional
-     * @param protect_content Protects the contents of the sent message from forwarding and saving
+     * @param protectContent Protects the contents of the sent message from forwarding and saving
      * @return {@link SendChecklist}
      */
-    public SendChecklist protectContent(boolean protect_content) {
-        return add("protect_content", protect_content);
+    public SendChecklist protectContent(boolean protectContent) {
+        return add("protect_content", protectContent);
     }
 
     /**
      * Optional
-     * @param message_effect_id Unique identifier of the message effect to be added to the message
+     * @param messageEffectId Unique identifier of the message effect to be added to the message
      * @return {@link SendChecklist}
      */
-    public SendChecklist messageEffectId(String message_effect_id) {
-        return add("message_effect_id", message_effect_id);
+    public SendChecklist messageEffectId(String messageEffectId) {
+        return add("message_effect_id", messageEffectId);
     }
 
     /**
      * Optional
-     * @param reply_parameters Description of the message to reply to
+     * @param replyParameters Description of the message to reply to
      * @return {@link SendChecklist}
      */
-    public SendChecklist replyParameters(ReplyParameters reply_parameters) {
-        return add("reply_parameters", reply_parameters);
+    public SendChecklist replyParameters(ReplyParameters replyParameters) {
+        return add("reply_parameters", replyParameters);
     }
 
     /**
      * Optional
-     * @param reply_markup Additional interface options. A JSON-serialized object for an inline keyboard
+     * @param replyMarkup Additional interface options. A JSON-serialized object for an inline keyboard
      * @return {@link SendChecklist}
      */
-    public SendChecklist replyMarkup(Markup reply_markup) {
-        return add("reply_markup", reply_markup);
+    public SendChecklist replyMarkup(Markup replyMarkup) {
+        return add("reply_markup", replyMarkup);
     }
 }
