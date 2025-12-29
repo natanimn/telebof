5dfb495d691bcb7a6a962a845ce7b511244a35fa
natanimn <natanimco@gmail.com>
2025-12-29 08:23:35 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendInvoice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendInvoice.java
@@ -5,19 +5,18 @@ import io.github.natanimn.telebof.BotLog;
 import io.github.natanimn.telebof.requests.Api;
 import io.github.natanimn.telebof.types.payments.LabeledPrice;
 import io.github.natanimn.telebof.types.updates.Message;
-import java.util.List;
 
 /**
  * SendInvoice class. Returns the sent {@link Message} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#sendInvoice
  */
 public class SendInvoice extends SendBuilder<SendInvoice, Message> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param title title
      * @param description description
      * @param payload payload
@@ -25,9 +24,9 @@ public class SendInvoice extends SendBuilder<SendInvoice, Message> {
      * @param prices prices
      * @param api api
      */
-    public SendInvoice(Object chat_id, String title, String description, String payload,
+    public SendInvoice(Object chatId, String title, String description, String payload,
                        String currency, LabeledPrice[] prices, Api api) {
-        super(chat_id, api, "sendInvoice");
+        super(chatId, api, "sendInvoice");
         add("title", title);
         add("description", description);
         add("payload", payload);
@@ -37,158 +36,158 @@ public class SendInvoice extends SendBuilder<SendInvoice, Message> {
 
     /**
      * Optional
-     * @param provider_token Payment provider token, obtained via @BotFather. Pass an empty string for payments in Telegram Stars.
+     * @param providerToken Payment provider token, obtained via @BotFather. Pass an empty string for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice providerToken(String provider_token){
-        return add("provider_token", provider_token);
+    public SendInvoice providerToken(String providerToken){
+        return add("provider_token", providerToken);
     }
 
     /**
      * Optional
-     * @param max_tip_amount The maximum accepted amount for tips in the smallest units of the currency (integer, not float/double).
+     * @param maxTipAmount The maximum accepted amount for tips in the smallest units of the currency (integer, not float/double).
      *                       For example, for a maximum tip of US$ 1.45 pass max_tip_amount = 145.
      *                       See the exp parameter in <a href="https://core.telegram.org/bots/payments/currencies.json">currencies.json</a>, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
      *                       Defaults to 0. Not supported for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice maxTipAmount(int max_tip_amount) {
-        return add("max_tip_amount", max_tip_amount);
+    public SendInvoice maxTipAmount(int maxTipAmount) {
+        return add("max_tip_amount", maxTipAmount);
     }
 
     /**
      * Optional
-     * @param suggested_tip_amounts A JSON-serialized array of suggested amounts of tips in the smallest units of the currency (integer, not float/double).
+     * @param suggestedTipAmounts A JSON-serialized array of suggested amounts of tips in the smallest units of the currency (integer, not float/double).
      *                              At most 4 suggested tip amounts can be specified. The suggested tip amounts must be positive, passed in a strictly increased order and must not exceed max_tip_amount.
      * @return {@link SendInvoice}
      */
-    public SendInvoice suggestedTipAmounts(int[] suggested_tip_amounts) {
-        return add("suggested_tip_amounts", List.of(suggested_tip_amounts));
+    public SendInvoice suggestedTipAmounts(Integer[] suggestedTipAmounts) {
+        return add("suggested_tip_amounts", suggestedTipAmounts);
     }
 
     /**
      * Optional
-     * @param start_parameter Unique deep-linking parameter.
+     * @param startParameter Unique deep-linking parameter.
      *                        If left empty, forwarded copies of the sent message will have a Pay button, allowing multiple users to pay directly from the forwarded message, using the same invoice.
      *                        If non-empty, forwarded copies of the sent message will have a URL button with a deep link to the bot (instead of a Pay button), with the value used as the start parameter
      * @return {@link SendInvoice}
      */
-    public SendInvoice startParameter(String start_parameter) {
-        return add("start_parameter", start_parameter);
+    public SendInvoice startParameter(String startParameter) {
+        return add("start_parameter", startParameter);
     }
 
     /**
      * Optional
-     * @param provider_data JSON-serialized data about the invoice, which will be shared with the payment provider.
+     * @param providerData JSON-serialized data about the invoice, which will be shared with the payment provider.
      *                      A detailed description of required fields should be provided by the payment provider.
      * @return {@link SendInvoice}
      */
-    public SendInvoice providerData(String provider_data) {
-        return add("provider_data", provider_data);
+    public SendInvoice providerData(String providerData) {
+        return add("provider_data", providerData);
     }
 
     /**
      * Optional
-     * @param photo_url URL of the product photo for the invoice.
+     * @param photoUrl URL of the product photo for the invoice.
      *                  Can be a photo of the goods or a marketing image for a service.
      *                  People like it better when they see what they are paying for.
      * @return {@link SendInvoice}
      */
-    public SendInvoice photoUrl(String photo_url) {
-        return add("photo_url", photo_url);
+    public SendInvoice photoUrl(String photoUrl) {
+        return add("photo_url", photoUrl);
     }
 
     /**
      * Optional
-     * @param photo_size Photo size in bytes
+     * @param photoSize Photo size in bytes
      * @return {@link SendInvoice}
      */
-    public SendInvoice photoSize(int photo_size) {
-        return add("photo_size", photo_size);
+    public SendInvoice photoSize(int photoSize) {
+        return add("photo_size", photoSize);
     }
 
     /**
      * Optional
-     * @param photo_width Photo width
+     * @param photoWidth Photo width
      * @return {@link SendInvoice}
      */
-    public SendInvoice photoWidth(int photo_width) {
-        return add("photo_width", photo_width);
+    public SendInvoice photoWidth(int photoWidth) {
+        return add("photo_width", photoWidth);
     }
 
     /**
      * Optional
-     * @param photo_height Photo height
+     * @param photoHeight Photo height
      * @return {@link SendInvoice}
      */
-    public SendInvoice photoHeight(int photo_height) {
-        return add("photo_height", photo_height);
+    public SendInvoice photoHeight(int photoHeight) {
+        return add("photo_height", photoHeight);
     }
 
     /**
      * Optional
-     * @param need_name Pass True if you require the user's full name to complete the order. Ignored for payments in Telegram Stars.
+     * @param needName Pass True if you require the user's full name to complete the order. Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice needName(boolean need_name) {
-        return add("need_name", need_name);
+    public SendInvoice needName(boolean needName) {
+        return add("need_name", needName);
     }
 
     /**
      * Optional
-     * @param need_phone_number Pass True if you require the user's phone number to complete the order. Ignored for payments in Telegram Stars.
+     * @param needPhoneNumber Pass True if you require the user's phone number to complete the order. Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice needPhoneNumber(boolean need_phone_number) {
-        return add("need_phone_number", need_phone_number);
+    public SendInvoice needPhoneNumber(boolean needPhoneNumber) {
+        return add("need_phone_number", needPhoneNumber);
     }
 
     /**
      * Optional
-     * @param need_email Pass True if you require the user's email address to complete the order. Ignored for payments in Telegram Stars.
+     * @param needEmail Pass True if you require the user's email address to complete the order. Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice needEmail(boolean need_email) {
-        return add("need_email", need_email);
+    public SendInvoice needEmail(boolean needEmail) {
+        return add("need_email", needEmail);
     }
 
     /**
      * Optional
-     * @param need_shipping_address Pass True if you require the user's shipping address to complete the order.
+     * @param needShippingAddress Pass True if you require the user's shipping address to complete the order.
      *                             Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice needShippingAddress(boolean need_shipping_address) {
-        return add("need_shipping_address", need_shipping_address);
+    public SendInvoice needShippingAddress(boolean needShippingAddress) {
+        return add("need_shipping_address", needShippingAddress);
     }
 
     /**
      * Optional
-     * @param send_phone_number_to_provider Pass True if the user's phone number should be sent to the provider.
+     * @param sendPhoneNumberToProvider Pass True if the user's phone number should be sent to the provider.
      *                                     Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice sendPhoneNumberToProvider(boolean send_phone_number_to_provider) {
-        return add("send_phone_number_to_provider", send_phone_number_to_provider);
+    public SendInvoice sendPhoneNumberToProvider(boolean sendPhoneNumberToProvider) {
+        return add("send_phone_number_to_provider", sendPhoneNumberToProvider);
     }
 
     /**
      * Optional
-     * @param send_email_to_provider Pass True if the user's email address should be sent to the provider.
+     * @param sendEmailToProvider Pass True if the user's email address should be sent to the provider.
      *                               Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice sendEmailToProvider(boolean send_email_to_provider) {
-        return add("send_email_to_provider", send_email_to_provider);
+    public SendInvoice sendEmailToProvider(boolean sendEmailToProvider) {
+        return add("send_email_to_provider", sendEmailToProvider);
     }
 
     /**
      * Optional
-     * @param is_flexible Pass True if the final price depends on the shipping method. Ignored for payments in Telegram Stars.
+     * @param isFlexible Pass True if the final price depends on the shipping method. Ignored for payments in Telegram Stars.
      * @return {@link SendInvoice}
      */
-    public SendInvoice isFlexible(boolean is_flexible) {
-        return add("is_flexible", is_flexible);
+    public SendInvoice isFlexible(boolean isFlexible) {
+        return add("is_flexible", isFlexible);
     }
 
     /*
@@ -196,7 +195,7 @@ public class SendInvoice extends SendBuilder<SendInvoice, Message> {
      */
     @Override
     @Deprecated(forRemoval = true)
-    public SendInvoice businessConnectionId(String business_connection_id) {
+    public SendInvoice businessConnectionId(String businessConnectionId) {
         BotLog.warn("`sendInvoice` method doesn't support business_connection_id");
         return this;
     }
