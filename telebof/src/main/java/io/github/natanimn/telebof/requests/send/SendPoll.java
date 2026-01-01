65b60a5972bdd630926b45efd03f2d8bb411d14c
natanimn <natanimco@gmail.com>
2026-01-01 21:23:57 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendPoll.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendPoll.java
@@ -10,37 +10,36 @@ import io.github.natanimn.telebof.types.keyboard.Markup;
 import io.github.natanimn.telebof.types.updates.Message;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
 import io.github.natanimn.telebof.types.ReplyParameters;
-import java.util.List;
 
 /**
  * SendPoll class. Returns sent {@link Message} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#sendPoll
  */
 public class SendPoll extends AbstractBaseRequest<SendPoll, Message> {
 
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param question question
      * @param options options
      * @param api api
      */
-    public SendPoll(Object chat_id, String question, InputPollOption[] options, Api api) {
-        super(chat_id, api, "sendPoll", Message.class);
+    public SendPoll(Object chatId, String question, InputPollOption[] options, Api api) {
+        super(chatId, api, "sendPoll", Message.class);
         add("question", question);
-        add("options", List.of(options));
+        add("options", options);
     }
 
     /**
      * Optional
-     * @param is_anonymous True, if the poll needs to be anonymous, defaults to True
+     * @param isAnonymous True, if the poll needs to be anonymous, defaults to True
      * @return {@link SendPoll}
      */
-    public SendPoll isAnonymous(boolean is_anonymous){
-        return add("is_anonymous", is_anonymous);
+    public SendPoll isAnonymous(boolean isAnonymous){
+        return add("is_anonymous", isAnonymous);
     }
 
     /**
@@ -54,20 +53,20 @@ public class SendPoll extends AbstractBaseRequest<SendPoll, Message> {
 
     /**
      * Optional
-     * @param allows_multiple_answers True, if the poll allows multiple answers, ignored for polls in quiz mode, defaults to False
+     * @param allowsMultipleAnswers True, if the poll allows multiple answers, ignored for polls in quiz mode, defaults to False
      * @return {@link SendPoll}
      */
-    public SendPoll allowsMultipleAnswers(boolean allows_multiple_answers){
-        return add("allows_multiple_answers", allows_multiple_answers);
+    public SendPoll allowsMultipleAnswers(boolean allowsMultipleAnswers){
+        return add("allows_multiple_answers", allowsMultipleAnswers);
     }
 
     /**
      * Optional
-     * @param correct_option_id 0-based identifier of the correct answer option, required for polls in quiz mode
+     * @param correctOptionId 0-based identifier of the correct answer option, required for polls in quiz mode
      * @return {@link SendPoll}
      */
-    public SendPoll correctOptionId(int correct_option_id){
-        return add("correct_option_id", correct_option_id);
+    public SendPoll correctOptionId(int correctOptionId){
+        return add("correct_option_id", correctOptionId);
     }
 
     /**
@@ -82,133 +81,133 @@ public class SendPoll extends AbstractBaseRequest<SendPoll, Message> {
 
     /**
      * Optional
-     * @param explanation_parse_mode Mode for parsing entities in the explanation.
+     * @param explanationParseMode Mode for parsing entities in the explanation.
      * @return {@link SendPoll}
      */
-    public SendPoll explanationParseMode(ParseMode explanation_parse_mode){
-        return add("explanation_parse_mode", explanation_parse_mode);
+    public SendPoll explanationParseMode(ParseMode explanationParseMode){
+        return add("explanation_parse_mode", explanationParseMode);
     }
 
     /**
      * Optional
-     * @param explanation_entities A JSON-serialized list of special entities that appear in the poll explanation.
+     * @param explanationEntities A JSON-serialized list of special entities that appear in the poll explanation.
      *                             It can be specified instead of explanation_parse_mode
      * @return {@link SendPoll}
      */
-    public SendPoll explanationEntities(MessageEntity[] explanation_entities){
-        return add("explanation_entities", List.of(explanation_entities));
+    public SendPoll explanationEntities(MessageEntity[] explanationEntities){
+        return add("explanation_entities", explanationEntities);
     }
 
     /**
      * Optional
-     * @param open_period Amount of time in seconds the poll will be active after creation, 5-600. Can't be used together with close_date.
+     * @param openPeriod Amount of time in seconds the poll will be active after creation, 5-600. Can't be used together with close_date.
      * @return {@link SendPoll}
      */
-    public SendPoll openPeriod(int open_period){
-        return add("open_period", open_period);
+    public SendPoll openPeriod(int openPeriod){
+        return add("open_period", openPeriod);
     }
 
     /**
      * Optional
-     * @param close_date Point in time (Unix timestamp) when the poll will be automatically closed.
+     * @param closeDate Point in time (Unix timestamp) when the poll will be automatically closed.
      *                   Must be at least 5 and no more than 600 seconds in the future. Can't be used together with open_period.
      * @return {@link SendPoll}
      */
-    public SendPoll closeDate(int close_date){
-        return add("close_date", close_date);
+    public SendPoll closeDate(int closeDate){
+        return add("close_date", closeDate);
     }
 
     /**
      * Optional
-     * @param is_closed Pass True if the poll needs to be immediately closed. This can be useful for poll preview.
+     * @param isClosed Pass True if the poll needs to be immediately closed. This can be useful for poll preview.
      * @return {@link SendPoll}
      */
-    public SendPoll isClosed(boolean is_closed){
-        return add("is_closed", is_closed);
+    public SendPoll isClosed(boolean isClosed){
+        return add("is_closed", isClosed);
     }
 
     /**
      * Optional
-     * @param disable_notification Sends the message silently. Users will receive a notification with no sound.
+     * @param disableNotification Sends the message silently. Users will receive a notification with no sound.
      * @return {@link SendPoll}
      */
-    public SendPoll disableNotification(boolean disable_notification) {
-        return add("disable_notification", disable_notification);
+    public SendPoll disableNotification(boolean disableNotification) {
+        return add("disable_notification", disableNotification);
     }
 
     /**
      * Optional
-     * @param protect_content Protects the contents of the sent message from forwarding and saving
+     * @param protectContent Protects the contents of the sent message from forwarding and saving
      * @return {@link SendPoll}
      */
-    public SendPoll protectContent(boolean protect_content) {
-        return add("protect_content", protect_content);
+    public SendPoll protectContent(boolean protectContent) {
+        return add("protect_content", protectContent);
     }
 
     /**
      * Optional
-     * @param allow_paid_broadcast Pass True to allow up to 1000 messages per second, ignoring broadcasting limits for a fee of 0.1 Telegram Stars per message.
+     * @param allowPaidBroadcast Pass True to allow up to 1000 messages per second, ignoring broadcasting limits for a fee of 0.1 Telegram Stars per message.
      *                             The relevant Stars will be withdrawn from the bot's balance
      * @return {@link SendPoll}
      */
-    public SendPoll allowPaidBroadcast(boolean allow_paid_broadcast){
-        return add("allow_paid_broadcast", allow_paid_broadcast);
+    public SendPoll allowPaidBroadcast(boolean allowPaidBroadcast){
+        return add("allow_paid_broadcast", allowPaidBroadcast);
     }
 
     /**
      * Optional
-     * @param reply_parameters Description of the message to reply to
+     * @param replyParameters Description of the message to reply to
      * @return {@link SendPoll}
      */
-    public SendPoll replyParameters(ReplyParameters reply_parameters){
-        return add("reply_parameters", reply_parameters);
+    public SendPoll replyParameters(ReplyParameters replyParameters){
+        return add("reply_parameters", replyParameters);
     }
 
     /**
      * Optional
-     * @param reply_markup Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard,
+     * @param replyMarkup Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard,
      *                     instructions to remove a reply keyboard or to force a reply from the user
      * @return {@link SendPoll}
      */
-    public SendPoll replyMarkup(Markup reply_markup) {
-        return add("reply_markup", reply_markup);
+    public SendPoll replyMarkup(Markup replyMarkup) {
+        return add("reply_markup", replyMarkup);
     }
 
     /**
      * Optional
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message will be sent
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message will be sent
      * @return {@link SendPoll}
      */
-    public SendPoll businessConnectionId(String business_connection_id){
-        return add("business_connection_id", business_connection_id);
+    public SendPoll businessConnectionId(String businessConnectionId){
+        return add("business_connection_id", businessConnectionId);
     }
 
     /**
      * Optional
-     * @param question_parse_mode Mode for parsing entities in the question.
+     * @param questionParseMode Mode for parsing entities in the question.
      * @return {@link SendPoll}
      */
-    public SendPoll questionParseMode(ParseMode question_parse_mode){
-        return add("question_parse_mode", question_parse_mode);
+    public SendPoll questionParseMode(ParseMode questionParseMode){
+        return add("question_parse_mode", questionParseMode);
     }
 
     /**
      * Optional
-     * @param question_entities A JSON-serialized list of special entities that appear in the poll question.
+     * @param questionEntities A JSON-serialized list of special entities that appear in the poll question.
      *                          It can be specified instead of question_parse_mode
      * @return {@link SendPoll}
      */
-    public SendPoll questionEntities(MessageEntity[] question_entities){
-        return add("question_entities", question_entities);
+    public SendPoll questionEntities(MessageEntity[] questionEntities){
+        return add("question_entities", questionEntities);
     }
 
     /**
      * Optional
-     * @param message_effect_id Unique identifier of the message effect to be added to the message; for private chats only
+     * @param messageEffectId Unique identifier of the message effect to be added to the message; for private chats only
      * @return {@link SendPoll}
      */
-    public SendPoll messageEffectId(String message_effect_id){
-        return add("message_effect_id ", message_effect_id);
+    public SendPoll messageEffectId(String messageEffectId){
+        return add("message_effect_id ", messageEffectId);
     }
 
 }
