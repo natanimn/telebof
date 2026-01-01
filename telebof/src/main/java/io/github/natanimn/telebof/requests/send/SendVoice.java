ba53d264cb879c536f6938c7bef083c61008f37d
natanimn <natanimco@gmail.com>
2026-01-01 21:24:51 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendVoice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendVoice.java
@@ -5,37 +5,36 @@ import io.github.natanimn.telebof.BotContext;
 import io.github.natanimn.telebof.types.updates.Message;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
 import java.io.File;
-import java.util.List;
 
 /**
  * SendVoice class. Returns {@link Message} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
 public class SendVoice extends SendBuilder<SendVoice, Message> {
 
     /**
      * Required
-     * @param chat_id Chat id
+     * @param chatId Chat id
      * @param voice file or url
      * @param api api
      * @see BotContext#sendVoice(Object, String)
      */
-    public SendVoice(Object chat_id, String voice, Api api) {
-        super(chat_id, api, "sendVoice");
+    public SendVoice(Object chatId, String voice, Api api) {
+        super(chatId, api, "sendVoice");
         add("voice", voice);
     }
 
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param voice File
      * @param api api
      * @see BotContext#sendVoice(Object, File)
      */
-    public SendVoice(Object chat_id, File voice, Api api) {
-        super(chat_id, api, "sendVoice");
+    public SendVoice(Object chatId, File voice, Api api) {
+        super(chatId, api, "sendVoice");
         add("voice", voice);
     }
 
@@ -50,11 +49,11 @@ public class SendVoice extends SendBuilder<SendVoice, Message> {
 
     /**
      * Optional
-     * @param caption_entities A list of special entities that appear in the caption, which can be specified instead of parse_mode
+     * @param captionEntities A list of special entities that appear in the caption, which can be specified instead of parse_mode
      * @return {@link SendVoice}
      */
-    public SendVoice captionEntities(MessageEntity[] caption_entities) {
-        return add("caption_entities", List.of(caption_entities));
+    public SendVoice captionEntities(MessageEntity[] captionEntities) {
+        return add("caption_entities", captionEntities);
     }
 
 }
