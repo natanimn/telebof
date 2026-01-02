b969b96074105627cc7269f4aa08d4c4d6735f70
natanimn <natanimco@gmail.com>
2026-01-02 09:39:26 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/ApproveSuggestedPost.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/ApproveSuggestedPost.java
@@ -8,30 +8,30 @@ import io.github.natanimn.telebof.requests.Api;
  * ApproveSuggestedPost class. Returns True on success.
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
+ * @version 1.3.0
  * @see BotContext#approveSuggestedPost
  */
 public class ApproveSuggestedPost extends AbstractBaseRequest<ApproveSuggestedPost, Boolean> {
 
     /**
      * Required
-     * @param chat_id Unique identifier for the target direct messages chat
-     * @param message_id Identifier of a suggested post message to approve
+     * @param chatId Unique identifier for the target direct messages chat
+     * @param messageId Identifier of a suggested post message to approve
      * @param api api
      */
-    public ApproveSuggestedPost(Object chat_id, Integer message_id, Api api) {
-        super(chat_id, api, "approveSuggestedPost", Boolean.class);
-        add("message_id", message_id);
+    public ApproveSuggestedPost(Object chatId, Integer messageId, Api api) {
+        super(chatId, api, "approveSuggestedPost", Boolean.class);
+        add("message_id", messageId);
     }
 
     /**
      * Optional
-     * @param send_date Point in time (Unix timestamp) when the post is expected to be published;
+     * @param sendDate Point in time (Unix timestamp) when the post is expected to be published;
      *                  omit if the date has already been specified when the suggested post was created.
      *                  If specified, then the date must be not more than 2678400 seconds (30 days) in the future
      * @return {@link ApproveSuggestedPost}
      */
-    public ApproveSuggestedPost sendDate(Integer send_date) {
-        return add("send_date", send_date);
+    public ApproveSuggestedPost sendDate(Integer sendDate) {
+        return add("send_date", sendDate);
     }
 }
