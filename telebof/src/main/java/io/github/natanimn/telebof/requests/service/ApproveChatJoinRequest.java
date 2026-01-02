6fafb57b2ba5f4b813a578c7191b86a0e89a200e
natanimn <natanimco@gmail.com>
2026-01-02 09:39:19 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/ApproveChatJoinRequest.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/ApproveChatJoinRequest.java
@@ -8,19 +8,19 @@ import io.github.natanimn.telebof.requests.Api;
  * ApproveChatJoinRequest class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#approveChatJoinRequest
  */
 public class ApproveChatJoinRequest extends AbstractBaseRequest<ApproveChatJoinRequest, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param user_id user id
+     * @param chatId chat id
+     * @param userId user id
      * @param api api
      */
-    public ApproveChatJoinRequest(Object chat_id, long user_id, Api api) {
-        super(chat_id, api, "approveChatJoinRequest", Boolean.class);
-        add("user_id", user_id);
+    public ApproveChatJoinRequest(Object chatId, long userId, Api api) {
+        super(chatId, api, "approveChatJoinRequest", Boolean.class);
+        add("user_id", userId);
     }
 
 }
