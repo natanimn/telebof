a173b18e8360c73d6b029cedb21b8375f2cbf9e5
natanimn <natanimco@gmail.com>
2026-01-03 07:46:08 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/CloseForumTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/CloseForumTopic.java
@@ -8,18 +8,18 @@ import io.github.natanimn.telebof.BotContext;
  * CloseForumTopic class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#closeForumTopic(Object, int)
  */
 public class CloseForumTopic extends AbstractBaseRequest<Close, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_thread_id message thread id
+     * @param chatId chat id
+     * @param messageThreadId message thread id
      * @param api api
      */
-    public CloseForumTopic(Object chat_id, int message_thread_id, Api api) {
-        super(chat_id, api, "closeForumTopic", Boolean.class);
-        add("message_thread_id", message_thread_id);
+    public CloseForumTopic(Object chatId, int messageThreadId, Api api) {
+        super(chatId, api, "closeForumTopic", Boolean.class);
+        add("message_thread_id", messageThreadId);
     }
 }
