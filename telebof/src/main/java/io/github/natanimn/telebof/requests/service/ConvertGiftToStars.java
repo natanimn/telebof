e276808ec06d8b856fe74c11c6f0cdf1f8fd9c34
natanimn <natanimco@gmail.com>
2026-01-03 07:46:25 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/ConvertGiftToStars.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/ConvertGiftToStars.java
@@ -7,19 +7,19 @@ import io.github.natanimn.telebof.requests.Api;
  * ConvertGiftToStars class. Returns True on success.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  * @see io.github.natanimn.telebof.BotContext#convertGiftToStars
  */
 public class ConvertGiftToStars extends AbstractBaseRequest<ConvertGiftToStars, Boolean> {
     /**
      * Required
-     * @param business_connection_id Unique identifier of the business connection
-     * @param owned_gift_id Unique identifier of the regular gift that should be converted to Telegram Stars
+     * @param businessConnectionId Unique identifier of the business connection
+     * @param ownedGiftId Unique identifier of the regular gift that should be converted to Telegram Stars
      * @param api api
      */
-    public ConvertGiftToStars(String business_connection_id, String owned_gift_id, Api api) {
+    public ConvertGiftToStars(String businessConnectionId, String ownedGiftId, Api api) {
         super(api, "convertGiftToStars", Boolean.class);
-        add("business_connection_id", business_connection_id);
-        add("owned_gift_id", owned_gift_id);
+        add("business_connection_id", businessConnectionId);
+        add("owned_gift_id", ownedGiftId);
     }
 }
