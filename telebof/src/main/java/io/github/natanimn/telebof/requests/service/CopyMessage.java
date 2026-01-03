9da238d4f5846e005e9cb67515271420e88b5f44
natanimn <natanimco@gmail.com>
2026-01-03 07:46:33 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/CopyMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/CopyMessage.java
@@ -10,76 +10,74 @@ import io.github.natanimn.telebof.types.media_and_service.MessageId;
 import io.github.natanimn.telebof.types.ReplyParameters;
 import io.github.natanimn.telebof.types.suggested.SuggestedPostParameters;
 
-import java.util.List;
-
 /**
  * CopyMessage class.
  * Return {@link MessageId} on success
  * @see BotContext#copy
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
 public class CopyMessage extends AbstractBaseRequest<CopyMessage, MessageId> {
 
-    public CopyMessage(Object chat, Object fromChatId, int messageId, Api api) {
-        super(chat, api, "copyMessage", MessageId.class);
+    public CopyMessage(Object chatId, Object fromChatId, int messageId, Api api) {
+        super(chatId, api, "copyMessage", MessageId.class);
         add("from_chat_id", fromChatId);
         add("message_id", messageId);
     }
 
     /**
      * Optional
-     * @param message_thread_id Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+     * @param messageThreadId Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
      * @return {@link CopyMessage}
      */
-    public CopyMessage messageThreadId(int message_thread_id) {
-        return add("message_thread_id", message_thread_id);
+    public CopyMessage messageThreadId(int messageThreadId) {
+        return add("message_thread_id", messageThreadId);
     }
 
     /**
      * Optional
-     * @param parse_mode Mode for parsing entities in the new caption.
+     * @param parseMode Mode for parsing entities in the new caption.
      * @return {@link CopyMessage}
      */
-    public CopyMessage parseMode(ParseMode parse_mode) {
-        return add("parse_mode", parse_mode);
+    public CopyMessage parseMode(ParseMode parseMode) {
+        return add("parse_mode", parseMode);
     }
 
     /**
      * Optional
-     * @param disable_notification Sends the message silently. Users will receive a notification with no sound.
+     * @param disableNotification Sends the message silently. Users will receive a notification with no sound.
      * @return {@link CopyMessage}
      */
-    public CopyMessage disableNotification(boolean disable_notification) {
-        return add("disable_notification", disable_notification);
+    public CopyMessage disableNotification(boolean disableNotification) {
+        return add("disable_notification", disableNotification);
     }
 
     /**
      * Optional
-     * @param protect_content Protects the contents of the sent message from forwarding and saving
+     * @param protectContent Protects the contents of the sent message from forwarding and saving
      * @return {@link CopyMessage}
      */
-    public CopyMessage protectContent(boolean protect_content) {
-        return add("protect_content", protect_content);
+    public CopyMessage protectContent(boolean protectContent) {
+        return add("protect_content", protectContent);
     }
 
     /**
      * Optional
-     * @param reply_parameters Description of the message to reply to
+     * @param replyParameters Description of the message to reply to
      * @return {@link CopyMessage}
      */
-    public CopyMessage replyParameters(ReplyParameters reply_parameters){
-        return add("reply_parameters", reply_parameters);
+    public CopyMessage replyParameters(ReplyParameters replyParameters){
+        return add("reply_parameters", replyParameters);
     }
 
     /**
      * Optional
-     * @param reply_markup Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove a reply keyboard or to force a reply from the user
+     * @param replyMarkup Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove a reply keyboard or to force a reply from the user
      * @return {@link CopyMessage}
      */
-    public CopyMessage replyMarkup(Markup reply_markup) {
-        return add("reply_markup", reply_markup);
+    public CopyMessage replyMarkup(Markup replyMarkup) {
+        return add("reply_markup", replyMarkup);
     }
 
     /**
@@ -93,48 +91,48 @@ public class CopyMessage extends AbstractBaseRequest<CopyMessage, MessageId> {
 
     /**
      * Optional
-     * @param caption_entities A JSON-serialized list of special entities that appear in the new caption, which can be specified instead of parse_mode
+     * @param captionEntities A JSON-serialized list of special entities that appear in the new caption, which can be specified instead of parse_mode
      * @return {@link CopyMessage}
      */
-    public CopyMessage captionEntities(MessageEntity[] caption_entities) {
-        return add("caption_entities", List.of(caption_entities));
+    public CopyMessage captionEntities(MessageEntity[] captionEntities) {
+        return add("caption_entities", captionEntities);
     }
 
     /**
      * Optional
-     * @param show_caption_above_media Pass True, if the caption must be shown above the message media.
+     * @param showCaptionAboveMedia Pass True, if the caption must be shown above the message media.
      *                                 Ignored if a new caption isn't specified.
      * @return {@link CopyMessage}
      */
-    public CopyMessage showCaptionAboveMedia(boolean show_caption_above_media){
-        return add("show_caption_above_media", show_caption_above_media);
+    public CopyMessage showCaptionAboveMedia(boolean showCaptionAboveMedia){
+        return add("show_caption_above_media", showCaptionAboveMedia);
     }
 
     /**
      * Optional
-     * @param video_start_timestamp New start timestamp for the forwarded video in the message
+     * @param videoStartTimestamp New start timestamp for the forwarded video in the message
      * @return {@link CopyMessage}
      */
-    public CopyMessage videoStartTimestamp(int video_start_timestamp){
-        return add("video_start_timestamp", video_start_timestamp);
+    public CopyMessage videoStartTimestamp(int videoStartTimestamp){
+        return add("video_start_timestamp", videoStartTimestamp);
     }
 
     /***
      * Optional
-     * @param direct_messages_topic_id Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat
+     * @param directMessagesTopicId Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat
      * @return {@link CopyMessage}
      */
-    public CopyMessage directMessagesTopicId(int direct_messages_topic_id){
-        return add("direct_messages_topic_id", direct_messages_topic_id);
+    public CopyMessage directMessagesTopicId(int directMessagesTopicId){
+        return add("direct_messages_topic_id", directMessagesTopicId);
     }
 
     /**
      * Optional
-     * @param suggested_post_parameters A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.
+     * @param suggestedPostParameters A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.
      *                                  If the message is sent as a reply to another suggested post, then that suggested post is automatically declined.
      * @return {@link CopyMessage}
      */
-    public CopyMessage suggestedPostParameters(SuggestedPostParameters suggested_post_parameters){
-        return add("suggested_post_parameters", suggested_post_parameters);
+    public CopyMessage suggestedPostParameters(SuggestedPostParameters suggestedPostParameters){
+        return add("suggested_post_parameters", suggestedPostParameters);
     }
 }
