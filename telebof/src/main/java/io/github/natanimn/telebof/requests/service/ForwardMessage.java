6547fa5e57c971164ef3c2dc1016d1516e8c4f62
natanimn <natanimco@gmail.com>
2026-01-04 09:15:33 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/ForwardMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/ForwardMessage.java
@@ -11,77 +11,77 @@ import io.github.natanimn.telebof.types.updates.Message;
  * Returns {@link Message} on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#forward
  */
 public class ForwardMessage extends AbstractBaseRequest<ForwardMessage, Message> {
 
     /**
      * Required
-     * @param chat_id chat id
-     * @param from_chat_id from chat id
-     * @param message_id message id
+     * @param chatId chat id
+     * @param fromChatId from chat id
+     * @param messageId message id
      * @param api api
      */
-    public ForwardMessage(Object chat_id, Object from_chat_id, int message_id, Api api) {
-        super(chat_id, api, "forwardMessage", Message.class);
-        add("from_chat_id", from_chat_id);
-        add("message_id", message_id);
+    public ForwardMessage(Object chatId, Object fromChatId, int messageId, Api api) {
+        super(chatId, api, "forwardMessage", Message.class);
+        add("from_chat_id", fromChatId);
+        add("message_id", messageId);
     }
 
     /**
      * Optional
-     * @param message_thread_id Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+     * @param messageThreadId Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
      * @return {@link ForwardMessage}
      */
-    public ForwardMessage messageThreadId(int message_thread_id) {
-        return add("message_thread_id", message_thread_id);
+    public ForwardMessage messageThreadId(int messageThreadId) {
+        return add("message_thread_id", messageThreadId);
     }
 
     /**
      * Optional
-     * @param disable_notification Sends the message silently. Users will receive a notification with no sound.
+     * @param disableNotification Sends the message silently. Users will receive a notification with no sound.
      * @return {@link ForwardMessage}
      */
-    public ForwardMessage disableNotification(boolean disable_notification) {
-        return add("disable_notification", disable_notification);
+    public ForwardMessage disableNotification(boolean disableNotification) {
+        return add("disable_notification", disableNotification);
     }
 
     /**
      * Optional
-     * @param protect_content Protects the contents of the forwarded message from forwarding and saving
+     * @param protectContent Protects the contents of the forwarded message from forwarding and saving
      * @return {@link ForwardMessage}
      */
-    public ForwardMessage protectContent(boolean protect_content) {
-        return add("protect_content", protect_content);
+    public ForwardMessage protectContent(boolean protectContent) {
+        return add("protect_content", protectContent);
     }
 
     /**
      * Optional
-     * @param video_start_timestamp New start timestamp for the forwarded video in the message
+     * @param videoStartTimestamp New start timestamp for the forwarded video in the message
      * @return {@link ForwardMessage}
      */
-    public ForwardMessage videoStartTimestamp(int video_start_timestamp){
-        return add("video_start_timestamp", video_start_timestamp);
+    public ForwardMessage videoStartTimestamp(int videoStartTimestamp){
+        return add("video_start_timestamp", videoStartTimestamp);
     }
 
     /***
      * Optional
-     * @param direct_messages_topic_id Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat
+     * @param directMessagesTopicId Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat
      * @return {@link ForwardMessage}
      */
-    public ForwardMessage directMessagesTopicId(int direct_messages_topic_id){
-        return add("direct_messages_topic_id", direct_messages_topic_id);
+    public ForwardMessage directMessagesTopicId(int directMessagesTopicId){
+        return add("direct_messages_topic_id", directMessagesTopicId);
     }
 
     /**
      * Optional
-     * @param suggested_post_parameters A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.
+     * @param suggestedPostParameters A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.
      *                                  If the message is sent as a reply to another suggested post, then that suggested post is automatically declined.
      * @return {@link ForwardMessage}
      */
-    public ForwardMessage suggestedPostParameters(SuggestedPostParameters suggested_post_parameters){
-        return add("suggested_post_parameters", suggested_post_parameters);
+    public ForwardMessage suggestedPostParameters(SuggestedPostParameters suggestedPostParameters){
+        return add("suggested_post_parameters", suggestedPostParameters);
     }
 }
 
