80c502134702b1c9fda9c38bdac0fdf9bded89e9
natanimn <natanimco@gmail.com>
2026-01-05 06:18:41 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/LeaveChat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/LeaveChat.java
@@ -8,17 +8,17 @@ import io.github.natanimn.telebof.requests.Api;
  * LeaveChat class. Returns True on success
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#leaveChat
  */
 public class LeaveChat extends AbstractBaseRequest<LeaveChat, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public LeaveChat(Object chat_id, Api api) {
-        super(chat_id, api, "leaveChat", Boolean.class);
+    public LeaveChat(Object chatId, Api api) {
+        super(chatId, api, "leaveChat", Boolean.class);
     }
 
 }
