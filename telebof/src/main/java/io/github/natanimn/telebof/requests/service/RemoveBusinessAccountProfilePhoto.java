e2aa2527501a6d258dc4112ccd64845be43f2951
natanimn <natanimco@gmail.com>
2026-01-05 06:19:34 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/RemoveBusinessAccountProfilePhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/RemoveBusinessAccountProfilePhoto.java
@@ -7,27 +7,27 @@ import io.github.natanimn.telebof.requests.Api;
  * RemoveBusinessAccountProfilePhoto class. Returns True on success.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  * @see io.github.natanimn.telebof.BotContext#removeBusinessAccountProfilePhoto
  */
 public class RemoveBusinessAccountProfilePhoto extends AbstractBaseRequest<RemoveBusinessAccountProfilePhoto, Boolean> {
     /**
      * Required
-     * @param business_connection_id business connection id
+     * @param businessConnectionId business connection id
      * @param api api
      */
-    public RemoveBusinessAccountProfilePhoto(String business_connection_id, Api api){
+    public RemoveBusinessAccountProfilePhoto(String businessConnectionId, Api api){
         super(api, "removeBusinessAccountProfilePhoto", Boolean.class);
-        add("business_connection_id", business_connection_id);
+        add("business_connection_id", businessConnectionId);
     }
 
     /**
      * Optional
-     * @param is_public Pass True to remove the public photo, which is visible even if the main photo is hidden by the business account's privacy settings.
+     * @param isPublic Pass True to remove the public photo, which is visible even if the main photo is hidden by the business account's privacy settings.
      *                  After the main photo is removed, the previous profile photo (if present) becomes the main photo.
      * @return {@link RemoveBusinessAccountProfilePhoto}
      */
-    public RemoveBusinessAccountProfilePhoto isPublic(boolean is_public){
-        return add("is_public", is_public);
+    public RemoveBusinessAccountProfilePhoto isPublic(boolean isPublic){
+        return add("is_public", isPublic);
     }
 }
