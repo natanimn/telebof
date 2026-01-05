ffd3185741d293c6dbb0282f40a1412c2adab3c2
natanimn <natanimco@gmail.com>
2026-01-05 06:19:00 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/PromoteChatMember.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/PromoteChatMember.java
@@ -8,166 +8,166 @@ import io.github.natanimn.telebof.requests.Api;
  * PromoteChatMember class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 1.2.0
+ * @version 1.3.0
  * @see BotContext#promoteChatMember
  */
 public class PromoteChatMember extends AbstractBaseRequest<PromoteChatMember, Boolean> {
 
     /**
      * Required
-     * @param chat_id chat id
-     * @param user_id user id
+     * @param chatId chat id
+     * @param userId user id
      * @param api api
      */
-    public PromoteChatMember(Object chat_id, long user_id, Api api) {
-        super(chat_id, api, "promoteChatMember", Boolean.class);
-        add("user_id", user_id);
+    public PromoteChatMember(Object chatId, long userId, Api api) {
+        super(chatId, api, "promoteChatMember", Boolean.class);
+        add("user_id", userId);
     }
 
     /**
      * Optional
-     * @param is_anonymous Pass True if the administrator's presence in the chat is hidden
+     * @param isAnonymous Pass True if the administrator's presence in the chat is hidden
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember isAnonymous(boolean is_anonymous) {
-        return add("is_anonymous", is_anonymous);
+    public PromoteChatMember isAnonymous(boolean isAnonymous) {
+        return add("is_anonymous", isAnonymous);
     }
 
     /**
      * Optional
-     * @param can_manage_chat Pass True if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members, report spam messages,
+     * @param canManageChat Pass True if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members, report spam messages,
      *                        ignore slow mode, and send messages to the chat without paying Telegram Stars. Implied by any other administrator privilege.
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canManageChat(boolean can_manage_chat) {
-        return add("can_manage_chat", can_manage_chat);
+    public PromoteChatMember canManageChat(boolean canManageChat) {
+        return add("can_manage_chat", canManageChat);
     }
 
     /**
      * Optional
-     * @param can_post_messages Pass True if the administrator can post messages in the channel, approve suggested posts, or access channel statistics; for channels only
+     * @param canPostMessages Pass True if the administrator can post messages in the channel, approve suggested posts, or access channel statistics; for channels only
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canPostMessages(boolean can_post_messages) {
-        return add("can_post_messages", can_post_messages);
+    public PromoteChatMember canPostMessages(boolean canPostMessages) {
+        return add("can_post_messages", canPostMessages);
     }
 
     /**
      * Optional
-     * @param can_edit_messages Pass True if the administrator can edit messages of other users and can pin messages; for channels only
+     * @param canEditMessages Pass True if the administrator can edit messages of other users and can pin messages; for channels only
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canEditMessages(boolean can_edit_messages) {
-        return add("can_edit_messages", can_edit_messages);
+    public PromoteChatMember canEditMessages(boolean canEditMessages) {
+        return add("can_edit_messages", canEditMessages);
     }
 
     /**
      * Optional
-     * @param can_delete_messages Pass True if the administrator can delete messages of other users
+     * @param canDeleteMessages Pass True if the administrator can delete messages of other users
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canDeleteMessages(boolean can_delete_messages) {
-        return add("can_delete_messages", can_delete_messages);
+    public PromoteChatMember canDeleteMessages(boolean canDeleteMessages) {
+        return add("can_delete_messages", canDeleteMessages);
     }
 
     /**
      * Optional
-     * @param can_manage_video_chats Pass True if the administrator can manage video chats
+     * @param canManageVideoChats Pass True if the administrator can manage video chats
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canManageVideoChats(boolean can_manage_video_chats) {
-        return add("can_manage_video_chats", can_manage_video_chats);
+    public PromoteChatMember canManageVideoChats(boolean canManageVideoChats) {
+        return add("can_manage_video_chats", canManageVideoChats);
     }
 
     /**
      * Optional
-     * @param can_restrict_members Pass True if the administrator can restrict, ban or unban chat members, or access supergroup statistics
+     * @param canRestrictMembers Pass True if the administrator can restrict, ban or unban chat members, or access supergroup statistics
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember can_restrict_members(boolean can_restrict_members) {
-        return add("can_restrict_members", can_restrict_members);
+    public PromoteChatMember canRestrictMembers(boolean canRestrictMembers) {
+        return add("can_restrict_members", canRestrictMembers);
     }
 
     /**
      * Optional
-     * @param can_promote_members Pass True if the administrator can add new administrators with a subset of their own privileges or demote administrators that they have promoted,
+     * @param canPromoteMembers Pass True if the administrator can add new administrators with a subset of their own privileges or demote administrators that they have promoted,
      *                            directly or indirectly (promoted by administrators that were appointed by him)
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember can_promote_members(boolean can_promote_members) {
-        return add("can_promote_members", can_promote_members);
+    public PromoteChatMember canPromoteMembers(boolean canPromoteMembers) {
+        return add("can_promote_members", canPromoteMembers);
     }
 
     /**
      * Optional
-     * @param can_change_info Pass True if the administrator can change chat title, photo and other settings
+     * @param canChangeInfo Pass True if the administrator can change chat title, photo and other settings
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canChangeInfo(boolean can_change_info) {
-        return add("can_change_info", can_change_info);
+    public PromoteChatMember canChangeInfo(boolean canChangeInfo) {
+        return add("can_change_info", canChangeInfo);
     }
 
     /**
      * Optional
-     * @param can_invite_users Pass True if the administrator can invite new users to the chat
+     * @param canInviteUsers Pass True if the administrator can invite new users to the chat
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canInviteUsers(boolean can_invite_users) {
-        return add("can_invite_users", can_invite_users);
+    public PromoteChatMember canInviteUsers(boolean canInviteUsers) {
+        return add("can_invite_users", canInviteUsers);
     }
 
     /**
      * Optional
-     * @param can_pin_messages Pass True if the administrator can pin messages; for supergroups only
+     * @param canPinMessages Pass True if the administrator can pin messages; for supergroups only
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canPinMessages(boolean can_pin_messages) {
-        return add("can_pin_messages", can_pin_messages);
+    public PromoteChatMember canPinMessages(boolean canPinMessages) {
+        return add("can_pin_messages", canPinMessages);
     }
 
     /**
      * Optional
-     * @param can_manage_topics Pass True if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
+     * @param canManageTopics Pass True if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canManageTopics(boolean can_manage_topics) {
-        return add("can_manage_topics", can_manage_topics);
+    public PromoteChatMember canManageTopics(boolean canManageTopics) {
+        return add("can_manage_topics", canManageTopics);
     }
 
     /**
      * Optional
-     * @param can_post_stories Pass True if the administrator can post stories to the chat
+     * @param canPostStories Pass True if the administrator can post stories to the chat
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canPostStories(boolean can_post_stories){
-        return add("can_post_stories", can_post_stories);
+    public PromoteChatMember canPostStories(boolean canPostStories){
+        return add("can_post_stories", canPostStories);
     }
 
     /**
      * Optional
-     * @param can_edit_stories Pass True if the administrator can edit stories posted by other users, post stories to the chat page,
+     * @param canEditStories Pass True if the administrator can edit stories posted by other users, post stories to the chat page,
      *                         pin chat stories, and access the chat's story archive
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canEditStories(boolean can_edit_stories){
-        return add("can_edit_stories", can_edit_stories);
+    public PromoteChatMember canEditStories(boolean canEditStories){
+        return add("can_edit_stories", canEditStories);
     }
 
     /**
      * Optional
-     * @param can_delete_stories Pass True if the administrator can delete stories posted by other users
+     * @param canDeleteStories Pass True if the administrator can delete stories posted by other users
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canDeleteStories(boolean can_delete_stories){
-        return add("can_delete_stories", can_delete_stories);
+    public PromoteChatMember canDeleteStories(boolean canDeleteStories){
+        return add("can_delete_stories", canDeleteStories);
     }
 
     /**
      * Optional
-     * @param can_manage_direct_messages Pass True if the administrator can manage direct messages within the channel and decline suggested posts; for channels only
+     * @param canManageDirectMessages Pass True if the administrator can manage direct messages within the channel and decline suggested posts; for channels only
      * @return {@link PromoteChatMember}
      */
-    public PromoteChatMember canManageDirectMessages(boolean can_manage_direct_messages){
-        return add("can_manage_direct_messages", can_manage_direct_messages);
+    public PromoteChatMember canManageDirectMessages(boolean canManageDirectMessages){
+        return add("can_manage_direct_messages", canManageDirectMessages);
     }
 }
