e1bdb10f6714106b432bab927e86a17fda8f2fe5
natanimn <natanimco@gmail.com>
2026-01-05 06:18:52 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/PostStory.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/PostStory.java
@@ -12,22 +12,22 @@ import io.github.natanimn.telebof.types.story.StoryArea;
  * PostStory class. Returns {@link Story} on success.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  * @see io.github.natanimn.telebof.BotContext#postStory
  */
 public class PostStory extends AbstractBaseRequest<PostStory, Story> {
     /**
      * Required
-     * @param business_connection_id Unique identifier of the business connection
+     * @param businessConnectionId Unique identifier of the business connection
      * @param content Content of the story
-     * @param active_period Period after which the story is moved to the archive, in seconds
+     * @param activePeriod Period after which the story is moved to the archive, in seconds
      * @param api api
      */
-    public PostStory(String business_connection_id, InputStoryContent content, Integer active_period, Api api) {
+    public PostStory(String businessConnectionId, InputStoryContent content, Integer activePeriod, Api api) {
         super(api, "postStory", Story.class);
-        add("business_connection_id", business_connection_id);
+        add("business_connection_id", businessConnectionId);
         add("content", content);
-        add("active_period", active_period);
+        add("active_period", activePeriod);
     }
 
     /**
@@ -41,20 +41,20 @@ public class PostStory extends AbstractBaseRequest<PostStory, Story> {
 
     /**
      * Optional
-     * @param parse_mode Mode for parsing entities in the story caption
+     * @param parseMode Mode for parsing entities in the story caption
      * @return {@link PostStory}
      */
-    public PostStory parseMode(ParseMode parse_mode) {
-        return add("parse_mode", parse_mode);
+    public PostStory parseMode(ParseMode parseMode) {
+        return add("parse_mode", parseMode);
     }
 
     /**
      * Optional
-     * @param caption_entities A JSON-serialized list of special entities that appear in the caption, which can be specified instead of {@link #parseMmode}
+     * @param captionEntities A JSON-serialized list of special entities that appear in the caption, which can be specified instead of {@link #parseMode}
      * @return {@link PostStory}
      */
-    public PostStory captionEntities(MessageEntity[] caption_entities) {
-        return add("caption_entities", caption_entities);
+    public PostStory captionEntities(MessageEntity[] captionEntities) {
+        return add("caption_entities", captionEntities);
     }
 
     /**
@@ -68,19 +68,19 @@ public class PostStory extends AbstractBaseRequest<PostStory, Story> {
 
     /**
      * Optional
-     * @param post_to_chat_page Pass True to keep the story accessible after it expires
+     * @param postToChatPage Pass True to keep the story accessible after it expires
      * @return {@link PostStory}
      */
-    public PostStory postToChatPage(boolean post_to_chat_page) {
-        return add("post_to_chat_page", post_to_chat_page);
+    public PostStory postToChatPage(boolean postToChatPage) {
+        return add("post_to_chat_page", postToChatPage);
     }
 
     /**
      * Optional
-     * @param protect_content Pass True if the content of the story must be protected from forwarding and screenshotting
+     * @param protectContent Pass True if the content of the story must be protected from forwarding and screenshotting
      * @return {@link PostStory}
      */
-    public PostStory protectContent(boolean protect_content) {
-        return add("protect_content", protect_content);
+    public PostStory protectContent(boolean protectContent) {
+        return add("protect_content", protectContent);
     }
 }
