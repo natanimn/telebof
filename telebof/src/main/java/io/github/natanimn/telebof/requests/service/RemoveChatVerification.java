73be9a77eb7fdc6b8848aa329d8347cb71f7a9a9
natanimn <natanimco@gmail.com>
2026-01-06 05:13:27 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/RemoveChatVerification.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/RemoveChatVerification.java
@@ -7,15 +7,15 @@ import io.github.natanimn.telebof.requests.Api;
  * Removes verification from a chat that is currently verified on <a hre="https://telegram.org/verify#third-party-verification">behalf of the organization</a> represented by the bot.
  * @author Natanim
  * @since 1 August 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class RemoveChatVerification extends AbstractBaseRequest<RemoveChatVerification, Boolean> {
     /**
-     * @param chat_id Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+     * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
      * @param rs api
      */
-    public RemoveChatVerification(Object chat_id, Api rs){
-        super(chat_id, rs, "removeChatVerification", Boolean.class);
+    public RemoveChatVerification(Object chatId, Api rs){
+        super(chatId, rs, "removeChatVerification", Boolean.class);
     }
 
 }
