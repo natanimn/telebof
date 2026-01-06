3ffc187360d91ed68660242c0259263d2c9123af
natanimn <natanimco@gmail.com>
2026-01-06 05:13:47 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/RemoveUserVerification.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/RemoveUserVerification.java
@@ -7,16 +7,16 @@ import io.github.natanimn.telebof.requests.Api;
  * Removes verification from a user who is currently verified on <a hre="https://telegram.org/verify#third-party-verification">behalf of the organization</a> represented by the bot.
  * @author Natanim
  * @since 1 August 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class RemoveUserVerification extends AbstractBaseRequest<RemoveUserVerification, Boolean> {
     /**
-     * @param user_id Unique identifier of the target user
-     * @param rs api
+     * @param userId Unique identifier of the target user
+     * @param api api
      */
-    public RemoveUserVerification(long user_id, Api rs){
-        super(rs, "removeUserVerification");
-        add("user_id", user_id);
+    public RemoveUserVerification(long userId, Api api){
+        super(api, "removeUserVerification");
+        add("user_id", userId);
     }
 
 }
