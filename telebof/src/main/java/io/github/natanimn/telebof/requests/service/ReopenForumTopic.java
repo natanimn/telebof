a3b30c8aab475d0ce08b807b42feabff0845ad34
natanimn <natanimco@gmail.com>
2026-01-06 05:13:56 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/ReopenForumTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/ReopenForumTopic.java
@@ -8,20 +8,20 @@ import io.github.natanimn.telebof.BotContext;
  * ReopenForumTopic class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#reopenForumTopic
  */
 public class ReopenForumTopic extends AbstractBaseRequest<ReopenForumTopic, Boolean> {
 
     /**
      * Required
-     * @param chat_id chat id
-     * @param message_thread_id message thread id
+     * @param chatId chat id
+     * @param messageThreadId message thread id
      * @param api api
      */
-    public ReopenForumTopic(Object chat_id, int message_thread_id, Api api) {
-        super(chat_id, api, "reopenForumTopic", Boolean.class);
-        add("message_thread_id", message_thread_id);
+    public ReopenForumTopic(Object chatId, int messageThreadId, Api api) {
+        super(chatId, api, "reopenForumTopic", Boolean.class);
+        add("message_thread_id", messageThreadId);
     }
 
 }
