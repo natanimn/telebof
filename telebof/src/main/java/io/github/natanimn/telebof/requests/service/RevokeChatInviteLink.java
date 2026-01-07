2d0ebacc127518db232513da6a3839b5733eccee
natanimn <natanimco@gmail.com>
2026-01-07 11:23:07 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/RevokeChatInviteLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/RevokeChatInviteLink.java
@@ -9,18 +9,18 @@ import io.github.natanimn.telebof.types.chat_and_user.ChatInviteLink;
  * RevokeChatInviteLink class
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#revokeChatInviteLink
  */
 public class RevokeChatInviteLink extends AbstractBaseRequest<RevokeChatInviteLink, ChatInviteLink> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param invite_link invite link
      * @param api api
      */
-    public RevokeChatInviteLink(Object chat_id, String invite_link, Api api) {
-        super(chat_id, api, "revokeChatInviteLink", ChatInviteLink.class);
+    public RevokeChatInviteLink(Object chatId, String invite_link, Api api) {
+        super(chatId, api, "revokeChatInviteLink", ChatInviteLink.class);
         add("invite_link", invite_link);
     }
 
