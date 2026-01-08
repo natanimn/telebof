b82a6870a2b23000756f736286c132c7ba2c0508
natanimn <natanimco@gmail.com>
2026-01-09 02:21:16 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetBusinessAccountName.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetBusinessAccountName.java
@@ -7,28 +7,28 @@ import io.github.natanimn.telebof.requests.Api;
  * SetBusinessAccountName class. Returns True on success.
  * @author Natanim
  * @since 19 August 2025
- * @version 1.0.0
+ * @version 1.3.0
  * @see io.github.natanimn.telebof.BotContext#setBusinessAccountName
  */
 public class SetBusinessAccountName extends AbstractBaseRequest<SetBusinessAccountName, Boolean> {
     /**
      * Required
-     * @param business_connection_id business connection id
-     * @param first_name first name
+     * @param businessConnectionId business connection id
+     * @param firstName first name
      * @param api api
      */
-    public SetBusinessAccountName(String business_connection_id, String first_name, Api api){
+    public SetBusinessAccountName(String businessConnectionId, String firstName, Api api){
         super(api, "setBusinessAccountName", Boolean.class);
-        add("business_connection_id", business_connection_id);
-        add("first_name", first_name);
+        add("business_connection_id", businessConnectionId);
+        add("first_name", firstName);
     }
 
     /**
      * Optional
-     * @param last_name The new value of the last name for the business account; 0-64 characters
+     * @param lastName The new value of the last name for the business account; 0-64 characters
      * @return {@link SetBusinessAccountName}
      */
-    public SetBusinessAccountName lastName(String last_name){
-        return add("last_name", last_name);
+    public SetBusinessAccountName lastName(String lastName){
+        return add("last_name", lastName);
     }
 }
