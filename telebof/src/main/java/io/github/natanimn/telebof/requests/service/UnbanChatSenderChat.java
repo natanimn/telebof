63e1b4cc3ec774221e119ce62053d7df050ff6b1
natanimn <natanimco@gmail.com>
2026-01-08 09:46:32 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/UnbanChatSenderChat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/UnbanChatSenderChat.java
@@ -8,19 +8,19 @@ import io.github.natanimn.telebof.requests.Api;
  * UnbanChatSenderChat class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#unbanChatSenderChat(Object, long)
  */
 public class UnbanChatSenderChat extends AbstractBaseRequest<UnbanChatSenderChat, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param sender_chat_id sender chat id
+     * @param chatId chat id
+     * @param senderChatId sender chat id
      * @param api api
      */
-    public UnbanChatSenderChat(Object chat_id, long sender_chat_id, Api api) {
-        super(chat_id, api, "unbanChatSenderChat", Boolean.class);
-        add("sender_chat_id", sender_chat_id);
+    public UnbanChatSenderChat(Object chatId, long senderChatId, Api api) {
+        super(chatId, api, "unbanChatSenderChat", Boolean.class);
+        add("sender_chat_id", senderChatId);
     }
 
 }
