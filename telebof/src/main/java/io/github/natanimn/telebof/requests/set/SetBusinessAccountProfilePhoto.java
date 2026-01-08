f26bde3f16ace900759b1ae7dac9250abdfdf4fc
natanimn <natanimco@gmail.com>
2026-01-09 02:21:29 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetBusinessAccountProfilePhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetBusinessAccountProfilePhoto.java
@@ -6,34 +6,32 @@ import io.github.natanimn.telebof.types.input.InputProfilePhoto;
 
 /**
  * SetBusinessAccountProfilePhoto class. Returns True on success.
- *
  * @author Natanim
- * @version 1.0.0
+ * @version 1.3.0
  * @see io.github.natanimn.telebof.BotContext#setBusinessAccountProfilePhoto
  * @since 19 August 2025
  */
 public class SetBusinessAccountProfilePhoto extends AbstractBaseRequest<SetBusinessAccountProfilePhoto, Boolean> {
     /**
      * Required
-     *
-     * @param business_connection_id business connection id
+     * @param businessConnectionId business connection id
      * @param photo                  photo
      * @param api                    api
      */
-    public SetBusinessAccountProfilePhoto(String business_connection_id, InputProfilePhoto photo, Api api) {
+    public SetBusinessAccountProfilePhoto(String businessConnectionId, InputProfilePhoto photo, Api api) {
         super(api, "setBusinessAccountProfilePhoto", Boolean.class);
-        add("business_connection_id", business_connection_id);
+        add("business_connection_id", businessConnectionId);
         add("photo", photo);
     }
 
     /**
      * Optional
      *
-     * @param is_public Pass True to set the public photo, which will be visible even if the main photo is hidden by the business account's privacy settings.
+     * @param isPublic Pass True to set the public photo, which will be visible even if the main photo is hidden by the business account's privacy settings.
      *                  An account can have only one public photo.
      * @return {@link SetBusinessAccountProfilePhoto}
      */
-    public SetBusinessAccountProfilePhoto isPublic(boolean is_public) {
-        return add("is_public", is_public);
+    public SetBusinessAccountProfilePhoto isPublic(boolean isPublic) {
+        return add("is_public", isPublic);
     }
 }
