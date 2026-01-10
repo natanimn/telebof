bc945f376d4966d872ff4fe047487aa86dac5813
natanimn <natanimco@gmail.com>
2026-01-10 09:20:00 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/VerifyChat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/VerifyChat.java
@@ -7,24 +7,24 @@ import io.github.natanimn.telebof.requests.Api;
  * Verifies a chat on <a hre="https://telegram.org/verify#third-party-verification">behalf of the organization</a> which is represented by the bot.
  * @author Natanim
  * @since 1 August 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class VerifyChat extends AbstractBaseRequest<VerifyChat, Boolean> {
     /**
-     * @param chat_id Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-     * @param rs Request
+     * @param chatId Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+     * @param api api
      */
-    public VerifyChat(Object chat_id, Api rs){
-        super(chat_id, rs, "verifyChat", Boolean.class);
+    public VerifyChat(Object chatId, Api api){
+        super(chatId, api, "verifyChat", Boolean.class);
     }
 
     /**
      * Optional
-     * @param custom_description Custom description for the verification; 0-70 characters. Must be empty if the organization
+     * @param customDescription Custom description for the verification; 0-70 characters. Must be empty if the organization
      *                           isn't allowed to provide a custom verification description.
      * @return {@link #VerifyChat}
      */
-    public VerifyChat customDescription(String custom_description){
-        return add("custom_description", custom_description);
+    public VerifyChat customDescription(String customDescription){
+        return add("custom_description", customDescription);
     }
 }
