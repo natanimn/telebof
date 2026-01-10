05b5ff695f46ce8ad4c05f1bd8c797b84c3e62f5
natanimn <natanimco@gmail.com>
2026-01-10 09:19:33 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/UnHideGeneralForumTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/UnHideGeneralForumTopic.java
@@ -8,17 +8,17 @@ import io.github.natanimn.telebof.requests.Api;
  * UnHideGeneralForumTopic class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#unHideGeneralForumTopic
  */
 public class UnHideGeneralForumTopic extends AbstractBaseRequest<UnHideGeneralForumTopic, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param api api
      */
-    public UnHideGeneralForumTopic(Object chat_id, Api api) {
-        super(chat_id, api, "unHideGeneralForumTopic", Boolean.class);
+    public UnHideGeneralForumTopic(Object chatId, Api api) {
+        super(chatId, api, "unHideGeneralForumTopic", Boolean.class);
     }
 
 }
