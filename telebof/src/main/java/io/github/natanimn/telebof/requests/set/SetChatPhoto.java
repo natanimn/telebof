4271e1db713b5ed372c3e5b568f691f1852fed81
natanimn <natanimco@gmail.com>
2026-01-11 08:53:51 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetChatPhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetChatPhoto.java
@@ -10,18 +10,18 @@ import java.io.File;
  * SetChatPhoto class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#setChatPhoto
  */
 public class SetChatPhoto extends AbstractBaseRequest<SetChatPhoto, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param photo photo
      * @param api api
      */
-    public SetChatPhoto(Object chat_id, File photo, Api api) {
-        super(chat_id, api, "setChatPhoto", Boolean.class);
+    public SetChatPhoto(Object chatId, File photo, Api api) {
+        super(chatId, api, "setChatPhoto", Boolean.class);
         add("photo", photo);
     }
 
