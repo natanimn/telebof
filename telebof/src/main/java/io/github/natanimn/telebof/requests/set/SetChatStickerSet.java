b6f43d61634d102cec661e50950591105ec8cbea
natanimn <natanimco@gmail.com>
2026-01-11 08:53:55 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetChatStickerSet.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetChatStickerSet.java
@@ -8,19 +8,19 @@ import io.github.natanimn.telebof.BotContext;
  * SetChatStickerSet class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#setChatStickerSet
  */
 public class SetChatStickerSet extends AbstractBaseRequest<SetChatStickerSet, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
-     * @param sticker_set_name sticker set name
+     * @param chatId chat id
+     * @param stickerSetName sticker set name
      * @param api api
      */
-    public SetChatStickerSet(Object chat_id, String sticker_set_name, Api api) {
-        super(chat_id, api, "setChatStickerSet", Boolean.class);
-        add("sticker_set_name", sticker_set_name);
+    public SetChatStickerSet(Object chatId, String stickerSetName, Api api) {
+        super(chatId, api, "setChatStickerSet", Boolean.class);
+        add("sticker_set_name", stickerSetName);
     }
 
 }
