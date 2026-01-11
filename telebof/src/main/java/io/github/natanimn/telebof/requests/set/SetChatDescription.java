e7650e8211413b610fac9aca07e342dc8d8e0eb7
natanimn <natanimco@gmail.com>
2026-01-11 08:53:32 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetChatDescription.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetChatDescription.java
@@ -8,18 +8,18 @@ import io.github.natanimn.telebof.requests.Api;
  * SetChatDescription class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#setChatDescription
  */
 public class SetChatDescription extends AbstractBaseRequest<SetChatDescription, Boolean> {
     /**
      * Required
-     * @param chat_id chat id
+     * @param chatId chat id
      * @param title title
      * @param api api
      */
-    public SetChatDescription(Object chat_id, String title, Api api) {
-        super(chat_id, api, "setChatDescription", Boolean.class);
+    public SetChatDescription(Object chatId, String title, Api api) {
+        super(chatId, api, "setChatDescription", Boolean.class);
         add("description", title);
     }
 
