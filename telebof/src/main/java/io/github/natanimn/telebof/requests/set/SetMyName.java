b3e517325764292f4f4c7b2abd21c1460b7ffcaa
natanimn <natanimco@gmail.com>
2026-01-12 07:14:24 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetMyName.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetMyName.java
@@ -8,7 +8,7 @@ import io.github.natanimn.telebof.requests.Api;
  * SetMyName class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#setMyName()
  */
 public class SetMyName extends AbstractBaseRequest<SetMyName, Boolean> {
@@ -32,10 +32,10 @@ public class SetMyName extends AbstractBaseRequest<SetMyName, Boolean> {
 
     /**
      * Optional
-     * @param language_code A two-letter ISO 639-1 language code. If empty, the name will be shown to all users for whose language there is no dedicated name.
+     * @param languageCode A two-letter ISO 639-1 language code. If empty, the name will be shown to all users for whose language there is no dedicated name.
      * @return {@link SetMyName}
      */
-    public SetMyName languageCode(String language_code){
-        return add("language_code", language_code);
+    public SetMyName languageCode(String languageCode){
+        return add("language_code", languageCode);
     }
 }
