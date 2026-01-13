f1e3db6310090573cd15e3f4b8df06cb942e9511
natanimn <natanimco@gmail.com>
2026-01-13 12:50:25 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetStickerKeywords.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetStickerKeywords.java
@@ -4,26 +4,23 @@ import io.github.natanimn.telebof.BotContext;
 import io.github.natanimn.telebof.requests.AbstractBaseRequest;
 import io.github.natanimn.telebof.requests.Api;
 
-import java.util.List;
-
 /**
  * SetStickerKeywords class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#setStickerKeywords
  */
 public class SetStickerKeywords extends AbstractBaseRequest<SetStickerKeywords, Boolean> {
     /**
      * Required
-     * @param sticker stciker
+     * @param sticker sticker
      * @param keywords keywords
      * @param api api
      */
     public SetStickerKeywords(String sticker, String[] keywords, Api api) {
         super(api, "setStickerKeywords");
         add("sticker", sticker);
-        add("keywords", List.of(keywords));
+        add("keywords", keywords);
     }
-
 }
