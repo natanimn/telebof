44dd731ba74a8f20a0529f45b7df4d0c441647b6
natanimn <natanimco@gmail.com>
2026-01-13 12:50:18 +0300

    Refactor: replaced snake case params with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetStickerEmojiList.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/set/SetStickerEmojiList.java
@@ -4,26 +4,23 @@ import io.github.natanimn.telebof.BotContext;
 import io.github.natanimn.telebof.requests.AbstractBaseRequest;
 import io.github.natanimn.telebof.requests.Api;
 
-import java.util.List;
-
 /**
  * SetStickerEmojiList class. Returns True on success.
  * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  * @see BotContext#setStickerEmojiList
  */
 public class SetStickerEmojiList extends AbstractBaseRequest<SetStickerEmojiList, Boolean> {
     /**
      * Required
      * @param sticker sticker
-     * @param emoji_list emoji list
+     * @param emojiList emoji list
      * @param api api
      */
-    public SetStickerEmojiList(String sticker, String[] emoji_list, Api api) {
+    public SetStickerEmojiList(String sticker, String[] emojiList, Api api) {
         super(api, "setStickerEmojiList");
         add("sticker", sticker);
-        add("emoji_list", List.of(emoji_list));
+        add("emoji_list", emojiList);
     }
-
 }
