64ec4f9a6c0b13fc7d4b6ad6662e58884f31ef18
natanimn <natanimco@gmail.com>
2026-01-14 14:35:05 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/passport/PassportFile.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/passport/PassportFile.java
@@ -12,17 +12,49 @@ import java.io.Serializable;
  *<br>
  * This class represents a file uploaded to Telegram Passport.
  * Currently, all Telegram Passport files are in JPEG format when decrypted and don't exceed 10MB.
- * @param fileId Identifier for this file, which can be used to download or reuse the file
- * @param fileUniqueId Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
- * @param fileSize File size in bytes
- * @param fileDate Unix time when the file was uploaded
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record PassportFile(
-        @SerializedName("file_id") String fileId,
-        @SerializedName("file_unique_id") String fileUniqueId,
-        @SerializedName("file_size") Integer fileSize,
-        @SerializedName("file_date") Integer fileDate
-) {}
+public class PassportFile{
+    /**
+     * Identifier for this file, which can be used to download or reuse the file
+     */
+    @SerializedName("file_id")
+    private String fileId;
+
+    /**
+     * Unique identifier for this file, which is supposed to be the same over time and for different bots.
+     * Can't be used to download or reuse the file.
+     */
+    @SerializedName("file_unique_id")
+    private String fileUniqueId;
+
+    /**
+     * File size in bytes
+     */
+    @SerializedName("file_size")
+    private Integer fileSize;
+
+    /**
+     *  Unix time when the file was uploaded
+     */
+    @SerializedName("file_date")
+    private Integer fileDate;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileUniqueId() {
+        return fileUniqueId;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+
+    public Integer getFileDate() {
+        return fileDate;
+    }
+}
