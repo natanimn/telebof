653a18c69577f48ab80d2ab33d5e2592bef9d3b4
natanimn <natanimco@gmail.com>
2026-01-15 09:02:43 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/InlineKeyboardButton.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/InlineKeyboardButton.java
@@ -1,28 +1,46 @@
 package io.github.natanimn.telebof.types.keyboard;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.natanimn.telebof.types.media_and_service.CallbackGame;
 import io.github.natanimn.telebof.types.media_and_service.CopyTextButton;
 import io.github.natanimn.telebof.types.media_and_service.SwitchInlineQueryChosenChat;
 import io.github.natanimn.telebof.types.web.WebAppInfo;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * This object represents one button of an inline keyboard.
  * Exactly one of the optional fields must be used to specify type of the button.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class InlineKeyboardButton implements Serializable {
-    public String text, url, callback_data, switch_inline_query_current_chat;
-    public WebAppInfo wep_app;
-    public LoginUrl login_url;
-    public Boolean pay;
-    public CallbackGame callback_game;
-    public SwitchInlineQueryChosenChat switch_inline_query_chosen_chat;
-    public CopyTextButton copy_text;
+    private final String text;
+    private String url;
+
+    @SerializedName("callback_data")
+    private String callbackData;
+
+    @SerializedName("switch_inline_query_current_chat")
+    private String switchInlineQueryCurrentChat;
+
+    @SerializedName("web_app")
+    private WebAppInfo wepApp;
+
+    @SerializedName("login_url")
+    private LoginUrl loginUrl;
+
+    private Boolean pay;
+
+    @SerializedName("callback_game")
+    private CallbackGame callbackGame;
+
+    @SerializedName("switch_inline_query_chosen_chat")
+    private SwitchInlineQueryChosenChat switchInlineQueryChosenChat;
+
+    @SerializedName("copy_text")
+    private CopyTextButton copyText;
 
     /**
      * Required
@@ -35,11 +53,11 @@ public class InlineKeyboardButton implements Serializable {
     /**
      *
      * @param text Required. Label text on the button
-     * @param callback_data Optional. Data to be sent in a callback query to the bot when the button is pressed, 1-64 bytes
+     * @param callbackData Optional. Data to be sent in a callback query to the bot when the button is pressed, 1-64 bytes
      */
-    public InlineKeyboardButton(String text, String callback_data) {
+    public InlineKeyboardButton(String text, String callbackData) {
         this.text = text;
-        this.callback_data = callback_data;
+        this.callbackData = callbackData;
     }
 
     /**
@@ -48,55 +66,55 @@ public class InlineKeyboardButton implements Serializable {
      *            can be used to mention a user by their identifier without using a username, if this is allowed by their privacy settings.
      * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton url(String url) {
+    public InlineKeyboardButton setUrl(String url) {
         this.url = url;
         return this;
     }
 
     /**
      * Optional
-     * @param web_app Description of the Web App that will be launched when the user presses the button.
+     * @param webApp Description of the Web App that will be launched when the user presses the button.
      *                The Web App will be able to send an arbitrary message on behalf of the user using the method answerWebAppQuery.
      *                Available only in private chats between a user and the bot.
      *                Not supported for messages sent on behalf of a Telegram Business account.
      * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton webApp(WebAppInfo web_app) {
-        this.wep_app = web_app;
+    public InlineKeyboardButton setWebApp(WebAppInfo webApp) {
+        this.wepApp = webApp;
         return this;
     }
 
     /**
      * Optional
-     * @param login_url An HTTPS URL used to automatically authorize the user.
+     * @param loginUrl An HTTPS URL used to automatically authorize the user.
      *                  Can be used as a replacement for the <a href="https://core.telegram.org/widgets/login">Telegram Login Widget.</a>
      * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton loginUrl(LoginUrl login_url) {
-        this.login_url = login_url;
+    public InlineKeyboardButton setLoginUrl(LoginUrl loginUrl) {
+        this.loginUrl = loginUrl;
         return this;
     }
 
     /**
      * Optional
-     * @param switch_inline_query_current_chat  If set, pressing the button will insert the bot's username and the specified inline query in the current chat's input field.
+     * @param switchInlineQueryCurrentChat  If set, pressing the button will insert the bot's username and the specified inline query in the current chat's input field.
      *                                          May be empty, in which case only the bot's username will be inserted.
      * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton switchInlineQueryCurrentChat(String switch_inline_query_current_chat) {
-        this.switch_inline_query_current_chat = switch_inline_query_current_chat;
+    public InlineKeyboardButton setSwitchInlineQueryCurrentChat(String switchInlineQueryCurrentChat) {
+        this.switchInlineQueryCurrentChat = switchInlineQueryCurrentChat;
         return this;
     }
 
     /**
      * Optional
-     * @param callback_game Description of the game that will be launched when the user presses the button.<br>
+     * @param callbackGame Description of the game that will be launched when the user presses the button.<br>
      *
      * <b>NOTE:</b> This type of button must always be the first button in the first row.
      * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton callbackGame(CallbackGame callback_game) {
-        this.callback_game = callback_game;
+    public InlineKeyboardButton setCallbackGame(CallbackGame callbackGame) {
+        this.callbackGame = callbackGame;
         return this;
     }
 
@@ -106,50 +124,67 @@ public class InlineKeyboardButton implements Serializable {
      *             Substrings “⭐” and “XTR” in the buttons's text will be replaced with a Telegram Star icon.<br>
      *
      * <b>NOTE:</b> This type of button must always be the first button in the first row and can only be used in invoice messages.
-     * @return
+     * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton pay(boolean pay) {
+    public InlineKeyboardButton setPay(Boolean pay) {
         this.pay = pay;
         return this;
     }
 
     /**
      * Optional
-     * @param switch_inline_query_chosen_chat If set, pressing the button will prompt the user to select one of their chats of the specified type,
+     * @param switchInlineQueryChosenChat If set, pressing the button will prompt the user to select one of their chats of the specified type,
      *                                        open that chat and insert the bot's username and the specified inline query in the input field.
      *                                        Not supported for messages sent on behalf of a Telegram Business account.
      * @return {@link InlineKeyboardButton}
      */
-    public InlineKeyboardButton switchInlineQueryChosenChat(SwitchInlineQueryChosenChat switch_inline_query_chosen_chat){
-        this.switch_inline_query_chosen_chat = switch_inline_query_chosen_chat;
+    public InlineKeyboardButton setSwitchInlineQueryChosenChat(SwitchInlineQueryChosenChat switchInlineQueryChosenChat){
+        this.switchInlineQueryCurrentChat = switchInlineQueryCurrentChat;
         return this;
     }
 
-    public InlineKeyboardButton copyText(CopyTextButton copy_text){
-        this.copy_text = copy_text;
+    public InlineKeyboardButton copyText(CopyTextButton copyText){
+        this.copyText = copyText;
         return this;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        InlineKeyboardButton button = (InlineKeyboardButton) object;
-        return Objects.equals(text, button.text) &&
-                Objects.equals(url, button.url) &&
-                Objects.equals(callback_data, button.callback_data) &&
-                Objects.equals(switch_inline_query_current_chat, button.switch_inline_query_current_chat) &&
-                Objects.equals(wep_app, button.wep_app) &&
-                Objects.equals(login_url, button.login_url) &&
-                Objects.equals(pay, button.pay) &&
-                Objects.equals(callback_game, button.callback_game) &&
-                Objects.equals(switch_inline_query_chosen_chat, button.switch_inline_query_chosen_chat) &&
-                Objects.equals(copy_text, button.copy_text);
+    public String getText() {
+        return text;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(text, url, callback_data, switch_inline_query_current_chat, wep_app, login_url, pay,
-                callback_game, switch_inline_query_chosen_chat, copy_text);
+    public String getUrl() {
+        return url;
+    }
+
+    public String getCallbackData() {
+        return callbackData;
+    }
+
+    public String getSwitchInlineQueryCurrentChat() {
+        return switchInlineQueryCurrentChat;
+    }
+
+    public WebAppInfo getWepApp() {
+        return wepApp;
+    }
+
+    public LoginUrl getLoginUrl() {
+        return loginUrl;
+    }
+
+    public Boolean getPay() {
+        return pay;
+    }
+
+    public CallbackGame getCallbackGame() {
+        return callbackGame;
+    }
+
+    public SwitchInlineQueryChosenChat getSwitchInlineQueryChosenChat() {
+        return switchInlineQueryChosenChat;
+    }
+
+    public CopyTextButton getCopyText() {
+        return copyText;
     }
 }
