5600820d800545cb6baa2f1aeb13b9a086066e49
natanimn <natanimco@gmail.com>
2026-01-15 09:02:28 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/ForceReply.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/ForceReply.java
@@ -1,33 +1,36 @@
 package io.github.natanimn.telebof.types.keyboard;
 
-import java.util.Objects;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Upon receiving a message with this object, Telegram clients will display a reply interface to the user (act as if the user has selected the bot's message and tapped 'Reply').
  * This can be extremely useful if you want to create user-friendly step-by-step interfaces without having to sacrifice privacy mode. Not supported in channels and for messages sent on behalf of a Telegram Business account.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
 public class ForceReply implements Markup {
     /**
      * Shows reply interface to the user, as if they manually selected the bot's message and tapped 'Reply'
      */
-    public final boolean force_reply;
-    public Boolean selective;
-    public String input_field_placeholder;
+    @SerializedName("force_reply")
+    private final boolean forceReply;
+    private Boolean selective;
+
+    @SerializedName("input_field_placeholder")
+    private String inputFieldPlaceholder;
 
     public ForceReply() {
-        this.force_reply = true;
+        this.forceReply = true;
     }
 
     /**
      * Optional.
-     * @param input_field_placeholder The placeholder to be shown in the input field when the reply is active; 1-64 characters
+     * @param inputFieldPlaceholder The placeholder to be shown in the input field when the reply is active; 1-64 characters
      * @return {@link ForceReply}
      */
-    public ForceReply inputFieldPlaceholder(String input_field_placeholder){
-        this.input_field_placeholder = input_field_placeholder;
+    public ForceReply setInputFieldPlaceholder(String inputFieldPlaceholder){
+        this.inputFieldPlaceholder = inputFieldPlaceholder;
         return this;
     }
 
@@ -39,23 +42,8 @@ public class ForceReply implements Markup {
      *                      2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
      * @return {@link ForceReply}
      */
-    public ForceReply selective(boolean selective) {
+    public ForceReply setSelective(Boolean selective) {
         this.selective = selective;
         return this;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ForceReply that = (ForceReply) object;
-        return force_reply == that.force_reply &&
-                Objects.equals(selective, that.selective) &&
-                Objects.equals(input_field_placeholder, that.input_field_placeholder);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(force_reply, selective, input_field_placeholder);
     }
 }
