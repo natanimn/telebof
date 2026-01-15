d14f1d6e2eb048af528e7d4d9d842dec1fb836b0
natanimn <natanimco@gmail.com>
2026-01-15 09:01:42 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/AffiliateInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/AffiliateInfo.java
@@ -6,19 +6,57 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This class Contains information about the affiliate that received a commission via this transaction.
- * @param affiliateUser The bot or the user that received an affiliate commission if it was received by a bot or a user
- * @param affiliateChat The chat that received an affiliate commission if it was received by a chat
- * @param commissionPerMille The number of Telegram Stars received by the affiliate for each 1000 Telegram Stars received by the bot from referred users
- * @param amount Integer amount of Telegram Stars received by the affiliate from the transaction, rounded to 0; can be negative for refunds
- * @param nanostarAmount The number of 1/1000000000 shares of Telegram Stars received by the affiliate; from -999999999 to 999999999; can be negative for refunds
  * @author Natanim
  * @since July 3, 2025
  * @version 1.3.0
  */
-public record AffiliateInfo(
-        @SerializedName("affiliate_user") User affiliateUser,
-        @SerializedName("affiliate_chat") Chat affiliateChat,
-        @SerializedName("commission_per_mille") Integer commissionPerMille,
-        Integer amount,
-        @SerializedName("nanostar_amount") Integer nanostarAmount
-) {}
+public class AffiliateInfo {
+    /**
+     * The bot or the user that received an affiliate commission if it was received by a bot or a user
+     */
+    @SerializedName("affiliate_user")
+    private User affiliateUser;
+
+    /**
+     * The chat that received an affiliate commission if it was received by a chat
+     */
+    @SerializedName("affiliate_chat")
+    private Chat affiliateChat;
+
+    /**
+     * The number of Telegram Stars received by the affiliate for each 1000 Telegram Stars received by the bot from referred users
+     */
+    @SerializedName("commission_per_mille")
+    private Integer commissionPerMille;
+
+    /**
+     * Integer amount of Telegram Stars received by the affiliate from the transaction, rounded to 0; can be negative for refunds
+     */
+    private Integer amount;
+
+    /**
+     * The number of 1/1000000000 shares of Telegram Stars received by the affiliate; from -999999999 to 999999999; can be negative for refunds
+     */
+    @SerializedName("nanostar_amount")
+    private Integer nanostarAmount;
+
+    public User getAffiliateUser() {
+        return affiliateUser;
+    }
+
+    public Chat getAffiliateChat() {
+        return affiliateChat;
+    }
+
+    public Integer getCommissionPerMille() {
+        return commissionPerMille;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Integer getNanostarAmount() {
+        return nanostarAmount;
+    }
+}
