c7a7a01510242aa23021eb9baf8edaaed4df8b55
natanimn <natanimco@gmail.com>
2026-01-15 09:01:55 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/passport/EncryptedCredentials.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/passport/EncryptedCredentials.java
@@ -1,7 +1,5 @@
 package io.github.natanimn.telebof.types.passport;
 
-import java.io.Serializable;
-
 /**
  * <p>
  * <strong>Telegram Passport</strong> is a unified authorization method for services that require personal identification.
@@ -11,15 +9,35 @@ import java.io.Serializable;
  * <br>
  * This class describes data required for decrypting and authenticating <a href="https://core.telegram.org/bots/api#encryptedpassportelement">EncryptedPassportElement.</a>
  * @see <a href="https://core.telegram.org/passport#receiving-information">The Telegram Passport Documentation</a> for a complete description of the data decryption and authentication processes.
- * @param data Base64-encoded encrypted JSON-serialized data with unique user's payload, data hashes and secrets required for EncryptedPassportElement decryption and authentication
- * @param hash Base64-encoded data hash for data authentication
- * @param secret Base64-encoded secret, encrypted with the bot's public RSA key, required for data decryption
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record EncryptedCredentials(
-        String data,
-        String hash,
-        String secret
-) {}
+public class EncryptedCredentials {
+    /**
+     * Base64-encoded encrypted JSON-serialized data with unique user's payload, data hashes and secrets required for EncryptedPassportElement decryption and authentication
+     */
+    private String data;
+
+    /**
+     * Base64-encoded data hash for data authentication
+     */
+    private String hash;
+
+    /**
+     * Base64-encoded secret, encrypted with the bot's public RSA key, required for data decryption
+     */
+    private String secret;
+
+    public String getData() {
+        return data;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+}
