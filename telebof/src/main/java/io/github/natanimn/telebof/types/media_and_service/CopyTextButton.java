d0c573d51e3dea23d4f81c01a8cb763dfa4aff7e
natanimn <natanimco@gmail.com>
2026-01-16 20:43:16 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CopyTextButton.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CopyTextButton.java
@@ -1,12 +1,25 @@
 package io.github.natanimn.telebof.types.media_and_service;
 
+import java.io.Serializable;
+
 /**
  * This class represents an inline keyboard button that copies specified text to the clipboard.
- * @param text The text to be copied to the clipboard; 1-256 characters
  * @author Natanim
  * @since Jul 22, 2025
  * @version 1.3.0
  */
-public record CopyTextButton(
-        String text
-) {}
+public class CopyTextButton implements Serializable {
+    /**
+     * The text to be copied to the clipboard; 1-256 characters
+     */
+    private String text;
+
+    public CopyTextButton(){}
+
+    public CopyTextButton(String text){
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+}
