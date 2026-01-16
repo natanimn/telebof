fe785d6dce2776bb9986ab02ecf8ad8ee1802b02
natanimn <natanimco@gmail.com>
2026-01-16 20:43:23 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Dice.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/Dice.java
@@ -2,13 +2,26 @@ package io.github.natanimn.telebof.types.media_and_service;
 
 /**
  * This object represents an animated emoji that displays a random value.
- * @param emoji Emoji on which the dice throw animation is based
- * @param value Value of the dice, 1-6 for “🎲”, “🎯” and “🎳” base emoji, 1-5 for “🏀” and “⚽” base emoji, 1-64 for “🎰” base emoji
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record Dice(
-        String emoji,
-        Integer value
-) {}
+public class Dice{
+    /**
+     * Emoji on which the dice throw animation is based
+     */
+    private String emoji;
+
+    /**
+     * Value of the dice, 1-6 for “🎲”, “🎯” and “🎳” base emoji, 1-5 for “🏀” and “⚽” base emoji, 1-64 for “🎰” base emoji
+     */
+    private Integer value;
+
+    public String getEmoji() {
+        return emoji;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
