d7fbbdfa7896bc4251643b18af2003bdf718cc9f
natanimn <natanimco@gmail.com>
2026-01-16 20:44:27 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputLocationMessageContent.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputLocationMessageContent.java
@@ -36,35 +36,43 @@ public class InputLocationMessageContent implements InputMessageContent {
     /**
      * Optional.
      * @param horizontalAccuracy The radius of uncertainty for the location, measured in meters; 0-1500
+     * @return this
      */
-    public void setHorizontalAccuracy(Double horizontalAccuracy) {
+    public InputLocationMessageContent setHorizontalAccuracy(Double horizontalAccuracy) {
         this.horizontalAccuracy = horizontalAccuracy;
+        return this;
     }
 
     /**
      * Optional.
      * @param livePeriod Period in seconds during which the location can be updated,
      *                   should be between 60 and 86400, or 0x7FFFFFFF for live locations that can be edited indefinitely.
+     * @return this
      */
-    public void setLivePeriod(Integer livePeriod) {
+    public InputLocationMessageContent setLivePeriod(Integer livePeriod) {
         this.livePeriod = livePeriod;
+        return this;
     }
 
     /**
      * Optional.
      * @param heading For live locations, a direction in which the user is moving, in degrees.
      *                Must be between 1 and 360 if specified.
+     * @return this
      */
-    public void setHeading(Integer heading) {
+    public InputLocationMessageContent setHeading(Integer heading) {
         this.heading = heading;
+        return this;
     }
 
     /**
      * Optional.
      * @param proximityAlertRadius For live locations, a maximum distance for proximity alerts about approaching another chat member, in meters.
      *                             Must be between 1 and 100000 if specified
+     * @return this
      */
-    public void setProximityAlertRadius(Integer proximityAlertRadius) {
+    public InputLocationMessageContent setProximityAlertRadius(Integer proximityAlertRadius) {
         this.proximityAlertRadius = proximityAlertRadius;
+        return this;
     }
 }
