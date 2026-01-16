85f4a74d31077c72db076655835177bd4f6cf3a2
natanimn <natanimco@gmail.com>
2026-01-16 20:43:43 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/GameHighScore.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/GameHighScore.java
@@ -4,15 +4,36 @@ import io.github.natanimn.telebof.types.chat_and_user.User;
 
 /**
  * This object represents one row of the high scores table for a game.
- * @param position Position in high score table for the game
- * @param score Score
- * @param user User
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record GameHighScore(
-        Integer position,
-        Integer score,
-        User user
-) {}
+public class GameHighScore {
+    /**
+     * Position in high score table for the game
+     */
+    private Integer position;
+
+    /**
+     * Score
+     */
+    private Integer score;
+
+    /**
+     * User
+     */
+    private User user;
+
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
