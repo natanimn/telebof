48f5707167d6644eaa8290cde018dd0044884286
natanimn <natanimco@gmail.com>
2026-01-17 05:44:40 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputSticker.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputSticker.java
@@ -1,5 +1,6 @@
 package io.github.natanimn.telebof.types.input;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.natanimn.telebof.enums.StickerFormat;
 import io.github.natanimn.telebof.types.media_and_service.MaskPosition;
 
@@ -11,14 +12,20 @@ import java.util.List;
  * This object describes a sticker to be added to a sticker set.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.9.4
+ * @version 1.3.0
  */
 public abstract class InputSticker implements InputMedia {
     private String sticker;
     private StickerFormat format;
-    private List<String> emoji_list, keywords;
-    private MaskPosition mask_position;
-    transient private boolean hasFile;
+
+    @SerializedName("emoji_list")
+    private List<String> emojiList;
+
+    private List<String> keywords;
+
+    @SerializedName("mask_position")
+    private MaskPosition maskPosition;
+
     transient private List<File> files = new ArrayList<>();
 
     /**
@@ -26,13 +33,12 @@ public abstract class InputSticker implements InputMedia {
      * @param sticker The added sticker. Pass an object of {@link File} to upload from your local machine.
      * @param format Format of the added sticker, must be one of {@link StickerFormat#STATIC} for a .WEBP or .PNG image,
      *               {@link StickerFormat#ANIMATED} for a .TGS animation, {@link StickerFormat#VIDEO} for a .WEBM video
-     * @param emoji_list List of 1-20 emoji associated with the sticker
+     * @param emojiList List of 1-20 emoji associated with the sticker
      */
-    public InputSticker(File sticker, StickerFormat format, String[] emoji_list) {
+    public InputSticker(File sticker, StickerFormat format, String[] emojiList) {
         this.sticker = "attach://"+sticker.getName();
         this.format = format;
-        this.emoji_list = List.of(emoji_list);
-        setHasFile(true);
+        this.emojiList = List.of(emojiList);
         this.files.add(sticker);
     }
 
@@ -42,22 +48,21 @@ public abstract class InputSticker implements InputMedia {
      *               pass an HTTP URL as a String for Telegram to get a file from the Internet.
      * @param format Format of the added sticker, must be one of {@link StickerFormat#STATIC} for a .WEBP or .PNG image,
      *               {@link StickerFormat#ANIMATED} for a .TGS animation, {@link StickerFormat#VIDEO} for a .WEBM video
-     * @param emoji_list List of 1-20 emoji associated with the sticker
+     * @param emojiList List of 1-20 emoji associated with the sticker
      */
-    public InputSticker(String sticker, StickerFormat format, String[] emoji_list) {
+    public InputSticker(String sticker, StickerFormat format, String[] emojiList) {
         this.sticker = sticker;
         this.format = format;
-        this.emoji_list = List.of(emoji_list);
-        setHasFile(false);
+        this.emojiList = List.of(emojiList);
     }
 
     /**
      * Optional
-     * @param mask_position Position where the mask should be placed on faces. For “mask” stickers only.
+     * @param maskPosition Position where the mask should be placed on faces. For “mask” stickers only.
      * @return {@link InputSticker}
      */
-    public InputSticker maskPosition(MaskPosition mask_position) {
-        this.mask_position = mask_position;
+    public InputSticker setMaskPosition(MaskPosition maskPosition) {
+        this.maskPosition = maskPosition;
         return this;
     }
 
@@ -67,17 +72,9 @@ public abstract class InputSticker implements InputMedia {
      *                For {@link io.github.natanimn.telebof.enums.StickerType#REGULAR} and {@link io.github.natanimn.telebof.enums.StickerType#CUSTOM_EMOJI} stickers only.
      * @return {@link InputSticker}
      */
-    public InputSticker keywords(String[] keywords) {
+    public InputSticker setKeywords(String[] keywords) {
         this.keywords = List.of(keywords);
         return this;
-    }
-
-    /**
-     * Used only for internal purpose
-     * @param has has
-     */
-    private void setHasFile(boolean has){
-        this.hasFile = has;
     }
 
     /**
@@ -86,7 +83,7 @@ public abstract class InputSticker implements InputMedia {
      */
     @Override
     public boolean hasFile() {
-        return hasFile;
+        return !files.isEmpty();
     }
 
     /**
