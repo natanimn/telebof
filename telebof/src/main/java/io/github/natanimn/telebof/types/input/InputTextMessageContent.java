9b3371f90b2d5b09b60b5ab97adeaaac34bddd4c
natanimn <natanimco@gmail.com>
2026-01-17 05:45:40 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputTextMessageContent.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputTextMessageContent.java
@@ -8,61 +8,51 @@ import java.util.List;
 
 /**
  * Represents the content of a text message to be sent as the result of an inline query.
- * @param messageText Text of the message to be sent, 1-4096 characters
- * @param parseMode Optional. Mode for parsing entities in the message text
- * @param entities Optional. List of special entities that appear in message text, which can be specified instead of parseMode
- * @param linkPreviewOptions Optional. Link preview generation options for the message
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record InputTextMessageContent(
-        @SerializedName("message_text") String messageText,
-        @SerializedName("parse_mode") ParseMode parseMode,
-        List<MessageEntity> entities,
-        @SerializedName("link_preview_options") LinkPreviewOptions linkPreviewOptions
-) implements InputMessageContent {
+public class InputTextMessageContent implements InputMessageContent {
+    @SerializedName("message_text")
+    private String messageText;
+
+    @SerializedName("parse_mode")
+    private ParseMode parseMode;
+
+    private List<MessageEntity> entities;
+
+    @SerializedName("link_preview_options")
+    private LinkPreviewOptions linkPreviewOptions;
 
     /**
-     * Creates a new InputTextMessageContent builder
-     * @param text Text of the message to be sent, 1-4096 characters
+     * Required
+     * @param messageText Text of the message to be sent, 1-4096 characters
      */
-    public static InputTextMessageContentBuilder builder(String text) {
-        return new InputTextMessageContentBuilder(text);
+    public InputTextMessageContent(String messageText) {
+        this.messageText = messageText;
     }
 
     /**
-     * Builder class for InputTextMessageContent
+     * @param parseMode Mode for parsing entities in the message text
      */
-    public static class InputTextMessageContentBuilder {
-        private final String messageText;
-        private ParseMode parseMode;
-        private List<MessageEntity> entities;
-        private LinkPreviewOptions linkPreviewOptions;
+    public InputTextMessageContent setParseMode(ParseMode parseMode) {
+        this.parseMode = parseMode;
+        return this;
+    }
 
-        public InputTextMessageContentBuilder(String messageText) {
-            this.messageText = messageText;
-        }
+    /**
+     * @param entities List of special entities that appear in message text, which can be specified instead of parseMode
+     */
+    public InputTextMessageContent setEntities(List<MessageEntity> entities) {
+        this.entities = entities;
+        return this;
+    }
 
-        public InputTextMessageContentBuilder parseMode(ParseMode parseMode) {
-            this.parseMode = parseMode;
-            return this;
-        }
-
-        public InputTextMessageContentBuilder entities(List<MessageEntity> entities) {
-            this.entities = entities;
-            return this;
-        }
-
-        public InputTextMessageContentBuilder linkPreviewOptions(LinkPreviewOptions linkPreviewOptions) {
-            this.linkPreviewOptions = linkPreviewOptions;
-            return this;
-        }
-
-        public InputTextMessageContent build() {
-            return new InputTextMessageContent(
-                    messageText, parseMode, entities, linkPreviewOptions
-            );
-        }
+    /**
+     * @param linkPreviewOptions Link preview generation options for the message
+     */
+    public InputTextMessageContent setLinkPreviewOptions(LinkPreviewOptions linkPreviewOptions) {
+        this.linkPreviewOptions = linkPreviewOptions;
+        return this;
     }
 }
