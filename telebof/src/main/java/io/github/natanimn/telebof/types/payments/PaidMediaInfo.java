be1f13a1412a727c30ed76d770decd025d002aa7
natanimn <natanimco@gmail.com>
2026-01-18 10:18:15 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/PaidMediaInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/PaidMediaInfo.java
@@ -5,13 +5,28 @@ import java.util.List;
 
 /**
  * Describes the paid media added to a message.
- * @param starCount The number of Telegram Stars that must be paid to buy access to the media
- * @param paidMedia Information about the paid media
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record PaidMediaInfo(
-        @SerializedName("star_count") Integer starCount,
-        @SerializedName("paid_media") List<PaidMedia> paidMedia
-) {}
+public class PaidMediaInfo {
+    /**
+     * The number of Telegram Stars that must be paid to buy access to the media
+     */
+    @SerializedName("star_count")
+    private Integer starCount;
+
+    /**
+     * Information about the paid media
+     */
+    @SerializedName("paid_media")
+    private List<PaidMedia> paidMedia;
+
+    public Integer getStarCount() {
+        return starCount;
+    }
+
+    public List<PaidMedia> getPaidMedia() {
+        return paidMedia;
+    }
+}
