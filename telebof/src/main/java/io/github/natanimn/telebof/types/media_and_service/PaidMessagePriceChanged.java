f12e512cacb5fc744e8eff9a5ef8553904f2c689
natanimn <natanimco@gmail.com>
2026-01-18 10:18:26 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PaidMessagePriceChanged.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PaidMessagePriceChanged.java
@@ -4,11 +4,18 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes a service message about a change in the price of paid messages within a chat.
- * @param paidMessageStarCount The new number of Telegram Stars that must be paid by non-administrator users of the supergroup chat for each sent message
  * @author Natanim
  * @since 20 August 2025
  * @version 1.3.0
  */
-public record PaidMessagePriceChanged(
-        @SerializedName("paid_message_star_count") Integer paidMessageStarCount
-) {}
+public class PaidMessagePriceChanged {
+    /**
+     * The new number of Telegram Stars that must be paid by non-administrator users of the supergroup chat for each sent message
+     */
+    @SerializedName("paid_message_star_count")
+    private Integer paidMessageStarCount;
+
+    public Integer getPaidMessageStarCount() {
+        return paidMessageStarCount;
+    }
+}
