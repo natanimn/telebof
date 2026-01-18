9281d514f439e08010c887499b41cd73f2737cda
natanimn <natanimco@gmail.com>
2026-01-18 10:17:27 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageAutoDeleteTimerChanged.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageAutoDeleteTimerChanged.java
@@ -4,11 +4,18 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about a change in auto-delete timer settings.
- * @param messageAutoDeleteTime New auto-delete time for messages in the chat; in seconds
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record MessageAutoDeleteTimerChanged(
-        @SerializedName("message_auto_delete_time") Integer messageAutoDeleteTime
-) {}
+public class MessageAutoDeleteTimerChanged {
+    /**
+     * New auto-delete time for messages in the chat; in seconds
+     */
+    @SerializedName("message_auto_delete_time")
+    private Integer messageAutoDeleteTime;
+
+    public Integer getMessageAutoDeleteTime() {
+        return messageAutoDeleteTime;
+    }
+}
