e0e5187c0ae5d5a7156eba4070aa87f00da683a4
natanimn <natanimco@gmail.com>
2026-01-18 10:17:33 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageId.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/MessageId.java
@@ -4,11 +4,19 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This class represents a unique message identifier.
- * @param messageId Unique message identifier. In specific instances (e.g., message containing a video sent to a big chat), the server might automatically schedule a message instead of sending it immediately. In such cases, this field will be 0 and the relevant message will be unusable until it is actually sent
  * @author Natanim
  * @since 3 March, 2025
  * @version 1.3.0
  */
-public record MessageId(
-        @SerializedName("message_id") Integer messageId
-) {}
+public class MessageId {
+    /**
+     * Unique message identifier. In specific instances (e.g., message containing a video sent to a big chat), the server might automatically schedule a message instead of sending it immediately.
+     * In such cases, this field will be 0 and the relevant message will be unusable until it is actually sent
+     */
+    @SerializedName("message_id")
+    private Integer messageId;
+
+    public Integer getMessageId() {
+        return messageId;
+    }
+}
