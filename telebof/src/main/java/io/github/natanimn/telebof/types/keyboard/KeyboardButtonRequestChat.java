fff95b4246dce0a7c3f6876ad03cd55ec2fcbc2d
natanimn <natanimco@gmail.com>
2026-01-18 10:16:56 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/KeyboardButtonRequestChat.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/KeyboardButtonRequestChat.java
@@ -1,5 +1,6 @@
 package io.github.natanimn.telebof.types.keyboard;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.natanimn.telebof.types.chat_and_user.ChatAdministratorRights;
 
 import java.io.Serializable;
@@ -10,119 +11,151 @@ import java.io.Serializable;
  * The bot will be granted requested rights in the chat if appropriate.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  * @since <a href="https://core.telegram.org/bots/features#chat-and-user-selection">More about requesting chats ».</a>
  */
 public class KeyboardButtonRequestChat implements Serializable {
-    private ChatAdministratorRights user_administrator_rights, bot_administrator_rights;
-    private Boolean bot_is_member, chat_is_forum, chat_has_username, chat_is_created, chat_is_channel, request_name,
-    request_title, request_username, request_photo;
-    private Integer request_id;
+
+    @SerializedName("user_administrator_rights")
+    private ChatAdministratorRights userAdministratorRights;
+
+    @SerializedName("bot_administrator_rights")
+    private ChatAdministratorRights botAdministratorRights;
+
+    @SerializedName("bot_is_member")
+    private Boolean botIsMember;
+
+    @SerializedName("chat_is_forum")
+    private Boolean chatIsForum;
+
+    @SerializedName("chat_has_username")
+    private Boolean chatHasUsername;
+
+    @SerializedName("chat_is_created")
+    private Boolean chatIsCreated;
+
+    @SerializedName("chat_is_channel")
+    private Boolean chatIsChannel;
+
+    @SerializedName("request_name")
+    private Boolean requestName;
+
+    @SerializedName("request_title")
+    private Boolean requestTitle;
+
+    @SerializedName("request_username")
+    private Boolean requestUsername;
+
+    @SerializedName("request_photo")
+    private Boolean requestPhoto;
+
+    @SerializedName("request_id")
+    private Long requestId;
 
     /**
      * Required
-     * @param request_id Signed 32-bit identifier of the request, which will be received back in the {@link io.github.natanimn.telebof.types.chat_and_user.ChatShared} object.
+     * @param requestId Signed 32-bit identifier of the request, which will be received back in the {@link io.github.natanimn.telebof.types.chat_and_user.ChatShared} object.
      *                   Must be unique within the message
-     * @param chat_is_channel Pass True to request a channel chat, pass False to request a group or a supergroup chat.
+     * @param chatIsChannel Pass True to request a channel chat, pass False to request a group or a supergroup chat.
      */
-    public KeyboardButtonRequestChat(int request_id, boolean chat_is_channel) {
-        this.request_id = request_id;
-        this.chat_is_channel = chat_is_channel;
+    public KeyboardButtonRequestChat(Long requestId, boolean chatIsChannel) {
+        this.requestId = requestId;
+        this.chatIsCreated = chatIsChannel;
 
     }
 
     /**
      * Optional
-     * @param chat_is_forum Pass True to request a forum supergroup, pass False to request a non-forum chat.
+     * @param chatIsForum Pass True to request a forum supergroup, pass False to request a non-forum chat.
      *                      If not specified, no additional restrictions are applied.
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat chatIsForum(boolean chat_is_forum) {
-        this.chat_is_forum = chat_is_forum;
+    public KeyboardButtonRequestChat setChatIsForum(boolean chatIsForum) {
+        this.chatIsForum = chatIsForum;
         return this;
     }
 
     /**
      * Optional
-     * @param chat_has_username Pass True to request a supergroup or a channel with a username, pass False to request a chat without a username.
+     * @param chatHasUsername Pass True to request a supergroup or a channel with a username, pass False to request a chat without a username.
      *                          If not specified, no additional restrictions are applied.
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat chatHasUsername(boolean chat_has_username) {
-        this.chat_has_username = chat_has_username;
+    public KeyboardButtonRequestChat setChatHasUsername(boolean chatHasUsername) {
+        this.chatHasUsername = chatHasUsername;
         return this;
     }
 
     /**
      * Optional
-     * @param chat_is_created Pass True to request a chat owned by the user. Otherwise, no additional restrictions are applied.
+     * @param chatIsCreated Pass True to request a chat owned by the user. Otherwise, no additional restrictions are applied.
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat chatIsCreated(boolean chat_is_created) {
-        this.chat_is_created = chat_is_created;
+    public KeyboardButtonRequestChat setChatIsCreated(boolean chatIsCreated) {
+        this.chatIsCreated = chatIsCreated;
         return this;
     }
 
     /**
      * Optional
-     * @param user_administrator_rights A JSON-serialized object listing the required administrator rights of the user in the chat.
+     * @param userAdministratorRights A JSON-serialized object listing the required administrator rights of the user in the chat.
      *                                  The rights must be a superset of bot_administrator_rights.
      *                                  If not specified, no additional restrictions are applied.
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat userAdministratorRights(ChatAdministratorRights user_administrator_rights) {
-        this.user_administrator_rights = user_administrator_rights;
+    public KeyboardButtonRequestChat setUserAdministratorRights(ChatAdministratorRights userAdministratorRights) {
+        this.userAdministratorRights = userAdministratorRights;
         return this;
     }
 
     /**
      * Optional
-     * @param bot_administrator_rights A JSON-serialized object listing the required administrator rights of the bot in the chat.
-     *                                 The rights must be a subset of {@link #userAdministratorRights(ChatAdministratorRights)}. If not specified, no additional restrictions are applied.
+     * @param botAdministratorRights A JSON-serialized object listing the required administrator rights of the bot in the chat.
+     *                                 The rights must be a subset of {@link #setUserAdministratorRights(ChatAdministratorRights)}. If not specified, no additional restrictions are applied.
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat botAdministratorRights(ChatAdministratorRights bot_administrator_rights) {
-        this.bot_administrator_rights = bot_administrator_rights;
+    public KeyboardButtonRequestChat setBotAdministratorRights(ChatAdministratorRights botAdministratorRights) {
+        this.botAdministratorRights = botAdministratorRights;
         return this;
     }
 
     /**
      * Optional
-     * @param bot_is_member Pass True to request a chat with the bot as a member. Otherwise, no additional restrictions are applied.
+     * @param botIsMember Pass True to request a chat with the bot as a member. Otherwise, no additional restrictions are applied.
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat botIsMember(boolean bot_is_member) {
-        this.bot_is_member = bot_is_member;
+    public KeyboardButtonRequestChat setBotIsMember(boolean botIsMember) {
+        this.botIsMember = botIsMember;
         return this;
     }
 
     /**
      * Optional
-     * @param request_title Pass True to request the chat's title
+     * @param requestTitle Pass True to request the chat's title
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat requestTitle(boolean request_title){
-        this.request_title = request_title;
+    public KeyboardButtonRequestChat setRequestTitle(boolean requestTitle){
+        this.requestTitle = requestTitle;
         return this;
     }
 
     /**
      * Optional
-     * @param request_username Pass True to request the chat's username
+     * @param requestUsername Pass True to request the chat's username
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat requestUsername(boolean request_username){
-        this.request_username = request_username;
+    public KeyboardButtonRequestChat setRequestUsername(boolean requestUsername){
+        this.requestUsername = requestUsername;
         return this;
     }
 
     /**
      * Optional
-     * @param request_photo Pass True to request the chat's photo
+     * @param requestPhoto Pass True to request the chat's photo
      * @return {@link KeyboardButtonRequestChat}
      */
-    public KeyboardButtonRequestChat requestPhoto(boolean request_photo){
-        this.request_photo = request_photo;
+    public KeyboardButtonRequestChat setRequestPhoto(boolean requestPhoto){
+        this.requestPhoto = requestPhoto;
         return this;
     }
 
