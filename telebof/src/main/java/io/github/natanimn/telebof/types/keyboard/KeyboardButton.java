550758f288a9ca23c01dab260af1187e1581914e
natanimn <natanimco@gmail.com>
2026-01-18 10:16:45 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/KeyboardButton.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/keyboard/KeyboardButton.java
@@ -1,5 +1,6 @@
 package io.github.natanimn.telebof.types.keyboard;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.natanimn.telebof.types.web.WebAppInfo;
 
 import java.io.Serializable;
@@ -8,18 +9,31 @@ import java.io.Serializable;
  * This object represents one button of the reply keyboard.
  * At most one of the optional fields must be used to specify type of the button.
  * For simple text buttons, String can be used instead of this object to specify the button text.
- * @author Natanim Negash
+ * @author Natanim
  * @since 3 March 2025
- * @version 0.9
+ * @version 1.3.0
  */
 public class KeyboardButton implements Serializable {
+
     private String text;
-    private KeyboardButtonRequestUsers request_users;
-    private KeyboardButtonRequestChat request_chat;
-    private KeyboardButtonPollType request_poll;
-    private Boolean request_contact;
-    private Boolean request_location;
-    private WebAppInfo wep_app;
+
+    @SerializedName("request_users")
+    private KeyboardButtonRequestUsers requestUsers;
+
+    @SerializedName("request_chat")
+    private KeyboardButtonRequestChat requestChat;
+
+    @SerializedName("request_poll")
+    private KeyboardButtonPollType requestPoll;
+
+    @SerializedName("request_contact")
+    private Boolean requestContact;
+
+    @SerializedName("request_location")
+    private Boolean requestLocation;
+
+    @SerializedName("web_app")
+    private WebAppInfo wepApp;
 
 
     /**
@@ -32,68 +46,68 @@ public class KeyboardButton implements Serializable {
 
     /**
      * Optional
-     * @param request_users If specified, pressing the button will open a list of suitable users.
+     * @param requestUsers If specified, pressing the button will open a list of suitable users.
      *                      Identifiers of selected users will be sent to the bot in a “users_shared” service message.
      *                      Available in private chats only.
      * @return {@link KeyboardButton}
      */
-    public KeyboardButton requestUser(KeyboardButtonRequestUsers request_users) {
-        this.request_users = request_users;
+    public KeyboardButton setRequestUsers(KeyboardButtonRequestUsers requestUsers) {
+        this.requestUsers = requestUsers;
         return this;
     }
 
     /**
      * Optional
-     * @param request_chat If specified, pressing the button will open a list of suitable chats.
+     * @param requestChat If specified, pressing the button will open a list of suitable chats.
      *                     Tapping on a chat will send its identifier to the bot in a “chat_shared” service message.
      *                     Available in private chats only.
      * @return {@link KeyboardButton}
      */
-    public KeyboardButton requestChat(KeyboardButtonRequestChat request_chat) {
-        this.request_chat = request_chat;
+    public KeyboardButton setRequestChat(KeyboardButtonRequestChat requestChat) {
+        this.requestChat = requestChat;
         return this;
     }
 
     /**
      * Optional
-     * @param request_contact If True, the user's phone number will be sent as a contact when the button is pressed.
+     * @param requestContact If True, the user's phone number will be sent as a contact when the button is pressed.
      *                        Available in private chats only.
      * @return {@link KeyboardButton}
      */
-    public KeyboardButton requestContact(boolean request_contact) {
-        this.request_contact = request_contact;
+    public KeyboardButton setRequestContact(Boolean requestContact) {
+        this.requestContact = requestContact;
         return this;
     }
 
     /**
      * Optional
-     * @param request_location If True, the user's current location will be sent when the button is pressed.
+     * @param requestLocation If True, the user's current location will be sent when the button is pressed.
      *                        Available in private chats only.
      * @return {@link KeyboardButton}
      */
-    public KeyboardButton requestLocation(boolean request_location) {
-        this.request_location = request_location;
+    public KeyboardButton setRequestLocation(Boolean requestLocation) {
+        this.requestLocation = requestLocation;
         return this;
     }
 
     /**
      * Optional
-     * @param request_poll
+     * @param requestPoll
      * @return {@link KeyboardButton}
      */
-    public KeyboardButton requestPoll(KeyboardButtonPollType request_poll) {
-        this.request_poll = request_poll;
+    public KeyboardButton setRequestPoll(KeyboardButtonPollType requestPoll) {
+        this.requestPoll = requestPoll;
         return this;
     }
 
     /**
      * Optional
-     * @param wep_app If specified, the described <a href="https://core.telegram.org/bots/webapps">Web App</a> will be launched when the button is pressed.
+     * @param wepApp If specified, the described <a href="https://core.telegram.org/bots/webapps">Web App</a> will be launched when the button is pressed.
      *                The Web App will be able to send a “web_app_data” service message. Available in private chats only.
      * @return {@link KeyboardButton}
      */
-    public KeyboardButton webApp(WebAppInfo wep_app) {
-        this.wep_app = wep_app;
+    public KeyboardButton setWebApp(WebAppInfo wepApp) {
+        this.wepApp = wepApp;
         return this;
     }
 
