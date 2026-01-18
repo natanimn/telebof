68ddcdf68bce4dbb63f5fe3a257bd7bed230a576
natanimn <natanimco@gmail.com>
2026-01-18 10:18:03 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/PaidMedia.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/PaidMedia.java
@@ -6,21 +6,62 @@ import java.util.List;
 
 /**
  * This class describes paid media.
- * @param type Type of media
- * @param width Media width as defined by the sender
- * @param height Media height as defined by the sender
- * @param duration Duration of the media in seconds as defined by the sender
- * @param photo The photo
- * @param video The video
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record PaidMedia(
-        String type,
-        Integer width,
-        Integer height,
-        Integer duration,
-        List<PhotoSize> photo,
-        Video video
-) {}
+public class PaidMedia {
+    /**
+     * Type of media
+     */
+    private String type;
+
+    /**
+     * Media width as defined by the sender
+     */
+    private Integer width;
+
+    /**
+     * Media height as defined by the sender
+     */
+    private Integer height;
+
+    /**
+     * Duration of the media in seconds as defined by the sender
+     */
+    private Integer duration;
+
+    /**
+     * The photo
+     */
+    private List<PhotoSize> photo;
+
+    /**
+     * The video
+     */
+    private Video video;
+
+    public String getType() {
+        return type;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public List<PhotoSize> getPhoto() {
+        return photo;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+}
