f18cc146c8823b61c9d8f0c8aaf7298a67d4b9a3
natanimn <natanimco@gmail.com>
2026-01-18 10:15:27 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/DirectMessagePriceChanged.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/DirectMessagePriceChanged.java
@@ -4,13 +4,29 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about a change in the price of direct messages sent to a channel chat.
- * @param areDirectMessagesEnabled True, if direct messages are enabled for the channel chat; false otherwise
- * @param directMessageStarCount Optional. The new number of Telegram Stars that must be paid by users for each direct message sent to the channel. Does not apply to users who have been exempted by administrators. Defaults to 0.
  * @author Natanim
  * @since 1 September 2025
  * @version 1.3.0
  */
-public record DirectMessagePriceChanged(
-        @SerializedName("are_direct_messages_enabled") Boolean areDirectMessagesEnabled,
-        @SerializedName("direct_message_star_count") Integer directMessageStarCount
-) {}
+public class DirectMessagePriceChanged {
+    /**
+     * True, if direct messages are enabled for the channel chat; false otherwise
+     */
+    @SerializedName("are_direct_messages_enabled")
+    private Boolean areDirectMessagesEnabled;
+
+    /**
+     * Optional. The new number of Telegram Stars that must be paid by users for each direct message sent to the channel.
+     * Does not apply to users who have been exempted by administrators. Defaults to 0.
+     */
+    @SerializedName("direct_message_star_count")
+    private Integer directMessageStarCount;
+
+    public Boolean getAreDirectMessagesEnabled() {
+        return areDirectMessagesEnabled;
+    }
+
+    public Integer getDirectMessageStarCount() {
+        return directMessageStarCount;
+    }
+}
