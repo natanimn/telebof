be35f40c377f14c00172d7e35bcf365dfd106e54
natanimn <natanimco@gmail.com>
2026-01-19 07:26:31 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ReactionType.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ReactionType.java
@@ -6,59 +6,45 @@ import java.io.Serializable;
 
 /**
  * This object describes the type of a reaction.
- * @param type Type of the reaction
- * @param emoji Reaction emoji. Currently, it can be one of
- * "❤", "👍", "👎", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊",
- * "🤡", "🥱", "🥴", "😍", "🐳", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋",
- * "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍", "🤗", "🫡", "🎅", "🎄", "☃",
- * "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂", "🤷", "🤷‍♀", "😡"
- * @param customEmojiId Custom emoji identifier
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ReactionType(
-        TypeReaction type,
-        String emoji,
-        @SerializedName("custom_emoji_id") String customEmojiId
-) implements Serializable {
+public class ReactionType implements Serializable {
+    private TypeReaction type;
+    private String emoji;
+    @SerializedName("custom_emoji_id")
+    private String customEmojiId;
 
     /**
-     * Creates a new ReactionType builder
+     * Required
      * @param type Type of the reaction
      */
-    public static ReactionTypeBuilder builder(TypeReaction type) {
-        return new ReactionTypeBuilder(type);
+    public ReactionType(TypeReaction type) {
+        this.type = type;
     }
 
     /**
-     * Builder class for ReactionType
+     * Optional.
+     * @param emoji Reaction emoji. Currently, it can be one of
+     *              "❤", "👍", "👎", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", "🤮", "💩", "🙏", "👌", "🕊",
+     *              "🤡", "🥱", "🥴", "😍", "🐳", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋",
+     *              "🖕", "😈", "😴", "😭", "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍", "🤗", "🫡", "🎅", "🎄", "☃",
+     *              "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂", "🤷", "🤷‍♀", "😡"
      */
-    public static class ReactionTypeBuilder {
-        private final TypeReaction type;
-        private String emoji;
-        private String customEmojiId;
-
-        public ReactionTypeBuilder(TypeReaction type) {
-            this.type = type;
-        }
-
-        public ReactionTypeBuilder emoji(String emoji) {
-            this.emoji = emoji;
-            return this;
-        }
-
-        public ReactionTypeBuilder customEmojiId(String customEmojiId) {
-            this.customEmojiId = customEmojiId;
-            return this;
-        }
-
-        public ReactionType build() {
-            return new ReactionType(
-                    type,
-                    emoji,
-                    customEmojiId
-            );
-        }
+    public ReactionType setEmoji(String emoji) {
+        this.emoji = emoji;
+        return this;
     }
+
+    /**
+     * Optional.
+     * @param customEmojiId Custom emoji identifier
+     */
+    public ReactionType setCustomEmojiId(String customEmojiId) {
+        this.customEmojiId = customEmojiId;
+        return this;
+    }
+
+
 }
