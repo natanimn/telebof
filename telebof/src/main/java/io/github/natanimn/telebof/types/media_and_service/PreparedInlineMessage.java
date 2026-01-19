7b188a4abd11dff7bf9dc6e43c105c7b221420eb
natanimn <natanimco@gmail.com>
2026-01-19 07:26:03 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PreparedInlineMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PreparedInlineMessage.java
@@ -4,13 +4,27 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This class represents an inline message to be sent by a user of a Mini App.
- * @param id Unique identifier of the prepared message
- * @param expirationDate Expiration date of the prepared message, in Unix time. Expired prepared messages can no longer be used
  * @author Natanim
- * @since jul 25, 2025
+ * @since July 25, 2025
  * @version 1.3.0
  */
-public record PreparedInlineMessage(
-        String id,
-        @SerializedName("expiration_date") Integer expirationDate
-) {}
+public class PreparedInlineMessage {
+    /**
+     * Unique identifier of the prepared message
+     */
+    private String id;
+
+    /**
+     * Expiration date of the prepared message, in Unix time. Expired prepared messages can no longer be used
+     */
+    @SerializedName("expiration_date")
+    private Integer expirationDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public Integer getExpirationDate() {
+        return expirationDate;
+    }
+}
