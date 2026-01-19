1d1b2d370e15c16011c2a24fe326ce3425ac295b
natanimn <natanimco@gmail.com>
2026-01-19 07:27:25 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/SentWebAppMessage.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/SentWebAppMessage.java
@@ -4,11 +4,19 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes an inline message sent by a Web App on behalf of a user.
- * @param inlineMessageId Optional. Identifier of the sent inline message. Available only if there is an inline keyboard attached to the message.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record SentWebAppMessage(
-        @SerializedName("inline_message_id") String inlineMessageId
-) {}
+public class SentWebAppMessage {
+    /**
+     * Optional.
+     * Identifier of the sent inline message. Available only if there is an inline keyboard attached to the message.
+     */
+    @SerializedName("inline_message_id")
+    private String inlineMessageId;
+
+    public String getInlineMessageId() {
+        return inlineMessageId;
+    }
+}
