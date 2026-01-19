3666d655f3342d194dbaf6124074ae0f570cd433
natanimn <natanimco@gmail.com>
2026-01-19 07:27:09 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/RevenueWithdrawalState.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/RevenueWithdrawalState.java
@@ -4,15 +4,35 @@ import io.github.natanimn.telebof.enums.RevenueWithdrawalStateType;
 
 /**
  * This class describes the state of a revenue withdrawal operation
- * @param type Type of the state
- * @param date Date the withdrawal was completed in Unix time
- * @param url An HTTPS URL that can be used to see transaction details
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record RevenueWithdrawalState(
-        RevenueWithdrawalStateType type,
-        Integer date,
-        String url
-) {}
+public class RevenueWithdrawalState {
+    /**
+     * Type of the state
+     */
+    private RevenueWithdrawalStateType type;
+
+    /**
+     * Date the withdrawal was completed in Unix time
+     */
+    private Integer date;
+
+    /**
+     * An HTTPS URL that can be used to see transaction details
+     */
+    private String url;
+
+    public RevenueWithdrawalStateType getType() {
+        return type;
+    }
+
+    public Integer getDate() {
+        return date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
