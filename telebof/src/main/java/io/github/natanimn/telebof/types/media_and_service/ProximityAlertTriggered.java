9d33524b9c97141493ee6c1e7bc62130a11c8c60
natanimn <natanimco@gmail.com>
2026-01-19 07:26:09 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ProximityAlertTriggered.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ProximityAlertTriggered.java
@@ -4,15 +4,35 @@ import io.github.natanimn.telebof.types.chat_and_user.User;
 
 /**
  * This object represents the content of a service message, sent whenever a user in the chat triggers a proximity alert set by another user.
- * @param traveler User that triggered the alert
- * @param watcher User that set the alert
- * @param distance The distance between the users
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ProximityAlertTriggered(
-        User traveler,
-        User watcher,
-        Integer distance
-) {}
+public class ProximityAlertTriggered {
+    /**
+     * User that triggered the alert
+     */
+    private User traveler;
+
+    /**
+     * User that set the alert
+     */
+    private User watcher;
+
+    /**
+     * The distance between the users
+     */
+    private Integer distance;
+
+    public User getTraveler() {
+        return traveler;
+    }
+
+    public User getWatcher() {
+        return watcher;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+}
