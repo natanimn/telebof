fc29dcdd294100b70d1c13fdc6dc7001803e3850
natanimn <natanimco@gmail.com>
2026-01-19 07:26:49 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/RefundedPayment.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/RefundedPayment.java
@@ -4,19 +4,57 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This class contains basic information about a refunded payment.
- * @param currency Three-letter ISO 4217 currency code, or “XTR” for payments in Telegram Stars. Currently, always “XTR”
- * @param totalAmount Total refunded price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45, total_amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies)
- * @param invoicePayload Bot-specified invoice payload
- * @param telegramPaymentChargeId Telegram payment identifier
- * @param providerPaymentChargeId Provider payment identifier
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record RefundedPayment(
-        String currency,
-        @SerializedName("total_amount") Integer totalAmount,
-        @SerializedName("invoice_payload") String invoicePayload,
-        @SerializedName("telegram_payment_charge_id") String telegramPaymentChargeId,
-        @SerializedName("provider_payment_charge_id") String providerPaymentChargeId
-) {}
+public class RefundedPayment {
+    /**
+     * Three-letter ISO 4217 currency code, or “XTR” for payments in Telegram Stars. Currently, always “XTR”
+     */
+    private String currency;
+
+    /**
+     * Total refunded price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45, total_amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies)
+     */
+    @SerializedName("total_amount")
+    private Integer totalAmount;
+
+    /**
+     * Bot-specified invoice payload
+     */
+    @SerializedName("invoice_payload")
+    private String invoicePayload;
+
+    /**
+     * Telegram payment identifier
+     */
+    @SerializedName("telegram_payment_charge_id")
+    private String telegramPaymentChargeId;
+
+    /**
+     * Provider payment identifier
+     */
+    @SerializedName("provider_payment_charge_id")
+    private String providerPaymentChargeId;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public String getInvoicePayload() {
+        return invoicePayload;
+    }
+
+    public String getTelegramPaymentChargeId() {
+        return telegramPaymentChargeId;
+    }
+
+    public String getProviderPaymentChargeId() {
+        return providerPaymentChargeId;
+    }
+}
