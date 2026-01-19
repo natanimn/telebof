71dfd4a0ddc2e6faf385034bcd7e409189fafc22
natanimn <natanimco@gmail.com>
2026-01-19 07:26:16 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ReactionCount.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ReactionCount.java
@@ -4,13 +4,28 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a reaction added to a message along with the number of times it was added.
- * @param type Type of the reaction
- * @param totalCount Number of times the reaction was added
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ReactionCount(
-        ReactionType type,
-        @SerializedName("total_count") Integer totalCount
-) {}
+public class ReactionCount {
+
+    /**
+     * Type of the reaction
+     */
+    private ReactionType type;
+
+    /**
+     * Number of times the reaction was added
+     */
+    @SerializedName("total_count")
+    private Integer totalCount;
+
+    public ReactionType getType() {
+        return type;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+}
