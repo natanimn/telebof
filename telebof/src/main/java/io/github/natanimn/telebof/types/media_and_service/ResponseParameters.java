9c3c4aa4b0fdd7d353b9625c3046e310e8b8bdd8
natanimn <natanimco@gmail.com>
2026-01-19 07:27:00 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ResponseParameters.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/ResponseParameters.java
@@ -4,13 +4,30 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes why a request was unsuccessful.
- * @param migrateToChatId Optional. The group has been migrated to a supergroup with the specified identifier.
- * @param retryAfter Optional. In case of exceeding flood control, the number of seconds left to wait before the request can be repeated
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ResponseParameters(
-        @SerializedName("migrate_to_chat_id") Long migrateToChatId,
-        @SerializedName("retry_after") Integer retryAfter
-) {}
+public class ResponseParameters {
+    /**
+     * Optional.
+     * The group has been migrated to a supergroup with the specified identifier.
+     */
+    @SerializedName("migrate_to_chat_id")
+    private Long migrateToChatId;
+
+    /**
+     * Optional.
+     * In case of exceeding flood control, the number of seconds left to wait before the request can be repeated
+     */
+    @SerializedName("retry_after")
+    private Integer retryAfter;
+
+    public Integer getRetryAfter() {
+        return retryAfter;
+    }
+
+    public Long getMigrateToChatId() {
+        return migrateToChatId;
+    }
+}
