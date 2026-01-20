89b357f1ffe120b355e371e96f1ae8459db2bd8e
natanimn <natanimco@gmail.com>
2026-01-20 14:00:22 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/ShippingOption.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/ShippingOption.java
@@ -5,15 +5,43 @@ import java.util.List;
 
 /**
  * This object represents one shipping option.
- * @param id Shipping option identifier
- * @param title Option title
- * @param prices List of price portions
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ShippingOption(
-        String id,
-        String title,
-        List<LabeledPrice> prices
-) implements Serializable {}
+public class ShippingOption implements Serializable {
+    /**
+     * Shipping option identifier
+     */
+    private String id;
+
+    /**
+     * Option title
+     */
+    private String title;
+
+    /**
+     * List of price portions
+     */
+    private List<LabeledPrice> prices;
+
+    public ShippingOption(){}
+
+    public ShippingOption(String id, String title, List<LabeledPrice> prices) {
+        this.id = id;
+        this.title = title;
+        this.prices = prices;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<LabeledPrice> getPrices() {
+        return prices;
+    }
+}
