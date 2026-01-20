998e421ca0410b5f570cb3b4c9587bacaa4e7811
natanimn <natanimco@gmail.com>
2026-01-20 14:00:28 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/StarAmount.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/StarAmount.java
@@ -4,13 +4,27 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes an amount of Telegram Stars.
- * @param amount Integer amount of Telegram Stars, rounded to 0; can be negative
- * @param nanostarAmount Optional. The number of 1/1000000000 shares of Telegram Stars; from -999999999 to 999999999; can be negative if and only if amount is non-positive
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record StarAmount(
-        Integer amount,
-        @SerializedName("nanostar_amount") Integer nanostarAmount
-) {}
+public class StarAmount {
+    /**
+     * Integer amount of Telegram Stars, rounded to 0; can be negative
+     */
+    private Integer amount;
+
+    /**
+     * Optional. The number of 1/1000000000 shares of Telegram Stars; from -999999999 to 999999999; can be negative if and only if amount is non-positive
+     */
+    @SerializedName("nanostar_amount")
+    private Integer nanostarAmount;
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Integer getNanostarAmount() {
+        return nanostarAmount;
+    }
+}
