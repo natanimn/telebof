5439fe66b5d2380e97c4ca1ad520c17f6a59cd8d
natanimn <natanimco@gmail.com>
2026-01-20 13:59:02 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/payments/ShippingAddress.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/payments/ShippingAddress.java
@@ -5,21 +5,77 @@ import java.io.Serializable;
 
 /**
  * This class represents a shipping address.
- * @param countryCode Two-letter ISO 3166-1 alpha-2 country code
- * @param state State, if applicable
- * @param city City
- * @param streetLine1 First line for the address
- * @param streetLine2 Second line for the address
- * @param postCode Address of post code
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record ShippingAddress(
-        @SerializedName("country_code") String countryCode,
-        String state,
-        String city,
-        @SerializedName("street_line1") String streetLine1,
-        @SerializedName("street_line2") String streetLine2,
-        @SerializedName("post_code") String postCode
-) implements Serializable {}
+public class ShippingAddress implements Serializable {
+    /**
+     * Two-letter ISO 3166-1 alpha-2 country code
+     */
+    @SerializedName("country_code")
+    private String countryCode;
+
+    /**
+     * State, if applicable
+     */
+    private String state;
+
+    /**
+     * City
+     */
+    private String city;
+
+    /**
+     * First line for the address
+     */
+    @SerializedName("street_line1")
+    private String streetLine1;
+
+    /**
+     * Second line for the address
+     */
+    @SerializedName("street_line2")
+    private String streetLine2;
+
+    /**
+     * Address of post code
+     */
+    @SerializedName("post_code")
+    private String postCode;
+
+    public ShippingAddress(){}
+
+    public ShippingAddress(String countryCode, String state, String city, String streetLine1, String streetLine2, String postCode) {
+        this.countryCode = countryCode;
+        this.state = state;
+        this.city = city;
+        this.streetLine1 = streetLine1;
+        this.streetLine2 = streetLine2;
+        this.postCode = postCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreetLine1() {
+        return streetLine1;
+    }
+
+    public String getStreetLine2() {
+        return streetLine2;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+}
