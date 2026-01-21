b18706731ccdcc7bd3a9374446227ea8564f5100
natanimn <natanimco@gmail.com>
2026-01-21 13:36:55 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApprovalFailed.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApprovalFailed.java
@@ -6,13 +6,29 @@ import com.google.gson.annotations.SerializedName;
 /**
  * This object represents a service message about the failed approval of a suggested post.
  * Currently, only caused by insufficient user funds at the time of approval.
- * @param suggestedPostMessage Optional. Message containing the suggested post whose approval has failed. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
- * @param price Expected price of the post
  * @author Natanim
  * @since 3 September 2025
  * @version 1.3.0
  */
-public record SuggestedPostApprovalFailed(
-        @SerializedName("suggested_post_message") Message suggestedPostMessage,
-        SuggestedPostPrice price
-) {}
+public class SuggestedPostApprovalFailed {
+    /**
+     * Optional.
+     * Message containing the suggested post whose approval has failed.
+     * Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+     */
+    @SerializedName("suggested_post_message")
+    private Message suggestedPostMessage;
+
+    /**
+     * Expected price of the post
+     */
+    private SuggestedPostPrice price;
+
+    public Message getSuggestedPostMessage() {
+        return suggestedPostMessage;
+    }
+
+    public SuggestedPostPrice getPrice() {
+        return price;
+    }
+}
