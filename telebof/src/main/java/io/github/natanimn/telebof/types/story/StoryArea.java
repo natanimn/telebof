bd661cb0429c4cb95fa5b7222a0d741d76ccf917
natanimn <natanimco@gmail.com>
2026-01-21 13:35:50 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryArea.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryArea.java
@@ -4,13 +4,33 @@ import java.io.Serializable;
 
 /**
  * Describes a clickable area on a story media.
- * @param position Position of the area
- * @param type Type of the area
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record StoryArea(
-        StoryAreaPosition position,
-        StoryAreaType type
-) implements Serializable {}
+public class StoryArea implements Serializable {
+    /**
+     * Position of the area
+     */
+    private StoryAreaPosition position;
+
+    /**
+     * Type of the area
+     */
+    private StoryAreaType type;
+
+    public StoryArea(){}
+
+    public StoryArea(StoryAreaPosition position, StoryAreaType type) {
+        this.position = position;
+        this.type = type;
+    }
+
+    public StoryAreaPosition getPosition() {
+        return position;
+    }
+
+    public StoryAreaType getType() {
+        return type;
+    }
+}
