719b0c6402779ee691c1e6c5c581d0fd04708a2c
natanimn <natanimco@gmail.com>
2026-01-21 13:37:27 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostDeclined.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostDeclined.java
@@ -5,13 +5,29 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the rejection of a suggested post.
- * @param suggestedPostMessage Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
- * @param comment Optional. Comment with which the post was declined
  * @author Natanim
  * @since 3 September 2025
  * @version 1.3.0
  */
-public record SuggestedPostDeclined(
-        @SerializedName("suggested_post_message") Message suggestedPostMessage,
-        String comment
-) {}
+public class SuggestedPostDeclined {
+    /**
+     * Optional.
+     * Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+     */
+    @SerializedName("suggested_post_message")
+    private Message suggestedPostMessage;
+
+    /**
+     * Optional.
+     * Comment with which the post was declined
+     */
+    private String comment;
+
+    public Message getSuggestedPostMessage() {
+        return suggestedPostMessage;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
