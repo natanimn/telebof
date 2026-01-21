04629298f311e954260e7c47ebb8624882ed8676
natanimn <natanimco@gmail.com>
2026-01-21 13:37:08 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApproved.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostApproved.java
@@ -5,15 +5,39 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about the approval of a suggested post.
- * @param suggestedPostMessage Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
- * @param price Optional. Amount paid for the post
- * @param sendDate Date when the post will be published
  * @author Natanim
  * @since 3 September 2025
  * @version 1.3.0
  */
-public record SuggestedPostApproved(
-        @SerializedName("suggested_post_message") Message suggestedPostMessage,
-        SuggestedPostPrice price,
-        @SerializedName("send_date") Integer sendDate
-) {}
+public class SuggestedPostApproved {
+    /**
+     * Optional.
+     * Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+     */
+    @SerializedName("suggested_post_message")
+    private Message suggestedPostMessage;
+
+    /**
+     * Optional.
+     * Amount paid for the post
+     */
+    private SuggestedPostPrice price;
+
+    /**
+     * Date when the post will be published
+     */
+    @SerializedName("send_date")
+    private Integer sendDate;
+
+    public Message getSuggestedPostMessage() {
+        return suggestedPostMessage;
+    }
+
+    public SuggestedPostPrice getPrice() {
+        return price;
+    }
+
+    public Integer getSendDate() {
+        return sendDate;
+    }
+}
