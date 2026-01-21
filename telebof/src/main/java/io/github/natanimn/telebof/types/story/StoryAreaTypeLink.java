7e31cda4274e571e8ff98b31f7836c27ef6ca454
natanimn <natanimco@gmail.com>
2026-01-21 13:37:38 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaTypeLink.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/story/StoryAreaTypeLink.java
@@ -2,15 +2,22 @@ package io.github.natanimn.telebof.types.story;
 
 /**
  * Describes a story area pointing to an HTTP or tg:// link. Currently, a story can have up to 3 link areas.
- * @param url HTTP or tg:// URL to be opened when the area is clicked
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
  */
-public record StoryAreaTypeLink(
-        String url
-) implements StoryAreaType {
-    public String type() {
-        return "link";
+public class StoryAreaTypeLink implements StoryAreaType {
+    /**
+     * HTTP or tg:// URL to be opened when the area is clicked
+     */
+    private String url;
+    private final String type = "link";
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getType() {
+        return type;
     }
 }
