fc40668672cbb7467604640a793f872dddc8f439
natanimn <natanimco@gmail.com>
2026-01-21 13:38:01 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostInfo.java
@@ -4,15 +4,38 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Contains information about a suggested post.
- * @param state State of the suggested post. Currently, it can be one of "pending", "approved", "declined".
- * @param price Optional. Proposed price of the post. If the field is omitted, then the post is unpaid.
- * @param sendDate Optional. Proposed send date of the post. If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user or administrator who approves it.
  * @author Natanim
  * @since 3 September 2025
  * @version 1.3.0
  */
-public record SuggestedPostInfo(
-        String state,
-        SuggestedPostPrice price,
-        @SerializedName("send_date") Integer sendDate
-) {}
+public class SuggestedPostInfo {
+    /**
+     * State of the suggested post. Currently, it can be one of "pending", "approved", "declined".
+     */
+    private String state;
+
+    /**
+     * Optional.
+     * Proposed price of the post. If the field is omitted, then the post is unpaid.
+     */
+    private SuggestedPostPrice price;
+
+    /**
+     * Optional.
+     * Proposed send date of the post. If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user or administrator who approves it.
+     */
+    @SerializedName("send_date")
+    private Integer sendDate;
+
+    public String getState() {
+        return state;
+    }
+
+    public SuggestedPostPrice getPrice() {
+        return price;
+    }
+
+    public Integer getSendDate() {
+        return sendDate;
+    }
+}
