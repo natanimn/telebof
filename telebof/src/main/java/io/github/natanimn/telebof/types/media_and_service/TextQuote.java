918559fd321c79c2af962afd4fd40e6f72940c92
natanimn <natanimco@gmail.com>
2026-01-22 08:56:58 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/TextQuote.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/TextQuote.java
@@ -5,17 +5,46 @@ import java.util.List;
 
 /**
  * This object contains information about the quoted part of a message that is replied to by the given message.
- * @param text Text of the quoted part of a message that is replied to by the given message
- * @param entities Optional. Special entities that appear in the quote. Currently, only bold, italic, underline, strikethrough, spoiler, and custom_emoji entities are kept in quotes.
- * @param position Approximate quote position in the original message in UTF-16 code units as specified by the sender
- * @param isManual Optional. True, if the quote was chosen manually by the message sender. Otherwise, the quote was added automatically by the server.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record TextQuote(
-        String text,
-        List<MessageEntity> entities,
-        Integer position,
-        @SerializedName("is_manual") Boolean isManual
-) {}
+public class TextQuote {
+    /**
+     * Text of the quoted part of a message that is replied to by the given message
+     */
+    private String text;
+
+    /**
+     * Optional. Special entities that appear in the quote.
+     * Currently, only bold, italic, underline, strikethrough, spoiler, and custom_emoji entities are kept in quotes.
+     */
+    private List<MessageEntity> entities;
+
+    /**
+     * Approximate quote position in the original message in UTF-16 code units as specified by the sender
+     */
+    private Integer position;
+
+    /**
+     * Optional. True, if the quote was chosen manually by the message sender. Otherwise, the quote was added automatically by the server.
+     */
+    @SerializedName("is_manual")
+    private Boolean isManual;
+
+    public String getText() {
+        return text;
+    }
+
+    public List<MessageEntity> getEntities() {
+        return entities;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public Boolean getIsManual() {
+        return isManual;
+    }
+}
