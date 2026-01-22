bb6d11c910a08743068df4c717f0aac4ec040785
natanimn <natanimco@gmail.com>
2026-01-22 08:56:43 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostRefunded.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostRefunded.java
@@ -5,13 +5,28 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about a payment refund for a suggested post.
- * @param suggestedPostMessage Optional. Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
- * @param reason Reason for the refund. Currently, one of "post_deleted" if the post was deleted within 24 hours of being posted or removed from scheduled messages without being posted, or "payment_refunded" if the payer refunded their payment.
  * @author Natanim
  * @since 3 September 2025
  * @version 1.3.0
  */
-public record SuggestedPostRefunded(
-        @SerializedName("suggested_post_message") Message suggestedPostMessage,
-        String reason
-) {}
+public class SuggestedPostRefunded {
+    /**
+     * Optional.
+     * Message containing the suggested post. Note that the Message object in this field will not contain the reply_to_message field even if it itself is a reply.
+     */
+    @SerializedName("suggested_post_message")
+    private Message suggestedPostMessage;
+
+    /**
+     * Reason for the refund. Currently, one of "post_deleted" if the post was deleted within 24 hours of being posted or removed from scheduled messages without being posted, or "payment_refunded" if the payer refunded their payment.
+     */
+    private String reason;
+
+    public Message getSuggestedPostMessage() {
+        return suggestedPostMessage;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+}
