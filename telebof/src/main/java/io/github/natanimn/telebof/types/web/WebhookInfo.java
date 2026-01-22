7a8be40548b120bd7296ed9a31e3d2d7da26a591
natanimn <natanimco@gmail.com>
2026-01-22 08:58:00 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/web/WebhookInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/web/WebhookInfo.java
@@ -7,26 +7,92 @@ import java.util.List;
 
 /**
  * Describes the current status of a webhook.
- *
- * @param url Webhook URL, may be empty if webhook is not set up
- * @param ipAddress Optional. Currently used webhook IP address
- * @param lastErrorMessage Optional. Error message in human-readable format for the most recent error that happened when trying to deliver an update via webhook
- * @param pendingUpdateCount Number of updates awaiting delivery
- * @param lastSynchronizationErrorDate Optional. Unix time of the most recent error that happened when trying to synchronize available updates with Telegram datacenters
- * @param maxConnections Optional. The maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
- * @param hasCustomCertificate True, if a custom certificate was provided for webhook certificate checks
- * @param allowedUpdates Optional. A list of update types the bot is subscribed to. Defaults to all update types except chat_member
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record WebhookInfo(
-        String url,
-        @SerializedName("ip_address") String ipAddress,
-        @SerializedName("last_error_message") String lastErrorMessage,
-        @SerializedName("pending_update_count") Integer pendingUpdateCount,
-        @SerializedName("last_synchronization_error_date") Integer lastSynchronizationErrorDate,
-        @SerializedName("max_connections") Integer maxConnections,
-        @SerializedName("has_custom_certificate") Boolean hasCustomCertificate,
-        @SerializedName("allowed_updates") List<Updates> allowedUpdates
-) {}
+public class WebhookInfo {
+    /**
+     * Webhook URL, may be empty if webhook is not set up
+     */
+    private String url;
+
+    /**
+     * Optional.
+     * Currently used webhook IP address
+     */
+    @SerializedName("ip_address")
+    private String ipAddress;
+
+    /**
+     * Optional.
+     * Error message in human-readable format for the most recent error that happened when trying to deliver an update via webhook
+     */
+    @SerializedName("last_error_message")
+    private String lastErrorMessage;
+
+    /**
+     * Number of updates awaiting delivery
+     */
+    @SerializedName("pending_update_count")
+    private Integer pendingUpdateCount;
+
+    /**
+     * Optional.
+     * Unix time of the most recent error that happened when trying to synchronize available updates with Telegram datacenters
+     */
+    @SerializedName("last_synchronization_error_date")
+    private Integer lastSynchronizationErrorDate;
+
+    /**
+     * Optional.
+     * The maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+     */
+    @SerializedName("max_connections")
+    private Integer maxConnections;
+
+    /**
+     * True, if a custom certificate was provided for webhook certificate checks
+     */
+    @SerializedName("has_custom_certificate")
+    private Boolean hasCustomCertificate;
+
+    /**
+     * Optional.
+     * A list of update types the bot is subscribed to. Defaults to all update types except chat_member
+     */
+    @SerializedName("allowed_updates")
+    private List<Updates> allowedUpdates;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public String getLastErrorMessage() {
+        return lastErrorMessage;
+    }
+
+    public Integer getPendingUpdateCount() {
+        return pendingUpdateCount;
+    }
+
+    public Integer getLastSynchronizationErrorDate() {
+        return lastSynchronizationErrorDate;
+    }
+
+    public Integer getMaxConnections() {
+        return maxConnections;
+    }
+
+    public Boolean getHasCustomCertificate() {
+        return hasCustomCertificate;
+    }
+
+    public List<Updates> getAllowedUpdates() {
+        return allowedUpdates;
+    }
+}
