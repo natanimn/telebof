74295b2b369e007f860a1f527e6cf2ca36232bb4
natanimn <natanimco@gmail.com>
2026-01-22 08:56:12 +0300

    Feat: Added getter and setter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostParameters.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/suggested/SuggestedPostParameters.java
@@ -4,43 +4,37 @@ import java.io.Serializable;
 
 /**
  * Contains parameters of a post that is being suggested by the bot.
- * @param price Optional. Proposed price for the post. If the field is omitted, then the post is unpaid.
- * @param date Optional. Proposed send date of the post. If specified, then the date must be between 300 second and 2678400 seconds (30 days) in the future. If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user who approves it.
  * @author Natanim
  * @since 3 September 2025
  * @version 1.3.0
  */
-public record SuggestedPostParameters(
-        SuggestedPostPrice price,
-        Integer date
-) implements Serializable {
+public class SuggestedPostParameters implements Serializable{
+    /**
+     * Optional.
+     * Proposed price for the post. If the field is omitted, then the post is unpaid.
+     */
+    private SuggestedPostPrice price;
 
     /**
-     * Creates a new SuggestedPostParameters builder
+     * Optional.
+     * Proposed send date of the post. If specified, then the date must be between 300 second and 2678400 seconds (30 days) in the future.
+     * If the field is omitted, then the post can be published at any time within 30 days at the sole discretion of the user who approves it.
      */
-    public static SuggestedPostParametersBuilder builder() {
-        return new SuggestedPostParametersBuilder();
+    private Long date;
+
+    public SuggestedPostPrice getPrice() {
+        return price;
     }
 
-    /**
-     * Builder class for SuggestedPostParameters
-     */
-    public static class SuggestedPostParametersBuilder {
-        private SuggestedPostPrice price;
-        private Integer date;
+    public Long getDate() {
+        return date;
+    }
 
-        public SuggestedPostParametersBuilder price(SuggestedPostPrice price) {
-            this.price = price;
-            return this;
-        }
+    public void setDate(Long date) {
+        this.date = date;
+    }
 
-        public SuggestedPostParametersBuilder date(Integer date) {
-            this.date = date;
-            return this;
-        }
-
-        public SuggestedPostParameters build() {
-            return new SuggestedPostParameters(price, date);
-        }
+    public void setPrice(SuggestedPostPrice price) {
+        this.price = price;
     }
 }
