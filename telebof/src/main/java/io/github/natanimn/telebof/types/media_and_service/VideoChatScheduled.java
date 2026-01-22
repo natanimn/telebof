f5ef09598badbcac9512bee45e889b000647ca3d
natanimn <natanimco@gmail.com>
2026-01-22 08:57:14 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatScheduled.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatScheduled.java
@@ -4,11 +4,18 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object represents a service message about a video chat scheduled in the chat.
- * @param startDate Point in time (Unix timestamp) when the video chat is supposed to be started by a chat administrator
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record VideoChatScheduled(
-        @SerializedName("start_date") Integer startDate
-) {}
+public class VideoChatScheduled {
+    /**
+     * Point in time (Unix timestamp) when the video chat is supposed to be started by a chat administrator
+     */
+    @SerializedName("start_date")
+    private Integer startDate;
+
+    public Integer getStartDate() {
+        return startDate;
+    }
+}
