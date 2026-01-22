5bffad2c8de4cca568ba23dbdf72f473afe05ec4
natanimn <natanimco@gmail.com>
2026-01-22 08:57:37 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/web/WebAppInfo.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/web/WebAppInfo.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 /**
  * Describes a <a href="https://core.telegram.org/bots/webapps">Web App.</a>
- *
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
@@ -14,6 +13,8 @@ public class WebAppInfo implements Serializable {
      * An HTTPS URL of a Web App to be opened with additional data as specified in Initializing Web Apps
      */
     private String url;
+
+    public WebAppInfo(){}
 
     public WebAppInfo(String url) {
         this.url = url;
