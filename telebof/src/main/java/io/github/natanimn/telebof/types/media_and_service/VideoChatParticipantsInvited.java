f966232cda87e7b5597cea916a7aba1764237471
natanimn <natanimco@gmail.com>
2026-01-22 08:57:09 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatParticipantsInvited.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/VideoChatParticipantsInvited.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 /**
  * This object represents a service message about new members invited to a video chat.
- * @param users New members that were invited to the video chat
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record VideoChatParticipantsInvited(
-        List<User> users
-) {}
+public class VideoChatParticipantsInvited {
+    /**
+     * New members that were invited to the video chat
+     */
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+}
