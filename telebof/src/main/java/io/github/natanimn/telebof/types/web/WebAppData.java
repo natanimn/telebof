133e38833bc041328f153ebd3a8c59fb1ebd2e57
natanimn <natanimco@gmail.com>
2026-01-22 08:57:26 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/web/WebAppData.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/web/WebAppData.java
@@ -4,14 +4,27 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Describes data sent from a <a href="https://core.telegram.org/bots/webapps">Web App</a> to the bot.
- *
- * @param data The data. Be aware that a bad client can send arbitrary data in this field.
- * @param buttonText Text of the web_app keyboard button from which the Web App was opened. Be aware that a bad client can send arbitrary data in this field.
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record WebAppData(
-        String data,
-        @SerializedName("button_text") String buttonText
-) {}
+public class WebAppData {
+    /**
+     * The data. Be aware that a bad client can send arbitrary data in this field.
+     */
+    private String data;
+
+    /**
+     * Text of the web_app keyboard button from which the Web App was opened. Be aware that a bad client can send arbitrary data in this field.
+     */
+    @SerializedName("button_text")
+    private String buttonText;
+
+    public String getData() {
+        return data;
+    }
+
+    public String getButtonText() {
+        return buttonText;
+    }
+}
