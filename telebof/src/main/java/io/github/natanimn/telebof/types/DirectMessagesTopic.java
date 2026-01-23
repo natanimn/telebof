36cbcb9a0f37dac93a6cdfaa3355ec498ffa67ee
natanimn <natanimco@gmail.com>
2026-01-23 08:06:00 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/DirectMessagesTopic.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/DirectMessagesTopic.java
@@ -7,12 +7,26 @@ import io.github.natanimn.telebof.types.chat_and_user.User;
  * Describes a topic of a direct messages chat.
  * @author Natanim
  * @since 3 September 2025
- * @version 1.2.0
- * @param topicId Unique identifier of the topic.
- * @param user Optional. Information about the user that created the topic. Currently, it is always present
+ * @version 1.3.0
  */
 
-public record DirectMessagesTopic(
-    @SerializedName("topic_id") Long topicId,
-    User user
-) { }
+public class DirectMessagesTopic {
+    /**
+     * Unique identifier of the topic.
+     */
+    @SerializedName("topic_id")
+    private Long topicId;
+
+    /**
+     * Optional. Information about the user that created the topic. Currently, it is always present
+     */
+    private User user;
+
+    public Long getTopicId() {
+        return topicId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
