bfdce64cc042811a5ced35b3280d32565815ae66
natanimn <natanimco@gmail.com>
2026-01-23 08:04:35 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPaidMedia.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPaidMedia.java
@@ -4,6 +4,4 @@ package io.github.natanimn.telebof.types.input;
  * This object describes the paid media to be sent
  * @author Natanim
  */
-public interface InputPaidMedia extends InputMedia {
-    String type();
-}
+public interface InputPaidMedia extends InputMedia { }
