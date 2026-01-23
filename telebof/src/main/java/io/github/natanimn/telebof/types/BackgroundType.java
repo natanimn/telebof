d798a21b04e25b3e210b161a19affe225adfa245
natanimn <natanimco@gmail.com>
2026-01-23 08:08:09 +0300

    Feat: Added getter methods

--- telebof/src/main/java/io/github/natanimn/telebof/types/BackgroundType.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/BackgroundType.java
@@ -6,28 +6,94 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * This object describes the type of background.
- *
- * @param type Type of the background
- * @param themeName Name of the chat theme, which is usually an emoji
- * @param fill The background fill that is combined with the pattern
- * @param darkThemeDimming Dimming of the background in dark themes, as a percentage; 0-100
- * @param intensity Intensity of the pattern when it is shown above the filled background; 0-100
- * @param document Document with the pattern
- * @param isBlurred True, if the wallpaper is downscaled to fit in a 450x450 square and then box-blurred with radius 12
- * @param isMoving True, if the background moves slightly when the device is tilted
- * @param isInverted True, if the background fill must be applied only to the pattern itself. All other pixels are black in this case. For dark themes only
  * @author Natanim
  * @since 3 March 2025
  * @version 1.3.0
  */
-public record BackgroundType(
-        BackgroundTypeType type,
-        @SerializedName("theme_name") String themeName,
-        BackgroundFill fill,
-        @SerializedName("dark_theme_dimming") Integer darkThemeDimming,
-        Integer intensity,
-        Document document,
-        @SerializedName("is_blurred") Boolean isBlurred,
-        @SerializedName("is_moving") Boolean isMoving,
-        @SerializedName("is_inverted") Boolean isInverted
-) {}
+public class BackgroundType {
+    /**
+     * Type of the background
+     */
+    BackgroundTypeType type;
+
+    /**
+     * Name of the chat theme, which is usually an emoji
+     */
+    @SerializedName("theme_name")
+    private String themeName;
+
+    /**
+     * The background fill that is combined with the pattern
+     */
+    private BackgroundFill fill;
+
+    /**
+     * Dimming of the background in dark themes, as a percentage; 0-100
+     */
+    @SerializedName("dark_theme_dimming")
+    private Integer darkThemeDimming;
+
+    /**
+     * Intensity of the pattern when it is shown above the filled background; 0-100
+     */
+    private Integer intensity;
+
+    /**
+     * Document with the pattern
+     */
+    private Document document;
+
+    /**
+     * True, if the wallpaper is downscaled to fit in a 450x450 square and then box-blurred with radius 12
+     */
+    @SerializedName("is_blurred")
+    private Boolean isBlurred;
+
+    /**
+     * True, if the background moves slightly when the device is tilted
+     */
+    @SerializedName("is_moving")
+    private Boolean isMoving;
+
+    /**
+     * True, if the background fill must be applied only to the pattern itself. All other pixels are black in this case. For dark themes only
+     */
+    @SerializedName("is_inverted")
+    private Boolean isInverted;
+
+    public BackgroundTypeType getType() {
+        return type;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public BackgroundFill getFill() {
+        return fill;
+    }
+
+    public Integer getDarkThemeDimming() {
+        return darkThemeDimming;
+    }
+
+    public Integer getIntensity() {
+        return intensity;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public Boolean getIsBlurred() {
+        return isBlurred;
+    }
+
+    public Boolean getIsMoving() {
+        return isMoving;
+    }
+
+    public Boolean getIsInverted() {
+        return isInverted;
+    }
+}
