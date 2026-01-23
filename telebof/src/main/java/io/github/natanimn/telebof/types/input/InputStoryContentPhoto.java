1844b94d058a17f09a96743c18ac0a22f127b785
natanimn <natanimco@gmail.com>
2026-01-23 08:04:13 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContentPhoto.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputStoryContentPhoto.java
@@ -8,12 +8,19 @@ import java.util.List;
  * @author Natanim
  * @since 19 August 2025
  * @version 1.3.0
- * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
  */
-public record InputStoryContentPhoto(String photo, File file) implements InputStoryContent{
-    @Override
-    public String type() {
-        return "photo";
+public class InputStoryContentPhoto implements InputStoryContent {
+    private final String type = "photo";
+    private String photo;
+    transient private File file;
+
+    /**
+     * Required
+     * @param photo The photo to post as a story. The photo must be of the size 1080x1920 and must not exceed 10 MB
+     */
+    public InputStoryContentPhoto(File photo) {
+        this.file = photo;
+        this.photo = "attach://" + photo.getName();
     }
 
     /**
@@ -32,27 +39,5 @@ public record InputStoryContentPhoto(String photo, File file) implements InputSt
     @Override
     public List<File> getFiles() {
         return List.of(file);
-    }
-
-    public static InputStoryContentVideo.InputStoryContentVideoBuilder builder(File video) {
-        return new InputStoryContentVideo.InputStoryContentVideoBuilder(video);
-    }
-
-    /**
-     * Builder class for InputStoryContentPhoto
-     */
-    public static class InputStoryContentPhotoBuilder {
-        private final File photo;
-
-        public InputStoryContentPhotoBuilder(File photo) {
-            this.photo = photo;
-        }
-
-        public InputStoryContentPhoto build() {
-            return new InputStoryContentPhoto(
-                    "attach://" + photo.getName(),
-                    photo
-            );
-        }
     }
 }
