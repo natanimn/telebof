fc2c05dc1ff5629c6c1192c6be5a9961c1cf7d07
natanimn <natanimco@gmail.com>
2026-01-23 08:04:04 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/requests/service/LogOut.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/service/LogOut.java
@@ -4,12 +4,12 @@ import io.github.natanimn.telebof.requests.AbstractBaseRequest;
 import io.github.natanimn.telebof.requests.Api;
 
 /**
- * Natanim Negash
- *  3 March 2025
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 1.3.0
  */
 public class LogOut extends AbstractBaseRequest<LogOut, Boolean> {
     public LogOut(Api api) {
         super(api, "logOut");
     }
-
 }
