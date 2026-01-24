0d0696d88a264a8265099d67fae35115f3d238cb
natanimn <natanimco@gmail.com>
2026-01-24 09:22:18 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/exceptions/FloodError.java
+++ telebof/src/main/java/io/github/natanimn/telebof/exceptions/FloodError.java
@@ -6,14 +6,13 @@ import io.github.natanimn.telebof.types.media_and_service.ResponseParameters;
  * Base class for flood errors.
  * @author Natanim
  * @since 18 August 2025
- * @version 0.9.5
+ * @version 1.3.0
  */
 public class FloodError extends TelegramApiException {
     /**
      * @param description human readable description
-     * @param name canonical Telegram error name
      */
-    public FloodError(String description, String name, ResponseParameters parameters) {
-        super(420, description, parameters, name);
+    public FloodError(String description, ResponseParameters parameters) {
+        super(420, description, parameters, "FLOOD_ERROR");
     }
 }
