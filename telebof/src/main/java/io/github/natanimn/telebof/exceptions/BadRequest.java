239148a6777601d0f4189491c7972e2732a9606e
natanimn <natanimco@gmail.com>
2026-01-24 09:21:57 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/exceptions/BadRequest.java
+++ telebof/src/main/java/io/github/natanimn/telebof/exceptions/BadRequest.java
@@ -4,15 +4,14 @@ package io.github.natanimn.telebof.exceptions;
  * Base class for bad_request errors.
  * @author Natanim
  * @since 18 August 2025
- * @version 0.9.5
+ * @version 1.3.0
  */
 public class BadRequest extends TelegramApiException {
     /**
      * @param description human-readable description
-     * @param name canonical Telegram error name
      */
-    public BadRequest(String description, String name) {
-        super(400, description, name);
+    public BadRequest(String description) {
+        super(400, description, "BAD_REQUEST");
     }
 
 }
