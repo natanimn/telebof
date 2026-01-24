e34651448b14d8aa7499b2a6ddf8e86ad0f61f67
natanimn <natanimco@gmail.com>
2026-01-24 09:23:10 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/exceptions/Forbidden.java
+++ telebof/src/main/java/io/github/natanimn/telebof/exceptions/Forbidden.java
@@ -4,14 +4,13 @@ package io.github.natanimn.telebof.exceptions;
  * Base class for forbidden errors.
  * @author Natanim
  * @since 18 August 2025
- * @version 0.9.5
+ * @version 1.3.0
  */
 public class Forbidden extends TelegramApiException {
     /**
      * @param description human readable description
-     * @param name canonical Telegram error name
      */
-    public Forbidden(String description, String name) {
-        super(403, description, name);
+    public Forbidden(String description) {
+        super(403, description, "FORBIDDEN");
     }
 }
