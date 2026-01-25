9689f32d643e4e94d304189a73dfd8da0d1b5100
natanimn <natanimco@gmail.com>
2026-01-25 05:21:13 +0300

    Refactor: replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotCommandScopeChatMember.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/bot/BotCommandScopeChatMember.java
@@ -1,15 +1,19 @@
 package io.github.natanimn.telebof.types.bot;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents the scope of bot commands, covering a specific member of a group or supergroup chat.
  * @author Natanim 
  * @since 3 March 2025
- * @version 0.8
+ * @version 1.3.0
  */
 public class BotCommandScopeChatMember extends BotCommandScope {
-    public Object chat_id;
+    @SerializedName("chat_id")
+    private Object chatId;
+
     public BotCommandScopeChatMember(Object chatId) {
         super("chat_member");
-        this.chat_id = chatId;
+        this.chatId = chatId;
     }
 }
