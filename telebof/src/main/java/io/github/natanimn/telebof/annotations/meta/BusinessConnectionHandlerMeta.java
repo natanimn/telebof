00313b1fcdd93a8b7d2e4b979ab0292032417ce3
natanimn <natanimco@gmail.com>
2026-01-26 08:42:45 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/BusinessConnectionHandlerMeta.java
+++ telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/BusinessConnectionHandlerMeta.java
@@ -1,7 +1,6 @@
 package io.github.natanimn.telebof.annotations.meta;
 
 import io.github.natanimn.telebof.annotations.BusinessConnectionHandler;
-import io.github.natanimn.telebof.annotations.PollAnswerHandler;
 import io.github.natanimn.telebof.filters.CustomFilter;
 import io.github.natanimn.telebof.filters.DefaultCustomFilter;
 import io.github.natanimn.telebof.filters.Filter;
@@ -9,6 +8,12 @@ import io.github.natanimn.telebof.filters.Filter;
 import java.lang.invoke.MethodHandle;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * BusinessConnectionHandlerMeta class
+ * @author Natanim
+ * @since 1.2.0
+ * @version 1.3.0
+ */
 public class BusinessConnectionHandlerMeta {
     final MethodHandle method;
     final CustomFilter customFilter;
