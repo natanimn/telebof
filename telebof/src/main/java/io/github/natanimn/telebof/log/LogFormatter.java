793c377a3a70e5f80b1d1e19ff135fe737ab14db
natanimn <natanimco@gmail.com>
2026-01-29 08:47:09 +0300

    Fix: Moved to package log

---
+++ telebof/src/main/java/io/github/natanimn/telebof/log/LogFormatter.java
@@ -0,0 +1,94 @@
+package io.github.natanimn.telebof.log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
+
+/**
+ * Log Formater
+ * @author Natanim
+ * @since 3 March 2025
+ * @version 1.3.0
+ */
+class LogFormatter extends Formatter {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    private final boolean useColor;
+
+    public LogFormatter(boolean useColor) {
+        this.useColor = useColor;
+    }
+
+    @Override
+    public String format(LogRecord record) {
+        StringBuilder builder = new StringBuilder();
+
+        String date = calcDate(record.getMillis());
+        String levelName;
+        String color = "";
+        String reset = "";
+
+        if (useColor) reset = ANSI_RESET;
+
+        switch (record.getLevel().getName()) {
+            case "SEVERE":
+                levelName = "ERROR";
+                if (useColor) color = ANSI_RED;
+                break;
+            case "CONFIG":
+                levelName = "DEBUG";
+                if (useColor) color = ANSI_WHITE;
+                break;
+            case "WARNING":
+                levelName = "WARN";
+                if (useColor) color = ANSI_YELLOW;
+                break;
+            default:
+                levelName = record.getLevel().getName();
+                if (useColor) color = ANSI_GREEN;
+                break;
+        }
+
+        String caller = getCallerClassName();
+
+        builder.append(useColor ? ANSI_CYAN : "")
+                .append(date).append(" ")
+                .append(color)
+                .append("[Telebof][").append(levelName).append("]")
+                .append(reset).append(" ")
+                .append(caller)
+                .append(" - ")
+                .append(record.getMessage())
+                .append("\n");
+
+        return builder.toString();
+    }
+
+    private String calcDate(long millis) {
+        String pattern = System.getProperty("telebof.dateFormat", "yyyy-MM-dd HH:mm:ss");
+        return new SimpleDateFormat(pattern).format(new Date(millis));
+    }
+
+    private String getCallerClassName() {
+        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+        boolean foundLoggerClass = false;
+
+        for (StackTraceElement element : stack) {
+            String className = element.getClassName();
+            if (className.equals(BotLog.class.getName()) || className.startsWith("java.util.logging")) {
+                foundLoggerClass = true;
+                continue;
+            }
+            if (foundLoggerClass) {
+                return className.substring(className.lastIndexOf('.') + 1);
+            }
+        }
+        return "UnknownClass";
+    }
+}
+
