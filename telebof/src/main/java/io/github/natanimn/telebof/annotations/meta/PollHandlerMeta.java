171d9f1c4fe08f44399a1f49f22e910fda35a4cf
natanimn <natanimco@gmail.com>
2026-01-30 12:16:05 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/PollHandlerMeta.java
+++ telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/PollHandlerMeta.java
@@ -8,6 +8,12 @@ import io.github.natanimn.telebof.filters.Filter;
 import java.lang.invoke.MethodHandle;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * PollHandlerMeta class.
+ * @author Natanim
+ * @since 1.2.0
+ * @version 1.3.0
+ */
 public class PollHandlerMeta {
     final MethodHandle method;
     final CustomFilter customFilter;
