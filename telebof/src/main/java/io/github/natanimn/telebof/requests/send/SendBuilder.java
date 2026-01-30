d1c184d77faf7cab8defc49c5cef74722643e1d7
natanimn <natanimco@gmail.com>
2026-01-30 12:25:09 +0300

    Refactor: Replaced snake case fields with camel case

--- telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendBuilder.java
+++ telebof/src/main/java/io/github/natanimn/telebof/requests/send/SendBuilder.java
@@ -27,108 +27,108 @@ abstract public class SendBuilder<T, R> extends AbstractBaseRequest<T, R> {
 
     /**
      * Optional
-     * @param message_thread_id Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+     * @param messageThreadId Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
      * @return {@link T}
      */
-    public T messageThreadId(int message_thread_id) {
-        return add("message_thread_id", message_thread_id);
+    public T messageThreadId(int messageThreadId) {
+        return add("message_thread_id", messageThreadId);
     }
 
     /**
      * Optional
-     * @param parse_mode Mode for parsing entities in the message text.
+     * @param parseMode Mode for parsing entities in the message text.
      * @return {@link T}
      */
-    public T parseMode(ParseMode parse_mode) {
-        return add("parse_mode",  parse_mode);
+    public T parseMode(ParseMode parseMode) {
+        return add("parse_mode",  parseMode);
     }
 
 
-    public T disableWebPagePreview(boolean disable_web_page_preview) {
-        return add("disable_web_page_preview", disable_web_page_preview);
+    public T disableWebPagePreview(boolean disableWebPagePreview) {
+        return add("disable_web_page_preview", disableWebPagePreview);
     }
 
     /**
      * Optional
-     * @param disable_notification Sends the message silently. Users will receive a notification with no sound.
+     * @param disableNotification Sends the message silently. Users will receive a notification with no sound.
      * @return {@link T}
      */
-    public T disableNotification(boolean disable_notification) {
-        return add("disable_notification", disable_notification);
+    public T disableNotification(boolean disableNotification) {
+        return add("disable_notification", disableNotification);
     }
 
     /**
      * Optional
-     * @param protect_content Protects the contents of the sent message from forwarding and saving
+     * @param protectContent Protects the contents of the sent message from forwarding and saving
      * @return {@link T}
      */
-    public T protectContent(boolean protect_content) {
-        return add("protect_content", protect_content);
+    public T protectContent(boolean protectContent) {
+        return add("protect_content", protectContent);
     }
 
     /**
      * Optional
-     * @param reply_parameters Description of the message to reply to
+     * @param replyParameters Description of the message to reply to
      * @return {@link T}
      */
-    public T replyParameters(ReplyParameters reply_parameters){
-        return add("reply_parameters", reply_parameters);
+    public T replyParameters(ReplyParameters replyParameters){
+        return add("reply_parameters", replyParameters);
     }
 
     /**
      * Optional
-     * @param reply_markup Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard,
+     * @param replyMarkup Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard,
      *                     instructions to remove a reply keyboard or to force a reply from the user
      * @return {@link T}
      */
-    public T replyMarkup(Markup reply_markup) {
-        return add("reply_markup", reply_markup);
+    public T replyMarkup(Markup replyMarkup) {
+        return add("reply_markup", replyMarkup);
     }
 
     /**
      * Optional
-     * @param business_connection_id Unique identifier of the business connection on behalf of which the message will be sent
+     * @param businessConnectionId Unique identifier of the business connection on behalf of which the message will be sent
      * @return {@link T}
      */
-    public T businessConnectionId(String business_connection_id){
-        return add("business_connection_id", business_connection_id);
+    public T businessConnectionId(String businessConnectionId){
+        return add("business_connection_id", businessConnectionId);
     }
 
     /**
      * Optional
-     * @param message_effect_id Unique identifier of the message effect to be added to the message; for private chats only
+     * @param messageEffectId Unique identifier of the message effect to be added to the message; for private chats only
      * @return {@link T}
      */
-    public T messageEffectId(String message_effect_id ){
-        return add("message_effect_id", message_effect_id);
+    public T messageEffectId(String messageEffectId){
+        return add("message_effect_id", messageEffectId);
     }
 
     /**
      * Optional
-     * @param allow_paid_broadcast Pass True to allow up to 1000 messages per second, ignoring broadcasting limits for a fee of 0.1 Telegram Stars per message.
+     * @param allowPaidBroadcast Pass True to allow up to 1000 messages per second, ignoring broadcasting limits for a fee of 0.1 Telegram Stars per message.
      *                             The relevant Stars will be withdrawn from the bot's balance
      * @return {@link T}
      */
-    public T allowPaidBroadcast(boolean allow_paid_broadcast){
-        return add("allow_paid_broadcast", allow_paid_broadcast);
+    public T allowPaidBroadcast(boolean allowPaidBroadcast){
+        return add("allow_paid_broadcast", allowPaidBroadcast);
     }
 
     /***
      * Optional
-     * @param direct_messages_topic_id Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat
+     * @param directMessagesTopicId Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat
      * @return {@link T}
      */
-    public T directMessagesTopicId(int direct_messages_topic_id){
-        return add("direct_messages_topic_id", direct_messages_topic_id);
+    public T directMessagesTopicId(int directMessagesTopicId){
+        return add("direct_messages_topic_id", directMessagesTopicId);
     }
 
     /**
      * Optional
-     * @param suggested_post_parameters A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.
+     * @param suggestedPostParameters A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.
      *                                  If the message is sent as a reply to another suggested post, then that suggested post is automatically declined.
      * @return {@link T}
      */
-    public T suggestedPostParameters(SuggestedPostParameters suggested_post_parameters){
-        return add("suggested_post_parameters", suggested_post_parameters);
+    public T suggestedPostParameters(SuggestedPostParameters suggestedPostParameters){
+        return add("suggested_post_parameters", suggestedPostParameters);
     }
 }
