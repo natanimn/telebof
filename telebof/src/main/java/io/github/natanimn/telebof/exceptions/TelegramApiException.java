f63346a30ab2cca09df67f116a9a3b1172cfe23d
natanimn <natanimco@gmail.com>
2026-01-31 11:15:55 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/exceptions/TelegramApiException.java
+++ telebof/src/main/java/io/github/natanimn/telebof/exceptions/TelegramApiException.java
@@ -12,28 +12,28 @@ import io.github.natanimn.telebof.types.media_and_service.ResponseParameters;
 public class TelegramApiException extends RuntimeException {
     public boolean ok;
     public String description;
-    public int error_code;
+    public Integer errorCode;
     public ResponseParameters parameters;
     private String name;
 
-    public TelegramApiException(ApiResponse response) {
-        super(String.format("Error code: %d. %s", response.error_code, response.description));
-        this.ok = response.ok;
-        this.description = response.description;
-        this.error_code = response.error_code;
-        this.parameters = response.parameters;
+    public TelegramApiException(ApiResponse<?> response) {
+        super(String.format("Error code: %d. %s", response.getErrorCode(), response.getDescription()));
+        this.ok = response.isOk();
+        this.description = response.getDescription();
+        this.errorCode = response.getErrorCode();
+        this.parameters = response.getParameters();
     }
 
-    public TelegramApiException(int error_code, String description, String name){
+    public TelegramApiException(int errorCode, String description, String name){
         super(String.format("[%s] - %s", name, description));
-        this.error_code  = error_code;
+        this.errorCode  = errorCode;
         this.description = description;
         this.name = name;
     }
 
-    public TelegramApiException(int error_code, String description, ResponseParameters parameters, String name){
+    public TelegramApiException(int errorCode, String description, ResponseParameters parameters, String name){
         super(String.format("[%s] - %s", name, description));
-        this.error_code  = error_code;
+        this.errorCode  = errorCode;
         this.description = description;
         this.parameters  = parameters;
         this.name        = name;
@@ -45,8 +45,8 @@ public class TelegramApiException extends RuntimeException {
 
     public static <T> TelegramApiException throwIt(ApiResponse<T> response){
 
-        var error_type    = response.description.split(":")[0];
-        var error_message = response.description.replaceAll("\\d+", "%d")
+        var error_type    = response.getDescription().split(":")[0];
+        var error_message = response.getDescription().replaceAll("\\d+", "%d")
                 .replaceAll(error_type, "")
                 .replaceFirst(":", "")
                 .trim();
@@ -54,7 +54,7 @@ public class TelegramApiException extends RuntimeException {
         return switch (error_type) {
             case "Bad Request" -> new BadRequest(error_message);
             case "Forbidden" -> new Forbidden(error_message);
-            case "Too Many Requests" -> new FloodError(response.description, response.parameters);
+            case "Too Many Requests" -> new FloodError(response.getDescription(), response.getParameters());
             default -> new TelegramApiException(response);
         };
     }
