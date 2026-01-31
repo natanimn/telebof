0ac8c7bd007dfff4be9a7fee219717464fc62497
natanimn <natanimco@gmail.com>
2026-01-31 11:15:38 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/ShippingHandlerMeta.java
+++ telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/ShippingHandlerMeta.java
@@ -1,6 +1,5 @@
 package io.github.natanimn.telebof.annotations.meta;
 
-import io.github.natanimn.telebof.annotations.ReactionHandler;
 import io.github.natanimn.telebof.annotations.ShippingHandler;
 import io.github.natanimn.telebof.filters.CustomFilter;
 import io.github.natanimn.telebof.filters.DefaultCustomFilter;
@@ -10,6 +9,12 @@ import java.lang.invoke.MethodHandle;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+/**
+ * ShippingHandlerMeta class.
+ * @author Natanim
+ * @since 1.2.0
+ * @version 1.3.0
+ */
 public class ShippingHandlerMeta {
     final MethodHandle method;
     final List<String> payload;
@@ -29,6 +34,6 @@ public class ShippingHandlerMeta {
 
     public boolean matches(Filter filter) {
         if (!(customFilter instanceof DefaultCustomFilter) && !(filter.customFilter(customFilter))) return false;
-        return payload.isEmpty() || filter.customFilter(update -> payload.contains(update.shippingQuery().invoicePayload()));
+        return payload.isEmpty() || filter.customFilter(update -> payload.contains(update.getShippingQuery().getInvoicePayload()));
     }
 }
