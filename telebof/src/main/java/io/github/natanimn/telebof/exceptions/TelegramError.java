bf7089ad3a23802e86f03a3f3046d71b5a65dfad
natanimn <natanimco@gmail.com>
2026-01-31 11:16:03 +0300

    Update version 1.3.0

--- telebof/src/main/java/io/github/natanimn/telebof/exceptions/TelegramError.java
+++ telebof/src/main/java/io/github/natanimn/telebof/exceptions/TelegramError.java
@@ -1,9 +1,10 @@
 package io.github.natanimn.telebof.exceptions;
 
 /**
- * Represents invalid bot token erro
+ * Represents invalid bot token error
  * @author Natanim
  * @since 3 March 2025
+ * @version 1.3.0
  */
 public class TelegramError extends RuntimeException{
     public TelegramError(String msg){
