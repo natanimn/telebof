b0c23f5184ffc7fea36cae85936c02c5a009e2bb
natanimn <natanimco@gmail.com>
2026-04-11 13:26:07 +0300

    feat: add PreparedKeyboardButton types class

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/PreparedKeyboardButton.java
@@ -0,0 +1,18 @@
+package io.github.natanimn.telebof.types.media_and_service;
+
+/**
+ * Describes a keyboard button to be used by a user of a Mini App.
+ * @author Natanim
+ * @since 1.6.0
+ * @version 1.6.0
+ */
+public class PreparedKeyboardButton {
+    /**
+     * Unique identifier of the keyboard button.
+     */
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+}
