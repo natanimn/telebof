baad04e12726c9c48cc431317e5dd4fed27abc90
natanimn <natanimco@gmail.com>
2026-04-11 13:28:25 +0300

    feat: add ManagedBotUpdated types class

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/ManagedBotUpdated.java
@@ -0,0 +1,29 @@
+package io.github.natanimn.telebof.types.updates;
+
+import io.github.natanimn.telebof.types.chat_and_user.User;
+
+/**
+ * This object contains information about the creation, token update, or owner update of a bot that is managed by the current bot.
+ * @author Natanim
+ * @since 1.6.0
+ * @version 1.6.0
+ */
+public class ManagedBotUpdated {
+    /**
+     * User that created the bot
+     */
+    private User user;
+
+    /**
+     * Information about the bot. Token of the bot can be fetched using the method getManagedBotToken.
+     */
+    private User bot;
+
+    public User getUser() {
+        return user;
+    }
+
+    public User getBot() {
+        return bot;
+    }
+}
