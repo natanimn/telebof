17f90942e2e560b9f14c9ff81e8fe947df9f580f
natanimn <natanimco@gmail.com>
2026-04-12 19:37:45 +0300

    feat: add handler meta ManagedBotHandlerMeta

---
+++ telebof/src/main/java/io/github/natanimn/telebof/annotations/meta/ManagedBotHandlerMeta.java
@@ -0,0 +1,36 @@
+package io.github.natanimn.telebof.annotations.meta;
+
+import io.github.natanimn.telebof.annotations.ManagedBotHandler;
+import io.github.natanimn.telebof.annotations.ShippingHandler;
+import io.github.natanimn.telebof.filters.CustomFilter;
+import io.github.natanimn.telebof.filters.DefaultCustomFilter;
+import io.github.natanimn.telebof.filters.Filter;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * ManagedBotHandlerMeta class.
+ * @author Natanim
+ * @since 1.6.0
+ * @version 1.6.0
+ */
+public class ManagedBotHandlerMeta {
+    final MethodHandle method;
+    final CustomFilter customFilter;
+
+    public ManagedBotHandlerMeta(ManagedBotHandler handler, MethodHandle method){
+        this.method = method;
+        try {
+            var ctor = handler.filter().getDeclaredConstructor();
+            ctor.setAccessible(true);
+            this.customFilter = ctor.newInstance();
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean matches(Filter filter) {
+        return customFilter instanceof DefaultCustomFilter || filter.customFilter(customFilter);
+    }
+}
