f7a39060f749cf1679b3f55972babd0293c3b095
natanimn <natanimco@gmail.com>
2026-04-12 19:42:05 +0300

    feat: add fields persistentId, addedByUser, addedByChat, additionDate

--- telebof/src/main/java/io/github/natanimn/telebof/types/updates/PollOption.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/updates/PollOption.java
@@ -1,17 +1,24 @@
 package io.github.natanimn.telebof.types.updates;
 
+import io.github.natanimn.telebof.types.chat_and_user.Chat;
+import io.github.natanimn.telebof.types.chat_and_user.User;
 import io.github.natanimn.telebof.types.media_and_service.MessageEntity;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * This class contains information about one answer option in a poll.
  * @author Natanim
  * @since 3 March 2025
- * @version 1.3.0
+ * @version 1.6.0
  */
 public class PollOption implements TelegramUpdate {
+    /**
+     * Unique identifier of the option, persistent on option addition and deletion
+     */
+    @SerializedName("persistent_id")
+    private String persistentId;
+
     /**
      * Option text, 1-100 characters
      */
@@ -29,6 +36,28 @@ public class PollOption implements TelegramUpdate {
     @SerializedName("voter_count")
     private Integer voterCount;
 
+    /**
+     * Optional. User who added the option; omitted if the option wasn't added by a user after poll creation
+     */
+    @SerializedName("added_by_user")
+    private User addedByUser;
+
+    /**
+     * Optional. Chat that added the option; omitted if the option wasn't added by a chat after poll creation
+     */
+    @SerializedName("added_by_chat")
+    private Chat addedByChat;
+
+    /**
+     * Optional. Point in time (Unix timestamp) when the option was added; omitted if the option existed in the original poll
+     */
+    @SerializedName("addition_date")
+    private Long additionDate;
+
+    public String getPersistentId() {
+        return persistentId;
+    }
+
     public String getText() {
         return text;
     }
@@ -41,12 +70,28 @@ public class PollOption implements TelegramUpdate {
         return voterCount;
     }
 
+    public User getAddedByUser() {
+        return addedByUser;
+    }
+
+    public Chat getAddedByChat() {
+        return addedByChat;
+    }
+
+    public Long getAdditionDate() {
+        return additionDate;
+    }
+
     @Override
     public String toString() {
         return "PollOption{" +
-                "text='" + text + '\'' +
+                "persistentId='" + persistentId + '\'' +
+                ", text='" + text + '\'' +
                 ", textEntities=" + textEntities +
                 ", voterCount=" + voterCount +
+                ", addedByUser=" + addedByUser +
+                ", addedByChat=" + addedByChat +
+                ", additionDate=" + additionDate +
                 '}';
     }
 }
