d1ce9079e2ad676ca39f127f4fad5ae993294dfc
natanimn <natanimco@gmail.com>
2026-05-31 22:29:04 +0300

    feat: add InputMediaVenue types class

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaVenue.java
@@ -0,0 +1,50 @@
+package io.github.natanimn.telebof.types.input;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Represents a venue to be sent.
+ * @author Natanim
+ * @since 2.0.0
+ * @version 2.0.0
+ */
+public class InputMediaVenue implements Serializable {
+    public final String type = "venue";
+
+    private final double latitude;
+    private final double longitude;
+    private final String title;
+    private final String address;
+
+    @SerializedName("foursquare_id")
+    private String foursquareId;
+
+    @SerializedName("foursquare_type")
+    private String foursquareType;
+
+    /**
+     * Required
+     * @param latitude Latitude of the location
+     * @param longitude Longitude of the location
+     * @param title Name of the venue
+     * @param address Address of the venue
+     */
+    public InputMediaVenue(double latitude, double longitude, String title, String address) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.title = title;
+        this.address = address;
+    }
+
+    public InputMediaVenue setFoursquareId(String foursquareId) {
+        this.foursquareId = foursquareId;
+        return this;
+    }
+
+    public InputMediaVenue setFoursquareType(String foursquareType) {
+        this.foursquareType = foursquareType;
+        return this;
+    }
+}
