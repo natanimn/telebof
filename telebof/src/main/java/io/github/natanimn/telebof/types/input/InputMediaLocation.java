9a153bc4d281c654a8949e31ca9be44366245670
natanimn <natanimco@gmail.com>
2026-05-31 22:27:17 +0300

    feat: add InputMediaLocation types class

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaLocation.java
@@ -0,0 +1,41 @@
+package io.github.natanimn.telebof.types.input;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Represents a location to be sent.
+ * @author Natanim
+ * @since 2.0.0
+ * @version 2.0.0
+ */
+public class InputMediaLocation implements Serializable {
+    private final String type = "location";
+
+    private double latitude;
+    private double longitude;
+
+    @SerializedName("horizontal_accuracy")
+    private Double horizontalAccuracy;
+
+    /**
+     * Required
+     * @param latitude Latitude of the location
+     * @param longitude Longitude of the location
+     */
+    public InputMediaLocation(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * Optional
+     * @param horizontalAccuracy The radius of uncertainty for the location, measured in meters; 0-1500
+     * @return {@link InputMediaLocation}
+     */
+    public InputMediaLocation setHorizontalAccuracy(Double horizontalAccuracy) {
+        this.horizontalAccuracy = horizontalAccuracy;
+        return this;
+    }
+}
