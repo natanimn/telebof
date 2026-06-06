42d5d231e46ecbf7888f04f4572ba96bc2389f52
natanimn <natanimco@gmail.com>
2026-06-06 15:49:41 +0300

    feat: add InputPollOptionMedia

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputPollOptionMedia.java
@@ -0,0 +1,9 @@
+package io.github.natanimn.telebof.types.input;
+
+/**
+ * This object represents the content of a poll option to be sent.
+ * @author Natanim
+ * @since 2.0.0
+ * @version 2.0.0
+ */
+public interface InputPollOptionMedia {}
