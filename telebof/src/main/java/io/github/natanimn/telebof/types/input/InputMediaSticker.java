4ca929374672e9a451d2e2bd485682f293a2db67
natanimn <natanimco@gmail.com>
2026-06-06 15:47:04 +0300

    feat: implemented InputPollOptionMedia

--- telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaSticker.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/InputMediaSticker.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @since 2.0.0
  * @version 2.0.0
  */
-public class InputMediaSticker implements InputMedia{
+public class InputMediaSticker implements InputMedia, InputPollOptionMedia{
     private final String type = "sticker";
     private String media;
     private transient final List<File> files = new ArrayList<>();
