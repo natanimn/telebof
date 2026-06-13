8456a8ffc65e1d08759cb223b9711f463dda3dd9
natanimn <natanimco@gmail.com>
2026-06-13 20:31:28 +0300

    feat: add RichText

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/rich/RichText.java
@@ -0,0 +1,10 @@
+package io.github.natanimn.telebof.types.rich;
+
+import java.io.Serializable;
+
+/**
+ * This object represents a rich formatted text.
+ * @author Natanim
+ * @since 2.1
+ */
+public interface RichText extends Serializable { }
