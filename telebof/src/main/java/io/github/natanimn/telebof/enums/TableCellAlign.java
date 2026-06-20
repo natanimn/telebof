5e41062198c6d62962a24c1c6f41603f3267ec25
natanimn <natanimco@gmail.com>
2026-06-20 09:10:54 +0300

    feat: add TableCellAlign

---
+++ telebof/src/main/java/io/github/natanimn/telebof/enums/TableCellAlign.java
@@ -0,0 +1,17 @@
+package io.github.natanimn.telebof.enums;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Represents horizontal cell content alignment.
+ */
+public enum TableCellAlign {
+    @SerializedName("left")
+    LEFT,
+
+    @SerializedName("center")
+    CENTER,
+
+    @SerializedName("right")
+    RIGHT
+}
