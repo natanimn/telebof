17da7b0cbd4d27a10ed6496ceba207860a1e052d
natanimn <natanimco@gmail.com>
2026-06-20 09:11:04 +0300

    feat: add TableCellValign

---
+++ telebof/src/main/java/io/github/natanimn/telebof/enums/TableCellValign.java
@@ -0,0 +1,18 @@
+package io.github.natanimn.telebof.enums;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Represents vertical cell content alignment.
+ */
+public enum TableCellValign {
+    @SerializedName("top")
+    TOP,
+
+    @SerializedName("middle")
+    MIDDLE,
+
+    @SerializedName("bottom")
+    BOTTOM
+
+}
