874edda7a70151a2f21aa4a527873f489b778641
natanimn <natanimco@gmail.com>
2026-06-20 09:11:24 +0300

    feat: add RichTextImpl

--- telebof/src/main/java/io/github/natanimn/telebof/types/rich/RichTextImpl.java
+++ telebof/src/main/java/io/github/natanimn/telebof/types/rich/RichTextImpl.java
@@ -4,7 +4,7 @@ public class RichTextImpl implements RichText {
     private final String type;
     private final String text;
 
-    public RichTextImpl(String type, String text){
+    public RichTextImpl(String text, String type){
         this.type = type;
         this.text = text;
     }
