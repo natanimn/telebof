78f56150a35f3042d3a6ece1339edf5c44a1a9dd
natanimn <natanimco@gmail.com>
2026-07-15 11:04:52 +0300

    feat: add Link

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/Link.java
@@ -0,0 +1,17 @@
+package io.github.natanimn.telebof.types;
+
+/**
+ * Represents an HTTP link.
+ * @author Natanim
+ * @since 2.1
+ */
+public class Link {
+    /**
+     * URL of the link
+     */
+    private String url;
+
+    public String getUrl(){
+        return url;
+    }
+}
