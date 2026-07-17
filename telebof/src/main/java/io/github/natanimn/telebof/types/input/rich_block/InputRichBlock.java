9c3a2fa110b668323b0d08264d663abf0a229722
natanimn <natanimco@gmail.com>
2026-07-17 14:28:23 +0300

    feat: add InputRichBlock

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/rich_block/InputRichBlock.java
@@ -0,0 +1,10 @@
+package io.github.natanimn.telebof.types.input.rich_block;
+
+import java.io.Serializable;
+
+/**
+ * This object represents a block in a rich formatted message to be sent.
+ * @author Natanim
+ * @since 2.2
+ */
+public interface InputRichBlock extends Serializable { }
