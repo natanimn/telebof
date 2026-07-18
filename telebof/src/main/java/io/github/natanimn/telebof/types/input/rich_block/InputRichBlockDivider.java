985fdc3af742aef680de10eb5afb5cbe91f7305b
natanimn <natanimco@gmail.com>
2026-07-18 14:07:49 +0300

    feat: add InputRichBlockDivider

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/input/rich_block/InputRichBlockDivider.java
@@ -0,0 +1,14 @@
+package io.github.natanimn.telebof.types.input.rich_block;
+
+/**
+ * A divider, corresponding to the HTML tag <hr/>.
+ * @author Natanim
+ * @since 2.2
+ */
+public class InputRichBlockDivider implements InputRichBlock {
+    /**
+     * Type of the block
+     */
+    private final String type = "divider";
+
+}
