4901f188ca4e27c332836dd3b66374777bd7dc37
natanimn <natanimco@gmail.com>
2026-07-22 22:44:15 +0300

    feat: add CommunityChatRemoved

---
+++ telebof/src/main/java/io/github/natanimn/telebof/types/media_and_service/CommunityChatRemoved.java
@@ -0,0 +1,8 @@
+package io.github.natanimn.telebof.types.media_and_service;
+
+/**
+ * Describes a service message about a chat being removed from a community. Currently, holds no information.
+ * @author Natanim
+ * @since 2.2
+ */
+public class CommunityChatRemoved { }
